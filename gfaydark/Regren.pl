6e512bf5142eb305e6a8b282100c47e97014b350
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2013-09-30 22:18:53 +0000

    update newbie script and removed out of era scripts.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@17 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- gfaydark/Regren.pl
+++ gfaydark/Regren.pl
@@ -1,32 +1,12 @@
-sub EVENT_SPAWN {
-  $x = $npc->GetX();
-  $y = $npc->GetY();
-  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
-}
-
-sub EVENT_ENTER {
-  if(plugin::check_hasitem($client, 18782)) { 
-		$client->Message(15,"A stern looking man turns to greet you as you get your bearings. 'Come, young recruit. I am Regren, Guild Master of the Emerald Warriors. Read the note in your inventory and then hand it to me to begin your training as a proud Warrior. Unless you would rather prance about with the pixies and such. The choice is yours.'");
-  }
-}
-
 sub EVENT_SAY {
 	if($text=~/hail/i){
-		quest::say("Welcome, warrior! Show the Emerald Warriors your mettle and bring me a ruined wolf pelt, some bat fur, some bone chips, and a spiderling eye from the depths of Greater Faydark. If you succeed, my admiration and a reward will be yours. To battle!");
-	}
-	if($text=~/trades/i) {
-		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
-		quest::summonitem(51121);
-	}
-	if($text=~/second book/i)	{
-		quest::say("Here is the second volume of the book you requested, may it serve you well!");
-		quest::summonitem(51122);
+		quest::say("Welcome, warrior! Show the Emerald Warriors your mettle and bring me a [ruined wolf pelt], some [bat fur], some [bone chips], and a [spiderling eye] from the depths of Greater Faydark. If you succeed, my admiration and a reward will be yours. To battle!");
 	}
 }
 
 sub EVENT_ITEM {
 	if(plugin::check_handin(\%itemcount, 18782 => 1)){ #Recruitment Letter
-		quest::say("Welcome to the Emerald Warriors. Hmmm, you have a lot of training to do, so let's get started right away. Here's our guild tunic, represent us well, young $name. Once you are ready to begin your training please make sure that you see Josylyn Greenblade, she can assist you in developing your hunting and gathering skills. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+		quest::say("Welcome to the Emerald Warriors. Hmmm, you have a lot of training to do, so let's get started right away. Here's our guild tunic, represent us well, young $name.");
 		quest::summonitem(13533); #Old Green Tunic
 		quest::ding();
 		quest::faction(92,10); #Emerald Warriors
