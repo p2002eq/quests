b06cf63dabbe9eb6ace7dd21ed2b76c724161268
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2013-10-09 21:58:55 +0000

    update Dark Elves newbie scripts and remove out of era.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@26 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- neriakb/Seloxia_Punox.pl
+++ neriakb/Seloxia_Punox.pl
@@ -1,15 +1,3 @@
-sub EVENT_SPAWN {
-  $x = -1126;
-  $y = -59;
-  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
-}
-
-sub EVENT_ENTER {
-  if(plugin::check_hasitem($client, 18751)) { 
-		$client->Message(15,"The cold voice of a female fills your mind with dread, 'I am Seloxia Punox. Seek me out in the Cauldron of Hate and hand me our note so that you may begin on the path of the Warrior. True power can be yours should you have the will to train hard enough!'");
-  }
-}
-
 sub EVENT_SAY { 
 	if ($text=~/hail/i) {
 		quest::emote("'s eyes flare with fury as her beautiful features twist into a snarl of hatred that echoes her hissing voice, 'How dare you address me in such a fashion?!  I am the leader of the Indigo Brotherhood!!  You are lucky I do not strike you down where you stand!  You shall address me as 'Mistress.'  Many warriors have died so that I might gain this title.'"); 
@@ -24,19 +12,11 @@ sub EVENT_SAY {
 		quest::say("Then you will go at once. Find your own way. Deliver this note to Ambassador K'Rynn and he shall give you the note to return to me. Do not stop to practice your skills. I offer this test to only the young warriors. If you die, then so be it. We need not inferior warriors. Now go!!");
 		quest::summonitem(18842); # Sealed Letter (Letter To Krynn)
 	}
-	if ($text=~/trade/i) {
-		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
-		quest::summonitem(51121);
-	}
-	if ($text=~/second book/i) {
-		quest::say("Here is the second volume of the book you requested, may it serve you well!");
-		quest::summonitem(51122);
-	}
 }
 
 sub EVENT_ITEM { 
 	if (plugin::check_handin(\%itemcount, 18751 => 1)) {
-		quest::say("I shall reserve any official welcoming until you have proven yourself a suitable member for the Indigo Brotherhood. That proof shall be obtained by your progression in your training. See Jarrex N`Ryt, one of my most trusted trainers in the Brotherhood and obey his command carefully if you wish to succeed as a member of the Brotherhood. Return to me when you have become more experienced in our art, I will be able to further instruct you on how to progress through your early ranks, as well as in some of the various [trades] you will have available to you.");
+		quest::say("I shall reserve any official welcoming until you have proven yourself a suitable member for the Indigo Brotherhood. That proof shall be obtained by your progression in your training. See Yegek B'Larin, one of my most trusted trainers in the Brotherhood and obey his command carefully if you wish to succeed as a member of the Brotherhood.");
 		quest::summonitem(13580); #Old Training Tunic*
 		quest::ding();
 		quest::faction(155, 10); #Indigo Brotherhood
--- neriakc/Nezzka_Tolax.pl
+++ neriakc/Nezzka_Tolax.pl
@@ -1,15 +1,3 @@
-sub EVENT_SPAWN {
-  $x = $npc->GetX();
-  $y = $npc->GetY();
-  quest::set_proximity($x - 50, $x + 50, $y - 50, $y + 50);
-}
-
-sub EVENT_ENTER {
-  if(plugin::check_hasitem($client, 18757)) { 
-		$client->Message(15,"Nezzka Tolax turns towards you. 'Welcome to the Lodge of the Dead. I am Nezzka Tolax and with my training you will learn to master teh ways of the Shadowknight. Read the note in your inventory and hand it to me when you are ready to begin your training.'");
-  }
-}
-
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 18757 => 1)) { #tattered note
     quest::say("Hmmm, I hope you're tougher than you look. Here, put this on. Go find Ulraz, he'll beat you into shape. There's no turning back now, punk. So, you'd better do well, got it?");
