5c7d1823e271bfe195b182437840f4c12858f50f
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2013-10-13 15:43:22 +0000

    removed some scripts relating to task selection (OoW Tasks)
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@34 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- qey2hh1/Guard_Gregor.pl
+++ qey2hh1/Guard_Gregor.pl
@@ -8,21 +8,6 @@ sub EVENT_COMBAT {
 	}
 }
 
-sub EVENT_SAY {
-  if(($ulevel >= 20) && ($ulevel <= 30)) {
-    if($text=~/hail/i) {
-      quest::say("Oh, hello $name. Would you like to assist me in a [task]?");
-    }
-    if($text=~/task/i) {
-      quest::say("Excellent! I was hoping you would be able to help me.");
-      quest::taskselector(143); #Task: Bringing Goods to the Karanas
-    }
-  }
-  else {
-    quest::say("Sorry, $name. I am quite busy...");
-  }
-}
-
 sub EVENT_ITEM {
   plugin::return_items(\%itemcount);
 }
