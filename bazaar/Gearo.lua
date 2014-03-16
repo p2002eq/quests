dece561c75970a0fe4ce2f4be7e4712d9f2a9ef2
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-03-16 01:55:43 +0000

    few more conversion
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@125 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- bazaar/Gearo.lua
+++ bazaar/Gearo.lua
@@ -1,13 +1,16 @@
 function event_say(e)
-	local qglobals = eq.get_qglobals(e.other);
+	local qglobals = eq.get_qglobals(e.self,e.other);
 
 	if(e.message:findi("hail")) then
 		e.self:Say("Hello there friend. My name is Gearo and as you can see I'm a Gnome, so what can I do for ya?");
-	elseif((e.message:findi("take the gems")) and qglobals["DeliveredToGearo"] == "1")) then
+	elseif(qglobals["DeliveredToGearo"] == "1") then
+		if(e.message:findi("take the gem")) then
 		e.self:Say("Good " .. e.other:GetName() .. "! Here they are, please take them back to that gargantuan barbarian and let him know I want a raise for all this hard work!");
 		e.other:SummonItem(4767);
 		eq.delete_global("DeliveredToGearo"); --Remove global
+		end
 	end
+
 end
 
 function event_trade(e)
@@ -21,7 +24,7 @@ function event_trade(e)
 		e.other:AddEXP(1000);		--This quest is doable at level 1, I didn't feel comfortable giving out more xp than this per step
 		eq.set_global("DeliveredToGearo","1",0,"D30");--Set a variable here.
 	end
-	item_lib.return_items(e.self, e.other, e.trade)
+	item_lib.return_items(e.self, e.other, e.trade);
 end
 
 -------------------------------------------------------------------------------------------------
