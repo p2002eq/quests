5205bb99dbd516c446fae1791e186105747019e2
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-05-03 00:17:28 +0000

    added the cast heal for Waltor Felligan and use the correct spellcast.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@170 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- halas/Waltor_Felligan.lua
+++ halas/Waltor_Felligan.lua
@@ -1,15 +1,21 @@
 function event_say(e)
 	if(e.message:findi("Hail")) then
 		e.self:Say("Hello, me friend! I'm the resident healer o' Halas. Please inform me when ye've a need fer me talents to [bind wounds], [cure disease] or [cure poison]. Might I add, if ye're a young shaman o' Halas, ye can also [assist in gathering fungus].");
-	elseif(e.message:findi("cure disease")) then
-		e.self:Say("Two small quantities o' wooly fungus are needed before we can cure yer malady.");
-	--elseif(e.message:findi("bind wounds")) then 									-- This part has no reported outcome anywhere probably broken on live
-	--	e.self:Say("Before I bind yer wounds, ye must pay tribute to the Tribunal in the amount of ten gold coins.");
-	elseif(e.message:findi("cure poison")) then
-		e.self:Say("I'll be needing mammoth steaks to feed the unfortunate.  Then we'll drain the poison from yer veins.");
-	elseif(e.message:findi("assist in gathering fungus")) then
-		e.self:Say("As the wooly spiderlings get ready to molt, they'll carry wooly fungus. Oftentimes, one can find wooly fungus growing on their bellies. I use this in me healing practices. I'll reward ye if ye can fill this jar full o' the valuable fungus. Don't forget to combine them before ye return it to me. And have yerself some rations handy, or ye may find yerself snacking on this tasteless food source.");
-		e.other:SummonItem(17946);
+	elseif(e.other:GetFaction(e.self) < 6) then
+		if(e.message:findi("cure disease")) then
+			e.self:Say("Two small quantities o' wooly fungus are needed before we can cure yer malady.");
+		elseif(e.message:findi("bind wounds")) then 
+			e.self:Say("Before I bind yer wounds, ye must pay tribute to the Tribunal in the amount of ten gold coins.");
+		elseif(e.message:findi("cure poison")) then
+			e.self:Say("I'll be needing mammoth steaks to feed the unfortunate.  Then we'll drain the poison from yer veins.");
+		elseif(e.message:findi("assist in gathering fungus")) then
+			e.self:Say("As the wooly spiderlings get ready to molt, they'll carry wooly fungus. Oftentimes, one can find wooly fungus growing on their bellies. I use this in me healing practices. I'll reward ye if ye can fill this jar full o' the valuable fungus. Don't forget to combine them before ye return it to me. And have yerself some rations handy, or ye may find yerself snacking on this tasteless food source.");
+			e.other:SummonItem(17946);
+		end
+	else
+		if(e.message:findi("cure disease") or e.message:findi("bind wounds") or e.message:findi("cure poison") or e.message:findi("assist in gathering fungus")) then
+			e.self:Say("The scales o' the Shamans o' Justice dinnae tip in yer favor. Ye'd best flee ye still have the chance.");
+		end
 	end
 end
 
@@ -26,11 +32,17 @@ function event_trade(e)
 		e.other:GiveCash(0,0,4,0);
 		e.other:AddEXP(50);
 		e.other:Ding();
+	elseif(item_lib.check_turn_in(e.trade, {gold = 10})) then
+		e.self:Say("The scales have been balanced and the Tribunal has spoken. Yer body shall be saved.");
+		e.other:Ding();
+		e.self:CastSpell(17,e.other:GetID());
 	elseif(item_lib.check_turn_in(e.trade, {item1 = 13445})) then					-- poison cure for steak totally bogus text
-		eq.SelfCast(203);
+		e.self:CastSpell(203,e.other:GetID());
+		e.other:Ding();
 		e.self:Say("Aye! I will draw the poison from your veins!");
 	elseif(item_lib.check_turn_in(e.trade, {item1 = 13967, item2 = 13967})) then 	-- disease cure for wooly fungus totally bogus text
-		eq.SelfCast(213);
+		e.self:CastSpell(213,e.other:GetID());
+		e.other:Ding();
 		e.self:Say("Ah! Now I can cure yer malady.");
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
