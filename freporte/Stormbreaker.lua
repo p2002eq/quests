fcd359aef817f7297b7d64d6ceafc6641badf138
cavedude00@gmail.com <cavedude00@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-05-17 01:31:56 +0000

    Added time to boat spawn.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@178 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- freporte/Stormbreaker.lua
+++ freporte/Stormbreaker.lua
@@ -1,5 +1,7 @@
 function event_spawn(e)
-	e.self:Shout("I've spawned! Name is: " .. e.self:GetName() .. "");
+	local zone_time = eq.get_zone_time(); -- Time here is off by 1, so 6AM = 5.
+	local hour = zone_time["zone_hour"] + 1;
+	e.self:Shout("I've spawned! Name is: " .. e.self:GetName() .. " Time is: " .. hour ..":00");
 end
 
 function event_waypoint_arrive(e)
