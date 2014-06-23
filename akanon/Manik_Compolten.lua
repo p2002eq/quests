5ebe4439bfd4e71d94f2f0c5356c6fe29aa2f3c2
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-06-23 07:05:47 +0000

    fixed the amount of turn in for Manik Compolten.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@219 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- akanon/Manik_Compolten.lua
+++ akanon/Manik_Compolten.lua
@@ -15,37 +15,30 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 13208})) then
-		e.self:Say("Ah! Excellent work! I think we may have further use for you if you like.");
-		e.other:AddEXP(1000);
-		e.other:GiveCash(1,3,1,2);
+	if(item_lib.check_turn_in(e.trade, {item1 = 13208,item2 = 13208})) then
+		e.self:Say("Excellent work! You were born to be a warrior. Here is a little bonus for the good job.");
 		e.other:Faction(115,3,0); 	-- gem choppers
 		e.other:Faction(176,3,0); 	-- King ak'anon
 		e.other:Faction(210,10,0); 	-- Merchants of Ak'Anon
 		e.other:Faction(39,-10,0); 	-- Clan Grikbar
 		e.other:Faction(71,-10,0); 	-- Dark Reflection
-		e.other:Ding();
+		e.other:QuestReward(e.self,1,3,1,2,0,1000);
 	elseif (item_lib.check_turn_in(e.trade, {item1 = 13212, item2 = 13211, item3 = 13213, item4 = 13214})) then
 		e.self:Say("Ah!! I see you had no problem finding all of the C series clockworks. Good work. Here. Take this blackbox. We received it from Clockwork 27C. Take it to Jogl Doobraugh. He is the operator of the only blackbox definer in Ak'Anon. He is out at the windmills checking on their operation.");
-		e.other:SummonItem(13209);
 		e.other:Faction(115,1,0); 	-- gem choppers
 		e.other:Faction(176,1,0); 	-- King ak'anon
 		e.other:Faction(210,1,0); 	-- Merchants of Ak'Anon
 		e.other:Faction(39,-1,0); 	-- Clan Grikbar
 		e.other:Faction(71,-1,0); 	-- Dark Reflection
-		e.other:AddEXP(5000);
-		e.other:Ding();
+		e.other:QuestReward(e.self,0,0,0,0,13209,5000);
 	elseif (item_lib.check_turn_in(e.trade, {item1 = 13215})) then -- text is not per live. Need original
 		e.self:Say("What's this? You've found and smashed Red V?! Amazing! Wonderful! Take this as a sign of my thanks!");
-		e.other:SummonItem(eq.ChooseRandom(13219, 13219, 13220, 13220, 1013, 1018, 1015, 1019, 1022, 1023, 1024, 1017, 1016, 1020, 1014, 1021));
 		e.other:Faction(115,1,0); 	-- gem choppers
 		e.other:Faction(176,1,0); 	-- King ak'anon
 		e.other:Faction(210,1,0); 	-- Merchants of Ak'Anon
 		e.other:Faction(39,-1,0); 	-- Clan Grikbar
 		e.other:Faction(71,-1,0); 	-- Dark Reflection
-		e.other:AddEXP(5000);
-		e.other:Ding();
-		e.other:GiveCash(1,3,0,0);
+		e.other:QuestReward(e.self,1,3,0,0,eq.ChooseRandom(13219, 13219, 13220, 13220, 1013, 1018, 1015, 1019, 1022, 1023, 1024, 1017, 1016, 1020, 1014, 1021),5000);
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
