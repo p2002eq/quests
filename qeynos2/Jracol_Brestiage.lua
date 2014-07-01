187ca611b45ca16af998731f536dee47b64dd64b
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-07-01 02:33:05 +0000

    Fixed the turn in for Jracol Brestiage.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@237 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- qeynos2/Jracol_Brestiage.lua
+++ qeynos2/Jracol_Brestiage.lua
@@ -8,23 +8,24 @@ function event_say(e)
 		proof = 1;
 	elseif(e.message:findi("friend") and proof == 1) then
 		e.self:Say("My memory is a bit fuzzy. If you are a friend, prove it to me.");
-		proof = 1;
+		proof = 2;
 	end
-end	
+end
 
 function event_trade(e)
 	local item_lib = require("items");
 
-	if(item_lib.check_turn_in(e.trade, {item1 = 13903}) and proof == 2) then -- Bent Card
-		e.self:Say("");
-		e.other:SummonItem(18722); --Sealed Note For Knargon 
-		e.other:Ding();
-		e.other:Faction(31,5,0); -- Carson McCabe
-		e.other:Faction(48,5,0); -- Coalition of Tradefolk Underground
-		e.other:Faction(273,-10,0); -- Ring of Scale
-		e.other:Faction(149,5,0); -- Highpass Guards
-		e.other:Faction(214,5,0); -- Merchants of Highpass
-		e.other:AddEXP(1000);
+	if(proof == 2) then
+		if(item_lib.check_turn_in(e.trade, {item1 = 13903})) then -- Bent Card
+			e.self:Say("'Ah, good, it seems we have much in common after all. Take this back to the Circle, before the city guards come nosing around over here.");
+			e.other:Faction(31,5,0); -- Carson McCabe
+			e.other:Faction(48,5,0); -- Coalition of Tradefolk Underground
+			e.other:Faction(273,-10,0); -- Ring of Scale
+			e.other:Faction(149,5,0); -- Highpass Guards
+			e.other:Faction(214,5,0); -- Merchants of Highpass
+			e.other:QuestReward(e.self,0,0,0,0,18722,1000);
+			proof = 0;
+		end
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
