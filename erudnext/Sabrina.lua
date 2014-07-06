5d5e71a65580e7319adb4effef328d3114054717
cavedude00 <cavedude00@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-07-06 19:38:02 +0000

    Fixed Sabrina.lua.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@256 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- erudnext/Sabrina.lua
+++ erudnext/Sabrina.lua
@@ -1,3 +1,11 @@
+function event_signal(e) 		
+	if(e.signal == 1) then 		
+ 		eq.start(21); 		
+ 	elseif(e.signal == 2) then 		
+ 		eq.stop();
+ 	end
+ end
+
 function event_spawn(e)
 	eq.spawn_condition("erudnext",0,1,0);
 	eq.spawn_condition("erudnext",0,2,0);
