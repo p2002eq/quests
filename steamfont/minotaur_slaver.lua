195344b9e83fe78d669ab1d9dea3b5baf743c6e2
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-07-24 16:47:21 +0000

    separate the two chances for lord and hero to spawn.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@272 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- steamfont/minotaur_slaver.lua
+++ steamfont/minotaur_slaver.lua
@@ -1,8 +1,8 @@
 function event_death_complete(e)
-	rand = math.random(100);
-	if(rand > 97) then
+	if(math.random(100) > 98) then
 		eq.unique_spawn(56152,0,0,-1294,1360,-103,0);
-	elseif(rand > 75 and rand < 97) then
+	end
+	if(math.random(100) < 10) then
 		eq.unique_spawn(56161,0,0,-2179,1319,-101.2,0);
 	end
 	e.self:Say("I die soon! Meldrath, help me!");
