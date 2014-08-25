21ab5f916c00e81faea20189da234ce16a501d06
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-08-25 21:58:00 +0000

    fixed the Newbie Armor DE Cleric script.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@303 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- neriakc/High_Priestess_Alexandria.lua
+++ neriakc/High_Priestess_Alexandria.lua
@@ -1,7 +1,7 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say("Greetings child, what business do you have here? I'm sorry but I will only deal with Clerics that are willing to prove their loyalty to Innoruuk. I cannot deal with every single heathen that feels it is necessary to bid me a good day. If you are a [Cleric of Innoruuk] I might be able to aid you in your training.");
-	elseif(e.other:GetClass() == 5 and e.other:GetRace() == 6) then
+	elseif(e.other:GetClass() == 2 and e.other:GetRace() == 6) then
 		if(e.message:findi("cleric of innoruuk")) then
 			e.self:Say("Is that so " .. e.other:Race() .. " " .. e.other:GetName() .. ". Well from the looks of you I wouldn't say you are much of anything yet. However, if you have the willingness and determination to serve your God then there might just be more hope for you then I would have thought. Obviously, to move forward in your training, you will need to shield yourself from your enemies and from the elements. I believe I have something that could help you if you are still [interested in proving yourself]");
 		elseif(e.message:findi("interested in proving myself")) then
