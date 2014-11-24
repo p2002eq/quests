d0c9d67775c8bad87d4490c1ff8a8da13cb83268
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-11-24 20:01:33 +0000

    fixed Dragoon_Zytl.lua and Kizdean_Gix.lua
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@378 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- commons/Kizdean_Gix.lua
+++ commons/Kizdean_Gix.lua
@@ -9,7 +9,7 @@ end
 
 function event_death_complete(e)
 	if(math.random(100) < 15) then
-		eq.spawn2(21141,0,0,3036,-706,-39,0); -- chance for Dragoon Zytl to spawn.
+		eq.unique_spawn(21141,0,0,3036,-706,-39,0); -- chance for Dragoon Zytl to spawn.
 	end
 end
 
