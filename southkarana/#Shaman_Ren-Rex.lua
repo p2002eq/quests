6f59a214b8575e3dbf35012013ed903bf9c7ab3d
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-12-01 03:09:54 +0000

    scripts for gnoll shaman in South Karana.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@386 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

---
+++ southkarana/#Shaman_Ren-Rex.lua
@@ -0,0 +1,7 @@
+function event_death_complete(e)
+	if(math.random(100) < 50) then
+		eq.spawn2(14011,0,0,-2482,-7426,53,0); -- spawn High Shaman Grisok
+	else
+		eq.spawn2(14011,0,0,-258,-6483,3,0);
+	end
+end
