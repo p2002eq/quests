958b677b5daaf31724b447b12703021be2259bd4
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-12-21 18:53:43 +0000

    added z-coord to proximities in runnyeye.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@407 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- runnyeye/You_are.lua
+++ runnyeye/You_are.lua
@@ -1,7 +1,8 @@
 function event_spawn(e)
 	local xloc = e.self:GetX();
 	local yloc = e.self:GetY();
-	eq.set_proximity(xloc - 50, xloc + 50, yloc - 50, yloc + 50);
+	local zloc = e.self:GetZ();
+	eq.set_proximity(xloc - 25, xloc + 25, yloc - 25, yloc + 25, zloc - 3, zloc + 6);
 end
 
 function event_enter(e)
