67301351b16f3b7f04a5fae5d632cbeff22a3cfa
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2014-12-23 01:36:21 +0000

    added a couple of missing text for Raffel_Minnmorn.lua
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@408 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- qeynos/Raffel_Minnmorn.lua
+++ qeynos/Raffel_Minnmorn.lua
@@ -1,5 +1,15 @@
 function event_spawn(e)
-	eq.set_timer("talk",60000);
+	eq.set_timer("talk",600000);
+end
+
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Hello, friend. I am visiting from the plains. I hope to make a FORTUNE here in Qeynos.");
+	elseif(e.message:findi("Flockwalker")) then
+		e.self:Say("Gash Flockwalker is a druid from Surefall Glade. I have a special delivery for him.");
+	elseif(e.message:findi("special delivery")) then
+		eq.attack(e.other:GetName());
+	end
 end
 
 function event_timer(e)
