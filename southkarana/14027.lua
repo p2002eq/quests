a209fc7fef5f9fc15886e0ff9f6a365156af3898
rregenn@gmail.com <rregenn@gmail.com@f9a8ffd5-5cd5-e8c3-71fd-11587b705891>
2015-01-17 06:52:55 +0000

    change the script name in south karana.
    git-svn-id: http://peqmacquests.googlecode.com/svn/trunk@435 f9a8ffd5-5cd5-e8c3-71fd-11587b705891

--- southkarana/14027.lua
+++ southkarana/14027.lua
@@ -12,9 +12,9 @@ function event_death_complete(e)
 	if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(quillmane) == false) then
 		if(random_result < 4) then
 			eq.spawn2(quillmane,1,0,3210,-6821.6,38.3,193.3);
-		elseif(random_result >= 4 and random_result < 8) then
+		elseif(random_result >= 4 and random_result < 7) then
 			eq.spawn2(quillmane,2,0,-2761.8,-6076,0.7,4.0);
-		elseif(random_result >= 8 and random_result < 12) then
+		elseif(random_result >= 7 and random_result < 10) then
 			eq.spawn2(quillmane,3,0,-2333.8,1296.5,38.7,247.3);
 		end
 	end
