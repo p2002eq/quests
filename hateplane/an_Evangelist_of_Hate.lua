758c3edd899b19cc99f4be9b8607db1f578b6408
scott <jetviper21@gmail.com>
2015-02-24 07:40:39 -0600

    the

--- hateplane/an_Evangelist_of_Hate.lua
+++ hateplane/an_Evangelist_of_Hate.lua
@@ -1,10 +1,11 @@
 function event_spawn(e)
-    eq.set_timer("Shout",600000);
+    eq.set_timer("Shout",60000);
+    anoy_the_zone(e);
 end
 
 function event_timer(e)
     if(e.timer == "Shout") then
-        anoy_the_zone(e)
+        anoy_the_zone(e);
     end
 end
 
@@ -12,10 +13,10 @@ function anoy_the_zone(e)
     if(math.random(100) < 50) then
         e.self:Shout('ALL PRAISE INNORUUK, OUR LORD AND CREATOR!');
     else
-        e.self:Shout('Did you honestly believe your pathetic efforts could extinguish the unstoppable might of HATE? I loathe you and your naivete with every fiber of my being. Know that our Lord and Master will survive as long as we children fuel his existence.');
+        e.self:Shout('Did you honestly believe your pathetic efforts could extinguish the unstoppable might of HATE? I loathe you and your naivete with every fiber of my being. Know that our Lord and Master will survive as long as we the children fuel his existence.');
     end
 end
 
 function event_death_complete(e)
-    e.stop_timer("Shout");
+    eq.stop_timer("Shout");
 end
