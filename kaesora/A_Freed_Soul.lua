77589a8d515c5c277060974802a7980acc7101db
Tom <tommytryharder@gmail.com>
2015-03-02 11:22:45 -0500

    added depop timer

--- kaesora/A_Freed_Soul.lua
+++ kaesora/A_Freed_Soul.lua
@@ -1,5 +1,10 @@
 -- on live it is a fire based aoe on death reported to be around 552 pts damage
 -- exact spell is not specified. using Firebomb, fire based aoe - max damage 499
+function event_spawn(e)
+	eq.set_timer("depop",300000);
+end
+
+
 function event_death_complete(e)
 	eq.SelfCast(6043);
 end
@@ -8,6 +13,12 @@ function event_trade(e)
 	local item_lib = require("items");
 	item_lib.return_items(e.self, e.other, e.trade)
 end
+
+function event_timer(e)
+	eq.stop_timer("depop");
+	eq.depop();
+end
+
 
 -------------------------------------------------------------------------------------------------
 -- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
