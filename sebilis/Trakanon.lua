78be5a8dd14e4ea53726bab5a7ee2620e31bc216
Tom <tommytryharder@gmail.com>
2015-03-03 09:29:07 -0500

    Trak lua

---
+++ sebilis/Trakanon.lua
@@ -0,0 +1,11 @@
+function event_spawn(e)
+	eq.depop(89168);
+end
+function event_death_complete(e)
+    eq.signal(89185, 1) -- signal UDB controller
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+	item_lib.return_items(e.self, e.other, e.trade)
+end
