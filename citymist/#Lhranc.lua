9b2950bf7c659fb947bdca8878103adf29a97f1a
Tom <tommytryharder@gmail.com>
2015-03-03 11:26:56 -0500

    80 min despawn timer for #Lhranc

--- citymist/#Lhranc.lua
+++ citymist/#Lhranc.lua
@@ -1,6 +1,7 @@
 -- Part of SK Epic 1.0
 function event_spawn(e)
 	e.self:Say("Young one, you cannot fathom how long it has been since I breathed air and felt flesh on my bones. I told you I would show you a weapon that would make you the most powerful of your kind. I said nothing of giving it to you. Feast your eyes on Innoruuk's Curse, mortal! Thank you for your service.");
+	eq.set_timer("depop",4800000);
 end
 
 function event_death_complete(e)
@@ -11,6 +12,12 @@ function event_trade(e)
 	local item_lib = require("items");
 	item_lib.return_items(e.self, e.other, e.trade)
 end
+
+function event_timer(e)
+	eq.stop_timer("depop");
+	eq.depop();
+end
+
 
 -- Quest by mystic414
 -------------------------------------------------------------------------------------------------
