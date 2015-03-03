b54938ad7f6db94375e3e4e6108e0128e4d6c83d
Tom <tommytryharder@gmail.com>
2015-03-03 10:16:13 -0500

    logic

--- overthere/Kurron_Ni.lua
+++ overthere/Kurron_Ni.lua
@@ -2,7 +2,8 @@
 function event_say(e)
 	if(e.message:findi("Hail")) then
 		e.self:Say("Out of my way, stranger!  I am on a delicate mission; interfere with my search and I shall relieve you of your head! Bother me no more.");
-	elseif(e.message:findi("mission")) then
+	end
+	if(e.message:findi("mission")) then
 		if(e.other:GetCharacterFactionLevel(342) < 7) then
 			e.self:Say("My mission is none of your concern!  Now you die, pitiful fool!");
 			eq.attack("" .. e.other:GetName() .. "");
@@ -10,11 +11,14 @@ function event_say(e)
 			e.self:Say("My mission was to find a pathetic shadowknight and bring him closer to Innoruuk. You volunteered. By exercising my superior powers of persuasion, I have now taken from you a fine suit of armor and enough coin to ensure that I will be sleeping neither soberly nor alone for quite some time! Now I ask you, worthy shadowknight, do you not feel the fires of hatred coursing through your veins like never before? That is the very reward I spoke of! You have been brought closer to my master. My mission is complete! You foolish excuse for a dark knight, I shall take your head and tell all of your generous donation to the mighty Teir'Dal rogues!");
 			eq.attack("" .. e.other:GetName() .. "");
 		end
-	elseif(e.message:findi("seek")) then
+	End
+	if(e.message:findi("seek")) then
 		e.self:Say("I have traveled here in hopes of finding a soul worthy of assisting me in completing my mission.");
-	elseif(e.message:findi("i am worthy")) then
+	end	
+	if(e.message:findi("i am worthy")) then
 		e.self:Say("I doubt it, but I have been wrong before. Before I share with you the details of my mission, however, you must first prove your worthiness. Agreed?");
-	elseif(e.message:findi("agreed")) then
+	end
+	if(e.message:findi("agreed")) then
 		e.self:Say("Very well then, my new friend. I have an acquaintance that stands in need of three pieces of Darkforge Armor. He requires the breast, greaves and helm to complete his set. I also must pay back a loan that has come due in the amount of 900 platinum. Return this to me and I shall share with you my dark mission, and the immeasurable reward that will be earned at its completion. Show your face here without fulfilling my request and I will offer you on the altar to Innoruuk himself. Be off!");
 	end
 end
