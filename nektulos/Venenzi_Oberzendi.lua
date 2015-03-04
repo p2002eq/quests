830893f543ed08f63458372a939a41c4bd312fff
Tom <tommytryharder@gmail.com>
2015-03-04 14:00:07 -0500

    fixed flowing black robe turn in

--- nektulos/Venenzi_Oberzendi.lua
+++ nektulos/Venenzi_Oberzendi.lua
@@ -22,7 +22,7 @@ function event_trade(e)
 		--e.other:Ding();
 		--e.other:AddEXP(500);
 	end
-	if(item_lib.check_turn_in(e.trade, {item1 = 20643,item2 = 1320})) then -- flowing black robe turn in
+	if(item_lib.check_turn_in(e.trade, {item1 = 1320})) then -- flowing black robe turn in
 		e.self:Say("Thank you, " .. e.other:GetName() .. ". I did not think you would track down Najena and slay her. I think that I am going to return to my old life in Neriak. I am sick of the way Kazen has been treating me. He never seems to teach me anything worthwhile. Don't forget to give his lapdog that symbol with the reagents. I'm sure he will give you a new one.");
 		e.other:QuestReward(e.self,0,0,0,0,20649,500);
 		--e.other:SummonItem(20649);
