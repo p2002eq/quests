58048202ecf638c9fd28d89c9b226b357de91006
regneq <rregenn@gmail.com>
2015-03-13 23:34:38 -0700

    implement the Corrupted Guards pathing in WKarana and flavor text to be more like live.

--- qey2hh1/Misty_Storyswapper.lua
+++ qey2hh1/Misty_Storyswapper.lua
@@ -1,6 +1,15 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say(string.format("Nice to meet you. %s. If you are a fellow bard. I must inform you that you will make no profit around here. Not with Guard McCluskey around.",e.other:GetName()));
+		e.self:Say("Nice to meet you. " .. e.other:GetName() .. ". If you are a fellow bard. I must inform you that you will make no profit around here. Not with Guard McCluskey around.");
+	elseif(e.message:findi("Guard McCluskey")) then
+		e.self:Say("I will tell you no more. I do not wish to end up boxed and buried. Just stay away from Guard McCluskey. He is no honorable man.");
+	end
+end
+
+function event_signal(e)
+	if(e.signal == 1) then
+		e.self:Say("This cannot be true! I will report this to Commander Kane Bayle himself!");
+		eq.signal(12090,2); -- Guard_McCluskey
 	end
 end
 
---
+++ qey2hh1/Sera_McMannus.lua
@@ -0,0 +1,5 @@
+function event_signal(e)
+	if(e.signal == 1) then
+		e.self:Say("I never knew Antonius Bayle to be such a tyrant. This cannot be his law!");
+	end
+end
