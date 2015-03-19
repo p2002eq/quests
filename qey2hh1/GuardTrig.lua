b9e102db7b94c92ab3bd8d2e586ffc95af5f0fe8
regneq <rregenn@gmail.com>
2015-03-18 17:27:09 -0700

    change the way it send signals to the guards.

--- qey2hh1/GuardTrig.lua
+++ qey2hh1/GuardTrig.lua
@@ -1,9 +1,4 @@
 function event_spawn(e)
-	eq.set_timer("depop",5000);
-end
-
-function event_timer(e)
 	eq.signal(12090,1); -- Guard_McCluskey
 	eq.signal(12147,1); -- Guard_Donlan
-	eq.depop();
 end
