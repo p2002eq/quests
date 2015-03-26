fc7b689330ecdf2362cb07c08a814233512ce1ae
Tom <tommytryharder@gmail.com>
2015-03-26 14:36:40 -0400

    6% chance

--- southkarana/14006.lua
+++ southkarana/14006.lua
@@ -10,11 +10,11 @@ function event_death_complete(e)
 	local quillmane = 14139;
 
 	if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(quillmane) == false) then
-		if(random_result < 4) then
+		if(random_result < 2) then
 			eq.spawn2(quillmane,1,0,3210,-6821.6,38.3,193.3);
-		elseif(random_result >= 4 and random_result < 7) then
+		elseif(random_result >= 2 and random_result < 4) then
 			eq.spawn2(quillmane,2,0,-2761.8,-6076,0.7,4.0);
-		elseif(random_result >= 7 and random_result < 10) then
+		elseif(random_result >= 4 and random_result < 6) then
 			eq.spawn2(quillmane,3,0,-2333.8,1296.5,38.7,247.3);
 		end
 	end
