f9065c6bd50e86267dff0985f5d93500a2bd1ebf
Tom <tommytryharder@gmail.com>
2015-04-06 12:43:12 -0400

    changed to 5%

--- steamfont/a_minotaur_slaver.lua
+++ steamfont/a_minotaur_slaver.lua
@@ -1,7 +1,7 @@
 --NPC: a_minotaur_slaver   Zone: steamfont
 
 function event_death_complete(e)
-	if(math.random(100) > 98) then
+	if(math.random(100) < 5) then
 		eq.unique_spawn(56152,0,0,-1294,1360,-103);
 	end
 	e.self:Say("I die soon! Meldrath, help me!");
