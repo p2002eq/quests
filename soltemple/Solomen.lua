eca34e03c277e7dec3ca09fa5975fbbf578493a5
regneq <rregenn@gmail.com>
2015-04-12 20:34:31 -0700

    simplify questsay for Solomen.lua

--- soltemple/Solomen.lua
+++ soltemple/Solomen.lua
@@ -1,11 +1,11 @@
 function event_say(e)
-	if(e.message:findi("Hail")) then
+	if(e.message:findi("hail")) then
 		e.self:Say("Ah, I see you have found my study.  This is where I study and scribe the history of magic in Norrath.");
-	elseif(e.message:findi("what history")) then
+	elseif(e.message:findi("history")) then
 		e.self:Say("I have worked to chronicle everything from the beginnings of magic to what we consider the modern day of magic.  Solusek Ro has been very generous to those who wield magic in Norrath.  But throughout history there is one thing I have noticed - too much power corrupts the soul.  I have seen and documented that the strongest wizards and those closest to Solusek Ro become corrupted by their power.");
-	elseif(e.message:findi("what wizards")) then
+	elseif(e.message:findi("wizard")) then
 		e.self:Say("Unfortunately, I don't have time to speak of such things right now.  But I could use your help since you are here.  I have here an envelope that I need delivered to one Camin.  You can find him near the wizard tower in the city of Erudin.  He is a sage like myself whose knowledge rivals my own.  Do you [agree] to undertake my task?");
-	elseif(e.message:findi("i agree")) then
+	elseif(e.message:findi("agree")) then
 		e.self:Say("Here you are, then.  Good luck on your journey.");
 		e.other:SummonItem(18088);
 	end
