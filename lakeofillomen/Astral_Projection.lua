0f871de7e1b08b5a4f5115d112791c71a1c9adf5
Tom <tommytryharder@gmail.com>
2015-04-29 14:20:00 -0400

    summon trigger

--- lakeofillomen/Astral_Projection.lua
+++ lakeofillomen/Astral_Projection.lua
@@ -9,10 +9,12 @@ function event_trade(e)
 	if((item_lib.check_turn_in(e.trade, {item1 = 1687})) and (e.other:GetLevel() > 45) and (e.other:Class() == "Monk")) then
 		-- Monk Epic 1.0
 		eq.depop();
+		eq.unique_spawn(85397,0,0,-541,-806,-380); -- summon trigger
 		-- Spawn: Vorash
 		eq.spawn2(85211,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 		-- Spawn: Deep
 		eq.spawn2(85223,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+		
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
