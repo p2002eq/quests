f1801cedde23d8ecf2f0b92855a7d7cbb1eca9ee
Tom <tommytryharder@gmail.com>
2015-04-29 11:10:38 -0400

    removed quest global (should not be in this part)

--- overthere/Astral_Projection.lua
+++ overthere/Astral_Projection.lua
@@ -17,14 +17,14 @@ function event_trade(e)
 	-- [FLAG: Check target: eq.get_globals()] local qglobals = eq.get_qglobals();
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.trade, {item1 = 1686})) then --Trunt's Head
-		if(qglobals["Trunt"] ~= nil and qglobals["Trunt"]=="1") then --Trunt's Head has been turned in to Deep in Lake Rathetear
+		--if(qglobals["Trunt"] ~= nil and qglobals["Trunt"]=="1") then --Trunt's Head has been turned in to Deep in Lake Rathetear
 			e.self:Emote("looks up and down.");
 			e.self:Say("Hmmm, I was hoping for something more impressive. This is just a small token of the last person who tried to interfere with my plans. He failed as shall you. But tell me truly, are you the one who has so rudely removed my students?");
 			e.other:QuestReward(e.self,0,0,0,0,1687); --Eye of Kaiaren
-			eq.delete_global("Trunt");
+			--eq.delete_global("Trunt");
 		else
 			e.self:Emote("ignores you.");
-			e.other:SummonItem(1686); --Trunt's Head
+		e.other:QuestReward(e.self,0,0,0,0,1686); --Trunt's Head
 		end
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
