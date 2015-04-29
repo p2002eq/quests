545fe5829b057abb0f4dd2078e40bd12c356cff9
Tom <tommytryharder@gmail.com>
2015-04-29 13:31:57 -0400

    monk epic trigger

---
+++ lakeofillomen/monk_trigger.lua
@@ -0,0 +1,12 @@
+function event_signal(e)
+    eq.zone_emote(12, "Signaled:" .. e.signal);
+    if(e.signal == 1) then --Deep died 
+        Deep = 1;
+    end
+    if(e.signal == 2) then --Vorash died 
+        Vorash = 1;
+    end
+    if(Deep = 1) and (Vorash = 1) then
+         eq.unique_spawn(85396,0,0,-541,-806,80); 
+    end
+end
