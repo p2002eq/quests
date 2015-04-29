911b397b370dd7608ce52fc352ff7aaf214ddd73
Tom <tommytryharder@gmail.com>
2015-04-29 09:39:50 -0400

    summonitem > quest reward

--- timorous/Alrik_Farsight.lua
+++ timorous/Alrik_Farsight.lua
@@ -9,7 +9,7 @@ function event_say(e)
 			e.self:Emote("rummages through his tattered bag and throws aside a towel. 'Too many towels. Oh well, someone told me they were good to have once. Here it is, a bowl pattern. Interesting bowl. Looks almost like a magical bowl but I'm no potter. You need it, you say? Well, it is interesting. Part of my research here is for the wizard guild in Felwithe. I'm sure they'd put a high price on this one, the enchanters guild being so interested in trading for magical paraphernalia and all. Of course if you helped me out on a chore I have, I'd be happy to give it to you. I need to deliver an artifact to the guild and I'd much rather stay here exploring. If you wish, you can take the artifact and return with the receipt and I'll give you the pattern.'");
 		elseif(e.message:findi("take the artifact")) then
 			e.self:Say("Hah! Okay, then. Here take this to Farios Elianos in Felwithe. He will give you the receipt.");
-			e.other:SummonItem(20457);
+			e.other:QuestReward(e.self,0,0,0,0,20457);
 		end
 	elseif(e.other:GetFaction(e.self) > 4) then
 		local random = math.random(3);
