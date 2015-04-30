88ecda27f36dbe4ec1dd8da666310d94868d34e6
Tom <tommytryharder@gmail.com>
2015-04-30 12:32:31 -0400

    .

--- citymist/Marl_Kastane.lua
+++ citymist/Marl_Kastane.lua
@@ -1,7 +1,15 @@
 -- Part of SK Epic 1.0
 function event_spawn(e)
-	e.self:Say("Alas, I cannot be the one to carry the sword back to my people as proof in fear they will kill me to possess it for their own. I think a simple trade is in order, perhaps you have a symbol or token of Lhranc's that I could take back to the others to ease their worries?");
+	--e.self:Say("Alas, I cannot be the one to carry the sword back to my people as proof in fear they will kill me to possess it for their own. I think a simple trade is in order, perhaps you have a symbol or token of Lhranc's that I could take back to the others to ease their worries?");
 	eq.set_timer("depop",4800000);
+end
+
+function event_say(e)
+	if(e.other:GetCharacterFactionLevel(342) > 81) then--True Spirit
+		e.self:Say("Alas, I cannot be the one to carry the sword back to my people as proof in fear they will kill me to possess it for their own. I think a simple trade is in order. Perhaps you have a symbol or token of Lhanrc's that I could take back to the others to ease their worries?");
+	else
+		e.self:Emote("seems to ignore you.");
+	end
 end
 
 function event_trade(e)
