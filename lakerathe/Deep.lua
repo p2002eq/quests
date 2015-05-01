bb1ea81f2d3f06c7bc00feccc13db17d34358d8d
Gary <gforce215@yahoo.com>
2015-05-01 09:56:40 -0400

    Removed Q-global

--- lakerathe/Deep.lua
+++ lakerathe/Deep.lua
@@ -10,7 +10,6 @@ function event_trade(e)
 		e.self:Emote("slowly opens her eyes and looks up at you. She stares at you a long while and then closes her eyes and lowers her head again.");
 		e.self:Say("Very well, " .. e.other:GetName() .. ", if you wish death so greatly, we will be happy to oblige. My master projects part of himself in the wilder lands known as the Overthere. He has granted you an audience. Find him and show him the head of our earth brother. At that point, we will discuss how we will end your life.");
 		e.other:SummonItem(1686); --Trunt's Head
-		eq.set_global("Trunt","1",5,"F");
 		e.other:Ding();
 		eq.depop_with_timer();
 	else
