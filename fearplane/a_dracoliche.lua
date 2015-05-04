47260f2ce73efa7518a13e0982c0ddcd1ff54dfa
regneq <rregenn@gmail.com>
2015-05-03 19:36:47 -0700

    a_dracoliche should now warp to CT when called.

--- fearplane/a_dracoliche.lua
+++ fearplane/a_dracoliche.lua
@@ -5,9 +5,6 @@ function event_signal(e)
 		e.self:Say("Such is the will of Cazic-Thule!");
 	elseif(e.signal == 2) then
 		local mobtypeID =  entity_list:GetMobByNpcTypeID(72003);
-		local follow_mob = mobtypeID:GetID();
-		eq.follow(follow_mob);
-	elseif(e.signal == 3) then
-		eq.stop_follow();
+		e.self:GMMove(mobtypeID:GetX(),mobtypeID:GetY(),mobtypeID:GetZ());
 	end
 end
