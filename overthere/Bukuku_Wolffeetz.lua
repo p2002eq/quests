2ee236fdd8c4ca4ab9fdfed85010dc07340e2ded
Tom <tommytryharder@gmail.com>
2015-05-04 14:27:15 -0400

    fixed spell turn ins

--- overthere/Bukuku_Wolffeetz.lua
+++ overthere/Bukuku_Wolffeetz.lua
@@ -8,7 +8,7 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 19269, item2 = 19238, item3 = 19264, item4 = 19272})) then
+	if(item_lib.check_turn_in(e.trade, {item1 = 19269}) or item_lib.check_turn_in(e.trade, {item1 = 19238}) or item_lib.check_turn_in(e.trade, {item1 = 19264}) or item_lib.check_turn_in(e.trade, {item1 = 19272})) then
 		e.self:Say("Here is the scroll that I promised. We have both gained much knowledge today. I hope to do business with you again soon. Farewell!");
 		e.other:QuestReward(e.self,0,0,0,0,eq.ChooseRandom(19267,19271,19274,19266),1000);
 	end
