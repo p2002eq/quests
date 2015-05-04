689a6b9b2e582f11bd14aecf7bbdac9a222e39be
Tom <tommytryharder@gmail.com>
2015-05-04 14:28:42 -0400

    fixed spell turn ins

--- overthere/Brinaa_Darkpact.lua
+++ overthere/Brinaa_Darkpact.lua
@@ -8,7 +8,7 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 19203, item2 = 19205, item3 = 19209, item4 = 19212})) then
+	if(item_lib.check_turn_in(e.trade, {item1 = 19203}) or item_lib.check_turn_in(e.trade, {item1 = 19205}) or item_lib.check_turn_in(e.trade, {item1 = 19209}) or item_lib.check_turn_in(e.trade, {item1 = 19212})) then
 		e.self:Say("Here is the scroll that I promised. We have both gained much knowledge today. I hope to do business with you again soon. Farewell!");
 		e.other:QuestReward(e.self,0,0,0,0,eq.ChooseRandom(19210,19224,19420,19206),1000);
 	end
