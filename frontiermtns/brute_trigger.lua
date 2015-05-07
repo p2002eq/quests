9241c81415ba6a23315b525a60703a38d580fad8
Tom <tommytryharder@gmail.com>
2015-05-07 14:09:01 -0400

    spawns misc mobs

---
+++ frontiermtns/brute_trigger.lua
@@ -0,0 +1,25 @@
+-- Brute cycle, involving the Brutes, Misc, and the Sarnak.
+
+function event_spawn(e)
+	--eq.spawn2(95000,0,0,2832,1667,-375,0);
+	
+end
+
+function event_signal(e)
+    eq.zone_emote(12, "Signaled:" .. e.signal);
+    if(e.signal == 1) then --mob died
+        local getnewloc = eq.ChooseRandom(1,2);
+	        if(getnewloc == 1) then
+		        newx = 3150;
+		        newy = -2112;
+		        newz = -408;
+	        elseif(getnewloc == 2) then
+		        newx = 2371;
+		        newy = -3013;
+		        newz = -513;
+	        end
+	        
+	   local getnewmobid = eq.ChooseRandom(92176, 92222, 92223, 92224, 92225, 92250);
+	        eq.spawn2(getnewmobid,0,0,newx,newy,newz,0);
+    end
+end
