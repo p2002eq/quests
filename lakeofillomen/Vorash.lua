0429a59f8ea8c150cf198a3a303a55c79d5fe7f0
Tom <tommytryharder@gmail.com>
2015-05-07 13:12:05 -0400

    fixed spawn of Xene

--- lakeofillomen/Vorash.lua
+++ lakeofillomen/Vorash.lua
@@ -15,12 +15,12 @@ function event_enter(e)
 	end
 end
 
---function event_death_complete(e)
+function event_death_complete(e)
 	-- MonkEpic 1.0
---	e.self:Say("Foolish mortal! you think you have defeated me? Now, witness the true power of Rallos Zek!");
+	e.self:Say("Foolish mortal! you think you have defeated me? Now, witness the true power of Rallos Zek!");
 	-- Spawn: Xenevorash
-	--eq.spawn2(85208,0,0,-542,-807,79.12,0);
--- end
+	eq.spawn2(85208,0,0,-542,-807,79.12,0);
+ end
 
 function event_trade(e)
 	local item_lib = require("items");
