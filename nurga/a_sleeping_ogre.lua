5683ca9b905e670539e0e86d1d9204e07d0f65ef
Tom <tommytryharder@gmail.com>
2015-05-07 13:18:16 -0400

    updated trunt's npc id

--- nurga/a_sleeping_ogre.lua
+++ nurga/a_sleeping_ogre.lua
@@ -9,7 +9,7 @@ function event_trade(e)
 	if((item_lib.check_turn_in(e.trade, {item1 = 1685}) and (e.other:GetLevel() > 45) and (e.other:Class() == "Monk")) then --Breath of Gwan
 		e.self:SetAppearance(0);
 		e.self:Say("Your path of wanton destruction ends here. Gwan and Eejag were impatient and hot-headed. You will not defeat me, for I have the patience and perseverance of stone, unlike the children you have beaten before me. Are you sure you want to challenge me?");
-		eq.spawn2(107161,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --Trunt
+		eq.spawn2(107139,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --Trunt
 		eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
