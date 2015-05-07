b0d3c9a0ad4f15f7d90b8235e930582a79c68657
Tom <tommytryharder@gmail.com>
2015-05-07 13:14:39 -0400

    comment out the death complete

--- lakeofillomen/Vorash.lua
+++ lakeofillomen/Vorash.lua
@@ -17,9 +17,9 @@ end
 
 function event_death_complete(e)
 	-- MonkEpic 1.0
-	e.self:Say("Foolish mortal! you think you have defeated me? Now, witness the true power of Rallos Zek!");
+	--e.self:Say("Foolish mortal! you think you have defeated me? Now, witness the true power of Rallos Zek!");
 	-- Spawn: Xenevorash
-	eq.spawn2(85208,0,0,-542,-807,79.12,0);
+	--eq.spawn2(85208,0,0,-542,-807,79.12,0);
  end
 
 function event_trade(e)
