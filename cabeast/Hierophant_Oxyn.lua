9c6c1e25ac2bcb87e16e07ec89b6619e9e4c40aa
Tom <tommytryharder@gmail.com>
2015-05-11 08:27:56 -0400

    faction update

--- cabeast/Hierophant_Oxyn.lua
+++ cabeast/Hierophant_Oxyn.lua
@@ -7,20 +7,20 @@ function event_say(e)
 	--Cursed Wafers Event_Say
 	if(e.message:findi("hail")) then
 		e.self:Say("Greetings, and may the pain of the ancients guide you. You have come to us for guidance, have you not? We are the Hierophants of Cabilis and we guide the young Scale Mystics. All petitioners shall speak with me of [temple tasks].");
-	elseif((e.message:findi("temple tasks")) and (e.other:GetCharacterFactionLevel(282) > 6) and (e.other:Class() == "Shaman")) then
+	elseif((e.message:findi("temple tasks")) and (e.other:GetFaction(e.self) < 3) and (e.other:Class() == "Shaman")) then
 		e.self:Say("The Temple of Terror requires all young Scaled Mystics to [perform daily tasks.]. The tasks are necessary to the upkeep of our order as well as that of our brothers, the Crusaders of Greenmist.");
-	elseif((e.message:findi("daily tasks")) and (e.other:GetCharacterFactionLevel(282) > 6) and (e.other:Class() == "Shaman")) then
+	elseif((e.message:findi("daily tasks")) and (e.other:GetFaction(e.self) < 3) and (e.other:Class() == "Shaman")) then
 		e.self:Say("We require many components for various rituals. Take this Component mortar and fill it with the following items - foraged [mud crabs]. two small mosquito wings and one portion of bone chips. You must then use the pestle and combine all the components. When you have a full component mortar, you may return to me and I shall pay you your wages, but most importantly, you shall prove your devotion to the Scaled Mystics.");
 		e.other:SummonItem(17020); 								--give the player the combine box: Component Mortar
 	elseif(e.message:findi("mud crabs")) then 					--Cursed Wafers quest
 		e.self:Say("Mud crabs are tiny crustaceans which live along the mudcaked shores of the Lake of Ill Omen. You can forage for them and find a handful of them at a time.");
 	--Shaman Skull Quest No.1 Event_Say
-	elseif((e.message:findi("lost skulls")) and (e.other:GetCharacterFactionLevel(282) > 6)) then
+	elseif((e.message:findi("lost skulls")) and (e.other:GetFaction(e.self) < 3)) then
 		e.self:Say("You must have heard of the Trilac Brotherhoods disappearance. They are the skulls of three soon to be ancients. They were taken from this temple by a silent intruder. Crusaders are always on duty. I do not know how they got into our vault. Every petitioner is ordered to search for the three skulls and return them to me along with their petitioner cudgel and then they shall become clairvoyants.");
-	elseif((e.message:findi("iron cudgel")) and (e.other:GetCharacterFactionLevel(282) > 6)) then
+	elseif((e.message:findi("iron cudgel")) and (e.other:GetFaction(e.self) < 3)) then
 		e.self:Say("If you are looking to be handed the Iron Cudgel of the Clairvoyant then you are sadly mistaken. Perhaps if you were to gather a few [lost skulls] for the temple we may find you worthy to wield one.");
 	--Shaman Skull Quest No.2 Event_Say
-	elseif((e.message:findi("other skulls")) and (e.other:GetCharacterFactionLevel(282) > 6)) then
+	elseif((e.message:findi("other skulls")) and (e.other:GetFaction(e.self) < 3)) then
 		e.self:Say("A couple of rogue mystics have taken two skulls from another one of our temples. They could not have gone far, but i fear that one of the skulls was [damaged] in their escape.");
 	elseif((e.message:findi("damaged")) and (e.other:GetCharacterFactionLevel(282) > 6)) then
 		e.self:Say("A guard had managed to attack one of the mystics, but hit the bag carrying the skull.  I fear that the skull has been damaged, but it can be repaired.  Acquire it's pieces and some Mendglow clay to put it back together.");
@@ -30,7 +30,7 @@ function event_say(e)
 end
 
 function event_trade(e)
-	local qglobals = eq.get_qglobals(e.self,e.other);
+	--local qglobals = eq.get_qglobals(e.self,e.other);
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.trade, {item1 = 3895})) then 	--Crusaders of Greenmist (Greenmist Quest 8/8)
 		e.self:Emote("takes the note and begins to howl into the air! 'The visions are true! The new prophecy begins today, Crusader,' the mystic growls with pleasure. He quickly turns and takes a bottle of murky liquid from one of his potion bags and hands it to you. 'Take this and keep it safe. Our visions have told of this day. We have been able to learn of the metal of prophecy. This [liquid] will help us to locate its true resting place!");
@@ -59,7 +59,7 @@ function event_trade(e)
 		e.other:Ding();
 	--Shaman Skull Quest No.1 turn in - Check for  Logrin Skull, Morgl Skull, and Waz Skull hand in along with Iron Cudgel of the Petitioner
 	end
-	if((item_lib.check_turn_in(e.trade, {item1 = 12721, item2 = 12722, item3 = 12723, item4 = 5140})) and (e.other:GetCharacterFactionLevel(282) > 6)) then
+	if((item_lib.check_turn_in(e.trade, {item1 = 12721, item2 = 12722, item3 = 12723, item4 = 5140})) and (e.other:GetFaction(e.self) < 3)) then
 		e.self:Say("Excellent, you have retrieved the three skulls of the ancients for us.  There are yet mroe skulls that I must ask you to retrieve before I can assist you in furthering your progress to becoming a clairvoyant. I will tell you with whom you need to speak to to continue the path of clairvoyance upon bringing me the [other skulls].");
 		--eq.set_global("shmskullquest","2",5,"F"); --set a global flag so that user can do shm skull quest part 2
 		e.other:SummonItem(5141); 				--Iron Cudgel of the Clairvoyant
@@ -70,7 +70,7 @@ function event_trade(e)
 		e.other:Ding();
 	--shaman skull quest no.2 turn in - check for Skull with I and Skull with II turn in and cudgel
 	end
-	if((item_lib.check_turn_in(e.trade, {item1 = 12724, item2 = 12725, item3 = 5141})) and (e.other:GetCharacterFactionLevel(282) > 6)) then
+	if((item_lib.check_turn_in(e.trade, {item1 = 12724, item2 = 12725, item3 = 5141})) and (e.other:GetFaction(e.self) < 3)) then
 		e.self:Say("We are in your debt. You are truly one who shall collect all the lost ancient skulls. Take your weapon. Go to Hierophant Zand and he shall guide you further. Tell him you are [the chosen saviour].");
 		--eq.set_global("shmskullquest","3",5,"F");	--set a global flag so that user can do shm skull quest part 3
 		e.other:SummonItem(5142); 				--Iron Cudgel of the Seer
