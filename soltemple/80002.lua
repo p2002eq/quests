a2ba2a3faa1b8dcd361391de2670eebe4309ec7a
regneq <rregenn@gmail.com>
2015-05-15 22:54:36 -0700

    remove the offset in GetX() position for spawn2.

--- soltemple/80002.lua
+++ soltemple/80002.lua
@@ -28,7 +28,7 @@ function event_trade(e)
 local item_lib = require("items");
 	if(item_lib.check_turn_in(e.trade, {item1 = 28051})) then --Coral Statue of Tarew
 		e.self:Emote("shakes violently as his hand closes on the coral statue and flames dance in the depths of his eyes! The statue begins to emit a reddish glow then shatters in a burst of fire and smoke! The flames in the plasmatic priest's eyes vanish with the explosion and only madness remains! 'The Triumvirate thinks that destroying my spirit of flame will stop me?!! I will kill you all now, the goblins will continue to war, and when my spirit is restored I will ensure that Ixiblat Fer consumes all of Norrath in his flames!!'");
-		eq.spawn2(80042,0,0,e.self:GetX()+10,e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1);
+		eq.spawn2(80042,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1);
 		eq.depop();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
