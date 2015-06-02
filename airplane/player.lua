973bde0f338098a02ab8e604d3a2d76f9fbb51e9
regneq <rregenn@gmail.com>
2015-06-02 10:20:24 -0700

    added Veeshan's Key (quest room) to be deleted when zoning.

--- airplane/player.lua
+++ airplane/player.lua
@@ -1,7 +1,7 @@
 function event_zone(e)
-	local airplanekey ={20915,20914,20913,20917,20911,20912,20916,20918};
+	local airplanekey ={20915,20914,20913,20917,20911,20912,20916,20918,20919};
 	
-	for i = 1, 8 do
+	for i = 1, 9 do
 		if(e.self:HasItem(airplanekey[i])) then
 			e.self:NukeItem(airplanekey[i]);
 		end
