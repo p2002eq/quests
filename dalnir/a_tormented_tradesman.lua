f3c81044d99a307eaca03cfedb09d09c24612e79
regneq <rregenn@gmail.com>
2015-06-07 14:07:23 -0700

    update Dalnir scripts.

--- dalnir/a_tormented_tradesman.lua
+++ dalnir/a_tormented_tradesman.lua
@@ -1,14 +1,20 @@
 --The tormented tradesman is actually the skeleton/spirit of the Haggle Baron, Dalnir. Giving this skeleton the Mirror of Self Loathing will spawn Haggle Baron Dalnir so you can finish the Greenmist quest.
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("End my suffering... I cannot think beyond this pain! Why am I in this place? What have I done?");
+	end
+end
+
 function event_trade(e)
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.trade, {item1 = 3889})) then --Crusaders of Greenmist (Greenmist Quest 8/8)
-		e.self:Emote("The undead entity looks in the mirror and appears to be stunned by the memory of his former self. He drops the mirror, which causes it to crack slightly. You immediately grab it and place it back in your bag. The spirit looks at you and begins to transform. Ethereal strands of muscle, blood, and flesh come together and the Haggle Baron Dalnir appears.");
+		eq.signal(104072,1);
 		e.other:QuestReward(e.self,0,0,0,0,3896);
 		eq.spawn2(104165,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 		eq.depop_with_timer();
 	end
 	if(item_lib.check_turn_in(e.trade, {item1 = 3896})) then --Crusaders of Greenmist (Greenmist Quest 8/8)
-		e.self:Emote("The undead entity looks in the mirror and appears to be stunned by the memory of his former self. He drops the mirror, which causes it to crack slightly. You immediately grab it and place it back in your bag. The spirit looks at you and begins to transform. Ethereal strands of muscle, blood, and flesh come together and the Haggle Baron Dalnir appears.");
+		eq.signal(104072,1);
 		e.other:Ding();
 		eq.spawn2(104165,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 		eq.depop_with_timer();
