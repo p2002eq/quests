f332cb462ddc9957a3d150a1f9ba9530acb44f82
regneq <rregenn@gmail.com>
2015-06-14 21:30:14 -0700

    Changed NPC for Rare Coins quest for the era instead of Assistant Bufford.  Added a hail text for Ailerina the Gentle.

--- firiona/Wellford_Fargin.lua
+++ firiona/Wellford_Fargin.lua
@@ -8,10 +8,15 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
+	-- You can purchase a Star of Odus for about 115pp in the jewel shop under the Erudin palace.
+	-- Giving it to Wellford Fargin results in a "Tin Box" with 6 slots.
+	if(item_lib.check_turn_in(e.trade, {item1 = 10059})) then
+		e.self:Say("Thank you!! I never could have gone to Odus to replace this. Perhaps now you can collect some rare coins.");
+		e.other:QuestReward(e.self,0,0,0,0,17045);
+	end
+	-- put the 6 coins into the tin box, combine, and return it to Wellford Fargin, get Nostrolo Tambourine
+	if(item_lib.check_turn_in(e.trade, {item1 = 12829})) then
+		e.other:QuestReward(e.self,0,0,0,0,12940);
+	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
-
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
