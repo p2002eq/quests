819c85004828f9deedb174916a9a30c53f7fe080
Tom <tommytryharder@gmail.com>
2015-07-10 14:38:02 -0400

    updated monk trigger

--- lakeofillomen/monk_trigger.lua
+++ lakeofillomen/monk_trigger.lua
@@ -1,13 +1,13 @@
 function event_signal(e)
-    eq.zone_emote(12, "Signaled:" .. e.signal);
+   local spawn = 0;
+
+    --eq.zone_emote(12, "Signaled:" .. e.signal);
     if(e.signal == 1) then --Deep died 
-        Deep = 1;
+        spawn = spawn + 1;
+        if(spawn == 2) then
+            spawn = spawn - 2;
+            eq.unique_spawn(85396,0,0,-541,-806,80);
+            end
     end
-    if(e.signal == 2) then --Vorash died 
-        Vorash = 1;
-    end
-    if(Deep == 1) and (Vorash == 1) then
-         eq.unique_spawn(85396,0,0,-541,-806,80); 
-         eq.depop();
-    end
+    
 end
