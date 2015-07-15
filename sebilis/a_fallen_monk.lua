8f039be98fdb86e9d9a86e5e75f3c2804992f6c5
Gary <gforce215@yahoo.com>
2015-07-15 13:02:40 -0400

    ID UPDATE

--- sebilis/a_fallen_monk.lua
+++ sebilis/a_fallen_monk.lua
@@ -9,7 +9,7 @@ function event_trade(e)
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.trade, {item1 = 4199})) then
 		e.self:Say("Earn what is mine. Show me you have the power worthy to wield what I possess.");
-		eq.spawn2(89183,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+		eq.spawn2(89209,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 		eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
