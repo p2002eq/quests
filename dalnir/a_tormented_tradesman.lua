305424ca742b0e56d3fe860dcbf0330697d791e9
Gary <gforce215@yahoo.com>
2015-07-15 13:10:08 -0400

    npc update id

--- dalnir/a_tormented_tradesman.lua
+++ dalnir/a_tormented_tradesman.lua
@@ -4,13 +4,13 @@ function event_trade(e)
 	if(item_lib.check_turn_in(e.trade, {item1 = 3889})) then --Crusaders of Greenmist (Greenmist Quest 8/8)
 		e.self:Emote("The undead entity looks in the mirror and appears to be stunned by the memory of his former self. He drops the mirror, which causes it to crack slightly. You immediately grab it and place it back in your bag. The spirit looks at you and begins to transform. Ethereal strands of muscle, blood, and flesh come together and the Haggle Baron Dalnir appears.");
 		e.other:QuestReward(e.self,0,0,0,0,3896);
-		eq.spawn2(104075,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+		eq.spawn2(104165,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 		eq.depop_with_timer();
 	end
 	if(item_lib.check_turn_in(e.trade, {item1 = 3896})) then --Crusaders of Greenmist (Greenmist Quest 8/8)
 		e.self:Emote("The undead entity looks in the mirror and appears to be stunned by the memory of his former self. He drops the mirror, which causes it to crack slightly. You immediately grab it and place it back in your bag. The spirit looks at you and begins to transform. Ethereal strands of muscle, blood, and flesh come together and the Haggle Baron Dalnir appears.");
 		e.other:Ding();
-		eq.spawn2(104075,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+		eq.spawn2(104165,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 		eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
