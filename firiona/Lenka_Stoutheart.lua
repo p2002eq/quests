3b70304a4691ec3b2739a6c05ce04bd7560861f7
regneq <rregenn@gmail.com>
2015-07-16 10:16:38 -0700

    one more correction for Lenka_Stoutheart.lua

--- firiona/Lenka_Stoutheart.lua
+++ firiona/Lenka_Stoutheart.lua
@@ -5,9 +5,10 @@ function event_say(e)
 		eq.signal(84129,1);
 	elseif(e.message:findi("interesting")) then
 		e.self:Emote("takes a jagged scimitar out of a wrap and lets you hold it.  You feel as though it is linked to the spirit world.  Lenka grabs it back.  'No, no!  You can't keep it.  But I can tell you there are plenty more weapons and armor like this.  Just have to do a little dragon slaying.'");
+		eq.signal(84129,2);
 		e.self:DoAnim(57);
 	elseif(e.message:findi("cabby pale ale")) then
-		eq.signal(84129,2);
+		eq.signal(84129,3);
 		eq.unique_spawn(84005,22,0,-3148,-1305,256,0);
 	elseif(e.message:findi("lizardtown")) then
 		e.self:Say("The trackers of Firiona Vie have come back with reports that the Iksar are back in force and have a city somewhere in the heart of Kunark. There goes the neighborhood...again.");
