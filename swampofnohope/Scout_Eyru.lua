30a998b32b2a8d8f2cec15921486cce38f7bbd60
Gary <gforce215@yahoo.com>
2015-07-17 13:42:04 -0400

    id update

--- swampofnohope/Scout_Eyru.lua
+++ swampofnohope/Scout_Eyru.lua
@@ -10,7 +10,7 @@ function event_trade(e)
 		e.self:Say("It is a relief to see you. I was beginning to fear that no one would be able to find me in this miserable swamp. I wanted to get word back to Shar Vahl about my location, but I was afraid that I would lose the trail off this spirit if I were to break off my pursuit. This location is as strange of a destination for our spirit as it is dangerous. I have seen the spirit several times and it is very out of place here. I can't help but think that the transformation has left the spirit confused beyond all repair.");
 		e.self:Say("If you move towards the large body of water just north of here, you should be able to find a vantage point that will allow you to see it. The spirit has been resting under the surface of the water there for several days now. Once you release the spirit, please return to me with the totem that binds it. I will prepare the totem, so that you may return it to Shar Vahl.");
 		e.self:Emote("A giant reptile roars, as it sinks below the surface of the pond. It appears that is senses your approach.");
-		eq.spawn2(83244,0,0,2289,-3001,-28,0);
+		eq.spawn2(83476,0,0,2289,-3001,-28,0);
 	end
 	if(item_lib.check_turn_in(e.trade, {item1 = 9042})) then
 		e.self:Say("Splendid job, " .. e.other:GetName() .. ". This wrapping should help to shield you from any of the negative properties that may be manifested in the totem. You should be fine if you keep it in the box that you were given. Once you collect all of the totems, please seal the box and return it to Shar Vahl. Be careful to protect the totems during the course of your travels. The balance of power in several realms is reliant on your ability to fulfill this mission. Please do not fail us.");
