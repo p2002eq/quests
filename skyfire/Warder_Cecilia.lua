6d646440375f73cf24ba805c11d1cd558579e215
Tom <tommytryharder@gmail.com>
2015-07-23 22:55:26 -0400

    updated quest reward

--- skyfire/Warder_Cecilia.lua
+++ skyfire/Warder_Cecilia.lua
@@ -10,6 +10,7 @@ function event_trade(e)
 	if(item_lib.check_turn_in(e.trade, {item1 = 24996})) then
 	e.self:Say("The Triumvirate thinks that destroying my spirit of flame will stop me?!! I will kill you all now, the goblins will continue to war, and when my spirit is restored I will ensure that Ixiblat Fer consumes all of Norrath in his flames!!");
 		eq.unique_spawn(91096,0,0,-3225,3011,-149);
+		e.other:QuestReward(e.self,0,0,0,0,24997,100000);
 		eq.depop();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
