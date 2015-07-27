9b6cfd7888c062e1f1dc5d165551869569d3ec78
regneq <rregenn@gmail.com>
2015-07-26 22:03:37 -0700

    removed revamped version of Droga scripts. Moved some FM scripts to Droga.

--- droga/a_goblin_traitor.lua
+++ droga/a_goblin_traitor.lua
@@ -26,7 +26,7 @@ end
 function event_trade(e)
 	local item_lib = require("items");
 	if(e.other:GetFaction(e.self) < 3) then  -- need to be warmly or better
-		if(item_lib.check_turn_in(e.trade, {item1 = 5728})) then -- Di'zok Signet of Service
+		if(item_lib.check_turn_in(e.self, e.trade, {item1 = 5728})) then -- Di'zok Signet of Service
 			e.self:Emote("looks relieved when he sees your signet of service and eyes you with a bit more respect.");
 			e.self:Say("Good! They tell'd me to wait here for ya, can't say I like it but here I sits. It be good to be rid of Skargus though, so I be a happy one, oh yes. Want to know the [plan] then?");
 			e.other:QuestReward(e.self,0,0,0,0,5728); -- 5728  Di'zok Signet of Service
@@ -40,3 +40,4 @@ end
 -------------------------------------------------------------------------------------------------
 -- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
 -- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
+-------------------------------------------------------------------------------------------------
