39fe7ce3ddc872ca4d5197622e98b90d313796dc
Scott Davis <me@sdavis.info>
2015-08-01 01:23:25 -0400

    fix sleeping ogre

--- nurga/a_sleeping_ogre.lua
+++ nurga/a_sleeping_ogre.lua
@@ -19,7 +19,7 @@ function event_trade(e)
 	if((item_lib.check_turn_in(e.self, e.trade, {item1 = 1685})) and (e.other:GetLevel() > 45) and (e.other:GetClass() == 7)) then --Breath of Gwan
 		e.self:SetAppearance(0);
 		e.self:Say("Your path of wanton destruction ends here. Gwan and Eejag were impatient and hot-headed. You will not defeat me, for I have the patience and perseverance of stone, unlike the children you have beaten before me. Are you sure you want to challenge me?");
-		eq.spawn2(107000,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1); --Trunt
+		eq.spawn2(107139,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1); --Trunt
 		eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
