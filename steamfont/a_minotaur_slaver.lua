155e62cb7e5f4f3247a7a3a62f4fd36fe9757ce8
Scott Davis <me@sdavis.info>
2015-08-01 01:15:11 -0400

    fix lua

--- steamfont/a_minotaur_slaver.lua
+++ steamfont/a_minotaur_slaver.lua
@@ -1,9 +1,10 @@
 --NPC: a_minotaur_slaver   Zone: steamfont
 
 function event_death_complete(e)
-	if(math.random(100) < 5) then
-		eq.unique_spawn(56152,0,0,-1294,1360,-103);
-	e.self:Say("I die soon! Meldrath, help me!");
+    if(math.random(100) < 5) then
+        eq.unique_spawn(56152,0,0,-1294,1360,-103);
+        e.self:Say("I die soon! Meldrath, help me!");
+    end
 end
 
 -------------------------------------------------------------------------------------------------
