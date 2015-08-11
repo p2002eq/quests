2857082c10c03c44af7c90ff2565e75f742c9da5
Gary <gforce215@yahoo.com>
2015-08-10 23:14:15 -0400

    upated miraghul

--- everfrost/Lich_of_Miragul.lua
+++ everfrost/Lich_of_Miragul.lua
@@ -8,7 +8,7 @@ function event_trade(e)
 	local item_lib = require("items");
 
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 19072})) then -- handin phlactery
-		eq.spawn2(30094, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading()); -- Spawn Miragul
+		eq.spawn2(30248, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading()); -- Spawn Miragul
 		eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade);
