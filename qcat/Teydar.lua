752b504d742dc62d51a9f0aef646e073f8d9a54e
Tom <tommytryharder@gmail.com>
2015-08-19 08:56:33 -0400

    removed faction issue

--- qcat/Teydar.lua
+++ qcat/Teydar.lua
@@ -8,16 +8,14 @@ function event_say(e)
 			e.self:Say("Hello " .. e.other:GetName() .. ", it's good to see another shadowknight around here. I'm Teydar. I've made it my life to fashion suitable scabbards and sheaths for our order.");
 		end
 	elseif(e.message:findi("decrepit sheath")) then
-		if(charfaction <= 6) then
-			e.self:Say("The object you desire is very difficult to fashion. Nearly impossible, really. All I can offer to do is try. The real difficulty is the contents that give the sheath its power. They have to be of an almost equal evil. I think some kind of decrepit hide and a drake spine, along with a bar of enchanted platinum, should prove enough to make this item.");
-		end
+		e.self:Say("The object you desire is very difficult to fashion. Nearly impossible, really. All I can offer to do is try. The real difficulty is the contents that give the sheath its power. They have to be of an almost equal evil. I think some kind of decrepit hide and a drake spine, along with a bar of enchanted platinum, should prove enough to make this item.");
 	end
 end
 
 function event_trade(e)
 	local item_lib = require("items");
 		local charfaction = e.other:GetCharacterFactionLevel(342);
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 14372, item2 = 14371, item3 = 16507}) and charfaction <= 4) then
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 14372, item2 = 14371, item3 = 16507})) then
 		e.self:Say("I did it! The pieces you got me were perfect! It took a huge effort on my part to craft this, of course. It saddens me that I'm unable to keep one of my best works, but a deal is a deal. There is honor among shadowknights.");
 		e.other:Faction(342, 70);
 		e.other:QuestReward(e.self,0,0,0,0,14366);
