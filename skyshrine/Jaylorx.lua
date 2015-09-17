78285837d9464bb850f74fb7e6e4eda6c7aaa1e9
Aaron Alcoser <aalcoser85@gmail.com>
2015-09-16 21:43:20 -0500

    Added Jaylorx LUA

---
+++ skyshrine/Jaylorx.lua
@@ -0,0 +1,10 @@
+
+function event_say(e)
+	if (e.message:findi("hail")) then
+		e.self:Say("Watch out in the water. Yelinak keeps a few small fish there.");
+end
+
+function event_trade(e)
+	local item_lib = require('items');
+	item_lib.return_items(event.self, event.other, event.trade);
+end
