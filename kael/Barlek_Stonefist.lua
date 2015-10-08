5b466baa2b5369e37ab9ef4032608bfc9c20a25b
aaron <aalcoser85@gmail.com>
2015-10-08 03:30:49 -0500

    Added Kael Cleric Armor NPC Update to Kael Armor Paladin

--- kael/Barlek_Stonefist.lua
+++ kael/Barlek_Stonefist.lua
@@ -18,23 +18,23 @@ function event_say(e)
   if (e.other:GetFaction(e.self) > 2) then -- Must be Ally
     if (e.message:findi("hail")) then
       e.self:Say("Greetings to you. I seek the service of devout knights who have distinguished themselves. Are you a paladin? If not then be gone.")
-    if (e.message:findi("paladin")) then
+    elseif (e.message:findi("paladin")) then
       e.self:Say("A paladin of your order has not made it this far for quite some time. Well, I wonder what it is you seek here. Do you seek heroic and chivalrous deeds to distinguish yourself from your brethren? Do you need accoutrements to aid you in your battles perhaps? You are in luck, knight for I have both for you. Do you wish to partake of my quests?")
-		if (e.message:findi("partake of your quests")) then
+		elseif (e.message:findi("partake of your quests")) then
 			e.self:Say("Excellent. By serving my cause, you, in turn, will further your own. I require certain components to aid me and if you bring them to me, I shall reward you for your honorable service. You may choose from among these items, a helm, a breastplate, armplates, bracers, gauntlets, greaves, and boots.")
-		if (e.message:findi("helm")) then
+		elseif (e.message:findi("helm")) then
 			e.self:Say("All that I require is an ancient tarnished plate helmet and three pieces of crushed coral. This should be a small task for one such as you. Go now and I shall await your return.")
-		if(e.message:findi("breastplate")) then
+		elseif(e.message:findi("breastplate")) then
 			e.self:Say("As the resolve of your faith shall protect you, so shall this breastplate. All that is required is an ancient tarnished breastplate, and three flawless diamonds. Do this quickly so that you may return to your place in the field of battle.")
-		if(e.message:findi("armplates")) then
+		elseif(e.message:findi("armplates")) then
 			e.self:Say("So, a pair of armplates, is what you require? Well, I require an ancient tarnished plate vambraces as well as three flawed emeralds before you will receive your prize.")
-		if(e.message:findi("bracers")) then
+		elseif(e.message:findi("bracers")) then
 			e.self:Say("Bracers for the mighty? Retrieve these components and I shall forge the item for you. Bring me an ancient tarnished plate bracelet as well as three crushed flame emeralds.")
-		if(e.message:findi("gauntlets")) then
+		elseif(e.message:findi("gauntlets")) then
 			e.self:Say("Mighty gauntlets to smite your foes for the glory of the greater good. It is no easy task but I shall require a pair of an ancient tarnished plate gauntlets and three crushed topaz.")
-		if(e.message:findi("greaves")) then
+		elseif(e.message:findi("greaves")) then
 			e.self:Say("Sturdy are the pillars that support the temple. I shall make you as strong with these leggings. Gather three flawed sapphires of the sea and a set of ancient tarnished plate greaves.")
-		if(e.message:findi("boots")) then
+		elseif(e.message:findi("boots")) then
 			e.self:Say("A pair of boots you shall have once you have gotten a pair of an ancient tarnished plate boots and a set of three crushed pieces of black marble.")
 		end
 	else
---
+++ kael/Vylleam_Vyaeltor.lua
@@ -0,0 +1,48 @@
+--cleric
+
+local quest_helper = require('velious_quest_helper');
+local SKYSHRINE_ARMOR = quest_helper.SKYSHRINE_ARMOR;
+
+local QUEST_ITEMS = {
+	quest_helper:priest_chest(KAEL_ARMOR.Breastplate, 25391), -- Breastplate
+	quest_helper:priest_arms(KAEL_ARMOR.Plate_Vambraces, 25392), -- Arms
+	quest_helper:priest_bracer(KAEL_ARMOR.Plate_Bracer, 25393), --Bracer
+	quest_helper:priest_gloves(KAEL_ARMOR.Plate_Gauntlets, 25394), --gloves
+	quest_helper:priest_legs(KAEL_ARMOR.Plate_Greaves, 25395), --legs
+	quest_helper:priest_boots(KAEL_ARMOR.Plate_Boots, 25396), --boots
+	
+}
+
+function event_say(e)
+	if(e.other:GetFaction(e.self) > 2) then -- Must be Ally
+		if(e.message:findi("hail")) then
+			e.self:Emote(" bows.");
+			e.self:Say("Greetings to you. You are far away from your homelands, stranger. I am looking for someone. Someone that the outlanders call a cleric. Are you a cleric?");
+		elseif(e.message:findi("cleric")) then
+			e.self:Say("Ahhh... most pious one. I sense the aura of your devotion and conviction and it is strong about you. A shame you are so short lived, mortal. But I see potential in you and for that I shall aid you in your calling. Do you wish my aid?");
+		elseif(e.message:findi("i wish to your aid")) then
+			e.self:Say("Then I have a set of goals for you. Once you have achieved them, you shall be rewarded with such as these; a helm, a breastplate, armplates, bracers, gauntlets, greaves and boots. May they protect you from your enemies.");
+		elseif(e.message:findi("helm")) then
+			e.self:Say("I shall craft a helm for you but you must seek these items out for me first. I have need of an ancient tarnished plate helmet and three crushed onyx sapphires.");
+		elseif(e.message:findi("breastplate")) then
+			e.self:Say("Such as your faith protects you so shall this breastplate. I must have an ancient tarnished breastplate and three pieces of black marble.");
+		elseif(e.message:findi("armplates")) then
+			e.self:Say("For the armplates, I shall require a pair of an ancient tarnished plate armplates as well as three jaundice gems. Go forth and seek these out.");
+		elseif(e.message:findi("bracers")) then
+			e.self:Say("For the bracers, faithful one, I shall require the acquisition of an ancient tarnished plate bracer and three crushed opals.");
+		elseif(e.message:findi("gauntlets")) then
+			e.self:Say("I will need a pair of a ancient tarnished gauntlets three crushed lava rubys, so that I may make your reward.");
+		elseif(e.message:findi("greaves")) then
+			e.self:Say("You shall acquire a pair of an ancient tarnished plate greaves and three chipped onyx sapphires for me. I await your return.");
+		elseif(e.message:findi("boots")) then
+			e.self:Say("With your aid I shall make a pair of fine boots for you. All that is needed is a pair of an ancient tarnished plate boots and a trilogy of crushed flame emeralds.");
+		end
+	else
+		e.self:Say("You must prove your dedication to Kael Drakkal and the Kromzek clan before I will speak to you.");
+	end
+end
+
+function event_trade(e)
+	quest_helper:quest_turn_in(e, 1, quest_helper.kael_armor_success) 
+end
+
