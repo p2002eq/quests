31c9565df0ea486fd9b48d65c0946cec56a75090
aaron <aalcoser85@gmail.com>
2015-10-13 04:44:22 -0500

    Fixed some text errors

--- skyshrine/#Supreme_Laochosmith_Psorin.lua
+++ skyshrine/#Supreme_Laochosmith_Psorin.lua
@@ -10,28 +10,29 @@ function event_trade(e)
 	local item_lib = require('items');
 
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 31140})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(31528);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 31119})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(31526);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 31133})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(31527);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 31098})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(31525);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 31182})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(31529);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 26025})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(2612);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 25194})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(2611);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 30507})) then
-		e.self:Say("Thank you, "..e.other:GetName..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
+		e.self:Say("Thank you, "..e.other:GetName()..". Please accept this Helm as a token of our gratitude for your service to the Shrine.");
 		e.other:SummonItem(2610);
+	end
 end
 
