914d0703e8465ee640592b493a58a3b5f10180f9
Tom <tommytryharder@gmail.com>
2015-10-20 14:26:27 -0400

    updated

---
+++ westwastes/A_sentinel-s_voice.lua
@@ -0,0 +1,11 @@
+function event_spawn(event)
+    xloc = e.self:GetX();
+    yloc = e.self:GetY();
+    zloc = e.self:GetZ();
+    eq.enable_proximity_say();
+    eq.set_proximity(xloc - 30, xloc + 30, yloc - 30, yloc + 30);
+end
+
+function event_enter(event)
+    e.self:Emote("invades your thoughts: 'Beware mortal!  This land is marked by Veeshan, and is sacred to her brood.  None but Dragonkin are welcome here.  Your life is forteit if you proceed.  Ignore this message at your peril!'");
+end
