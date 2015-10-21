44dee2b3a1d252df72e0633b97b776b3fda813ec
Tom <tommytryharder@gmail.com>
2015-10-21 09:25:59 -0400

    lua convert

---
+++ frozenshadow/Librarian_assistant.lua
@@ -0,0 +1,12 @@
+function event_death_complete(e)
+    local random_result = math.random(100);
+	if(random_result <= 10) then
+        eq.spawn2(111241,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1); --spawn the enraged relative on death
+    end
+   
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+	item_lib.return_items(e.self, e.other, e.trade)
+end
