72cc21916f02bf94820ea5ff74f12b0858f2776c
Tom <tommytryharder@gmail.com>
2015-10-23 10:13:40 -0400

    lua convert

---
+++ wakening/Disciple_Atharm.lua
@@ -0,0 +1,18 @@
+function event_spawn(e)
+	eq.set_timer("onset3", 90000); -- 
+end
+
+function event_timer(e)
+    if(e.timer == "onset3") then
+        e.self:MoveTo(-4357,-715,-183,-1,1);
+        eq.stop_timer("onset3");
+    end
+end
+
+function event_signal(e)
+    if(e.signal == 1) then --
+        -- quest::start(26);
+        
+    end
+    
+end
