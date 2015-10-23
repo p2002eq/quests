b23189432ab66f08e41f513f8ee1dbf16e1e50d2
Tom <tommytryharder@gmail.com>
2015-10-23 10:12:26 -0400

    lua convert

---
+++ wakening/Berzerker_Voldak.lua
@@ -0,0 +1,21 @@
+function event_spawn(e)
+	eq.set_timer("onset1", 58000); -- 
+end
+
+function event_timer(e)
+    if(e.timer == "onset1") then
+        e.self:MoveTo(-4311,-720,-183,-1,1);
+        eq.stop_timer("onset1");
+    end
+end
+
+function event_signal(e)
+    if(e.signal == 1) then --
+        -- quest::start(24);
+        eq.signal(119168,1,300);
+    end
+    if(e.signal == 2) then --
+        e.self:Say("Woah! we're being ambushed!!");
+        eq.signal(119168,2,300);
+    end
+end
