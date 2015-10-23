4e3dd48afbb05529950d355badb821c307e6e544
Tom <tommytryharder@gmail.com>
2015-10-23 16:09:12 -0400

    un comment

--- poknowledge/player.lua
+++ poknowledge/player.lua
@@ -1,3 +1,3 @@
 function event_zone(e)
-    -- e.self:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
+    e.self:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
 end
