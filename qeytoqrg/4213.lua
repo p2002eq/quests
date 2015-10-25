422ec480a8636b6ecf1bd41e8ea2657674a1f347
Tom <tommytryharder@gmail.com>
2015-10-24 22:36:00 -0400

    tumpy v4 final form

---
+++ qeytoqrg/4213.lua
@@ -0,0 +1,32 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Cheers mate...WAIT WAT DID YOU SAY!");
+	
+	end
+end
+
+function event_trade(e)
+	local drink = 0;
+	
+	local item_lib = require("items");
+	
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 12114,item2 = 12114,item3 = 12114,item4 = 12114})) then -- Green Goblin Skin
+		drink = drink + 4;
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 12114,item2 = 12114,item3 = 12114})) then -- Green Goblin Skin
+		drink = drink + 3;
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 12114,item2 = 12114})) then -- Green Goblin Skin
+		drink = drink + 2;
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 12114})) then -- Green Goblin Skin
+		drink = drink + 1;
+	end
+	if(drink > 100) then
+		e.self:Say("Cheers, keep em coming");
+		
+		e.other:QuestReward(e.self,0,0,0,0,0,17800);
+		drink = 0;
+		eq.depop();
+	end
+	
+
+	item_lib.return_items(e.self, e.other, e.trade)
+end
