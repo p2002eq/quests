40e6dcaa462a2beb1c5ab21511eb982120e3627b
Gary <gforce215@yahoo.com>
2015-11-13 09:16:01 -0500

    mob id update

--- growthplane/Fayl_Everstrong.lua
+++ growthplane/Fayl_Everstrong.lua
@@ -3,7 +3,7 @@ function event_combat(e)
 		-- grab the entity list
 		local entity_list = eq.get_entity_list();
 		-- 
-		local npc_table = {127027,127028,127103,127057,127102,127026};
+		local npc_table = {127027,127028,127103,127026};
 		for k,v in pairs(npc_table) do
 			local npc = entity_list:GetMobByNpcTypeID(v);
 			if (npc.valid) then
