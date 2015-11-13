4f6538ac3c3d568df7fd357e513a78a650dd1d6a
Gary <gforce215@yahoo.com>
2015-11-13 09:13:59 -0500

    npc id update

--- growthplane/Rumbleroot.lua
+++ growthplane/Rumbleroot.lua
@@ -3,7 +3,7 @@ function event_combat(e)
 		-- grab the entity list
 		local entity_list = eq.get_entity_list();
 		
-		local npc_table = {127024,127025,127031};
+		local npc_table = {127108,127025,127031};
 		for k,v in pairs(npc_table) do
 			local npc = entity_list:GetMobByNpcTypeID(v);
 			if (npc.valid) then
