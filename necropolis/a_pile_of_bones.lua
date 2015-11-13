11733aaeaf04ba2c142fc6f7bdf91e2cf0d48a4f
Tom <tommytryharder@gmail.com>
2015-11-13 08:24:40 -0500

    added in pile of bones lua based on the the hate plane imp trap

---
+++ necropolis/a_pile_of_bones.lua
@@ -0,0 +1,11 @@
+function event_spawn(event)
+    local xloc = event.self:GetX();
+    local yloc = event.self:GetY();
+    local zloc = event.self:GetZ();
+    eq.set_proximity(xloc - 45, xloc + 45, yloc - 45, yloc + 45, zloc - 45, zloc + 45);
+end
+
+function event_enter(event)
+    eq.spawn2(123097, 0, 0, event.self:GetX(), event.self:GetY(), event.self:GetZ(), event.self:GetHeading());
+    eq.depop_with_timer();
+end
