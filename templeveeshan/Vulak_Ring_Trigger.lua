ed6eb6c102d9459e78a9dcd4e0fe276c71307240
Tom <tommytryharder@gmail.com>
2015-11-17 15:00:12 -0500

    updated vulak ring

--- templeveeshan/Vulak_Ring_Trigger.lua
+++ templeveeshan/Vulak_Ring_Trigger.lua
@@ -105,5 +105,57 @@ function event_timer(e)
         eq.stop_timer(e.timer)
         eq.set_timer("wave4", 600000); -- starts wave 5
         eq.spawn2("Zruk the Lifestealer",0,0,xlocBoss,ylocBoss,zlocBoss,hlocBoss);
+    elseif(e.timer == "wave4") then
+        wave = wave + 1; -- wave = 5
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave5", 600000); -- starts wave 6
+    elseif(e.timer == "wave5") then
+        wave = wave + 1; -- wave = 6
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave6", 600000); -- starts wave 7
+    elseif(e.timer == "wave6") then
+        wave = wave + 1; -- wave = 7
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave7", 600000); -- starts wave 8
+    elseif(e.timer == "wave7") then
+        wave = wave + 1; -- wave = 8
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave8", 600000); -- starts wave 9
+    elseif(e.timer == "wave8") then
+        wave = wave + 1; -- wave = 9
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave9", 600000); -- starts wave 10
+    elseif(e.timer == "wave9") then
+        wave = wave + 1; -- wave = 10
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave10", 600000); -- starts wave 11 
+    elseif(e.timer == "wave10") then
+        wave = wave + 1; -- wave = 11
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave11", 600000); -- starts wave 12        
+    elseif(e.timer == "wave11") then
+        wave = wave + 1; -- wave = 12
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave12", 600000); -- starts wave 13
+    elseif(e.timer == "wave12") then
+        wave = wave + 1; -- wave = 13
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave13", 600000); -- starts wave 14
+    elseif(e.timer == "wave13") then
+        wave = wave + 1; -- wave = 14
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave14", 600000); -- starts wave 15        
+    elseif(e.timer == "wave14") then
+        wave = wave + 1; -- wave = 15
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave15", 600000); -- starts wave 16
+    elseif(e.timer == "wave15") then
+        wave = wave + 1; -- wave = 16
+        eq.stop_timer(e.timer)
+        eq.set_timer("wave16", 900000); -- starts wave 17  
+    elseif(e.timer == "wave16") then
+        wave = wave + 1; -- wave = 17
+        eq.stop_timer(e.timer)
+        eq.set_timer("depop", 900000); -- starts depop       
     end
 end
