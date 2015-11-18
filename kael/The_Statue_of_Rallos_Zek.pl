eac4fac28de22c06d8277fb9851dcfe9ca4386fe
Eric <erog84@gmail.com>
2015-11-17 22:38:01 -0500

    Idol

--- kael/The_Statue_of_Rallos_Zek.pl
+++ kael/The_Statue_of_Rallos_Zek.pl
@@ -1,9 +1,15 @@
 # On death, spawns the Idol of Rallos Zek
 
-sub EVENT_DEATH_COMPLETE {
-  quest::shout("Protect the Idol of Zek!");
-  quest::spawn2(113341,0,0,1289,1300,-90,254.5);
-}
 
-# Quest by mystic414
+function event_death_complete(e)
+    e.self:Shout("Protect the Idol of Zek!");
+	eq.spawn2(113627,0,0,1292,1267,-35,254.5); -- Idol of Rallos Zek
+end
+
+--sub EVENT_DEATH_COMPLETE {
+--  quest::shout("Protect the Idol of Zek!");
+-- quest::spawn2(113341,0,0,1289,1300,-90,254.5);
+--}
+
+--# Quest by mystic414
 
