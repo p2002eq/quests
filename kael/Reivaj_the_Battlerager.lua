b08fc1dddb588ca079802ee5838ae5469d0c4b69
Aaron <aalcoser85@gmail.com>
2015-11-28 20:08:11 -0600

    Added Valorant, Noble Helssen, Reivaj, Slaggak the trainer luas and moved perls into perl folder.

---
+++ kael/Reivaj_the_Battlerager.lua
@@ -0,0 +1,33 @@
+function event_say(e)
+
+	if (e.other:GetFaction(e.self) == 1) then --must be ally
+		if (e.message:findi("hail")) then	
+			e.self:Say("Greetings, $name. You tread in the halls of Iceshard manor. Are you here with a purpose or are you lost?");
+		elseif (e.message:findi("purpose")) then	
+			e.self:Say("I do not know what purpose you would serve to the Kromrif. If you seek to amass a fortune you have come to the wrong man. I am but a simple battlerager, wielding my blade for the Iceshard brothers when needed.");
+		elseif (e.message:findi("battlerager")) then	
+			e.self:Say("Yes, I am a warrior who thrives on battle. I have great faith in Rallos Zek and I call upon his power to make me more efficient in battle. I am not like the foolish berserkers. They howl like animals and create strange totems. They simply unleash their anger upon the world whereas I focus and destroy my foes with the aid of Rallos Zek.");
+		elseif (e.message:findi("rallos zek")) then	
+			e.self:Say("The great war god guides my blade and has helped shape my day to day life... I feel as if there is something greater mapped out for my life than what I have here.");
+		elseif (e.message:findi("plan")) then	
+			e.self:Say("I have been planning to travel forth from Kael Drakkel and destroy any Coldain who have spread from their safe home in Thurgadin. It is always a nice trip, I will take several of the elder Kromrif with me as well. Perhaps we will track down the elusive wurm, Blizzent.");
+		elseif (e.message:findi("blizzent")) then	
+			e.self:Say("Blizzent is an ancient ice wurm Vorken speaks of occasionally. If I remember the story correctly, Blizzent once bit Vorken and Klaggen and has had a desire to have another taste. I have never seen the wurm myself. He may not even be alive still. If I were able to slay Blizzent and retrieve proof of it, I could probably get in good with master Vorken.");
+		end
+	end
+
+end
+
+function event_trade(e)
+
+	local item_lib = require('items');
+	
+	if (e.other:GetFaction(e.self) == 1) then
+		if (item_lib.check_turn_in(e.self, e.trade, {item1 = 25117})) then
+			e.self:Say("Excellent, this will do nicely.  Take this sword for your efforts.");
+			e.other:SummonItem(25083);
+			e.other:AddEXP(1000);
+		end
+	end
+
+end
---
+++ kael/Slaggak_the_Trainer.lua
@@ -0,0 +1,44 @@
+function event_say(e)
+
+	if (e.other:GetFaction(e.self) <= 5) then --Indifferent or lower
+		if (e.message:findi("hail")) then
+			e.self:Say("Hello there, $race. I am Slaggak, the trainer. The Kromrif here will soon be ready to serve the great and honorable King Tormax.");
+		elseif (e.message:findi("serve")) then	
+			e.self:Say("So you say, little one. Why are you here, then? You should be out slaying those pestering little Coldain or maybe those damnable shard wurms and wyverns.");
+		elseif (e.message:findi("coldain")) then	
+			e.self:Say("The Coldain are the ice dwarves. They have defiled our lands for long enough. Thinking of them makes my blood run hot. With my new position as trainer, I am not allowed to roam the lands and slaughter their kind as I once did.' Slaggak sighs. 'I long for the days when I could fashion necklaces from their skulls.");
+		elseif (e.message:findi("skulls")) then
+			e.self:Say("The Coldain are the ice dwarves. They have defiled our lands for long enough. Thinking of them makes my blood run hot. With my new position as trainer, I am not allowed to roam the lands and slaughter their kind as I once did.' Slaggak sighs. 'I long for the days when I could fashion necklaces from their skulls.");
+		elseif (e.message:findi("shard wurm")) then	
+			e.self:Say("You must have seen the shard wurms by now! The lizards that roam the lands eating whatever they can find? They can grow to be quite ferocious! Not nearly as ferocious as their ancient cousins, the dragons, but a nuisance nonetheless. We have a bounty on those beasts here in Kael Drakkel.");
+		elseif (e.message:findi("bounty")) then	
+			e.self:Say("If you think you have what it takes to slay the shard wurms or their relatives, the wyverns, bring back proof of your exploits. For every four shard wurm fang you bring me, I am authorized to reward you with a pair of cerulean greaves. Every fourth ice wyvern stinger will grant you a pair of vambraces.");
+		end
+	else
+		e.self:Say("Your loyalty to our ideals has yet to be proven.");
+	end
+end
+
+function event_trade(e)
+function event_trade(e)
+
+	local item_lib = require('items');
+	if (e.other:GetFaction(e.self) <= 5) then --indifferent or lower
+		if (item_lib.check_turn_in(e.self, e.trade, {item1 = })) then
+			local randomScaleItem = eq.ChooseRandom(25011, 25016, 25013, 25010, 25017, 25014, 25007, 25005, 25015, 25009, 25006, 25012, 25018); --random Giant Scalemail piece
+			e.other:SummonItem(randomScaleItem);
+		elseif (item_lib.check_turn_in(e.self, e.trade, {item1 =  25100, item2 = 25100, item3 = 25100, item4 = 25100})) then	
+			e.other:SummonItem(25071);
+		elseif (item_lib.check_turn_in(e.self, e.trade, {item1 =  25101, item2 = 25101, item3 = 25101, item4 = 25101})) then	
+			e.other:SummonItem(25070);
+		end
+		e.other:Faction(188, 20); --kromrif
+		e.other:Faction(189, 20); --kromzek
+		e.other:Faction(49, -60); --coldain
+		e.other:Faction(42, -60); --CoV
+		e.other:AddEXP(100);
+	else
+		e.self:Say("I will do nothing to aid the likes of you.");
+	end
+
+end
