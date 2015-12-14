01efb645869422ad9fd212075111922558329cfa
Eric <erog84@gmail.com>
2015-12-13 23:19:53 -0500

    Fix Quest

--- akanon/Silox_Azrix.lua
+++ akanon/Silox_Azrix.lua
@@ -2,14 +2,14 @@
 
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:emote("gives you a crooked smile. 'Hello there, " .. e.other:GetName() .. ". I'm doin some important 'meditations' to my god here. Maybe you can go somewhere else?'");
+		e.self:Emote("gives you a crooked smile. 'Hello there, " .. e.other:GetName() .. ". I'm doin some important 'meditations' to my god here. Maybe you can go somewhere else?'");
 	end
 end
 
 function event_trade(e)
     local item_lib = require("items");
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 20694})) then
-		e.self:emote("looks up at you with a twisted gleam in his eyes. 'Something for me then, eh?' As he looks closer his skin begins to peel and stretch, a bright hot light emanating from his hands gripped around the unicorn horn. 'NOOOO!' The pure cleansing light burns away the gnome without a trace of his existence except for a tiny ball of light which you pick up.");
+		e.self:Emote("looks up at you with a twisted gleam in his eyes. 'Something for me then, eh?' As he looks closer his skin begins to peel and stretch, a bright hot light emanating from his hands gripped around the unicorn horn. 'NOOOO!' The pure cleansing light burns away the gnome without a trace of his existence except for a tiny ball of light which you pick up.");
 		e.other:QuestReward(e.self,0,0,0,0,20697,100000); 
 		e.other:Ding();
 		eq.depop_with_timer();
