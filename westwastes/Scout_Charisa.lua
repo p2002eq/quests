b3ea61af0bb993046dc16fae2bd7b8d51a7520a6
Aaron <aalcoser85@gmail.com>
2015-12-15 03:23:03 -0600

    Fixed the spawn on scout charisa and set a timer for her to despawn after 20 minutes of turn in

--- westwastes/Scout_Charisa.lua
+++ westwastes/Scout_Charisa.lua
@@ -1,22 +1,19 @@
 function event_say(e)
-	if(e.message:findi("hail")) then
-		e.self:Say("I... am... the... Master... of... Elements.. Elements... Forgive... me. I am... unaccustomed.. to.. speaking.. this.. mortal... tongue. Might.. you... be... the... One?");
-	elseif(e.message:findi("i am the one")) then
-		e.self:Say("Trilith... Magi'kot.. knelt.. before.. the.. orb.. in.. his.. his.. last.. few.. moments... of.. mortal.. life. He... too... believed.. that.. he.. had.. the.. power.. and.. balance.. needed.. to.. grasp... control.. of.. the.. elements. He.. was.... flawed. Coming.. to... me... is.. a.. matter.. of.. no.. small.. import. Only.. only... one.. who.. is.. of.. balance.. and.. power... power.. may.. control.. the.. Orb.. of... Mastery. Give.... give.. me.. the.. Elements.. you.. have... gathered.. and.. I.. shall.. see.. if.. you.. are.. ready.. ready.. to control.. the... Elements.. of... life.. life.. life.");
-	end
 end
 
 function event_trade(e)
 	local item_lib = require("items");
+
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 29683})) then -- Scout Tools
-		eq.spawn2(120115,0,0,-3501,-4868,-108); -- a Kromzek Captain
-		eq.spawn2(120121,0,0,-3507,-4897,-106); -- Kromzek Warrior
-		eq.spawn2(120121,0,0,-3496,-4874,-109); -- Kromzek Warrior
-        eq.spawn2(120121,0,0,-3474,-4863,-114); -- Kromzek Warrior
-        eq.spawn2(120121,0,0,-3463,-4852,-117); -- Kromzek Warrior
-        eq.spawn2(120121,0,0,-3452,-4841,-119); -- Kromzek Warrior
-        eq.spawn2(120121,0,0,-3441,-4830,-122); -- Kromzek Warrior
-        eq.spawn2(120121,0,0,-3430,-4819,-124); -- Kromzek Warrior
+		eq.set_timer("Scout", 1200000); -- 20 minutes
+		eq.spawn2(120115,0,0,-3501,-4868,-108, 0); -- a Kromzek Captain
+		eq.spawn2(120121,0,0,-3507,-4897,-106, 0); -- Kromzek Warrior
+		eq.spawn2(120121,0,0,-3496,-4874,-109, 0); -- Kromzek Warrior
+        eq.spawn2(120121,0,0,-3474,-4863,-114, 0); -- Kromzek Warrior
+        eq.spawn2(120121,0,0,-3463,-4852,-117, 0); -- Kromzek Warrior
+        eq.spawn2(120121,0,0,-3452,-4841,-119, 0); -- Kromzek Warrior
+        eq.spawn2(120121,0,0,-3441,-4830,-122, 0); -- Kromzek Warrior
+        eq.spawn2(120121,0,0,-3430,-4819,-124, 0); -- Kromzek Warrior
         e.other:Faction(42,30);   --Claws of Veeshan
 		e.other:Faction(362,30);  --Yelinak
 		e.other:Faction(189,-20); --Kromzek
@@ -43,8 +40,12 @@ function event_trade(e)
 end
 
 function event_timer(e)
+	if (e.timer == "Scout") then
+	eq.stop_timer("Scout");
+	eq.get_entity_list():GetNPCTypeID(120121):Depop();
+	eq.get_entity_list():GetNPCTypeID(120115):Depop();
 	eq.depop();
-	eq.stop_timer("depop");
+	end
 end
 
 
