5fa0eefe8235350f65b3dcd1e04d9dcb744acc53
Aaron <aalcoser85@gmail.com>
2015-12-15 14:50:12 -0600

    Made an edit to test in the tofs.

--- frozenshadow/player.lua
+++ frozenshadow/player.lua
@@ -1,3 +1,5 @@
+-- declare globals
+
 local player_list = nil;
 local player_list_count = nil;
 local client_e = nil;
@@ -26,27 +28,27 @@ function event_click_door(e)
 	if (door_id == 2) or (door_id == 166) then --First floor Door
 		if (e.self:HasItem(20033) == 1) then
 --			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 50, 660, 100, 40, 0); --Editing out to see if lua is working
-			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 50, 670, 750, 75, 0);
+			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 75, 670, 750, 75, 0);
 		end
 	elseif (door_id == 4) or (door_id == 167) then --Second Floor Door
 		if (e.self:HasItem(20034) == 1) then
-			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 50, 670, 750, 75, 0);
+			PortCharacters(cur_zone, e.self:GetX(), e.self:GetY(), e.self:GetZ(), 75, 670, 750, 75, 0);
 		end
 	elseif (door_id == 16) or (door_id == 165) then --Third Floor Door
 		if (e.self:HasItem(20035) == 1) then
-			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 50, 170, 775, 175, 0);		
+			PortCharacters(cur_zone, e.self:GetX(), e.self:GetY(), e.self:GetZ(), 75, 170, 775, 175, 0);		
 		end
 	elseif (door_id == 27) or (door_id == 169) then --Fourth Floor Door
 		if (e.self:HasItem(20036) == 1) then
-			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 50, -150, 160, 217, 0);
+			PortCharacters(cur_zone, e.self:GetX(), e.self:GetY(), e.self:GetZ(), 75, -150, 160, 217, 0);
 		end
 	elseif (door_id == 34) or (door_id == 168) then --Fifth Floor Door
 		if (e.self:HasItem(20037) == 1) then
-			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 50, -320, 725, 12, 0);
+			PortCharacters(cur_zone, e.self:GetX(), e.self:GetY(), e.self:GetZ(), 75, -320, 725, 12, 0);
 		end
 	elseif (door_id == 1) then --Sixth Floor Door	
 		if (e.self:HasItem(20039) == 1) then
-			PortCharacters(e.self:GetX(), e.self:GetY(), e.self:GetZ(), 50, 20, 250, 255, 0);
+			PortCharacters(cur_zone, e.self:GetX(), e.self:GetY(), e.self:GetZ(), 75, 20, 250, 255, 0);
 		end
 	end
 end
@@ -57,7 +59,7 @@ function PortCharacters(cur_zone, cur_x, cur_y, cur_z, distance, dest_x, dest_y,
 		for i = 0, player_list_count - 1, 1 do
 			local client_v = player_list:GetMember(i):CastToClient();
 			--validate client
-			if (client_v.valid) then
+			if (client_v.valid == true) then
 				--check distance
 				if (client_v:CalculateDistance(cur_x, cur_y, cur_z) <= distance) then
 					--port up
