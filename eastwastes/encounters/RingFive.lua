c01f202aabf310064b7fca1c6867b3bbf67a5df5
Aaron <aalcoser85@gmail.com>
2015-12-16 05:17:49 -0600

    Forgot to add a the correct function to set timers

--- eastwastes/encounters/RingFive.lua
+++ eastwastes/encounters/RingFive.lua
@@ -4,7 +4,7 @@ local spawnOracles = math.random(1, 3);
 local spawnInvaders = math.random(1, 3);
 
 function event_encounter_load(e)
-	eq.start_timer("depop", 120000);
+	eq.set_timer("depop", 120000);
 	--locs made up
 	eq.spawn2(116600, 0, 0, -650, -2744, 180, 45); --Scarbrow
 
