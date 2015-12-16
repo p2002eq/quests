aaf52c36bcd031f827c9d2819229e355d0001e6f
Aaron <aalcoser85@gmail.com>
2015-12-16 03:30:04 -0600

    Added a debug function for testing

--- eastwastes/a_coldain_lookout.lua
+++ eastwastes/a_coldain_lookout.lua
@@ -7,6 +7,7 @@ function event_trade(e)
 	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 30136})) then
 
 		e.self:Shout("Aahhh.. You're too late! They're upon us! To arms, men! Death to Scarbrow!");
+		e.self:Say("Used to debug");
 		eq.spawn2(116570, 0, 0, -642, -2591, 190, 0);
 --		eq.get_entity_list():GetMobByNpcTypeID(116570):CastToNPC():SignalNPC(1);	
 		eq.load_encounter("RingFive");
