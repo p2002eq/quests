b5c5723ea2865ea876e6ff1737605662fb222f01
Aaron <aalcoser85@gmail.com>
2015-12-18 09:10:09 -0600

    Added Spawned and Non spawned version of Peffin Ambersnow luas and moved old perls into perl folder

---
+++ eastwastes/#Peffin_Ambersnow.lua
@@ -0,0 +1,19 @@
+function event_spawn(e)
+	eq.spawn2(116182,0,0,3406.9,-1722.2,142.9,191.5);
+	eq.spawn2(116182,0,0,3408.7,-1693.2,142.5,191.5);
+	eq.spawn2(116182,0,0,3390.1,-1744.3,146.4,191.5);
+	eq.spawn2(116182,0,0,3393.8,-1777.2,149,191.5);
+	eq.spawn2(116182,0,0,3395.6,-1794.3,151.6,191.5);
+	eq.set_timer("depop", 600000); -- 10 minutes
+
+end
+
+function event_timer(e)
+	if (e.timer == "depop") then
+		eq.stop_timer("depop")
+		e.self:Say("The cowards seem to have fled friends, thank you for your protection.");
+		eq.depop_all(116182);
+		eq.depop();
+	end
+
+end
--- eastwastes/Peffin_Ambersnow.lua
+++ eastwastes/Peffin_Ambersnow.lua
@@ -1,15 +1,30 @@
-function event_spawn(e)
-	eq.spawn2(116182,0,0,3406.9,-1722.2,142.9,191.5);
-	eq.spawn2(116182,0,0,3406.9,-1722.2,142.9,191.5);
-	eq.spawn2(116182,0,0,3390.1,-1744.3,146.4,191.5);
-	eq.spawn2(116182,0,0,3393.8,-1777.2,149,191.5);
-	eq.spawn2(116182,0,0,3395.6,-1794.3,151.6,191.5);
-	eq.set_timer("depop", 600000);
+function event_say(e)
+	if (e.message:findi("hail")) then
+		e.self:Say("Oh, I uh... err, hello there. Umm, thank Brelll you're here, those bloody Kromrif were going to torture me! Yes, yes, and you've saved me! How can I ever repay you?");
+	elseif (e.message:findi("murdrick")) then	
+		e.self:Emote("looks away and stammers, Ehh, ahh, Mur who?");
+	elseif (e.message:findi("berradin")) then	
+		e.self:Say("goes even paler than usual, Hmm, Berradin you say? Sorry, but I've never even heard the name.");
+	end
+
+end
+
+function event_trade(e)
+
+	local item_lib = require('items');
+
+	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 18171})) then -- letter to peffin
+		eq.spawn2(116182,0,0,3364,-1579,163.8,35.8);
+		eq.spawn2(116182,0,0,3314,-1557,163.8,42.4);
+		eq.spawn2(116182,0,0,3355,-1625,163.8,36.9);
+		eq.set_timer("depop", 10000); -- 10 seconds
+		eq.move_to(3303,-1623,163.3);
+	end
+
 end
 
 function event_timer(e)
-    if(e.timer == "depop") then
-        eq.stop_timer(e.timer)
-        eq.depopall(116182);
-    end
+	eq.stop_timer("depop");
+	eq.spawn2(116574,0,0,3419.4,-1739.7,143.9,191.5);
+	eq.depop_with_timer();
 end
