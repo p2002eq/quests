76988e3ae41e7ddb2d0a5cb51f691cfa2198ab7e
Aaron <aalcoser85@gmail.com>
2015-12-18 09:37:00 -0600

    Added Tanik Greskil event

---
+++ eastwastes/#You_stumble.lua
@@ -0,0 +1,30 @@
+--Proximity trigger to spawn Tanik Greskill for 4th shawl
+
+--globals
+
+myX = nil;
+myY = nil;
+myZ = nil;
+exists = nil;
+
+function event_spawn(e)
+	myX = e.self:GetX();
+	myY = e.self:GetY();
+	eq.set_proximity(myX - 50, myX + 50, myY -50, myY + 50);
+end
+
+function event_enter(e)
+	myX = e.self:GetX();
+	myY = e.self:GetY();
+	myZ = e.self:GetZ();
+	exists = eq.get_entity_list():GetMobByNpcTypeID(116601);
+
+	if (exists) then
+		e.self:Emote("in the snow.");
+	else 
+		e.self:Emote("and uncover the body of an injured coldain.");
+		eq.spawn2(116601, 0, 0, myX, myY, myZ, 0);	
+		eq.depop_with_timer();
+	end
+
+end
