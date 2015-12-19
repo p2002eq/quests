9ee6415faa4b8c97bd975a83fe0610efd836651a
Scott Davis <me@sdavis.info>
2015-12-19 15:08:31 -0600

    move dwarf and delay proximity hook

--- kael/Plate_Cycle_Trigger.lua
+++ kael/Plate_Cycle_Trigger.lua
@@ -1,14 +1,20 @@
 function event_spawn(e)
-    xloc = e.self:GetX();
-    yloc = e.self:GetY();
-    zloc = e.self:GetZ();
-    eq.enable_proximity_say();
-    eq.set_proximity(xloc - 30, xloc + 30, yloc - 30, yloc + 30);
+    eq.set_timer("spawn", 5)
+end
+
+function event_timer(e)
+    if (e.timer == 'spawn') then
+        xloc = e.self:GetX();
+        yloc = e.self:GetY();
+        zloc = e.self:GetZ();
+        eq.enable_proximity_say();
+        eq.set_proximity(xloc - 30, xloc + 30, yloc - 30, yloc + 30);
+    end
 end
 
 function event_proximity_say(e)
     if(e.message:findi('The dain shall be slain for the peace we must obtain')) then
-        eq.spawn2(113636, 0, 0, 1393, -840.0, -116.8, 195.0);
+        eq.spawn2(113636, 0, 0, 1125, -839.0, -120.80, 70);
         eq.depop_with_timer();
     end
 end
