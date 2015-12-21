9c568d053477985b55254952961c84bb7b507968
Scott Davis <me@sdavis.info>
2015-12-21 12:12:47 -0500

    syntax

--- lua_modules/translocators.lua
+++ lua_modules/translocators.lua
@@ -2,8 +2,8 @@ local helper = {}
 
 function hail_text(e, zone)
     if (e.message:findi('hail')) then
-        e.self:Say("Greetings ".. e.other:GetCleanName()) .. ", I am " .. e.self:GetCleanName() .. "and i have been sent here by the Gnomish Academy of science
-        to assit you in traveling the oceans of norrath. If you would like to [Travel to " .. zone .."] please let me knok.";
+        e.self:Say("Greetings ".. e.other:GetCleanName() ..", I am ".. e.self:GetCleanName() .. "and i have been sent here by the Gnomish Academy of science
+        to assit you in traveling the oceans of norrath. If you would like to [Travel to ".. zone .."] please let me knok.");
     end
 end
 
