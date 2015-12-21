d7a7b2e4e72aba2bce0dc4b7278bd83386ebd1bb
Scott Davis <me@sdavis.info>
2015-12-21 12:14:21 -0500

    namespace

--- lua_modules/translocators.lua
+++ lua_modules/translocators.lua
@@ -1,6 +1,6 @@
 local helper = {}
 
-function hail_text(e, zone)
+function helper.hail_text(e, zone)
     if (e.message:findi('hail')) then
         e.self:Say("Greetings ".. e.other:GetCleanName() ..", I am ".. e.self:GetCleanName() .."and i have been sent here by the Gnomish Academy of Science to assit you in traveling the oceans of norrath. If you would like to [Travel to ".. zone .."] please let me know.");
     end
