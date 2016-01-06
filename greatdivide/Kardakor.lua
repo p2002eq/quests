986a13064e5eb5f994b76ff99c32a07e82748acd
Aaron <aalcoser85@gmail.com>
2016-01-05 23:22:12 -0600

    Fixing globals for Falinkan

--- greatdivide/Kardakor.lua
+++ greatdivide/Kardakor.lua
@@ -8,6 +8,9 @@ function event_say(e)
     		e.self:Say("This, I do not know. There are a few Giant forces that I have heard word of moving into the area. It is tough to say where they could have gone and if something happened to them or not who would be to blame. I highly doubt the Coldain would dare attack one of our units. The last I heard of them was when they left for the patrol of the Divide; this could have been anywhere. I remember that Glanitar carries a talisman from his mother. If you are to find any sign of Glanitar or any of the others please come to me first along with something that would symbolize who you received or took it from.");
         elseif(e.message:findi("rolandal")) then
 		    e.self:Say("Rolandal is an old and wise Wurm that is very well traveled and skilled in the magics of Identification. He will be able to tell you who this talisman came from. You can find him in his dwelling not too far from my home. Seek him out and inform him that i have sent you. If Rolandal can sense that your word is true proceed to tell him that you possess a talisman that you need Identified and he shall help you further. I thank you for your efforts and wish you luck, " ..e.other:GetName().. ".");
+		elseif(e.message:findi("delete global")) then
+			eq.delete_global("CircletFalinkan");
+			e.self:Say("Global deleted!");
         end
 	end
 end
@@ -21,7 +24,7 @@ function event_trade(e)
     		e.other:Faction(42,20); --# Claws of Veeshan
     		e.other:Faction(189,-60); --# Kromzek
     		e.other:QuestReward(e.self,0,0,0,0,1861,50000);
-			eq.set_global("CircletFalinkan", "1", 7, "Y1");
+			eq.target_global("CircletFalinkan", "1", "F", 119104, e.other:CharacterID(), 119); 
 	    end    
     end
 	item_lib.return_items(e.self, e.other, e.trade)
