3e316a42a6a0543460f2827078af0b86cb58a6ab
Aaron <aalcoser85@gmail.com>
2016-01-06 11:07:14 -0600

    Made a change to Rolandal so he can see the clients qglobals

--- wakening/Rolandal.lua
+++ wakening/Rolandal.lua
@@ -1,19 +1,19 @@
 function event_say(e)
-	local qglobals = eq.get_qglobals();
+	local qglobals = eq.get_qglobals(e.self, e.other);
 
 	if(e.message:findi("hail")) then
 		e.self:Say("Greetings, Traveler. I do not receive many visitors to my quarters here, besides the occasional unfortunate treasure seeker that often will make for a good snack.");
-    elseif(qglobals["CircletFalinkan"] == "1" and e.message:findi("kardakor sent me")) then
+    elseif((qglobals["CircletFalinkan"] == "1") and e.message:findi("kardakor sent me")) then
 		e.self:Say("You are sent from Kardakor are you? Well surely he must have sent you to stand before me for a reason.");
-    elseif(qglobals["CircletFalinkan"] == "1" and e.message:findi("talisman")) then
+    elseif((qglobals["CircletFalinkan"] == "1") and e.message:findi("talisman")) then
 		e.self:Say("Ah of course, It has been quite a long time sense my powers of identification have been requested. If you are sent from Kardakor then your intent must be well directed. I do have a favor to ask of you before I identify your talisman. I seek some rare treasures to further some studies that I have been working on for a long time.");
-    elseif(qglobals["CircletFalinkan"] == "1" and e.message:findi("treasures")) then
+    elseif((qglobals["CircletFalinkan"] == "1") and e.message:findi("treasures")) then
 		e.self:Say("There are 3 items that I seek to continue my studies of some various and musterious magics. Bring to me the teachings of a high ranking Kromzek that I hear goes by the name of Gkrean, a chipped fang from a beast deep within the Cursed Necropolis. Also for good measure, I require the Head of a Kromzek Staff Sergeant to prove to your dedication to our cause. Present these 3 items to me along with your Talisman that you wish to learn more about and I shall do my best to identify its origin for you.");
     end
 end
 
 function event_trade(e)
-	local qglobals = eq.get_qglobals();
+	local qglobals = eq.get_qglobals(e.self, e.other);
 	local item_lib = require("items");
 
 	if(e.other:GetFaction(e.self) == 1) and (qglobals["CircletFalinkan"] == "1") then -- Must be ally
