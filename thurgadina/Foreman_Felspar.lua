b0f67b04078caa0c43bf9449d9485c2a0ae9b9ef
Scott Davis <me@sdavis.info>
2016-01-07 09:27:41 -0500

    fixed text

--- thurgadina/Foreman_Felspar.lua
+++ thurgadina/Foreman_Felspar.lua
@@ -4,8 +4,8 @@ local quest_helper = require('velious_quest_helper');
 local THURG_ARMOR = quest_helper.THURG_ARMOR;
 local QUEST_TEXT = {
 
-    hail="Welcome to the Thurgadin Minin' Company, (-name). As foreman it's my job to see that me lads are well trained and well cared fer. Unfortunately, we're not in too good shape right now. Ye see, it seems a new vein was struck recently and a bunch of unwelcome critters have come crawlin outta the works attackin' me lads. In order ta help combat this menace I've engineered a new set of armor for the miners, but need some raw materials to get them started.",
-	armor="Excellent, (-name)! I knew ye were the right (-race) for the job. The pieces I can make are a coif, a tunic, sleeves, bracers, gauntlets, leggings, and boots. This stuff might not be fitting ya if ye're not stealthy like us miners though."
+    hail="Welcome to the Thurgadin Minin' Company, -name. As foreman it's my job to see that me lads are well trained and well cared fer. Unfortunately, we're not in too good shape right now. Ye see, it seems a new vein was struck recently and a bunch of unwelcome critters have come crawlin outta the works attackin' me lads. In order ta help combat this menace I've engineered a new set of armor for the miners, but need some raw materials to get them started.",
+	armor="Excellent, (-name)! I knew ye were the right -race for the job. The pieces I can make are a coif, a tunic, sleeves, bracers, gauntlets, leggings, and boots. This stuff might not be fitting ya if ye're not stealthy like us miners though."
 }
 
 QUEST_TEXT = quest_helper.merge_tables(QUEST_TEXT, quest_helper.THURG_CHAIN_TEXT);
