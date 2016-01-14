9654150bfb0f6cf8aabb81f98dbaad730cab4254
Eric <erog84@gmail.com>
2016-01-14 00:10:06 -0500

    Fix

--- eastwastes/Captain_Berradin.lua
+++ eastwastes/Captain_Berradin.lua
@@ -25,9 +25,9 @@ function event_timer(e)
 		e.self:DoAnim(21);
 		eq.set_timer("4", 5000);
 	elseif (e.timer == "4") then
+	    eq.depop_with_timer();
 		--eq.spawn2(116605, 0, 0, e.self:GetX(), e.self:GetY(), e.sef:GetZ(), e.self:GetHeading());
-		eq.spawn2(116605, 0, 0, 2350.00, -65.00, 150.00, 110.0);
-		eq.depop_with_timer();
+		--eq.spawn2(116605, 0, 0, 2350.00, -65.00, 150.00, 110.0);   -- wouldn't spawn with the GetX/etc line.
 	end
 
 end
