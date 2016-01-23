5e7b0740d5e3bec26053d6fc792b31526a39b2d2
Eric <erog84@gmail.com>
2016-01-23 17:57:19 -0500

    Fix upper case

--- wakening/human_mercenary.lua
+++ wakening/human_mercenary.lua
@@ -1,6 +1,6 @@
 function event_trade(e)
     if(item_lib.check_turn_in(e.self, e.trade, {item1 = 1724})) then
-            e.self:say("Ahh, thank you. I've been waiting for these supplies for ages."); --generic reward text
+            e.self:Say("Ahh, thank you. I've been waiting for these supplies for ages."); --generic reward text
             e.other:Faction(42,-30); --#CoV
             e.other:Faction(188,10); --#k-rif
             e.other:Faction(189,10); --#k-zek
@@ -11,4 +11,3 @@ function event_trade(e)
 	item_lib.return_items(e.self, e.other, e.trade)
 end
 
---fiddle
