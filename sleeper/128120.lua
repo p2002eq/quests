c39dcf1b74ab08d145989206530a5125c906f23e
Eric <erog84@gmail.com>
2016-02-15 16:43:24 -0500

    tst

--- sleeper/128120.lua
+++ sleeper/128120.lua
@@ -3,31 +3,31 @@ local spawnedMobs = 0;
 
 function event_combat(e)
     e.self:Say("testing this cycle");
-    if(combatStarted) then 
+    if(combatStarted == true) then 
         e.self:Say("combat ended");
         combatStarted = false;
         --despawn_cycle();
-    else
+    elseif(combatStarted == false)
         e.self:Say("combat started");
         combatStarted = true;
-        --start_cycle(e);
+        start_cycle(e);
     end
 end
 
 function start_cycle(e)
-e.self:Say("start cycle");
+    e.self:Say("start cycle");
     eq.set_timer("spawn",120000);
 	--eq.depop_with_timer();
 end
 
 function event_timer(e)
 	if (e.timer == "spawn") then
-	e.self:Say("spawning mobs");
-	    eq.spawn2(128110,28,0,e.self:GetX()+10,e.self:GetY()+10,e.self:GetZ(),126.6);
-	    eq.spawn2(128110,29,0,e.self:GetX()-10,e.self:GetY()-10,e.self:GetZ(),126.6);
-	    spawnedMobs = spawnedMobs + 2;
-	    if (spawnedMobs < 8)
-	        eq.set_timer("spawn",120000);
-	    end
+	    e.self:Say("spawning mobs");
+	   -- eq.spawn2(128110,28,0,e.self:GetX()+10,e.self:GetY()+10,e.self:GetZ(),126.6);
+	   -- eq.spawn2(128110,29,0,e.self:GetX()-10,e.self:GetY()-10,e.self:GetZ(),126.6);
+	    --spawnedMobs = spawnedMobs + 2;
+	    --if (spawnedMobs < 8)
+	     --   eq.set_timer("spawn",120000);
+	    --end
 	end
 end
