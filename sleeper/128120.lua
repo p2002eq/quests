2776c3d534a3e1a4a0a6b6bded85a0ce7678a0ed
Eric <erog84@gmail.com>
2016-02-15 15:24:34 -0500

    Test

--- sleeper/128120.lua
+++ sleeper/128120.lua
@@ -13,3 +13,21 @@ function event_combat(e)
         --start_cycle(e);
     end
 end
+
+function start_cycle(e)
+e.self:Say("start cycle");
+    eq.set_timer("spawn",120000);
+	--eq.depop_with_timer();
+end
+
+function event_timer(e)
+	if (e.timer == "spawn") then
+	e.self:Say("spawning mobs");
+	    eq.spawn2(128110,28,0,e.self:GetX()+10,e.self:GetY()+10,e.self:GetZ(),126.6);
+	    eq.spawn2(128110,29,0,e.self:GetX()-10,e.self:GetY()-10,e.self:GetZ(),126.6);
+	    spawnedMobs = spawnedMobs + 2;
+	    if (spawnedMobs < 8)
+	        eq.set_timer("spawn",120000);
+	    end
+	end
+end
