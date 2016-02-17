423cffe6caa213f27d280fb888d9c0740dc64efe
Eric <erog84@gmail.com>
2016-02-17 17:05:21 -0500

    Master of Guard Fix

--- sleeper/128120.lua
+++ sleeper/128120.lua
@@ -1,33 +1,26 @@
-local combatStarted = false;
 local spawnedMobs = 0;
 
 function event_combat(e)
-    e.self:Say("testing this cycle");
-    if(combatStarted == true) then 
-        e.self:Say("combat ended");
-        combatStarted = false;
-        --despawn_cycle();
-    elseif(combatStarted == false)
-        e.self:Say("combat started");
-        combatStarted = true;
- --       start_cycle(e);
+    if (e.joined)
+        eq.set_timer("spawn",120000);
+    else
+        eq.stop_timer("spawn");
+        eq.depop_all(128110);
     end
 end
 
---function start_cycle(e)
---    e.self:Say("start cycle");
-  --  eq.set_timer("spawn",120000);
-	--eq.depop_with_timer();
---end
+function event_timer(e)
+	if (e.timer == "spawn") then
+	    eq.spawn2(128110,28,0,e.self:GetX()+10,e.self:GetY()+10,e.self:GetZ(),126.6);
+	    eq.spawn2(128110,29,0,e.self:GetX()-10,e.self:GetY()-10,e.self:GetZ(),126.6);
+	    spawnedMobs = spawnedMobs + 2;
+	    if (spawnedMobs < 8)
+	        eq.set_timer("spawn",120000);
+	    end
+	end
+end
 
---function event_timer(e)
---	if (e.timer == "spawn") then
---	    e.self:Say("spawning mobs");
-	   -- eq.spawn2(128110,28,0,e.self:GetX()+10,e.self:GetY()+10,e.self:GetZ(),126.6);
-	   -- eq.spawn2(128110,29,0,e.self:GetX()-10,e.self:GetY()-10,e.self:GetZ(),126.6);
-	    --spawnedMobs = spawnedMobs + 2;
-	    --if (spawnedMobs < 8)
-	     --   eq.set_timer("spawn",120000);
-	    --end
---	end
---end
+function event_death_complete(e)
+    eq.stop_timer("spawn");
+    eq.depop_all(128110);
+end
