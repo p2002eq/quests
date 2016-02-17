b909bf20f7e9cfb3050383e94a2d34329998c4bf
Eric <erog84@gmail.com>
2016-02-17 18:07:35 -0500

    Test

--- sleeper/128120.lua
+++ sleeper/128120.lua
@@ -1,3 +1,27 @@
 function event_combat(e)
-    e.self:Say("joined");
+    if(e.joined) then
+        e.self:Say("joined");
+        eq.set_timer("spawn",120000);
+    else
+        e.self:Say("ended");
+        eq.stop_timer("spawn");
+        eq.depop_all(128110);
+    end
+end
+
+function event_timer(e)
+	if (e.timer == "spawn") then
+	    eq.stop_timer("spawn");
+	    eq.spawn2(128110,28,0,e.self:GetX()+10,e.self:GetY()+10,e.self:GetZ(),126.6);
+	    eq.spawn2(128110,29,0,e.self:GetX()-10,e.self:GetY()-10,e.self:GetZ(),126.6);
+	    spawnedMobs = spawnedMobs + 2;
+	    if (spawnedMobs < 8) then
+	        eq.set_timer("spawn",120000);
+	    end
+	end
+end
+
+function event_death_complete(e)
+    eq.stop_timer("spawn");
+    eq.depop_all(128110);
 end
