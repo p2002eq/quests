512594e60eb2cfb454fafffbf4b336b4118b0d96
Eric <erog84@gmail.com>
2016-02-21 15:08:41 -0500

    Fix Tormax quest

--- kael/King_Tormax.lua
+++ kael/King_Tormax.lua
@@ -2,9 +2,9 @@
 function event_say(e)
 	if (e.other:GetFaction(e.self) < 5) then --amiable or lower
 		if (e.message:findi("hail")) then	
-			e.self:Say("Greetings, "..eq.Race()..". You know who I am, but I only vaguely know of you. My purpose is simple, I will rule these lands like my father, and my grandfather before him. All tasks but two are inconsequential to me.");
+			e.self:Say("Greetings, " ..e.other:GetRace().. ". You know who I am, but I only vaguely know of you. My purpose is simple, I will rule these lands like my father, and my grandfather before him. All tasks but two are inconsequential to me.");
 		elseif (e.message:findi("tasks")) then	
-			e.self:Say("The tasks are simple, "..eq.Race()..". I wish the death of the other 'powers' of this frozen waste land. If you are able to destroy either of my mortal foes, bring me proof of your exploits and you will be known as the hero of Kael Drakkel.");
+			e.self:Say("The tasks are simple, " ..e.other:GetRace().. ". I wish the death of the other 'powers' of this frozen waste land. If you are able to destroy either of my mortal foes, bring me proof of your exploits and you will be known as the hero of Kael Drakkel.");
 		elseif (e.message:findi("powers")) then	
 			e.self:Emote("laughs deeply. 'I speak of the foolish old dragon Yelinak and that pitiful Dain Frostreaver.'");
 		end
@@ -17,19 +17,17 @@ function event_trade(e)
 	local item_lib = require('items');
 	if (e.other:GetFaction(e.self) == 1) then --ally
 		if (item_lib.check_turn_in(e.self, e.trade, {item1 = 30501})) then
-			e.other:SummonItem(25858);
+			e.other:QuestReward(e.self, 0,0,0,0, 25858, 200000);
 			e.other:Faction(49, -100); --coldain
 			e.other:Faction(67, -100); --dain
 			e.other:Faction(189, 75); --kromzek
 			e.other:Faction(179, 75); --KT
-			e.other:AddEXP(200000);	
 		elseif (item_lib.check_turn_in(e.self, e.trade, {item1 =  24984})) then	
-			e.other:SummonItem(25857);
+			e.other:QuestReward(e.self, 0,0,0,0, 25857, 200000);
 			e.other:Faction(49, -100); --coldain
 			e.other:Faction(67, -100); --dain
 			e.other:Faction(189, 75); --kromzek
 			e.other:Faction(179, 75); --KT
-			e.other:AddEXP(200000);	
 		end
 		e.self:Emote("laughs deeply as he takes the bloody head then gives you your reward.");
 	else
