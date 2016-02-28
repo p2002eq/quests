d345a0553c23eba7811b3b5cc3f2c687119f11a6
Eric <erog84@gmail.com>
2016-02-27 23:34:41 -0500

    Test

--- eastwastes/116569.lua
+++ eastwastes/116569.lua
@@ -1,6 +1,12 @@
 function event_spawn(e)
-    eq.set_proximity(e.self:GetX()-100,e.self:GetX()+100,e.self:GetY()-100,e.self:GetY()+100);
     e.self:Say("spawned");
-	eq.attack_npc_type(116119);
-	e.self:Say("attacked");
+    local corbin = eq.get_entity_list():GetMobID(116119);
+    e.self:Say("get entity");
+    e.self:Say(corbin);
+    local mob1attack = corbin:CastToNPC();
+    e.self:Say("casttonpc");
+    --eq.set_proximity(e.self:GetX()-100,e.self:GetX()+100,e.self:GetY()-100,e.self:GetY()+100);
+    mob1attack:AddToHateList(e.self, 1);
+--	eq.attack_npc_type(116119);
+	e.self:Say("add to hate list");
 end
