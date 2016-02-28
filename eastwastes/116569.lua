aec7c27d26b3a0edb8c68689436e51106f499eaa
Eric <erog84@gmail.com>
2016-02-27 23:48:42 -0500

    Test

--- eastwastes/116569.lua
+++ eastwastes/116569.lua
@@ -2,7 +2,6 @@ function event_spawn(e)
     e.self:Say("spawned");
     local corbin = eq.get_entity_list():GetMobID(116119);
     e.self:Say("get entity");
-    e.self:Say(corbin);
     local mob1attack = corbin:CastToNPC();
     e.self:Say("casttonpc");
     --eq.set_proximity(e.self:GetX()-100,e.self:GetX()+100,e.self:GetY()-100,e.self:GetY()+100);
