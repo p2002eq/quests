7d4bcd6d4ad66b5e8414655e72e8e0b4bd6c55dc
Eric <erog84@gmail.com>
2016-02-27 23:49:51 -0500

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -10,10 +10,10 @@ end
 function event_waypoint_depart(e)
 	if (corbin == 10) then
 	    e.self:Say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
-	    eq.spawn2(116569, 0, 0, -2067, 189, 146, 57);
-	    eq.spawn2(116569, 0, 0, -2067, 199, 146, 57);
-	    eq.spawn2(116569, 0, 0, -2077, 199, 146, 57);
-	    eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
+	    eq.spawn2(116569, 0, 0, -2067, 189, 146, 57):AddToHateList(e.self,1);
+	    eq.spawn2(116569, 0, 0, -2067, 199, 146, 57):AddToHateList(e.self,1);
+	    eq.spawn2(116569, 0, 0, -2077, 199, 146, 57):AddToHateList(e.self,1);
+	    eq.spawn2(116569, 0, 0, -2077, 189, 146, 57):AddToHateList(e.self,1);
 	elseif (corbin == 20) then
 	    e.self:Say("I'll hack at your knees 'til you fall down! Out of love for the Dain, for the glory of the crown!");
 	end
