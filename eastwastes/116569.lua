ae27fbcf11989b1f4db694e0d3d4e7c8616c1f08
Eric <erog84@gmail.com>
2016-02-27 23:04:52 -0500

    Aggro Range

--- eastwastes/116569.lua
+++ eastwastes/116569.lua
@@ -1,4 +1,5 @@
 function event_spawn(e)
+    eq.set_proximity(e.self:GetX()-100,e.self:GetX()+100,e.self:GetY()-100,e.self:GetY()+100);
     e.self:Say("spawned");
 	eq.attack_npc_type(116119);
 	e.self:Say("attacked");
