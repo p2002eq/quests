d31d70f6fc8716b7458a73d821bdf663998b7c49
Eric <erog84@gmail.com>
2016-02-28 15:13:20 -0500

    Test

--- eastwastes/116569.lua
+++ eastwastes/116569.lua
@@ -1,5 +1,11 @@
 function event_spawn(e)
     e.self:Say("spawned");
-    eq.attack_npc_type(116119);
-    e.self:Say("ended");
+end
+
+function event_signal(e)
+    if (e.signal == 1161111) then
+        e.self:Say("signal");
+        eq.attack_npc_type(116119);
+        e.self:Say("attack");
+    end
 end
