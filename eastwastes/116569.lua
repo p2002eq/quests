e99573b6992601c66e9130f9b69ef7149c302d21
Eric <erog84@gmail.com>
2016-02-28 00:22:10 -0500

    Test

--- eastwastes/116569.lua
+++ eastwastes/116569.lua
@@ -3,7 +3,7 @@ function event_spawn(e)
     local corbin = eq.get_entity_list():GetMobByNpcTypeID(15178);
     if ( corbin ) then
         e.self:Say("hatelist");
-        for mob in corbin.entires do
+        for mob in corbin.entries do
                 e.self:Say("mob");
             e.self.AddToHateList(mob);
                 e.self:Say("added");
