370e8cd08bf2f3c34505f4c284f8b6e33feda8ef
Eric <erog84@gmail.com>
2016-03-01 00:54:35 -0500

    Test

--- unrest/player.lua
+++ unrest/player.lua
@@ -4,15 +4,20 @@ local entity_list = nil;
 local client_e = nil;
 
 function event_click_door(e)
-    e.self:Message(15, "testtesttest 1");
     entity_list = eq.get_entity_list(); --get current entity list of zone
 	e.self:Message(15, "test 2");
 	local door_id = e.door:GetDoorID(); 
-    e.self:Message(15, "test 3");
+    e.self:Message(15, "3Door ID: " .. door_id));
 	client_e = e;
-    e.self:Message(15, "test 5");
+    e.self:Message(15, "test 4");
 	player_list = nil;
-	e.self:Message(15, "test 6");
+	e.self:Message(15, "test 5");
 	player_list_count = nil;
-	e.self:Message(15, "test 7");
+	e.self:Message(15, "test 6");
+	if (e.other:HasItem(5532) == 1) then
+	    e.self:Message(15, "has epic");
+	end
+	if (e.self:HasItem(5532)) then
+	    e.self:Message(15, "epic has");
+	end
 end
