0dca96878c3ee0fedd3e34328ef15188a3ba2f32
Eric <erog84@gmail.com>
2016-03-01 00:33:46 -0500

    Test

--- cshome/Llara_the_Mysterious.lua
+++ cshome/Llara_the_Mysterious.lua
@@ -21,16 +21,16 @@ function event_say(e)
     		e.other:Say("Test 4");
     		player_list_count = group:GroupCount();
     		e.other:Say("Test 5");
---    		for i -0, player_list_count - 1, 1 do
---    		e.other:Say("Test 6");
---    			local client_v = player_list:GetMember(i):CastToClient();
---    			e.other:Say("Test 7");
+    		for i -0, player_list_count - 1, 1 do
+    		e.other:Say("Test 6");
+    			local client_v = player_list:GetMember(i):CastToClient();
+    			e.other:Say("Test 7");
 --			    if (client_v.valid) then --valid client
 --			    e.other:Say("Test 8");
 --				    e.other:Say(client_v:GetName());
 --				    e.other:Say("Test 9");
 --				end
---			end
+			end
     		
     	end
 	end
