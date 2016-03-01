7151dd01d1f2d24b034b5e2b25920aa2608709f0
Eric <erog84@gmail.com>
2016-03-01 00:29:08 -0500

    Test

--- unrest/player.lua
+++ unrest/player.lua
@@ -6,15 +6,15 @@ local client_e = nil;
 function event_click_door(e)
     e.self:Message(15, "testtesttest 1");
     entity_list = eq.get_entity_list(); --get current entity list of zone
-	e.self:Message("test 2");
+	e.self:Message(15, "test 2");
 	local door_id = e.door:GetDoorID(); 
-    e.self:Message("test 3");
+    e.self:Message(15, "test 3");
 --	local open_type = entity_list:FindDoor(door_id):GetOpenType();
---	e.other:Say("test 4");
+--	e.self:Message("test 4");
 --	client_e = e;
---e.other:Say("test 5");
+--e.self:Message(15, "test 5");
 --	player_list = nil;
---	e.other:Say("test 6");
+--	e.self:Message(15, "test 6");
 --	player_list_count = nil;
---	e.other:Say("test 7");
+--	e.self:Message(15, "test 7");
 end
