a59218ffaad3242a8cf3cc6d447912946db3846a
scott <jetviper21@gmail.com>
2016-03-02 13:05:15 -0500

    gnome power

--- poknowledge/Cocain_God.lua
+++ poknowledge/Cocain_God.lua
@@ -1,6 +1,7 @@
 function event_say(e)
     if(e.message:findi("Hail")) and (e.other:HasItem(13078) == false) then
-        e.self:Say("Heres some bread, don't starve....but I did pee on it");
+        e.self:Say("Heres some bread, Now leave me alone!");
+        e.other:SetRace(12);
         e.self:CastSpell(2198, e.other);
     end
 end
