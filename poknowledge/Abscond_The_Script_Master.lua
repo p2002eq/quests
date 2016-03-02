92340508d0d5d77a737c125e84d6e331c6207fad
Eric <erog84@gmail.com>
2016-03-01 19:52:41 -0500

    test

---
+++ poknowledge/Abscond_The_Script_Master.lua
@@ -0,0 +1,6 @@
+function event_say(e)
+    if(e.message:findi("Hail")) then
+        e.self:Say("Of course you were able to finish that quest, you don't honestly think those scripts wrote themselves!");
+    end
+end
+
