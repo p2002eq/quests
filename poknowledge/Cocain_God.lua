19a9133a7472a5c117c622d2951929afecb22ab8
Eric <erog84@gmail.com>
2016-03-02 00:22:29 -0500

    Test

--- poknowledge/Cocain_God.lua
+++ poknowledge/Cocain_God.lua
@@ -1,5 +1,5 @@
 function event_say(e)
-    if(e.message:findi("Hail")) and (e.other:HasItem(13078) = false) then
+    if(e.message:findi("Hail")) and (e.other:HasItem(13078) == false) then
         e.self:Say("Heres some bread, don't starve....but I did pee on it");
         e.self:CastSpell(2198, e.other);
     end
