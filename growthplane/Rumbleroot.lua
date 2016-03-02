eef3653f1c30b765b324d0aaaf7b704ccdbeb012
Eric <erog84@gmail.com>
2016-03-02 01:09:00 -0500

    Test

--- growthplane/Rumbleroot.lua
+++ growthplane/Rumbleroot.lua
@@ -1,10 +1,13 @@
 function event_combat(e)
 	if (e.joined) then
+	    e.self:Shout("this is a test");
 		-- grab the entity list
 		local entity_list = eq.get_entity_list();
-		
+		e.self:Shout("test2");
 		local npc_table = {127108,127025,127031};
+		e.self:Shout("test3");
 		for k,v in pairs(npc_table) do
+		    e.self:Shout("testing");
 			local npc = entity_list:GetMobByNpcTypeID(v);
 			if (npc.valid) then
 				npc:AddToHateList(e.other,1);
