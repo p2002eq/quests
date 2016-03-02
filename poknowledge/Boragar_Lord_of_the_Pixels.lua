4edf950097a0bebcecb208fe832b2388082c365b
Eric <erog84@gmail.com>
2016-03-02 15:25:12 -0500

    Test

--- poknowledge/Boragar_Lord_of_the_Pixels.lua
+++ poknowledge/Boragar_Lord_of_the_Pixels.lua
@@ -10,17 +10,16 @@ function event_say(e)
     elseif (e.message:findi("tasks")) and (qglobals["Anniversary"] == "1") then     -- Finished first task already.
 	    e.self:Say("I have a pair of old [boots] or an old [mask] you may like, but you will need to collect the appropriate materials to make them fit along with a new cap for me to use.");
 	elseif (e.message:findi("boots")) and (qglobals["Anniversary"] == "1") then 
-	    e.self:Say("My old journeyman boots of course!  Bring me a Leatherfoot Raider Skullcap, a leather padding, some butter and a steel boning.");");
+	    e.self:Say("My old journeyman boots of course!  Bring me a Leatherfoot Raider Skullcap, a leather padding, some butter and a steel boning and we will make sure these boots fit.");
 	elseif (e.message:findi("mask")) and (qglobals["Anniversary"] == "1") then 
-	    e.self:Say("Many a times my old mask helped me fit into situations that would otherwise be cramped.");
+	    e.self:Say("Many a times my old mask helped me fit into situations that would otherwise be cramped, if you know what I mean. Bring me a Leatherfoot Raider Skullcap, a leather padding, some butter and a steel boning and we will make sure this mask fits.");
 	elseif (e.message:findi("prefer boots")) and (qglobals["Anniversary"] == "2") then 
 	    e.self:Say("Have fun traveling norrath!");
 	    e.other:QuestReward(e.self, 0,0,0,0, 2300, 1);
 	elseif (e.message:findi("prefer mask")) and (qglobals["Anniversary"] == "2") then 
 	    e.self:Say("Enjoy walking around Neriak!");
 	    e.other:QuestReward(e.self, 0,0,0,0, 2475, 1);
-
-	elseif (e.message:findi("tasks")) and (qglobals["Anniversary"] == "2") then     -- Finished second task already and haven't gotten reward yet.
+	elseif (e.message:findi("third task")) and (qglobals["Anniversary"] == "2") then     -- Finished second task already and haven't gotten reward yet.
 	    e.self:Say("ready for 3rd task");
 	elseif (e.message:findi("tasks")) then                                              -- Dialogue for first Task
         e.self:Say("I need you to bring to me a Strand of Nightmare which can be obtained by combining the following items in a fletching kit.  A Scorpion Pincer, Rhino Beetle Carapace, Bixie Wing, Forest Drakeling Scales, Steamfont Spring Water, Patch of Gnoll Fur, Black Mamba Skin and a Pristine Krag Claw.  After you finish this, I will have the next task for you");	
