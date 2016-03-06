425beb4a45bbfe683e5e0729c5d2911c51e8a1ef
Eric <erog84@gmail.com>
2016-03-06 17:55:30 -0500

    Test

--- eastwastes/Poxbreath_Yellowfang.lua
+++ eastwastes/Poxbreath_Yellowfang.lua
@@ -1,6 +1,6 @@
 function event_spawn(e)
     --eq.ModifyNPCStat('runspeed',3);
-    e.self:ModifyNPCStat('runspeed',3);
+    e.self:ModifyNPCStat("runspeed",3);
 end
 function event_death_complete(e)
     eq.depop_all(116534);
