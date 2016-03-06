249196dfe7be2e2fe9102a34e1bfaaa3331857f4
Eric <erog84@gmail.com>
2016-03-06 15:31:17 -0500

    Dupe

--- eastwastes/116591.lua
+++
@@ -1,9 +0,0 @@
-function event_signal(e)
-    if (e.signal == 1161112) then
-        eq.attack_npc_type(116591);
-    end
-end
-
-function event_death_complete(e)
-    eq.signal(116591, 1161114);     -- send signal to Corbin for each death
-end
