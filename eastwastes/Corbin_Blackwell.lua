8e61429b5f4744aee445fedf618c392ce6d8068e
Eric <erog84@gmail.com>
2016-03-06 15:12:13 -0500

    Terst

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -1,9 +1,12 @@
 local corbin = 0;
 
 function event_signal(e)
-    if (e.signal == 11611) then
-        eq.move_to(-3183, -586, 188);
+    if (e.signal == 1161111) then
+        eq.move_to(-2750, -250, 150);
         corbin = 20;
+    elseif (e.signal == 1161112) then
+        eq.move_to(-3188, -574, 1);
+        corbin = 30;
     end
 end
 
@@ -15,6 +18,14 @@ function event_waypoint_depart(e)
         eq.spawn2(116569, 0, 0, -2077, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
         eq.signal(116569, 1161111, 400); 
+    elseif (corbin == 20)
+        e.self:Say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
+        eq.spawn2(116591, 0, 0, -2819, -296, 149, 40);
+        eq.spawn2(116129, 0, 0, -2825, -286, 148, 40)        eq.spawn2(116129, 0, 0, -2831, -276, 150, 40);
+        eq.spawn2(116129, 0, 0, -2837, -266, 151, 40);
+        eq.spawn2(116129, 0, 0, -2843, -256, 151, 40);
+        eq.signal(116129, 1161112, 400); 
+    elseif (corbin == 30)
     end
 end
 		
