0ce014f78a8897e72f59c7044d5611276ad6a57b
Eric <erog84@gmail.com>
2016-03-07 16:23:51 -0500

    Bread

--- poknowledge/Cocain_God.lua
+++ poknowledge/Cocain_God.lua
@@ -2,7 +2,8 @@ function event_say(e)
     if(e.message:findi("Hail")) and (e.other:HasItem(13078) == false) then
         e.self:Say("Heres some bread, Now leave me alone!");
         e.other:SetRace(12);
-        e.self:CastSpell(2198, e.other);
+        e.other:SummonItem(13078, 20);
+        --e.self:CastSpell(2198, e.other);
     end
 end
 
