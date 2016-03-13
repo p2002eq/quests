dd70731da292efafc83c79e21a4d6b9fef236a55
Eric <erog84@gmail.com>
2016-03-12 23:22:05 -0500

    Add despawn timer

--- kael/The_Avatar_of_War.lua
+++ kael/The_Avatar_of_War.lua
@@ -1,3 +1,23 @@
+local timerUp = false;      -- Determines if AOW despawn timer is up.
+
 function event_spawn(e)
 	e.self:Shout("Who dares defile my temple?! Come forth and face me!");
+end
+
+function event_combat(e)
+    if (e.joined) then
+       -- eq.set_timer("AOW",60000000);   -- 1 hour
+        eq.set_timer("AOW",600000);   -- 1 minute for testing.
+    else
+        eq.stop_timer("AOW");
+        if (timerUp) then
+            eq.depop_all(113627);
+        endif
+    end
+end
+
+function event_timer(e)
+    if (e.timer == "AOW") then
+        timerUp = true;
+    endif
 end
