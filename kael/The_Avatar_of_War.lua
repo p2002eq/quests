096e2c3886c437b5f16bdf5e62173bc61224d67e
Eric <erog84@gmail.com>
2016-03-12 23:52:06 -0500

    Update

--- kael/The_Avatar_of_War.lua
+++ kael/The_Avatar_of_War.lua
@@ -2,13 +2,12 @@ local timerUp = false;      -- Determines if AOW despawn timer is up.
 
 function event_spawn(e)
 	e.self:Shout("Who dares defile my temple?! Come forth and face me!");
+	-- eq.set_timer("AOW",60000000);   -- 1 hour
+	eq.set_timer("AOW",600000);   -- 1 minute for testing.
 end
 
 function event_combat(e)
-    if (e.joined) then
-       -- eq.set_timer("AOW",60000000);   -- 1 hour
-        eq.set_timer("AOW",600000);   -- 1 minute for testing.
-    else
+    if (e.joined == false) then
         eq.stop_timer("AOW");
         if (timerUp) then
             eq.depop_all(113627);
