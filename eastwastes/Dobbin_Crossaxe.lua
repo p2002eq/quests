98ca38b2e892e66bdb2509d8c6aa25cd1fcd2637
Eric <erog84@gmail.com>
2016-03-19 13:59:49 -0400

    Fix turn in

--- eastwastes/Dobbin_Crossaxe.lua
+++ eastwastes/Dobbin_Crossaxe.lua
@@ -1,14 +1,12 @@
-local ring_seven_final = nil;
+local ring_seven_final = 0;
 
 function event_signal(e)
     e.self:Say("signal");
 	if (e.signal == 1) then
-	    e.self:Say("signal 1");
 		ring_seven_final = 1;	
 		e.self:Say("We thought it was too late, the Dain will be very pleased!");
 		e.self:Say("Please friend, show me your Mithril ring and I will show you our gratitude.");
 	elseif (e.signal == 2) then
-	    e.self:Say("signal 2");
 		ring_seven_final = nil;
 	end
 
@@ -18,7 +16,7 @@ function event_trade(e)
 
 	local item_lib = require('items');
 
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 30162} and (ring_seven_final == 1))) then
+	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 30162})) and (ring_seven_final == 1) then
 		e.self:Say("Thank you. " .. e.other:GetName().. ", your deeds will be mentioned to  the Dain. Please take this note from Corbin to Garadain. it may help him to achieve victory over the enemy.");
 		ring_seven_final = nil;
 		e.other:SummonItem(1047);
