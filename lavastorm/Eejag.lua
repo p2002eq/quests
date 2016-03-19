724e4c66d6f558bd6f34ac81116576b81f6ca86b
Eric <erog84@gmail.com>
2016-03-19 17:04:16 -0400

    Test

--- lavastorm/Eejag.lua
+++ lavastorm/Eejag.lua
@@ -9,11 +9,11 @@ function event_spawn(e)
 	--eq.set_proximity(e.self:GetX()-50,e.self:GetX()+50,e.self:GetY()-50,e.self:GetY()+50);
 end
 
-function event_enter(e)
-	if(e.other:Class() == "Monk" and e.other:GetLevel() >= 46) then
-		-- Monk Epic 1.0
-		eq.attack(e.other:GetName());
-	end
-end
+--function event_enter(e)
+--	if(e.other:Class() == "Monk" and e.other:GetLevel() >= 46) then
+--		-- Monk Epic 1.0
+--		eq.attack(e.other:GetName());
+--	end
+--end
 
 -- End of File
