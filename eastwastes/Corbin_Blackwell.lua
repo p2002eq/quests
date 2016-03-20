f92822e9a085cac1fdca64330073c28188a61e14
Eric <erog84@gmail.com>
2016-03-20 15:20:33 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -38,7 +38,6 @@ function event_trade(e)
         e.self:Say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
         e.self:SetRunning(true);
         eq.move_to(-2012, 197, 148);
-        eq.set
         corbin = 10;
     end
 end
