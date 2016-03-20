94a43c74cb765dc41486dce8548c9a8c5d380576
Eric <erog84@gmail.com>
2016-03-20 13:42:52 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -32,6 +32,7 @@ function event_waypoint_arrive(e)
         eq.spawn2(116569, 0, 0, -2077, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
         eq.signal(116569, 1161111, 1000);
+        e.self:SetWaypointPause(1000);
     elseif (corbin == 20) then
         e.self:Say("I'll hack at your knees 'til you fall down! Out of love for the Dain, for the glory of the crown!");
         eq.spawn2(116591, 0, 0, -2819, -296, 149, 40);
