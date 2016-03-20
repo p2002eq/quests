728c57829efd17612d0d757e7da620946a59de41
Eric <erog84@gmail.com>
2016-03-20 15:12:24 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -1,32 +1,3 @@
-local corbin = 0;   
-local giantsDead = 0;       -- will keep track of giants dead, so we know when to have him continue on after battle with giants
-local orcsDead = 0;         -- will keep track of giants dead, so we know when to have him continue on after battle with orcs
-
-function event_signal(e)
-    if (e.signal == 1) then
-        orcsDead = orcsDead + 1;
-        if (orcsDead > 3) then
-            e.self:Say("more than 3 dead");
-            --eq.move_to(-2750, -250, 150, 171, true);
-            e.self:SetRunning(true);
-            eq.move_to(-2750, -250, 150);
-            e.self:Say("moving to giants");
-            corbin = 20;
-            e.self:Say("set to 20");
-        end
-    elseif (e.signal == 2) then
-        giantsDead = giantsDead + 1;
-        e.self:Say("giant dead");
-        if (giantsDead > 4) then
-            e.self:Say("more than 4 dead");
-            e.self:SetRunning(true);
-            eq.move_to(-3178, -531, 150);
-            corbin = 30;
-        end
-    end
-end
-
-
 function event_waypoint_arrive(e)
     e.self:Say("waypoint arrive");
     if (corbin == 10) then
@@ -37,8 +8,8 @@ function event_waypoint_arrive(e)
         eq.spawn2(116569, 0, 0, -2077, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
         eq.signal(116569, 1161111, 600);
-        eq.move_to(-2066, 188, 146);
-        corbin = 15;
+        eq.move_to(-2819, -296, 149);
+        corbin = 20;
     elseif (corbin == 20) then
         e.self:SetRunning(false);
         e.self:Say("I'll hack at your knees 'til you fall down! Out of love for the Dain, for the glory of the crown!");
@@ -48,6 +19,8 @@ function event_waypoint_arrive(e)
         eq.spawn2(116129, 0, 0, -2813, -306, 151, 40);
         eq.spawn2(116129, 0, 0, -2807, -316, 151, 40);
         eq.signal(116129, 1161112, 600); 
+        eq.move_to(-3178, -531, 150);
+        corbin = 30;
     elseif (corbin == 30) then
         eq.move_to(-3175, -574.00, 156);
         corbin = 40;
@@ -62,11 +35,10 @@ function event_trade(e)
     local item_lib = require('items');
     if (item_lib.check_turn_in(e.self, e.trade, {item1 = 1046, item2 = 30162})) then
         e.other:QuestReward(e.self,0,0,0,0,30162,1000);
-        --eq.ModifyNPCStat('runspeed', 3.0);
         e.self:Say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
         e.self:SetRunning(true);
-        --eq.move_to(-2012, 197, 148, 270, true);
         eq.move_to(-2012, 197, 148);
+        eq.set
         corbin = 10;
     end
 end
