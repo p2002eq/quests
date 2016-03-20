f33263e1e3c0f3331d531a3a75c152d040c9c0b6
Eric <erog84@gmail.com>
2016-03-20 14:24:22 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -8,14 +8,18 @@ function event_signal(e)
         if (orcsDead > 3) then
             e.self:Say("more than 3 dead");
             --eq.move_to(-2750, -250, 150, 171, true);
+            e.self:SetRunning(true);
             eq.move_to(-2750, -250, 150);
+            e.self:Say("moving to giants");
             corbin = 20;
+            e.self:Say("set to 20");
         end
     elseif (e.signal == 2) then
         giantsDead = giantsDead + 1;
         e.self:Say("giant dead");
         if (giantsDead > 4) then
-        e.self:Say("more than 4 dead");
+            e.self:Say("more than 4 dead");
+            e.self:SetRunning(true);
             eq.move_to(-3178, -531, 150);
             corbin = 30;
         end
@@ -26,22 +30,22 @@ end
 function event_waypoint_arrive(e)
     e.self:Say("waypoint arrive");
     if (corbin == 10) then
-        e.self:Say("10");
+        e.self:SetRunning(false);
         e.self:Say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
         eq.spawn2(116569, 0, 0, -2067, 189, 146, 57);
         eq.spawn2(116569, 0, 0, -2067, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
-        eq.signal(116569, 1161111, 1000);
-        e.self:SetWaypointPause(1000);
+        eq.signal(116569, 1161111, 100);
     elseif (corbin == 20) then
+        e.self:SetRunning(false);
         e.self:Say("I'll hack at your knees 'til you fall down! Out of love for the Dain, for the glory of the crown!");
         eq.spawn2(116591, 0, 0, -2819, -296, 149, 40);
         eq.spawn2(116129, 0, 0, -2825, -286, 148, 40);      
         eq.spawn2(116129, 0, 0, -2831, -276, 150, 40);
         eq.spawn2(116129, 0, 0, -2813, -306, 151, 40);
         eq.spawn2(116129, 0, 0, -2807, -316, 151, 40);
-        eq.signal(116129, 1161112, 1000); 
+        eq.signal(116129, 1161112, 100); 
     elseif (corbin == 30) then
         eq.move_to(-3175, -574.00, 156);
         corbin = 40;
@@ -60,7 +64,6 @@ function event_trade(e)
         e.self:Say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
         e.self:SetRunning(true);
         --eq.move_to(-2012, 197, 148, 270, true);
-        e.self:SetWaypointPause(1000);
         eq.move_to(-2066, 188, 146);
         corbin = 10;
     end
