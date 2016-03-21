4d91b285af59dd1adeeba93fb86f3b873a3b5eb8
Eric <erog84@gmail.com>
2016-03-21 00:50:48 -0400

    Test

--- eastwastes/116129.lua
+++ eastwastes/116129.lua
@@ -18,5 +18,5 @@ function event_signal(e)
 end
 
 function event_death_complete(e)
-    eq.signal(116119, 1161114);     -- send signal to Corbin for each death
+    eq.signal(116119, 2);     -- send signal to Corbin for each death
 end
