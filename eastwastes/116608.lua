f7a7671d61e58970a14ee9b6acfae3b472275db1
Eric <erog84@gmail.com>
2016-03-20 22:55:32 -0400

    Test

--- eastwastes/116608.lua
+++ eastwastes/116608.lua
@@ -3,6 +3,6 @@ function event_trade(e)
     if (item_lib.check_turn_in(e.self, e.trade, {item1 = 1046, item2 = 30162})) then
         e.other:QuestReward(e.self,0,0,0,0,30162,1000);
         eq.depop();
-        e.spawn2(116119, 0,0,86,216,32,190.7);
+        eq.spawn2(116119, 0,0,86,216,32,190.7);
     end
 end
