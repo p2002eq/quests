5043083cd91ec615b775ab2a74cf420cb083908b
Eric <erog84@gmail.com>
2016-03-20 22:48:38 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -1,17 +1,41 @@
-function event_waypoint_depart(e)
-    e.self:Say("waypoint depart");
+local corbin = 0;   
+local giantsDead = 0;       -- will keep track of giants dead, so we know when to have him continue on after battle with giants
+local orcsDead = 0;         -- will keep track of giants dead, so we know when to have him continue on after battle with orcs
+
+function event_signal(e)
+    if (e.signal == 1) then
+        orcsDead = orcsDead + 1;
+        if (orcsDead > 3) then
+            e.self:Say("more than 3 dead");
+            e.self:SetRunning(true);
+            eq.move_to(-2750, -250, 150);
+            e.self:Say("moving to giants");
+            corbin = 20;
+            e.self:Say("set to 20");
+        end
+    elseif (e.signal == 2) then
+        giantsDead = giantsDead + 1;
+        e.self:Say("giant dead");
+        if (giantsDead > 4) then
+            e.self:Say("more than 4 dead");
+            e.self:SetRunning(true);
+            eq.move_to(-3178, -531, 150);
+            corbin = 30;
+        end
+    end
+end
+
+function event_waypoint_arrive(e)
+    e.self:Say("waypoint arrive");
     if (corbin == 10) then
         e.self:SetRunning(false);
-        eq.move_to(-2819, -296, 149);
         e.self:Say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
         eq.spawn2(116569, 0, 0, -2067, 189, 146, 57);
         eq.spawn2(116569, 0, 0, -2067, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
         eq.signal(116569, 1161111, 600);
-        corbin = 20;
     elseif (corbin == 20) then
-        eq.move_to(-3178, -531, 150);
         e.self:Say("I'll hack at your knees 'til you fall down! Out of love for the Dain, for the glory of the crown!");
         eq.spawn2(116591, 0, 0, -2819, -296, 149, 40);
         eq.spawn2(116129, 0, 0, -2825, -286, 148, 40);      
@@ -19,7 +43,6 @@ function event_waypoint_depart(e)
         eq.spawn2(116129, 0, 0, -2813, -306, 151, 40);
         eq.spawn2(116129, 0, 0, -2807, -316, 151, 40);
         eq.signal(116129, 1161112, 600); 
-        corbin = 30;
     elseif (corbin == 30) then
         eq.move_to(-3175, -574.00, 156);
         corbin = 40;
@@ -36,7 +59,7 @@ function event_trade(e)
         e.other:QuestReward(e.self,0,0,0,0,30162,1000);
         e.self:Say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
         e.self:SetRunning(true);
-        eq.move_to(-2012, 197, 148, 40, true);
+        eq.move_to(-2012, 197, 148);
         corbin = 10;
     end
 end
