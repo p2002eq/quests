83d888f4e7479fae03298575254f08a74a90ef93
Eric <erog84@gmail.com>
2016-03-22 13:59:57 -0400

    Remove running

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -25,7 +25,7 @@ end
 function event_spawn(e)
     e.self:AssignWaypoints(276);
     e.self:Say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
-    e.self:SetRunning(true);
+ --   e.self:SetRunning(true);
 end
 
 function event_timer(e)
