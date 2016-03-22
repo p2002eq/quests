11c30c45d3c71b2102a382a4b8f773020acbc446
Eric <erog84@gmail.com>
2016-03-21 23:49:57 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -1,68 +1,12 @@
-local corbin = 0;   
-local giantsDead = 0;       -- will keep track of giants dead, so we know when to have him continue on after battle with giants
-local orcsDead = 0;         -- will keep track of giants dead, so we know when to have him continue on after battle with orcs
---e.self:StopWandering();
---e.self:ResumeWandering();
-function event_signal(e)
-    if (e.signal == 1) then
-        orcsDead = orcsDead + 1;
-        if (orcsDead > 3) then
-            eq.set_timer("1",10000)
-        end
-    elseif (e.signal == 2) then
-        giantsDead = giantsDead + 1;
-        if (giantsDead > 4) then
-            e.self:Say("Giant timer");
-            eq.set_timer("2",10000)
-        end
-    end
-end
-
-function event_combat(e)
-    if (e.joined == false) then
-        if (corbin == 10) then
-            eq.move_to(-2012, 197, 148, 190, true);
-        elseif (corbin == 20) then
-            eq.move_to(-2750, -250, 150, 190, true);
-        elseif (corbin == 30) then
-            eq.move_to(-3178, -531, 150, 190, true);
-        end
-    end
-end
-
-function event_timer(e)
-    if (e.timer == "1") then
-        eq.stop_timer("1");
-        eq.move_to(-2750, -250, 150, 190, true);
-        corbin = 20;
-    elseif (e.timer == "2") then
-        eq.move_to(-3178, -531, 150, 190, true);
-        eq.stop_timer("2");
-        corbin = 30;
-    elseif (e.timer == "3") then
-        eq.stop_timer("3");
-        e.self:Say("stop timer 3");
-        e.self:ResumeWandering();
-        e.self:Say("resume wandering");
-        
-    end
-end
-
 function event_waypoint_arrive(e)
     if (e.wp == 1) then
-        e.self:Say("wp = 1");
-        e.self:StopWandering();
-        e.self:Say("stop wandering");
-        eq.set_timer("3",10000);
-    end
-    if (corbin == 10) then
         e.self:Say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
         eq.spawn2(116569, 0, 0, -2067, 189, 146, 57);
         eq.spawn2(116569, 0, 0, -2067, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 199, 146, 57);
         eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
         eq.signal(116569, 1161111, 600);
-    elseif (corbin == 20) then
+    elseif (e.wp == 2) then
         e.self:Say("I'll hack at your knees 'til you fall down! Out of love for the Dain, for the glory of the crown!");
         eq.spawn2(116591, 0, 0, -2819, -296, 149, 40);
         eq.spawn2(116129, 0, 0, -2825, -286, 148, 40);      
@@ -70,10 +14,9 @@ function event_waypoint_arrive(e)
         eq.spawn2(116129, 0, 0, -2813, -306, 151, 40);
         eq.spawn2(116129, 0, 0, -2807, -316, 151, 40);
         eq.signal(116129, 1161112, 600); 
-    elseif (corbin == 30) then
+    elseif (e.wp == 3) then
         eq.move_to(-3175, -574.00, 156, 190, true);
-        corbin = 40;
-    elseif (corbin == 40) then
+    elseif (e.wp == 4) then
         e.self:Say("I have escaped! With the help of our friends here I was saved from certain death. We are in their debt.");
         eq.signal(116118, 1);
         eq.depop();
@@ -84,5 +27,4 @@ function event_spawn(e)
     e.self:Say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
     e.self:SetRunning(true);
     eq.move_to(-2012, 197, 148, 190, true);
-    corbin = 10;
 end
