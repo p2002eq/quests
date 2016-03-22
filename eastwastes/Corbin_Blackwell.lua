9c7a65a4851f799810cc5a5c0760ca37a18e577b
Eric <erog84@gmail.com>
2016-03-22 00:02:58 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -1,5 +1,7 @@
 function event_waypoint_arrive(e)
+    e.self:Say("arrive");
     if (e.wp == 1) then
+        e.self:Say("1");
         e.self:Say("Uh oh, looks like they were tipped off somehow... I hope you can handle them.");
         eq.spawn2(116569, 0, 0, -2067, 189, 146, 57);
         eq.spawn2(116569, 0, 0, -2067, 199, 146, 57);
@@ -7,6 +9,7 @@ function event_waypoint_arrive(e)
         eq.spawn2(116569, 0, 0, -2077, 189, 146, 57);
         eq.signal(116569, 1161111, 600);
     elseif (e.wp == 2) then
+        e.self:Say("2");
         e.self:Say("I'll hack at your knees 'til you fall down! Out of love for the Dain, for the glory of the crown!");
         eq.spawn2(116591, 0, 0, -2819, -296, 149, 40);
         eq.spawn2(116129, 0, 0, -2825, -286, 148, 40);      
@@ -14,9 +17,8 @@ function event_waypoint_arrive(e)
         eq.spawn2(116129, 0, 0, -2813, -306, 151, 40);
         eq.spawn2(116129, 0, 0, -2807, -316, 151, 40);
         eq.signal(116129, 1161112, 600); 
-    elseif (e.wp == 3) then
-        eq.move_to(-3175, -574.00, 156, 190, true);
     elseif (e.wp == 4) then
+        e.self:Say("4");
         e.self:Say("I have escaped! With the help of our friends here I was saved from certain death. We are in their debt.");
         eq.signal(116118, 1);
         eq.depop();
