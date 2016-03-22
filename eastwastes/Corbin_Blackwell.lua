f563f4c39114ea8846a3dc9b8326753d8cefa4a5
Eric <erog84@gmail.com>
2016-03-22 13:20:14 -0400

    Test

--- eastwastes/Corbin_Blackwell.lua
+++ eastwastes/Corbin_Blackwell.lua
@@ -24,7 +24,8 @@ end
 
 function event_spawn(e)
     e.self:Say("test grid");
-    e.self:SetGrid(276);
+    --e.self:SetGrid(276);
+    e.self:AssignWaypoints(276);
     e.self:Say("I thought I was a dwarfskin rug there for a minute! Thank Brell for your help stranger! Now cover me while I make good my escape. I am weakened and cannot endure much more.");
     e.self:SetRunning(true);
 end
