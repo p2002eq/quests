eb5ea12e68e57469c9250aed391eb8bf3f85c69c
Eric <erog84@gmail.com>
2016-03-24 00:35:44 -0400

    Update

--- iceclad/110227.lua
+++ iceclad/110227.lua
@@ -2,7 +2,7 @@ function event_trade(e)
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 8897})) then
 		e.self:Say("The final orders, follow me closely! I pray that the Kromrif have not learned of our plans to allign with the gnomes.");
-        e.self:AssignWaypoints(276);
+        e.self:AssignWaypoints(67);
         e.self:SetRunning(true);
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
