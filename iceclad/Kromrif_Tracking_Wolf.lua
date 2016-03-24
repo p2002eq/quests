7d94cacdfb2552b307df36d2ee2c304b2ffbb074
Eric <erog84@gmail.com>
2016-03-24 12:11:01 -0400

    Test

--- iceclad/Kromrif_Tracking_Wolf.lua
+++ iceclad/Kromrif_Tracking_Wolf.lua
@@ -1,6 +1,10 @@
 -- Shawl 8 encounter.  Attack General Bragmur
+function event_spawn(e)
+    e.self:Say("wolf spawns");
+end
 
 function event_signal(e)
+    e.self:Say("Wolves Signal");
     if (e.signal == 1) then
         eq.attack_npc_type(110227);
     end
