5a83d651ee37109de6e7b62dcc36731ff231cfac
Eric <erog84@gmail.com>
2016-03-25 00:32:55 -0400

    Test

--- iceclad/frost_giant_skirmisher.lua
+++ iceclad/frost_giant_skirmisher.lua
@@ -1,11 +1,6 @@
 -- Shawl 8 encounter.  Attack General Bragmur
-function event_spawn(e)
-    e.self:Say("giants spawn");
-end
-
 function event_signal(e)
     if (e.signal == 1) then
-        e.self:Say("attack");
         eq.attack_npc_type(110227);
     end
 end
