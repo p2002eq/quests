d8cededce266f412faae730c313b83d319d28ee3
Eric <erog84@gmail.com>
2016-03-25 01:12:56 -0400

    Test

--- iceclad/frost_giant_skirmisher.lua
+++ iceclad/frost_giant_skirmisher.lua
@@ -1,7 +1,6 @@
 -- Shawl 8 encounter.  Attack General Bragmur
 function event_signal(e)
     if (e.signal == 1) then
-        e.self:Say("attack");
         eq.attack_npc_type(110227);
     end
 end
