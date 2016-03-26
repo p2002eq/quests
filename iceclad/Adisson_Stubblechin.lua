eee174ac7f8e4a5eaed48689e6eb80c6e8d2c6e5
Eric <erog84@gmail.com>
2016-03-26 17:01:09 -0400

    Update

--- iceclad/Adisson_Stubblechin.lua
+++ iceclad/Adisson_Stubblechin.lua
@@ -1,13 +1,8 @@
 function event_signal(e)
     if (e.signal == 1) then
         e.self:Say("Don't be thinkin' I was forgettin' ya, Nilham, ya spotty-faced salt licker. Remember, ya tell us where ya put our rum, an' we let ya out. Otherwise, ya have ta stay here!");
-        local npc2 = eq.get_entity_list():GetMobByNpcTypeID(110218);     -- Avatar of Below
-        if (npc2) then
-            eq.signal(110218,1,4000);
-        else
-            eq.spawn2(110218, 0, 0, 1318, 4557, 72, 6);
-            eq.signal(110218,1,4000);
-        end
+        eq.spawn2(110218, 0, 0, 1318, 4557, 72, 6);
+        eq.signal(110218,1,4000);
     end
 end
 
