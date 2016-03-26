f73e1b9001e2f1ae8613bc0d348504b01906f09f
Eric <erog84@gmail.com>
2016-03-25 23:23:20 -0400

    Test

--- iceclad/kromrif_tracking_wolf.lua
+++ iceclad/kromrif_tracking_wolf.lua
@@ -1,8 +1,8 @@
 -- Shawl 8 encounter.  Attack General Bragmur
 function event_signal(e)
-    if (e.signal == 1) then
+    if (e.signal == 2) then
         eq.attack_npc_type(110227);
-    elseif (e.signal == 2) then
+    elseif (e.signal == 1) then
         eq.attack_npc_type(110227);
         if (math.random(100) > 25) then
             e.self:AddItem(8905);
