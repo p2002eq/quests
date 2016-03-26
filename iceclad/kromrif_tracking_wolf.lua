81800adcd0b368fc8864a16f578b9bf7e3b34124
Eric <erog84@gmail.com>
2016-03-25 23:19:59 -0400

    Add 25% loot drop

--- iceclad/kromrif_tracking_wolf.lua
+++ iceclad/kromrif_tracking_wolf.lua
@@ -2,6 +2,11 @@
 function event_signal(e)
     if (e.signal == 1) then
         eq.attack_npc_type(110227);
+    elseif (e.signal == 2) then
+        eq.attack_npc_type(110227);
+        if (math.random(100) > 25) then
+            e.self:AddItem(8905);
+        end
     end
 end
 
