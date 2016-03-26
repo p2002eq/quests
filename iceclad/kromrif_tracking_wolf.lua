0ade0f3c852e4ce6d90eae0fc75111d1abd77f7a
Eric <erog84@gmail.com>
2016-03-26 00:43:30 -0400

    Test

--- iceclad/kromrif_tracking_wolf.lua
+++ iceclad/kromrif_tracking_wolf.lua
@@ -4,8 +4,10 @@ function event_signal(e)
         eq.attack_npc_type(110227);
     elseif (e.signal == 1) then
         eq.attack_npc_type(110227);
+        e.self:Say("going to random");
         if (math.random(100) < 26) then
             e.self:AddItem(8905);
+            e.self:Say("added item");
         end
     end
 end
