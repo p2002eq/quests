c638dfb569b816716a4c7d545beb16571bb47916
Eric <erog84@gmail.com>
2016-03-26 00:09:35 -0400

    Convert from Perl

---
+++ iceclad/Ratop.lua
@@ -0,0 +1,10 @@
+function event_signal(e)
+    if (e.signal == 1) then
+        e.self:Say("Squawk! Pieces of eight! Pieces of eight!");
+    elseif (e.signal == 2) then
+        e.self:Say("We be out. Youll be needin to make more. Arg!");
+    elseif (e.signal == 101) then
+        e.self:Say("Yar ye scruvy bearded dwarf, just over tha hill there.");
+    end
+end
+
