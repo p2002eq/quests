79a96599282491b1c37c78df62811f018ea2b212
Eric <erog84@gmail.com>
2016-03-26 00:56:41 -0400

    Timer

--- iceclad/Ratop.lua
+++ iceclad/Ratop.lua
@@ -1,7 +1,7 @@
 function event_signal(e)
     if (e.signal == 1) then
         e.self:Say("Squawk! Pieces of eight! Pieces of eight!");
-        eq.signal(110053,1,500);
+        eq.signal(110053,1,1000);
     elseif (e.signal == 2) then
         e.self:Say("We be out. Youll be needin to make more. Arg!");
     elseif (e.signal == 101) then
