aa7a4441d694ea6da0309a760af1fece2d1e6ab6
Eric <erog84@gmail.com>
2016-03-26 00:24:39 -0400

    8th Shawl Dialogue

---
+++ iceclad/Adisson_Stubblechin.lua
@@ -0,0 +1,5 @@
+function event_signal(e)
+    if (e.signal == 1) then
+        e.self:Say("Don't be thinkin' I was forgettin' ya, Nilham, ya spotty-faced salt licker. Remember, ya tell us where ya put our rum, an' we let ya out. Otherwise, ya have ta stay here!");
+    end
+end
