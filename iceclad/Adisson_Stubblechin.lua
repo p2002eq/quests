5af6caaca14bfe16b1f464c5bfff06a45981eba7
Eric <erog84@gmail.com>
2016-03-26 00:33:12 -0400

    Update

--- iceclad/Adisson_Stubblechin.lua
+++ iceclad/Adisson_Stubblechin.lua
@@ -9,19 +9,3 @@ function event_signal(e)
         end
     end
 end
-
-function event_say(e)
-    if(e.message:findi("runed coldain prayer shaw")) then
-        e.self:Say("So it is the sacred rune of the Coldain. Let me have a closer look at it.");
-    end
-end
-
-function event_trade(e)
-	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 8895})) then
-		e.self:Emote("Avatar of Below holds the shawl up close to one eye to look at the rune sewn into it. He smiles to himself and then drops it to the ground and places one hand on it. A bright blue glow starts to emanate in the snow, then there is a sudden flash! The Avatar has dissipated. All that is left behind is the Shawl lying in the snow, shimmering with a new power. Pirotess slowly bends down to pick it up, and is infused with a blessing from Brell.");
-		e.other:QuestReward(e.self,0,0,0,0,1200,10000);     -- Blessed Coldain Prayer Shawl
-		e.signal(110227,5,500);
-	end
-	item_lib.return_items(e.self, e.other, e.trade)
-end
