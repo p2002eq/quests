6cb24c01b3f01e1b91309f40dff25c05aae61171
Eric <erog84@gmail.com>
2016-03-25 22:36:20 -0400

    Test

--- iceclad/Commander_Vjorik.lua
+++ iceclad/Commander_Vjorik.lua
@@ -3,6 +3,7 @@
 function event_signal(e)
     if (e.signal == 1) then
         e.self:Shout("The tracking wolves have found the Coldain! To the South West, Charge him!");
+        eq.depop();
     elseif (e.signal == 2) then
         e.self:Shout("turn off invis");
         e.self:SetInvisible(1);
