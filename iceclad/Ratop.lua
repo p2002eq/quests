454aad59b08a0f807bd6e8106e03653e1b9af8b3
Eric <erog84@gmail.com>
2016-03-28 15:24:35 -0400

    Update

--- iceclad/Ratop.lua
+++ iceclad/Ratop.lua
@@ -3,7 +3,7 @@ function event_signal(e)
         e.self:Say("Squawk! Pieces of eight! Pieces of eight!");
         eq.signal(110053,1,4000);
     elseif (e.signal == 2) then
-        e.self:Say("We be out. Youll be needin to make more. Arg!");
+        e.self:Say("We be out. You'll be needin to make more. Arg!");
     elseif (e.signal == 101) then
         e.self:Say("Yar ye scruvy bearded dwarf, just over tha hill there.");
     end
