4b11d2420705125cd5688099c85e299864cd51d4
Eric <erog84@gmail.com>
2016-03-28 18:18:51 -0400

    Fixed spawn locations

--- frontiermtns/cycle_one.lua
+++ frontiermtns/cycle_one.lua
@@ -22,7 +22,7 @@ function event_spawn(e)
 	                elseif(getnewloc == 2) then
 		                newx = 3416;
 		                newy = 2013;
-		                newz = 172;
+		                newz = 194;
 	                end
 	        
 	            local getnewmobid = eq.ChooseRandom(92213, 92214, 92215, 92216, 92217, 92218, 92219, 92220);
@@ -32,11 +32,11 @@ function event_spawn(e)
 	                if(getnewloc == 1) then
 		                newx = 2889;
 		                newy = -1784;
-		                newz = -143;
+		                newz = -290;
 	                elseif(getnewloc == 2) then
 		                newx = -2111;
 		                newy = -2645;
-		                newz = -389;
+		                newz = 192;
 	                end
 	        
 	            local getnewmobid = eq.ChooseRandom(92197, 92198, 92199, 92200, 92201, 92202, 92203, 92204, 92205, 92206, 92207);
