e632ec0f7304e7a3826ed09c2d83db1e5cce0936
Eric <erog84@gmail.com>
2016-03-28 15:55:32 -0400

    Test

--- iceclad/Adisson_Stubblechin.lua
+++ iceclad/Adisson_Stubblechin.lua
@@ -1,7 +1,7 @@
 function event_signal(e)
     if (e.signal == 1) then
         e.self:Say("Don't be thinkin' I was forgettin' ya, Nilham, ya spotty-faced salt licker. Remember, ya tell us where ya put our rum, an' we let ya out. Otherwise, ya have ta stay here!");
-        eq.signal(110067,1,4000);
+        eq.signal(110067,2,4000);
     elseif (e.signal == 2) then     -- spawns avatar of below
         eq.spawn2(110218, 0, 0, 1318, 4557, 72, 6);
         eq.signal(110218,1,4000);
@@ -11,6 +11,6 @@ end
 function event_say(e)
     if(e.message:findi("hail")) then
         e.self:Say("Don't be thinkin' I was forgettin' ya, Nilham, ya spotty-faced salt licker. Remember, ya tell us where ya put our rum, an' we let ya out. Otherwise, ya have ta stay here!");
-        eq.signal(110067,1,4000);
+        eq.signal(110067,2,4000);
     end
 end
