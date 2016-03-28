d27d11f5530a83589100ffd790c9b8b2cb7b024e
Eric <erog84@gmail.com>
2016-03-28 15:40:51 -0400

    Test

--- iceclad/Adisson_Stubblechin.lua
+++ iceclad/Adisson_Stubblechin.lua
@@ -11,7 +11,6 @@ end
 function event_say(e)
     if(e.message:findi("hail")) then
         e.self:Say("Don't be thinkin' I was forgettin' ya, Nilham, ya spotty-faced salt licker. Remember, ya tell us where ya put our rum, an' we let ya out. Otherwise, ya have ta stay here!");
-        eq.spawn2(110218, 0, 0, 1318, 4557, 72, 6);
-        eq.signal(110218,1,4000);
+        eq.signal(110067,1,4000);
     end
 end
