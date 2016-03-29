29680303fdb91ceb2117db985bfdc06fdc99e560
Eric <erog84@gmail.com>
2016-03-28 23:48:03 -0400

    Fix for druid epic

--- misty/Ella_Foodcrafter.lua
+++ misty/Ella_Foodcrafter.lua
@@ -21,6 +21,7 @@ function event_say(e)
 	elseif(e.message:findi("two tasks")) then
 		e.self:Say("Well, ya ask before ya act. That's a good sign. What I need isn't easy to find. First, I need a mixture. Plain sounding enough but it's a special one. Cleanses the mind and focuses the earth's energies into a forest walker's mind. I don't know how to make it. I don't even know what it's called. I merely know it exists and we'll be needing it for our task. When ya do find how to make it ya can mix it in that bowl. Keep it when you're done, it's a good bowl.");
 		e.other:SummonItem(17860);
+		eq.delete_global("Druidepic");
 		
 	elseif(e.message:findi("second task")) then
 		e.self:Say("Good, ya didn't run off without finding out what else ya need to do. Second, I need a special type of bowl. An ancient type of bowl, which strengthened the food placed inside it, once made by the elves of old. Again, I cannot tell ya how it is made for the making was lost to the ages. Seek this knowledge and these items out in the lands. Come to me when ya have found both.");
@@ -31,6 +32,7 @@ function event_trade(e)
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 18959})) then
 		e.self:Say("Hmmm. I've been waiting for someone to bring me this. I'm sorry such a young one as you has taken up this task. Maybe the sky god blesses ya though. Aye, ya have that look in your eye. Our task is to focus the blessings of the gods and cleanse the filth from the land. I have two tasks for ya.");
+		eq.set_global("Druidepic","1",5,"F");
 	end
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 20473, item2 = 20460})) then
 		e.self:Emote("smiles slowly as she works the mixture in the bowl and it starts to shine with power. 'This is... a piece of a lost magic item known as Nature's Gem. When restored to completeness I've heard its healing powers will be extraordinary. Take it now. I will take the bowl and hopefully do some good from my end. From here, you can go many ways but I can't guide your path step by step. The only advice I can give you is to look for ones such as yourself.'");
