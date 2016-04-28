172ab99a5f327480d635669f52e799b63f88a82f
Eric <erog84@gmail.com>
2016-04-27 21:00:27 -0400

    Update loc

--- freporte/Translocator_Setikan.lua
+++ freporte/Translocator_Setikan.lua
@@ -1,5 +1,5 @@
 local helper = require('translocators');
 
 function event_say(e)
-    helper.hail_text(e, 'Ocean of Tears', {zone=93, x=2747, y=3451, z=-159.8, heading=142});
+    helper.hail_text(e, 'Ocean of Tears', {zone=69, x=7630, y=-2057, z=-1, heading=142});
 end
