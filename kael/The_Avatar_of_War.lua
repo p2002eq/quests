e1c2cd5a6ec7802f31f5ca55a36440dc01b351e8
Aaron <aalcoser85@gmail.com>
2016-05-12 21:49:04 -0400

    Changed timer to 1 hour

--- kael/The_Avatar_of_War.lua
+++ kael/The_Avatar_of_War.lua
@@ -3,8 +3,8 @@ local inCombat = false;
 
 function event_spawn(e)
 	e.self:Shout("Who dares defile my temple?! Come forth and face me!");
-	--eq.set_timer("AOW",3600000);   -- 1 hour
-	eq.set_timer("AOW",300000);   -- 1 minute for testing.
+	eq.set_timer("AOW",3600000);   -- 1 hour
+	--eq.set_timer("AOW",300000);   -- 1 minute for testing.
 end
 
 function event_combat(e)
