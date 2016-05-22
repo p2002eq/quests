28f944b1ed96aa70efbb92573715709863815642
Eric <erog84@gmail.com>
2016-05-22 14:07:11 -0400

    Dispawn Idol when Statue Spawns.

--- kael/The_Statue_of_Rallos_Zek.lua
+++ kael/The_Statue_of_Rallos_Zek.lua
@@ -3,3 +3,7 @@ function event_death_complete(e)
     e.self:Shout("Protect the Idol of Zek!");
 	eq.spawn2(113629,0,0,1292,1267,-35,254.5); -- Idol of Rallos Zek
 end
+
+function event_spawn(e)
+    eq.depop_all(113629);
+end
