df53309a783f385b1799eb3e83b15b8ac38d8076
Eric <erog84@gmail.com>
2016-05-22 16:20:49 -0400

    Update

--- qey2hh1/Maligar.lua
+++ qey2hh1/Maligar.lua
@@ -6,7 +6,7 @@ function event_trade(e)
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 20368})) then
 		e.self:Say("Hmmm. What's this? A note from that silly bard Baenar? I wasn't aware that those of his kind could write, much less read.' Maligar lets out a deep laugh. 'I see you do not share my sense of humor. Let's read it, shall we? Oh, no! She's dead? I knew that already, you fool. It was by my hand she died! Ooops! Did I let that slip out? Silly me. I guess I will have to kill you now!");
 		eq.spawn2(12173,0,0,-10909,1260,209,136):AddToHateList(e.other,1);
-		eq.signal(12173,e.other:GetName());
+		eq.signal(12173,e.other:GetName(),500);
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
