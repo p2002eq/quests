f6e33b81362ec20337908497e7736a27de66ea15
Eric <erog84@gmail.com>
2016-05-22 13:35:50 -0400

    Ring of Slime Start mob.

---
+++ cazicthule/a_swirling_ooze.lua
@@ -0,0 +1,6 @@
+function event_death_complete(e)
+    e.self:Say(.. e.self:GetNPCTypeID() ..);
+    if (e.self:GetNPCTypeID() == 48302) then
+        eq.spawn2(48320,28,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+    end
+end
