30ae66f70247e1876b358e676d4f720e859ab853
Eric <erog84@gmail.com>
2016-05-22 21:37:29 -0400

    Test

--- cazicthule/a_swirling_ooze.lua
+++ cazicthule/a_swirling_ooze.lua
@@ -1,6 +1,7 @@
 function event_death_complete(e)
-    e.self:Say(.. e.self:GetNPCTypeID() ..);
+    e.self:Say("test death");
     if (e.self:GetNPCTypeID() == 48302) then
+        e.self:Say("right npc ");
         eq.spawn2(48320,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
     end
 end
