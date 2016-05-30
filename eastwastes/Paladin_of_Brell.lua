45e956c4ca587b2f9105321ae763a9a64605828f
Eric <erog84@gmail.com>
2016-05-30 15:04:39 -0400

    Update Ring 8

--- eastwastes/Paladin_of_Brell.lua
+++ eastwastes/Paladin_of_Brell.lua
@@ -1,22 +1,19 @@
-local paladin = 0;
+local paladins = nil;
+
+function event_spawn(e)
+	eq.pause(2);
+	paladins = 0;
+end
 
 function event_signal(e)
-    e.self:Say("signal sent");
     if (e.signal == 1) then
-        paladin = paladin + 1;
-        e.self:Say("inside signal");
-        if(paladin == 1) then
-            e.self:Say("signal 1");
-            e.self:AssignWaypoints(278);
-        elseif(paladin == 2) then
-            e.self:AssignWaypoints(279);
-            e.self:Say("signal 2");
-        elseif(paladin == 3) then
-            e.self:AssignWaypoints(280);
-            e.self:Say("signal 3");
-        elseif(paladin == 4) then
-            e.self:AssignWaypoints(281);
-            e.self:Say("signal 4");
-        end
+		paladins = paladins + 1;
+		e.self:AssignWaypoints(277+paladins);
     end
+end
+
+function event_waypoint_depart(e)
+	if (e.wp == 1) then
+		e.self:SetRunning(true);
+	end
 end
