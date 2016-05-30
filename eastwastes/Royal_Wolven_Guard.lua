c03abb1270c51c51afadbd273479e9b817d937da
Eric <erog84@gmail.com>
2016-05-30 15:05:40 -0400

    update ring 8

--- eastwastes/Royal_Wolven_Guard.lua
+++ eastwastes/Royal_Wolven_Guard.lua
@@ -1,33 +1,19 @@
-local wolf = nil;
+local wolves = nil;
 
 function event_spawn(e)
 	eq.pause(2);
-	wolf = 0;
+	wolves = 0;
+end
+
+function event_signal(e)
+    if (e.signal == 1) then
+		wolves = wolves + 1;
+		e.self:AssignWaypoints(277+wolves);
+    end
 end
 
 function event_waypoint_depart(e)
-	if (wolf < 8) then
-		wolf = wolf + 1;
-		eq.pause(273);
-	elseif (wolf == 8) then
-		wolf = wolf + 1; 
-	elseif (wolf > 8) and (wolf < 16) then -- 8 < x > 16
-		wolf = wolf + 1;
-	elseif (wolf == 16) then
-		wolf = wolf + 1;
-		eq.ModifyNPCStat('runspeed', 2.5);
-	elseif (wolf > 16) and (wolf < 23) then --16 < x < 23
-		wolf = wolf + 1
-		eq.pause(155);
-		eq.ModifyNPCStat('runspeed', 2.5);
-		eq.set_timer("10", 755000); --12.5 Minutes	
+	if (e.wp == 1) then
+		e.self:SetRunning(true);
 	end
-	
-end
-
-function event_timer(e)
-	if (e.timer == "10") then
-		eq.depop_all(116563);
-	end
-
 end
