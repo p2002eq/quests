650b025ecffa93002fcca381f8537e7955b13066
Eric <erog84@gmail.com>
2016-05-30 15:04:55 -0400

    update ring 8

--- eastwastes/Priest_of_Brell.lua
+++ eastwastes/Priest_of_Brell.lua
@@ -1,33 +1,19 @@
-local priest = nil;
+local priests = nil;
 
 function event_spawn(e)
 	eq.pause(2);
-	priest = 0;
+	priests = 0;
+end
+
+function event_signal(e)
+    if (e.signal == 1) then
+		priests = priests + 1;
+		e.self:AssignWaypoints(293+priests);
+    end
 end
 
 function event_waypoint_depart(e)
-	if (priest < 8) then
-		priest = priest + 1;
-		eq.pause(273);
-	elseif (priest == 8) then
-		priest = priest + 1; 
-	elseif (priest > 8) and (priest < 16) then -- 8 < x > 16
-		priest = priest + 1;
-	elseif (priest == 16) then
-		priest = priest + 1;
-		eq.ModifyNPCStat('runspeed', 2.5);
-	elseif (priest > 16) and (priest < 23) then --16 < x < 23
-		priest = priest + 1
-		eq.pause(155);
-		eq.ModifyNPCStat('runspeed', 2.5);
-		eq.set_timer("8", 755000); --12.5 Minutes	
+	if (e.wp == 1) then
+		e.self:SetRunning(true);
 	end
-	
-end
-
-function event_timer(e)
-	if (e.timer == "8") then
-		eq.depop_all(116541);
-	end
-
 end
