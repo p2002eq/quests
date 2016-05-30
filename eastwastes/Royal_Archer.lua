ce108d9881d844b3033289e84fc98ff35053ebe1
Eric <erog84@gmail.com>
2016-05-30 15:05:10 -0400

    Update ring 8

--- eastwastes/Royal_Archer.lua
+++ eastwastes/Royal_Archer.lua
@@ -5,29 +5,15 @@ function event_spawn(e)
 	archer = 0;
 end
 
-function event_waypoint_depart(e)
-	if (archer < 8) then
+function event_signal(e)
+    if (e.signal == 1) then
 		archer = archer + 1;
-		eq.pause(273);
-	elseif (archer == 8) then
-		archer = archer + 1; 
-	elseif (archer > 8) and (archer < 16) then -- 8 < x > 16
-		archer = archer + 1;
-	elseif (archer == 16) then
-		archer = archer + 1;
-		eq.ModifyNPCStat('runspeed', 2.5);
-	elseif (archer > 16) and (archer < 23) then --16 < x < 23
-		archer = archer + 1
-		eq.pause(155);
-		eq.ModifyNPCStat('runspeed', 2.5);
-		eq.set_timer("9", 755000); --12.5 Minutes	
-	end
-	
+		e.self:AssignWaypoints(285+archer);
+    end
 end
 
-function event_timer(e)
-	if (e.timer == "9") then
-		eq.depop_all(116555);
+function event_waypoint_depart(e)
+	if (e.wp == 1) then
+		e.self:SetRunning(true);
 	end
-
 end
