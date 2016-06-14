9d3566091cd703473f35a987779aa5a2c3a53dea
Scott Davis <me@sdavis.info>
2016-06-14 16:43:33 -0400

    readded pok lua

---
+++ poknowledge/player.lua
@@ -0,0 +1,14 @@
+function event_enter_zone(e)
+    -- Halloween Event
+    --e.self:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
+end
+
+function event_level_up(e)
+	local discs = require('disciplines');
+	discs:train_discs(e, e.self:GetLevel());
+end
+
+function event_enter_zone(e)
+	local discs = require('disciplines');
+	discs:update_discs(e, e.self:GetLevel());
+end
