34a98fb87cfd4b21957de30b5b609aeb0f066ad0
Eric <erog84@gmail.com>
2016-06-26 19:39:19 -0400

    Update loc

--- butcher/Translocator_Fithop.lua
+++ butcher/Translocator_Fithop.lua
@@ -1,5 +1,5 @@
 local helper = require('translocators');
 
 function event_say(e)
-    helper.hail_text(e, 'Ocean of Tears', {zone=69, x=-9136, y=345, z=30, heading=142});
+    helper.hail_text(e, 'Ocean of Tears', {zone=69, x=-9196, y=319, z=3.2, heading=254});
 end
