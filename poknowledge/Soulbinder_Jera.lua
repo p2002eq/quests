ac689030980b6aac07a2ad8ba7623d3b11ffb90e
Eric <erog84@gmail.com>
2016-06-26 22:29:02 -0400

    Add item summons

--- poknowledge/Soulbinder_Jera.lua
+++ poknowledge/Soulbinder_Jera.lua
@@ -6,6 +6,7 @@ function event_say(e)
         e.self:Say('If you want plat just say [plat]');
         e.self:Say('If you want buffs just say [buff]');
         e.self:Say('If you want your corpses just say [corpses]');
+        e.self:Say('If you want [items], ask me more');
 	elseif(e.message:findi("bind my soul")) then
 		e.self:Say("Binding your soul. You will return here when you die.");
 		e.self:CastSpell(2049,e.other:GetID(),0,1);
@@ -24,6 +25,13 @@ function event_say(e)
 	--	e.other:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
 	elseif(e.message:findi('corpses')) then
 	    eq.summon_all_player_corpses(e.other:CharacterID(), e.other:GetX(), e.other:GetY(), e.other:GetZ(), e.other:GetHeading());
+	elseif(e.message:findi('items')) then
+	    e.self:Say("I can summon you any item your heart desires, you have only to tell me [Summon Item] along with the item number.  The Allaclone is a good resource for that");
+    elseif(e.message:findi('summon item')) then
+        local itemId = string.gsub(string.gsub(e.message,"summon item","")," ","");
+		if (itemId + 5) then
+			e.other:SummonItem(tonumber(itemId));
+		end
 	end
 end
 
