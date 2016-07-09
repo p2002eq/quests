5a27e3d8624bd2bb00e76f164d6df700856c63d7
Eric <erog84@gmail.com>
2016-07-09 14:29:30 -0400

    Warder Respawn

--- skyfire/Jennus_Lyklobar.lua
+++ skyfire/Jennus_Lyklobar.lua
@@ -15,6 +15,21 @@ function event_trade(e)
 	item_lib.return_items(e.self, e.other, e.trade)
 end
 
+function event_signal(e)
+	if (e.signal == 1) then
+		eq.set_timer("warder",1200000)
+	end
+end
+
+function event_timer(e)
+	if (e.timer == "warder") then
+		eq.stop_timer("warder");
+		local mob = eq.get_entity_list():GetMobByNpcTypeID(91037);
+		if(mob) then		-- check to see if tracker is up so we know to despawn him or not.
+			eq.spawn2(91037, 0, 0, -3225, 3011, -150.81, 67.3);
+		end
+	end
+end
 -------------------------------------------------------------------------------------------------
 -- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
 -- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
