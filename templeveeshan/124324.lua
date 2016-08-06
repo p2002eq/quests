57d0083f885cb6e3655c8446f4433a8f58fc7661
Eric <erog84@gmail.com>
2016-08-06 18:54:32 +0000

    Upload new file

---
+++ templeveeshan/124324.lua
@@ -0,0 +1,18 @@
+local guardians = nil;
+
+function event_spawn(e)
+	if (guardians ~= nil) then
+		guardians = guardians + 1;
+	else
+		guardians = 1;
+	end
+end
+
+function event_death(e)
+	if (guardians < 10) then
+		eq.spawn2(124324,0,0,-672,507,121,2.0)
+		eq.spawn2(124324,0,0,-739,897,121,2.0);
+	else
+		guardians = nil;
+	end
+end	
