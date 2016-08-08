3cc8c9876b740a55c92bd361c33fd2557b68a2f3
Eric <erog84@gmail.com>
2016-08-08 01:34:34 +0000

    Update Paladin_of_Brell.lua

--- eastwastes/Paladin_of_Brell.lua
+++ eastwastes/Paladin_of_Brell.lua
@@ -9,11 +9,15 @@ function event_signal(e)
     if (e.signal == 1) then
 		paladins = paladins + 1;
 		e.self:AssignWaypoints(277+paladins);
+	elseif (e.signal == 2) then
+		paladins = paladins + 1;
+		e.self:AssignWaypoints(317+paladins);
+		e.self:SetRunning(true);
     end
 end
 
-function event_waypoint_depart(e)
+function event_waypoint_arrive(e)
 	if (e.wp == 1) then
-		e.self:SetRunning(true);
+		paladins = 0;
 	end
 end
