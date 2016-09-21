3cdcbb9021881279f377ceb700e38c4a0686ebdb
Eric <erog84@gmail.com>
2016-09-21 23:30:15 +0000

    Fix Location

--- iceclad/an_enraged_walrus.lua
+++ iceclad/an_enraged_walrus.lua
@@ -1,3 +1,3 @@
 function event_death_complete(e)
-    eq.unique_spawn(110217,0,0,2049,852,-31.80,132); --spawn the agitated snowfang
+    eq.unique_spawn(110217,0,0,2032,882,-31.80,132); --spawn the agitated snowfang
 end
