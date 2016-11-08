41db289984487a2695bea4f6772b37d5f78a1803
Gary <gforce215@yahoo.com>
2016-11-08 23:27:53 +0000

    Eric debug script.lua

--- templeveeshan/124326.lua
+++ templeveeshan/124326.lua
@@ -1,17 +1,16 @@
 local guardians = nil;
-
 function event_spawn(e)
-	if (guardians ~= nil) then
-		guardians = guardians + 1;
-	else
-		guardians = 1;
-	end
+    if (guardians ~= nil) then
+        guardians = guardians + 1;
+		e.self:Shout(tostring(guardians));
+    else
+        guardians = 1;
+    end
 end
-
 function event_death(e)
-	if (guardians < 10) then
-		eq.spawn2(124317,0,0,-810,518,121,2.0)
-		eq.spawn2(124317,0,0,-739,671,121,2.0);
-		eq.spawn2(124317,0,0,-672,507,121,2.0);
-	end
-end	
+    if (guardians < 10) then
+        eq.spawn2(124317,0,0,-810,518,121,2.0)
+        eq.spawn2(124317,0,0,-739,671,121,2.0);
+        eq.spawn2(124317,0,0,-672,507,121,2.0);
+    end
+end
