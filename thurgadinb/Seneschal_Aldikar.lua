0d22945e96750437bfa041a432cfd3f23a83c903
Eric <erog84@gmail.com>
2016-11-11 21:51:09 +0000

    Update Seneschal_Aldikar.lua

--- thurgadinb/Seneschal_Aldikar.lua
+++ thurgadinb/Seneschal_Aldikar.lua
@@ -1,6 +1,6 @@
 function event_say(e)
 	local qglobals = eq.get_qglobals(e.self, e.other);
-	if(e.other:GetFaction(e.self) <= 6) then --Amiable or lower
+	if(e.other:GetFaction(e.self) <= 4) then --Amiable or lower
 		if(e.message:findi("hail")) then	
 			e.self:Say("And a good day to you, "..e.other:Race()..". I must say that i'm impressed that your people were even able to make it to our continent, let alone survive the frigid temperatures of it. I am Seneschal Aldikar, Commander of the Armies of Thurgadin, known by some as the Sword of the Dain. My armies are currently undergoing intensive training and are ready for a full assault on Kael Drakkel as soon as his majesty gives the order.");
 		elseif(e.message:findi("accept this task") and (qglobals["ring9"] == "1")) then	
@@ -18,6 +18,7 @@ function event_trade(e)
 		e.self:Say("Well done " .. e.other:GetName() .. ", I have heard of your victory over the Ry'Gorr. If you are willing to assist the crown further please follow me.");
 		eq.set_timer("1",10000);
 		eq.set_global("ring9","1",5,"F"); 
+		e.other:SummonItem(30164);
 	end
 	item_lib.return_items(e.self, e.other, e.trade);
 end
