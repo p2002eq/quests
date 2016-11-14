867e1c306f9e9e5d33c407153a7116d03ace040e
Gary <gforce215@yahoo.com>
2016-11-14 02:54:52 +0000

    fixed faction amount.lua

--- qey2hh1/Renux_Herkanor.lua
+++ qey2hh1/Renux_Herkanor.lua
@@ -2,7 +2,7 @@
 -- NPC Name: Renux Herkanor
 
 function event_say(e)
-	if(e.other:GetFaction(e.self) < 3 and e.other:GetSkill(56) > 277) then
+	if(e.other:GetFaction(e.self) < 3 and e.other:GetSkill(56) > 252) then
 		if(e.message:findi("hail")) then
 			e.self:Say("Master Hanns is going to kill me! I let that dark assassin look at his prized poison collection and he lifted it all when I wasn't looking! He even snatched his Grandmaster's Vial! Master Hanns was so furious he grabbed one of his old blades from his collection and jabbed it at the wall so hard it shattered! I really need some help replacing all that was [ruined] or I have a feeling Master Hanns is going to send me on a suicide mission!");
 		elseif(e.message:findi("ruined")) then
@@ -17,7 +17,7 @@ end
 function event_trade(e)
 	local item_lib = require("items");
 
-	if(e.other:GetFaction(e.self) < 3 and e.other:GetSkill(56) > 277) then
+	if(e.other:GetFaction(e.self) < 3 and e.other:GetSkill(56) > 252) then
 		if(item_lib.check_turn_in(e.self, e.trade, {item1 = 24072})) then
 			e.self:Say("Great! Looks like everything is here! Hopefully Master Hanns won't send me to Steamfront now, I have a really bad feeling about that assignment. Here take this Seal, it was dropped by that dark stranger. It glows with a dark power I just don't understand. Be wary of further sightings of that dark assassin.");
 			e.other:Faction(33,5); -- Circle Of Unseen Hands
