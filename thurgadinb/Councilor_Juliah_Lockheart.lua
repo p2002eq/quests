7ccc74cbf28874827acda2303c935539395fd70f
Eric <erog84@gmail.com>
2016-12-01 06:48:04 +0000

    Fix depop

--- thurgadinb/Councilor_Juliah_Lockheart.lua
+++ thurgadinb/Councilor_Juliah_Lockheart.lua
@@ -7,7 +7,7 @@ end
 function event_trade(e)
 	local item_lib = require('items');
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 1464})) then
-		eq.depop();
+		eq.depop_with_timer();
 		eq.spawn2(129102, 0, 0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading());
 	end
 	item_lib.return_items(e.self, e.other, e.trade)			
