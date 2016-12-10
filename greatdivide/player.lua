26902ffe0556371cde3d6a0e2305640b54aa8921
Vlad <vpresnyak@gmail.com>
2016-12-09 19:19:44 -0500

    Adding back disc code

--- greatdivide/player.lua
+++ greatdivide/player.lua
@@ -1,3 +1,14 @@
+function event_enter_zone(e)
+	eq.set_timer("spires",10000);
+	local discs = require('disciplines');
+	discs:update_discs(e, e.self:GetLevel());
+end
+
+function event_level_up(e)
+	local discs = require('disciplines');
+	discs:train_discs(e, e.self:GetLevel());
+end
+
 function event_say(e)
 	elseif e.self:Admin() > 100 then
 		if(e.message:findi("stage1")) then
