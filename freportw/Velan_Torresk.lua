6986b68cc60070729347c711e57902875aceaf5b
Vlad <vpresnyak@gmail.com>
2016-12-18 02:18:22 -0500

    Updating faction hits of sashes to match reports from alla. Added better faction handling in dialog. Lowered faction requirement for white sash.

--- freportw/Velan_Torresk.lua
+++ freportw/Velan_Torresk.lua
@@ -2,7 +2,7 @@ function event_say(e)
 	local fac = e.other:GetFaction(e.self);
 
 	if(e.message:findi("white training sash")) then
-		if(e.other:GetFaction(e.self) < 4) then
+		if(e.other:GetFaction(e.self) < 5) then
 			e.self:Say("The white training sash of the Ashen Order is awarded to our new members for completing a few simple tasks to prove their devotion to our clan. As you know, Freeport is a very hostile place, under constant attack from [orcs], wild beasts, and even the undead. To help keep this city and its citizens relatively safe, we must help the Militia protect the main gates. Bring me two Deathfist pawn scalps from those vile [Deathfist orcs], a snake fang, and some bone chips from a skeleton. Good luck, " .. e.other:GetName() .. ", represent us well!");
 		else
 			e.self:Say("I have been watching you, and appreciate the help you've given to the brothers and sisters of the Ashen Order, but I feel that such a vital matter should be left to one of our more trusted members.");
@@ -27,25 +27,23 @@ end
 function event_trade(e)
 	local item_lib = require("items");
 
-	-- Kindly Faction - Deathfist Pawn Scalp - Snake Fang - Bone Chips
-	if(e.other:GetFaction(e.self) < 4) then
-		if(item_lib.check_turn_in(e.self, e.trade, {item1 = 13794,item2 = 13794,item3 = 13067,item4 = 13073})) then
-			e.self:Say("Good work, " .. e.other:GetName() .. ", you've worked hard and proven yourself a valuable addition to the Ashen Order. Here's your white sash, wear it with pride.");
-			e.other:Faction(12,2,0); -- Ashen Order
-			e.other:Faction(184,2,0); -- Knights of Truth
-			e.other:Faction(300,2,0); -- Silent Fist Clan
-			e.other:QuestReward(e.self,0,0,0,0,10130,500); -- White Training Sash
-		end
-	end
+	-- Amiable Faction - Deathfist Pawn Scalp x 2 - Snake Fang - Bone Chips
+	if e.other:GetFaction(e.self) < 5 and item_lib.check_turn_in(e.self, e.trade, {item1 = 13794,item2 = 13794,item3 = 13067,item4 = 13073}) then
+		e.self:Say("Good work, " .. e.other:GetName() .. ", you've worked hard and proven yourself a valuable addition to the Ashen Order. Here's your white sash, wear it with pride.");
+		e.other:Faction(12,25,0); -- Ashen Order
+		e.other:Faction(184,3,0); -- Knights of Truth
+		e.other:Faction(300,1,0); -- Silent Fist Clan
+		e.other:QuestReward(e.self,0,0,0,0,10130,500); -- White Training Sash
+	
 	-- White Training Sash - Giant Snake Rattle - Deathfist Slashed Belt - Desert Tarantula Chitin
-	-- No need to check faction again since they must have the White Training Sash
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 10130,item2 = 13058,item3 = 13916,item4 = 20901})) then
+	elseif e.other:GetFaction(e.self) < 4 and item_lib.check_turn_in(e.self, e.trade, {item1 = 10130,item2 = 13058,item3 = 13916,item4 = 20901})  then
 		e.self:Say("Ah, well done, " .. e.other:GetName() .. ". You have proven that you are a very skillful fighter and it is an honor to have you as a member of the Ashen Order. On behalf of Master Closk, and under the watchful eyes of Quellious, I present you, " .. e.other:GetName() .. ", with this, the yellow Sash of Order. Go out and make us proud.");
-		e.other:Faction(12,5,0); -- Ashen Order
-		e.other:Faction(184,5,0); -- Knights of Truth
-		e.other:Faction(300,5,0); -- Silent Fist Clan
+		e.other:Faction(12,50,0); -- Ashen Order
+		e.other:Faction(184,7,0); -- Knights of Truth
+		e.other:Faction(300,2,0); -- Silent Fist Clan
 		e.other:QuestReward(e.self,0,0,0,0,10131,750); -- Yellow Sash of Order
 	end
+	
 	item_lib.return_items(e.self, e.other, e.trade);
 end
 
