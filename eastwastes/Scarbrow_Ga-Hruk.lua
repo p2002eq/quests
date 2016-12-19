2c290fe1fdba11322d23cfdfa388bc5af3969a7e
Vlad <vpresnyak@gmail.com>
2016-12-19 02:06:50 -0500

    Rework of ring 5 encounters. Reset should be more robust and not dependent on lookout dying and fragile encounter unload.

--- eastwastes/Scarbrow_Ga-Hruk.lua
+++ eastwastes/Scarbrow_Ga-Hruk.lua
@@ -1,9 +1,24 @@
 -- Part of Coldain Ring 5 Quest
 
 function event_spawn(e)
+	eq.set_timer("depop", 600000);
 	e.self:Shout("For the glory of the Ry`Gorr, charge!!");
+	e.self:SetRunning(true);
+	eq.move_to(-372, -2655, 178, 45, true);
 end
 
 function event_aggro(e)
 	e.self:Say("Crush, maim, kill, and defy! For dinner tonight, your brains we shall fry!");
+end
+
+
+function event_timer(e)
+	if e.timer == "depop" then
+		eq.signal(116177, 99);
+		eq.depop();
+	end
+end
+
+function event_death_complete(e)
+	eq.signal(116177, 99);
 end
--- eastwastes/a_coldain_lookout.lua
+++ eastwastes/a_coldain_lookout.lua
@@ -1,27 +1,50 @@
 -- Part of Coldain Ring 5
+local started;
+
+function event_spawn(e)
+	started = false;
+end
+
 function event_trade(e)
 
 	local item_lib = require('items');
 
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 30136})) then
-
+	if not started and item_lib.check_turn_in(e.self, e.trade, {item1 = 30136}) then
+		started = true;
 		e.self:Shout("Aahhh.. You're too late! They're upon us! To arms, men! Death to Scarbrow!");
-		eq.load_encounter("RingFive");
-		eq.set_timer("depop", 600000);
-		e.other:Faction(49, 30);
-		e.other:Faction(67, 30);
-		e.other:Faction(188, -30);
-		e.other:Faction(189, -30);	
+		spawn_event();
+		
+		e.other:Faction(49, 25);
+		e.other:Faction(67, 6);
+		e.other:Faction(188, -12);
+		e.other:Faction(189, -2);	
 	end
 
 end
 
-function event_timer(e)
-	eq.stop_timer("depop");
-	eq.depop_all(116586);
-	eq.depop_all(116587);
-	eq.depop(116600);
-	eq.unload_encounter("RingFive");
+function event_signal(e)
+	if e.signal == 99 then
+		started = false;
+	end
+end
+
+function spawn_event()
+	local spawnOracles = math.random(2, 3);
+	local spawnInvaders = math.random(2, 3);
 	
-	eq.depop_with_timer();
+	--locs made up
+	eq.spawn2(116600, 0, 0, -650, -2744, 180, 45); --Scarbrow
+
+	eq.spawn2(116586, 0, 0, -654, -2723, 180, 45); --Invader
+	eq.spawn2(116586, 0, 0, -643, -2768, 180, 45); --Invader
+	if (spawnInvaders == 3) then
+		eq.spawn2(116586, 0, 0, -658, -2760, 175, 45); --Invader
+	end
+
+	eq.spawn2(116587, 0, 0, -681, -2739, 180, 45); --Oracle
+	eq.spawn2(116587, 0, 0, -673, -2766, 180, 45); --Oracle
+	if (spawnOracles == 3) then
+		eq.spawn2(116587, 0, 0, -665, -2734, 180, 45); --Oracle
+	end
+
 end
