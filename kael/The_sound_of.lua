039065915e13f8c3cd24a6f9983f03f139502ce2
Vlad <vpresnyak@gmail.com>
2017-01-02 22:27:35 -0500

    Adding depop timer for Doldigun in case nobody hands him anything

--- kael/The_sound_of.lua
+++
@@ -1,9 +0,0 @@
-function event_spawn(e)
-	eq.set_timer("pass", 240000);
-end
-
-function event_timer(e)
-	if (e.timer == "pass") then
-		e.self:Emote("Who dares defile my temple?! Come forth and face me!");
-	end
-end
