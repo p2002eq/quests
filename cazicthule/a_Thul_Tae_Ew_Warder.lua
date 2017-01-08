9525fd40e24f8c4bc5bb7b0b184f67e918a8a6cd
Vlad <vpresnyak@gmail.com>
2017-01-08 17:21:04 -0500

    Increasing banish range slightly

--- cazicthule/a_Thul_Tae_Ew_Warder.lua
+++ cazicthule/a_Thul_Tae_Ew_Warder.lua
@@ -1,5 +1,5 @@
 function event_spawn(e)
-	eq.set_proximity(e.self:GetX()-20,e.self:GetX()+20,e.self:GetY()-20,e.self:GetY()+20);
+	eq.set_proximity(e.self:GetX()-40,e.self:GetX()+40,e.self:GetY()-40,e.self:GetY()+40);
 end
 
 function event_enter(e)
