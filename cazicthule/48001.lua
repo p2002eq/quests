c375561de62a6649910e29a9b2e0a8d7ac6fee25
Vlad <vpresnyak@gmail.com>
2017-01-08 15:28:30 -0500

    continued testing

--- cazicthule/48001.lua
+++ cazicthule/48001.lua
@@ -1,8 +1,9 @@
 -- a tae ew Justicar in Cazicthule
 
 function event_signal(e)
+	e.self:Shout("ZLOC IS " .. e.signal);
 	if e.signal == 99 then
-		e.self:Shout("ZLOC IS " .. e.self:GetZ);
+		e.self:Shout("ZLOC IS " .. e.self:GetZ());
 		if e.self:GetZ() > -100 and e.self:GetZ() < -90 then
 			e.self:SpellFinished(2127, self);
 			e.self:Depop();
