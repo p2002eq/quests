e102e5d64849bcbe5ed9e6fa14c52782aaf7f965
Vlad <vpresnyak@gmail.com>
2017-01-08 15:06:11 -0500

    Tweaks to make ring of fear scripts less fragile

--- cazicthule/#a_Thul_Tae_Ew_High_Priest.lua
+++ cazicthule/#a_Thul_Tae_Ew_High_Priest.lua
@@ -2,23 +2,17 @@
 
 function event_spawn(e)
 	e.self:ModifyNPCStat("runspeed","0");
-	eq.unique_spawn(48057, 0, 0, 587, 1090, -98, 0):SetAppearance(3);
-	justicars = {};
-	table.insert(justicars, eq.spawn2(48001, 0, 0, 587, 1055, -95, 0):GetID());
-	table.insert(justicars, eq.spawn2(48001, 0, 0, 587, 1128, -95, 128):GetID());
-	table.insert(justicars, eq.spawn2(48001, 0, 0, 550, 1090, -95, 64):GetID());
-	table.insert(justicars, eq.spawn2(48001, 0, 0, 625, 1090, -95, 192):GetID());
-
-	for _, v in ipairs(justicars) do
-		eq.get_entity_list():GetMob(v):SetBodyType(11, true);
-	end
+	eq.unique_spawn(48057, 0, 0, 587, 1090, -98, 0):SetAppearance(3); -- Tahia
+	eq.spawn2(48001, 0, 0, 587, 1055, -95, 0):SetBodyType(11, true); -- Justicars
+	eq.spawn2(48001, 0, 0, 587, 1128, -95, 128):SetBodyType(11, true);
+	eq.spawn2(48001, 0, 0, 550, 1090, -95, 64):SetBodyType(11, true);
+	eq.spawn2(48001, 0, 0, 625, 1090, -95, 192):SetBodyType(11, true);
 end
 
-function event_hate_list(e)
-	if e.joined then
-		eq.get_entity_list():GetMobByNpcTypeID(48378):AddToHateList(e.other, 1);
-		eq.get_entity_list():GetMobByNpcTypeID(48379):AddToHateList(e.other, 1);
-		eq.get_entity_list():GetMobByNpcTypeID(48380):AddToHateList(e.other, 1);
+function event_combat(e)
+	if(e.joined == true) then
+		e.self:Shout("Fools! It isss dangerouss to interrupt the sssacrifice! Guards, assssissst me!");
+		eq.set_timer("guards", 1000);
 	end
 end
 
@@ -40,21 +34,17 @@ function event_timer(e)
 	end
 end
 
-function event_combat(e)
-	if(e.joined == true) then
-		e.self:Shout("Fools! It isss dangerouss to interrupt the sssacrifice! Guards, assssissst me!");
-		eq.set_timer("guards", 1000);
+function event_hate_list(e)
+	if e.joined then
+		eq.get_entity_list():GetMobByNpcTypeID(48378):AddToHateList(e.other, 1);
+		eq.get_entity_list():GetMobByNpcTypeID(48379):AddToHateList(e.other, 1);
+		eq.get_entity_list():GetMobByNpcTypeID(48380):AddToHateList(e.other, 1);
 	end
 end
 
 function event_death_complete(e)
 	e.self:Shout("Oh facelesss one, I have fallen! Pleasse avenge the death of your faithful ssservant!");
-	eq.get_entity_list():GetMobByNpcTypeID(48057):Depop();
-	local ents = eq.get_entity_list();
-	for _, v in ipairs(justicars) do
-		local self = ents:GetMob(v);
-		self:SpellFinished(2127, self);
-		self:Depop();
-	end
-	eq.unique_spawn(48388, 0, 0, 587, 1090, -98, 0);
+	eq.signal(48001, 99); -- blow up Justicars
+	eq.get_entity_list():GetMobByNpcTypeID(48057):Depop(); -- depop laying Tahia
+	eq.unique_spawn(48388, 0, 0, 587, 1090, -98, 0); -- spawn standing Tahia
 end
---
+++ cazicthule/48001.lua
@@ -0,0 +1,10 @@
+-- a tae ew Justicar in Cazicthule
+
+function event_signal(e)
+	if e.signal == 99 then
+		if e.self:GetZ() > -100 and e.self:GetZ() < -90 then
+			e.self:SpellFinished(2127, self);
+			e.self:Depop();
+		end
+	end
+end
