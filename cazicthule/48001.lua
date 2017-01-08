c6234e095d462d73ac0f7157b1b15ac15d3ac180
Vlad <vpresnyak@gmail.com>
2017-01-08 15:23:20 -0500

    rofear testing

--- cazicthule/48001.lua
+++ cazicthule/48001.lua
@@ -2,6 +2,7 @@
 
 function event_signal(e)
 	if e.signal == 99 then
+		e.self:Shout("ZLOC IS " .. e.self:GetZ);
 		if e.self:GetZ() > -100 and e.self:GetZ() < -90 then
 			e.self:SpellFinished(2127, self);
 			e.self:Depop();
