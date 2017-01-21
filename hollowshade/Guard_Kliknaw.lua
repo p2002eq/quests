b7e18e3277f538894604a148be99480b00240586
Trust <fryguy503@gmail.com>
2017-01-20 22:15:04 -0500

    SharVahl - Khati Sha Apprentice (Beastlord #3)

--- hollowshade/Guard_Kliknaw.lua
+++ hollowshade/Guard_Kliknaw.lua
@@ -1,3 +1,7 @@
+----
+---- Need Event Logic for Hollowshade War
+---- See Perl for example design
+----
 function event_trade(e)
     local item_lib = require("items");
     if(item_lib.check_turn_in(e.self, e.trade, {item1 = 6220})) then -- Daily Status Report (2)
