1473514c973607d940dd0a911e794bc9a962948b
Vlad <vpresnyak@gmail.com>
2017-01-20 23:00:16 -0500

    updating posky horses to correctly spawn KoS

--- airplane/an_essence_harvester.lua
+++ airplane/an_essence_harvester.lua
@@ -1,4 +1,7 @@
 function event_death_complete(e)
+	-- send a signal to the Key_Master
+	eq.signal(71056,2);
+	-- on death of an essence harvester, two a soul harvester spawn
 	eq.spawn2(71110,0,0,e.self:GetX()+10,e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1);
 	eq.spawn2(71110,0,0,e.self:GetX()-10,e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1);
 end
