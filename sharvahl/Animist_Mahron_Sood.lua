28a2e6b9c30379fb3107fd1c37c04709fb90b3da
Trust <fryguy503@gmail.com>
2017-01-22 19:36:18 -0500

    SharVahl - Feed the Hopper

--- sharvahl/Animist_Mahron_Sood.lua
+++ sharvahl/Animist_Mahron_Sood.lua
@@ -1,4 +1,4 @@
---Feed the Hopper Quest
+-- Feed the Hopper Quest
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say("Hail mate. Did you need something from me or are you looking to do me a [favor]?");
@@ -9,12 +9,16 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 3918, item2 = 3918, item3 = 3918, item4 = 3918})) then --Finger bones x 4
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 3918, item2 = 3918, item3 = 3918, item4 = 3918})) then -- 4x Grimling Finger Bones
 		e.self:Say("Thanks, here is your reward!");
 		e.other:QuestReward(e.self,0,0,0,0,eq.ChooseRandom(7079,7074,7071,7078,7075,7072,7081,7073,7080,7076,7082,7077),1000);
-	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 3812, item2 = 3812, item3 = 3812, item4 = 3812})) then --Grimling toes x 4
+		-- Random -- Hopperhide Wristbands, Hopperhide Tunic, Hopperhide Skullcap, Hopperhide Sleeves, Hopperhide Shoulderpads, Hopperhide Mask
+		-- Random -- Hopperhide Leggings, Hopperhide Gorget, Hopperhide Gloves, Hopperhide Cloak, Hopperhide Boots, Hopperhide Belt
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 3812, item2 = 3812, item3 = 3812, item4 = 3812})) then -- 4x Grimling Toes
 		e.self:Say("Thanks, here is your reward!");
 		e.other:QuestReward(e.self,0,0,0,0,eq.ChooseRandom(7079,7074,7071,7078,7075,7072,7081,7073,7080,7076,7082,7077),1000);
+		-- Random -- Hopperhide Wristbands, Hopperhide Tunic, Hopperhide Skullcap, Hopperhide Sleeves, Hopperhide Shoulderpads, Hopperhide Mask
+		-- Random -- Hopperhide Leggings, Hopperhide Gorget, Hopperhide Gloves, Hopperhide Cloak, Hopperhide Boots, Hopperhide Belt
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 3670})) then -- Seal of Animist Poren - Khati Sha Apprentice (Beastlord #3)
 		e.self:Say("I can tell by the manner in which you approached me that Poren did not explain what I would require of you, little friend. Your development as a Khati Sha will be demanding. Our expectations of you should only be exceeded by your own. Our namesake, Khati Sha, was an unrivalled hero to our people. The story of his life is an example that you will be expected to learn. You will be expected to research and explore the path that he took and strive to understand what that path means for the future of our people. Take this map to Kery Miann. She will start you on the path");
 		e.other:QuestReward(e.self,0,0,0,0,5571); -- Worn Map of the Area
