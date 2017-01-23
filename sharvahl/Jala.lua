e8ce4befac647cdfdecbe8fc39f97844991caaba
Trust <fryguy503@gmail.com>
2017-01-22 20:13:15 -0500

    SharVahl - Silk Collection

--- sharvahl/Jala.lua
+++ sharvahl/Jala.lua
@@ -9,14 +9,15 @@ function event_say(e)
 end
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 2789, item2 = 2789, item3 = 2789, item4 = 2789})) then
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 2789, item2 = 2789, item3 = 2789, item4 = 2789})) then -- 4x Shadeling Silk
 		e.self:Say("Woohoo! Thank you so much " .. e.other:GetName() .. "! Hey, maybe you would like to try your hand at weaving as well. If you combine the silks in that loom over there, you can make some decent thread to work with. Make four of them and bring them to me. I will judge your craftsmanship.");
-		e.other:Faction(132,5);
-		e.other:QuestReward(e.self,0,0,0,0,0,1000);
+		e.other:Faction(132,5); -- Guardians of Shar Vahl
+		e.other:QuestReward(e.self,0,0,0,0,0,1000); -- Just EXP
 	end
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 2791, item2 = 2791, item3 = 2791, item4 = 2791})) then
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 2791, item2 = 2791, item3 = 2791, item4 = 2791})) then -- 4x Shade Silk Thread
 		e.self:Say("Not bad at all! You definitely have some hidden talent there " .. e.other:GetName() .. ". Keep at it and you may just become a master tailor yourself. Here, take this with you. It will help protect you from the nasty bite of the Xakra worms.");
 		e.other:QuestReward(e.self,0,0,0,0,eq.ChooseRandom(1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012),1000);
+		-- Random -- Cloth Cap, Cloth Veil, Cloth Choker, Cloth Shirt, Cloth Shawl, Cloth Cape, Cloth Cord, Cloth Sleeves, Cloth Wristband, Cloth Gloves, Cloth Pants, Cloth Sandals
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
