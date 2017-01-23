f53808a0a4a8771530a5d2c5a40d02f7c38bcbdf
Trust <fryguy503@gmail.com>
2017-01-22 20:03:17 -0500

    SharVahl - Luclin Smithing

--- sharvahl/Khala_Dun_Koby.lua
+++ sharvahl/Khala_Dun_Koby.lua
@@ -8,14 +8,13 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	-- takes repair order from master Barkhem
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 29803})) then
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 29803})) then -- A Shield Repair Order
 		e.self:Say("Oh this old shield does fine, but I guess if Barkhem needs to break in a new apprentice, it does have some dings in it. Thank you, my friend.");
-		e.other:QuestReward(e.self,0,0,0,0,29804);
+		e.other:QuestReward(e.self,0,0,0,0,29804); -- Kobys Dented Shield
 	end
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 29805})) then -- koby immaculate shield
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 29805})) then -- Kobys Immaculate Shield
 		e.self:Say("Tip-top work! Barkhem has done well by himself choosing you for an apprentice.");
-		e.other:QuestReward(e.self,0,0,0,0,29806);
+		e.other:QuestReward(e.self,0,0,0,0,29806); -- Kobys work order
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
