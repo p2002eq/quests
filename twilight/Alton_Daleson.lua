d345abe0b274c29ca3eed737acc03165a7dbf36d
Trust <fryguy503@gmail.com>
2017-01-25 19:03:41 -0500

    Twilight Armor - Ranger Cleanup
    Cleaned up Alton and Ellen Daelson

--- twilight/Alton_Daleson.lua
+++ twilight/Alton_Daleson.lua
@@ -1,60 +1,58 @@
+-- Twilight Armor - Ranger
 function event_say(e)
 	if(e.message:findi("Hail")) then
-		e.self:Say("Welcome to Jern's Rest!  I'm the owner and proprietor of this inn. its been in my family for generations.  My great grandaddy. Jern. built it back when Katta Castellum was first being built.  If ya be wantin' of anything don't hesitate to ask.'");
-	end
-	if(e.message:findi("armor")) then
-		e.self:Say("His armor has also been in the family for generations, if ye be a [ranger] I don't have much use for it running this inn. I'll just need something in return for it.");
-	end
-	if(e.message:findi("ranger")) then
-		e.self:Say("Excellent! If you are interested I have the Woodsman's [Helm], Woodsmand's [Mail], Woodsman's [Vambraces], Woodsman's [Greaves], Woodsman's [Bracer], Woodsman's [Boots], and the Woodsman's [Axe]. If you are interested in the rest of his armor you'll have to ask my wife Ellen, she'll have some errands you can run for her.");
-	end
-	if(e.message:findi("axe")) then
-		e.self:Say("If you want the Woodsman's Axe you will have to aquire these items: a sun jewel, a mark of the naturalist, some ancient pottery, and a fossilized skull.");
-	end
-	if(e.message:findi("boots")) then
-		e.self:Say("If you want the Woodsman's Boots you will have to acquire these items: a star jewel, a mark of timber, and a gilded steel rod.");
-	end
-	if(e.message:findi("bracer")) then
-		e.self:Say("If you want the Woodsman's Bracer you will have to acquire these items: a moon jewel, a mark of lakes, and some lightning dust.");
-	end
-	if(e.message:findi("greaves")) then
-		e.self:Say("If you want the Woodsman's Greaves you will have to acquire these items: an astral jewel, a mark of oceans, a pouch of fire stones, and a cat skin pouch.");
-	end
-	if(e.message:findi("helm")) then
-		e.self:Say("For the Woodsman's Helm you will have to acquire these items: a cloud jewel, a mark of leaves, a frost covered leaf, and some sea grass.");
-	end
-	if(e.message:findi("mail")) then
-		e.self:Say("To get the Woodsman's Mail you will have to acquire these items: a sky jewel, a mark of forests, a frozen vial, and a tro jeg brain.");
-	end
-	if(e.message:findi("vambraces")) then
-		e.self:Say("If you want the Woodsman's Vambraces you will have to acquire these items: a meteor jewel, a mark of mountains, a tro jeg eye, and a solstice rune.");
+		e.self:Say("Welcome to Jern's Rest! I'm the owner and proprietor of this inn, its been in my family for generations. My great grandaddy, Jern, built it back when Katta Castellum was first being built. If ya be wantin' of anything dont hesitate to ask.");
+	elseif(e.message:findi("armor")) then
+		e.self:Say("His armor has also been in the family for generations, if ye be a [ranger] I dont have much use for it running this inn. I'll just need something in return for it.");
+	elseif(e.message:findi("ranger")) then
+		e.self:Say("Excellent! If you are interested I have the Woodsman's [Helm], Woodsman's [Mail], Woodsman's [Vambraces], Woodsman's [Greaves], Woodsman's [Bracer], Woodsman's [Boots], and the Woodsman's [Axe]. If you are interested in the rest of his armor youll have to ask my wife Ellen, shell have some errands you can run for her.");
+	elseif(e.message:findi("axe")) then
+		e.self:Say("If you want the Woodsman's Axe you will have to acquire these items a sun jewel, a mark of the naturalist, some ancient pottery, and a fossillized skull.");
+	elseif(e.message:findi("boots")) then
+		e.self:Say("If you want the Woodsman's Boots you will have to acquire these items a star jewel, a mark of timber, and a gilded steel rod.");
+	elseif(e.message:findi("bracer")) then
+		e.self:Say("If you want the Woodsman's Bracer you will have to acquire these items a moon jewel, a mark of lakes, and some lightning dust.");
+	elseif(e.message:findi("greaves")) then
+		e.self:Say("If you want the Woodsman's Greaves you will have to acquire these items: An astral jewel, a mark of oceans, a pouch of fire stones, and a cat skin pouch.");
+	elseif(e.message:findi("helm")) then
+		e.self:Say("For the Woodsman's Helm you will have to acquire these items: A cloud jewel, a mark of leaves, a frost covered leaf, and some sea grass.");
+	elseif(e.message:findi("mail")) then
+		e.self:Say("To get the Woodsman's Mail you will have to acquire these items a sky jewel, a mark of forests, a frozen vial, and a tro jeg brain.");
+	elseif(e.message:findi("vambraces")) then
+		e.self:Say("If you want the Woodsman's Vambraces you will have to acquire these items a meteor jewel, a mark of mountains, a tro jeg eye, and a solstice rune.");
 	end
 end
+
 function event_trade(e)
 	local item_lib = require('items');
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5852, item2 = 5853, item3 = 5851, item4 = 4488})) then
+	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5852, item2 = 5853, item3 = 5851, item4 = 4488})) then -- Ancient Pottery, Fossilized Skull, Mark of the Naturalist & Sun Jewel
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
-		e.other:AddEXP(25000);
-		e.other:SummonItem(3958);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5695, item2 = 5694, item3 = 4490})) then
-		e.other:SummonItem(3952);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5693, item2 = 5692, item3 = 4489})) then
-		e.other:SummonItem(3951);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 4494, item2 = 5689, item3 = 5687, item4 = 5688})) then
-		e.other:SummonItem(3949);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 4491, item2 = 5679, item3 = 5678, item4 = 5680})) then
-		e.other:SummonItem(3946);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5682, item2 = 5681, item3 = 4492, item4 = 5683})) then
-		e.other:SummonItem(3947);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5684, item2 = 4493, item3 = 5686, item4 = 5685})) then
-		e.other:SummonItem(3948);
+		e.other:QuestReward(e.self,0,0,0,0,3958,25000); -- Woodsmans Axe
+	elseif (item_lib.check_turn_in(e.self, e.trade, {item1 = 5695, item2 = 5694, item3 = 4490})) then -- Gilded Steel Rod, Mark of Timber & Star Jewel
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3952,25000); -- Woodsmans Boots
+	elseif (item_lib.check_turn_in(e.self, e.trade, {item1 = 5693, item2 = 5692, item3 = 4489})) then -- Lightning Dust, Mark of Lakes & Moon Jewel
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3951,25000); -- Woodsmans Bracer
+	elseif (item_lib.check_turn_in(e.self, e.trade, {item1 = 4494, item2 = 5689, item3 = 5687, item4 = 5688})) then -- Astral Jewel, Cat Skin Pouch, Mark of Oceans & Pouch of Fire Stones
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3955,25000); -- Woodsmans Gauntlets
+	elseif (item_lib.check_turn_in(e.self, e.trade, {item1 = 4491, item2 = 5679, item3 = 5678, item4 = 5680})) then -- Cloud Jewel, Frost Covered Leaf, Mark of Leaves & Sea Grass
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3946,25000); -- Woodsmans Coif
+	elseif (item_lib.check_turn_in(e.self, e.trade, {item1 = 5682, item2 = 5681, item3 = 4492, item4 = 5683})) then -- Frozen Vial, Mark of Forests, Sky Jewel & Tro Jeg Brain
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3947,25000); -- Woodsmans Mail
+	elseif (item_lib.check_turn_in(e.self, e.trade, {item1 = 5684, item2 = 4493, item3 = 5686, item4 = 5685})) then -- Mark of Mountains, Meteor Jewel, Solstice Rune & Tro Jeg Eye
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3948,25000); -- Woodsmans Vambraces
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
--- twilight/Ellen_Daleson.lua
+++ twilight/Ellen_Daleson.lua
@@ -1,54 +1,52 @@
+-- Twilight Armor - Ranger
 function event_say(e)
 	if(e.message:findi("Hail")) then
 		e.self:Say("Oh hello there, I'm in charge of the docks here and wife to Alton.");
-	end
-	if(e.message:findi("ranger")) then
+	elseif(e.message:findi("ranger")) then
 		e.self:Say("Very well. If you want the Woodsman's [Mantle], Woodsman's [Mask], Woodsman's [Cape], Woodsman's [Guantlets], Woodsman's [Choker], or Woodsman's [Belt] I have some errands that need to be done.");
-	end
-	if(e.message:findi("armor")) then
+	elseif(e.message:findi("armor")) then
 		e.self:Say("Oh you wish to get the armor that Alton's great granddaddy used to wear. If you are a ranger just let me know.");
-	end
-	if(e.message:findi("belt")) then
+	elseif(e.message:findi("belt")) then
 		e.self:Say("If you want the Woodsman's Belt bring me a moon jewel, a mark of the environment, a gold pointer, and a broken ancient blade.");
-	end
-	if(e.message:findi("cape")) then
+	elseif(e.message:findi("cape")) then
 		e.self:Say("If you want the Woodsman's Cape bring me a sky jewel, a mark of creatures, a wondrous stone, and a marble statuette.");
-	end
-	if(e.message:findi("choker")) then
+	elseif(e.message:findi("choker")) then
 		e.self:Say("If you want the Woodsman's Choker bring me an astral jewel, a mark of ambidexterity, and a small ancient sculpture.");
-	end
-	if(e.message:findi("gauntlets")) then
+	elseif(e.message:findi("gauntlets")) then
 		e.self:Say("If you want the Woodsman's Guantlets bring me a meteor jewel, a mark of animals, and a tattered old card.");
-	end
-	if(e.message:findi("mantle")) then
+	elseif(e.message:findi("mantle")) then
 		e.self:Say("If you want the Woodsman's Mantle bring me a sun jewel, a mark of rivers, and a temporal sack.");
-	end
-	if(e.message:findi("mask")) then
+	elseif(e.message:findi("mask")) then
 		e.self:Say("If you want the Woodsman's Mask bring me a cloud jewel, a mark of stone, and a platinum chain.");
 	end
 end
+
 function event_trade(e)
 	local item_lib = require('items');
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5850, item2 = 5849, item3 = 5848, item4 = 4489})) then
-		e.other:SummonItem(3957);
-		e.self:Emote("smiles at you.");
-		e.self:Say("Thank You! Here is your payment.");
-		e.other:AddEXP(25000);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5844, item2 = 5698, item3 = 4492, item4 = 5699})) then
-		e.other:SummonItem(3954);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 4494, item2 = 5981, item3 = 5847})) then
-		e.other:SummonItem(3956);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5845, item2 = 4493, item3 = 5846})) then
-		e.other:SummonItem(3955);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5690, item2 = 4488, item3 = 5691})) then
-		e.other:SummonItem(3950);
-	end
-	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 4491, item2 = 5696, item3 = 5697})) then
-		e.other:SummonItem(3953);
+	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 5850, item2 = 5849, item3 = 5848, item4 = 4489})) then -- Broken Ancient Blade, Gold Pointer, Mark of the Environment & Moon Jewel
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3957,25000); -- Woodsmans Girdle
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 5844, item2 = 5698, item3 = 4492, item4 = 5699})) then -- Marble Statuette, Mark of Creatures, Sky Jewel & Wondrous Stone
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3954,25000); -- Woodsmans Cape
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4494, item2 = 5981, item3 = 5847})) then -- Astral Jewel, Mark of Ambidexterity & Small Ancient Sculpture
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3956,25000); -- Woodsmans Gorget
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 5845, item2 = 4493, item3 = 5846})) then -- Mark of Animals, Meteor Jewel & Tattered Old Card
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3955,25000); -- Woodsmans Gauntlets
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 5690, item2 = 4488, item3 = 5691})) then -- Mark of Rivers, Sun Jewel & Temporal Sack
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3950,25000); -- Woodsmans Mantle
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4491, item2 = 5696, item3 = 5697})) then -- Cloud Jewel, Mark of Stone & Platinum Chain
+		e.self:Emote("smiles.");
+		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
+		e.other:QuestReward(e.self,0,0,0,0,3953,25000); -- Woodsmans Mask
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
