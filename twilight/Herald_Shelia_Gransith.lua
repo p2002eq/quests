12cc6c9defe668df1f98a901bc0ebeb948145b0d
Trust <fryguy503@gmail.com>
2017-01-26 20:59:59 -0500

    Twilight Armor - Bard Fix2

--- twilight/Herald_Shelia_Gransith.lua
+++ twilight/Herald_Shelia_Gransith.lua
@@ -1,24 +1,24 @@
 ---- Twilight Armor - Bard
 function event_say(e)
 	if(e.message:findi("Hail")) then
-		e.self:Say("Welcome to the Twilight Sea!  This is the docking entrance to Katta Castellum. ".. e.other:GetName() .. ". Tell me do you know any songs?");
-	elseif(e.message:findi("i know some songs")) then
-		e.self:Say("Ah you do. You don't happen to be a bard do you?");
-	elseif(e.message:findi("i am a bard")) then
-		e.self:Say("Great. I have just what you need. Would you like to earn some armor?");
-	elseif(e.message:findi("i would like to earn some armor")) then
+		e.self:Say("Welcome to the Twilight Sea!  This is the docking entrance to Katta Castellum. ".. e.other:GetName() .. ", Tell me do you know any [songs]?");
+	elseif(e.message:findi("songs")) then
+		e.self:Say("Ah you do. You don't happen to be a [bard] do you?");
+	elseif(e.message:findi("bard")) then
+		e.self:Say("Great. I have just what you need. Would you like to earn some [armor]?");
+	elseif(e.message:findi("armor")) then
 		e.self:Say("I thought so I have a [helm], [breastplate], [vambraces], [greaves], [pauldrons], [bracer], and [boots]. For the rest speak to Brolan over there about armor.");
-	elseif(e.message:findi("what breastplate")) then
+	elseif(e.message:findi("breastplate")) then
 		e.self:Say("For the breastplate you must bring me an astral jewel. a mark of music. a diamond studded medal. and a glorious flower.");
-	elseif(e.message:findi("what vambracers")) then
+	elseif(e.message:findi("vambracers")) then
 		e.self:Say("For the vambraces you must bring me a sun jewel. a mark of entertainment. a sapphire studded medal. and velvet sleeves.");
-	elseif(e.message:findi("what greaves")) then
+	elseif(e.message:findi("greaves")) then
 		e.self:Say("For the greaves you must bring me a moon jewel. a mark of the drum. an emerald studded medal. and memory crystal.");
-	elseif(e.message:findi("what pauldrons")) then
+	elseif(e.message:findi("pauldrons")) then
 		e.self:Say("For the pauldrons you must bring me a star jewel. a mark of the mandolin. and an opal studded medal.");
-	elseif(e.message:findi("what bracer")) then
+	elseif(e.message:findi("bracer")) then
 		e.self:Say("For the bracer you must bring me a cloud jewel. a mark of song. and a pearl studded medal.");
-	elseif(e.message:findi("what boots")) then
+	elseif(e.message:findi("boots")) then
 		e.self:Say("For the boots you must bring me a sky jewel. a mark of poetry. and a star ruby studded medal.");
 	elseif(e.message:findi("helm")) then
 		e.self:Say("For the helm you must bring me a meteor jewel, a mark of rhythm, a ruby studded medal, and a fancy necklace.");
