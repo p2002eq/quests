4da93f09d32ff4d6e201435b660e5940d629a7d8
Trust <fryguy503@gmail.com>
2017-01-27 19:56:56 -0500

    Shadeweaver - Saurek Scales

--- shadeweaver/Trainee_Naria.lua
+++ shadeweaver/Trainee_Naria.lua
@@ -1,7 +1,8 @@
+---- Quest:Saurek Scales
 function event_say(e)
 	if(e.message:findi("Hail")) then
 		e.self:Say("Have you noticed the vicious Saurek [Shredders] out in the thicket? I wouldn't want to hunt one of those nasty things.");
-	elseif(e.message:findi("what shredders?")) then
+	elseif(e.message:findi("shredders")) then
 		e.self:Say("The shredder has been known tear young hunters apart in mere seconds. I would stay far away from those things if I were you. Tarief has offered a reward for any that bring him four of their scales, but I just don't think it would be worth facing such a beast in battle. The meat from the smaller ones are just as good to eat if you want my opinion.");
 	end
 end
