3134dde0be85a3a426ffdfca9e9cd51099feaebd
Trust <fryguy503@gmail.com>
2017-02-02 00:23:20 -0500

    Katta - Vampyre Troubles
    Autarkic_Lord_Sfarosh -- signal issues.

--- katta/Autarkic_Lord_Sfarosh.lua
+++ katta/Autarkic_Lord_Sfarosh.lua
@@ -1,4 +1,10 @@
 ---- Quest:Vampyre Troubles
+function event_say(e)
+    if(e.message:findi("hail")) then
+        e.self:Emote("chants in an ancient language"); -- adlib for debug.
+    end
+end
+
 function event_trade(e)
     local item_lib = require("items");
     item_lib.return_items(e.self, e.other, e.trade)
