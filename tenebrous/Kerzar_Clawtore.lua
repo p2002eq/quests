2713140a638de8d8829283fb5c825c5866d67530
Trust <fryguy503@gmail.com>
2017-02-01 23:24:20 -0500

    Katta - Vampyre Troubles
    Tenebrous - Kerzar_Clawtore -- Fixing questreward statement.

--- tenebrous/Kerzar_Clawtore.lua
+++ tenebrous/Kerzar_Clawtore.lua
@@ -10,7 +10,7 @@ function event_trade(e)
     if(item_lib.check_turn_in(e.self, e.trade, {item1 = 31754})) then -- Katta Castellum Badge of Service (*Second Badge of Service)
         e.self:Say("You've been sent by the Validus Custodus to question me!! You mortals are more foolish than I had presumed! The only information I have for you is the fact that you are about to face your death!!");
         eq.set_global("vampyre_vahshir_combat","1",1,"M7");
-        e.other:QuestReward(e.self,0,0,0,0,31757,exp); -- Katta Castellum Badge of Service (*Third Badge of Service)
+        e.other:QuestReward(e.self,0,0,0,0,31757,1000); -- Katta Castellum Badge of Service (*Third Badge of Service)
         e.self:Emote("roars flashing glistening fangs that are sharper and longer than the average Vah Shirs!");
         e.self:AddToHateList(e.other,1);
     end
