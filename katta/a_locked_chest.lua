eaf3c9581512f37604cd6fde0bcbed786be2292a
Trust <fryguy503@gmail.com>
2017-02-02 00:03:23 -0500

    Katta - Vampyre Troubles
    A_Locked_Chest -- adjusting adds to attack and chest to despawn with timer.

--- katta/a_locked_chest.lua
+++ katta/a_locked_chest.lua
@@ -2,9 +2,10 @@
 function event_trade(e)
     local item_lib = require("items");
     if(item_lib.check_turn_in(e.self, e.trade, {item1 = 31758})) then -- Old Bone Key
-        eq.signal(160129,1,1); -- Send Event
-        eq.signal(160130,1,1); -- Send Event
-        eq.spawn2(160480,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+        eq.signal(160129,1,1):AddToHateList(e.other,1);
+        eq.signal(160130,1,1):AddToHateList(e.other,1);
+        eq.spawn2(160480,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()):AddToHateList(e.other,1);
+        eq.depop_with_timer();
     end
     item_lib.return_items(e.self, e.other, e.trade)
 end
