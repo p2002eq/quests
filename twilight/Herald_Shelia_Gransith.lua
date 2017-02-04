c7c20b4b6d15aff77be5a27f2f1b949be809fd02
Trust <fryguy503@gmail.com>
2017-02-04 11:01:53 -0500

    Twilight - Bard Armor boots
    fix item number and flavor text.

--- twilight/Herald_Shelia_Gransith.lua
+++ twilight/Herald_Shelia_Gransith.lua
@@ -9,24 +9,24 @@ function event_say(e)
 	elseif(e.message:findi("armor")) then
 		e.self:Say("I thought so I have a [helm], [breastplate], [vambraces], [greaves], [pauldrons], [bracer], and [boots]. For the rest speak to Brolan over there about armor.");
 	elseif(e.message:findi("breastplate")) then
-		e.self:Say("For the breastplate you must bring me an astral jewel. a mark of music. a diamond studded medal. and a glorious flower.");
+		e.self:Say("For the breastplate you must bring me an astral jewel, a mark of music, a diamond studded medal, and a glorious flower.");
 	elseif(e.message:findi("vambracers")) then
-		e.self:Say("For the vambraces you must bring me a sun jewel. a mark of entertainment. a sapphire studded medal. and velvet sleeves.");
+		e.self:Say("For the vambraces you must bring me a sun jewel, a mark of entertainment, a sapphire studded medal, and velvet sleeves.");
 	elseif(e.message:findi("greaves")) then
-		e.self:Say("For the greaves you must bring me a moon jewel. a mark of the drum. an emerald studded medal. and memory crystal.");
+		e.self:Say("For the greaves you must bring me a moon jewel, a mark of the drum, an emerald studded medal, and memory crystal.");
 	elseif(e.message:findi("pauldrons")) then
-		e.self:Say("For the pauldrons you must bring me a star jewel. a mark of the mandolin. and an opal studded medal.");
+		e.self:Say("For the pauldrons you must bring me a star jewel, a mark of the mandolin, and an opal studded medal.");
 	elseif(e.message:findi("bracer")) then
-		e.self:Say("For the bracer you must bring me a cloud jewel. a mark of song. and a pearl studded medal.");
+		e.self:Say("For the bracer you must bring me a cloud jewel, a mark of song, and a pearl studded medal.");
 	elseif(e.message:findi("boots")) then
-		e.self:Say("For the boots you must bring me a sky jewel. a mark of poetry. and a star ruby studded medal.");
+		e.self:Say("For the boots you must bring me a sky jewel, a mark of poetry, and a star ruby studded medal.");
 	elseif(e.message:findi("helm")) then
 		e.self:Say("For the helm you must bring me a meteor jewel, a mark of rhythm, a ruby studded medal, and a fancy necklace.");
 	end
 end
 function event_trade(e)
 	local item_lib = require('items');
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 5347, item2 = 4492, item3 = 534})) then
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 5347, item2 = 4492, item3 = 5348})) then
 		e.self:Emote("sings loudly as she hands you your reward");
 		e.self:Say("Thank you. May your voice be ever true.");
 		e.other:QuestReward(e.self,0,0,0,0,3909,10000); --
