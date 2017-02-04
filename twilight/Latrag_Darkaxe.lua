b36931f8abbc78dbac167da9d4911c577c8ac80e
Trust <fryguy503@gmail.com>
2017-02-04 16:36:07 -0500

    Twilight Armor - Paladin Armor
    Fixed misc errors.

--- twilight/Latrag_Darkaxe.lua
+++ twilight/Latrag_Darkaxe.lua
@@ -1,18 +1,19 @@
 ---- Twilight Armor - Paladin
 function event_say(e)
 	if(e.message:findi("Hail")) then
+		e.self:Emote("lets out a resounding belch.");
 		e.self:Say("Har there lad! Ye be sure 'nuff to tell em all that Brewmaster Latrag is [retiring].");
 	elseif(e.message:findi("retiring")) then
 		e.self:Say("Ya got that right. I'm goin' to concentrate more on me devotion to Brell as a [holy knight]!");
-	elseif(e.message:findi("holy knoght")) then
-		e.self:Say("I be a paladin of Brell thar Daias . Might ye be a [paladin] yerself?");
+	elseif(e.message:findi("holy knight")) then
+		e.self:Say("I be a paladin of Brell thar Daias. Might ye be a [paladin] yerself?");
 	elseif(e.message:findi("paladin")) then
-		e.self:Say("That's good. Ya know I have some [armor] ye might be interested in.");
+		e.self:Say("That's good. Ya know, I have some [armor] ye might be interested in.");
 	elseif(e.message:findi("armor")) then
 		e.self:Say("Yar! I have me a [helm], [breastplate], [vambraces], [greaves], [pauldrons], and a [bracer]. If ye want the rest talk to the mighty holy warrior Trallen over there about armor.");
 	elseif(e.message:findi("breastplate")) then
 		e.self:Say("For the breastplate ye gotta bring me a star jewel, a mark of courage, a white marble bowl, and a jagged reed.");
-	elseif(e.message:findi("vambracers")) then
+	elseif(e.message:findi("vambraces")) then
 		e.self:Say("For the vambraces ye gotta bring me a cloud jewel, a mark of righteousness, a runed card, and a pristine ebony idol");
 	elseif(e.message:findi("greaves")) then
 		e.self:Say("For the greaves ye gotta bring me a sky jewel, a mark of bravery, a sunflower fruit, and a weathered bundle of wood");
@@ -30,7 +31,7 @@ local item_lib = require('items');
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3977,10000); --
-	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4490, item2 = 5891, item3 = 5892, item4 = 5983})) then
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4490, item2 = 5891, item3 = 5892, item4 = 5893})) then
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3973,10000); --
@@ -42,11 +43,11 @@ local item_lib = require('items');
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3972,10000); --
-	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4494, item2 = 5900, item3 = 5901})) then
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4493, item2 = 5900, item3 = 5901})) then
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3976,10000); --
-	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4488, item2 = 5894, item3 = 5895, item4 = 5896})) then
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4491, item2 = 5894, item3 = 5895, item4 = 5896})) then
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3974,10000); --
--- twilight/Trallen_Grimhammer.lua
+++ twilight/Trallen_Grimhammer.lua
@@ -1,30 +1,30 @@
 ---- Twilight Armor - Paladin
 function event_say(e)
 	if(e.message:findi("Hail")) then
-		e.self:Say("Greetings and well met ".. e.other:GetName() .. ". Don't listen to ol' Latrag over there he'll never stop brewin' the best durn ale to be found.");
+		e.self:Say("Greetings and well met, ".. e.other:GetName() .. ". Don't listen to ol' Latrag over there. He'll never stop brewin' the best durn ale to be found.");
 	elseif(e.message:findi("armor")) then
-		e.self:Say("Latrag is goin' on 'bout his armor again isn't he. Well friend be ye a [paladin]?");
+		e.self:Say("Latrag is goin' on 'bout his armor again isn't he. Well friend, be ye a [paladin]?");
 	elseif(e.message:findi("paladin")) then
-		e.self:Say("Of course ya are ".. e.other:GetName() .. ". why would have ya asked me about the armor if ye wasn't eh. I have the [boots], [mask], [cloak], [gauntlets], [gorget], [girdle], and a [sword]. Which do ye want?");
+		e.self:Say("Of course ya are ".. e.other:GetName() .. ". Why would ya have asked me about the armor if ye wasn't, eh? I have the [boots], [mask], [cloak], [gauntlets], [gorget], [girdle], and a [sword]. Which do ye want?");
 	elseif(e.message:findi("boots")) then
-		e.self:Say("For the boots yer gonna have to get me a sun jewel. a mark of destiny. and a fleshy vine.");
-	elseif(e.message:findi("wmask")) then
-		e.self:Say("For the mask yer gonna have to get me a moon jewel. a mark of blessings. and some crystallized dew.");
+		e.self:Say("For the boots yer gonna have to get me a sun jewel, a mark of destiny, and a fleshy vine.");
+	elseif(e.message:findi("mask")) then
+		e.self:Say("For the mask yer gonna have to get me a moon jewel, a mark of blessings, and some crystallized dew.");
 	elseif(e.message:findi("cloak")) then
-		e.self:Say("For the cloak yer gonna have to get me a star jewel. a mark of the steadfast. a lexicon of the sun. and some glade dew.");
+		e.self:Say("For the cloak yer gonna have to get me a star jewel, a mark of the steadfast, a lexicon of the sun, and some glade dew.");
 	elseif(e.message:findi("gauntlets")) then
-		e.self:Say("For the gauntlets yer gonna have to get me a cloud jewel. a mark of honor. and some naturally formed quartz.");
+		e.self:Say("For the gauntlets yer gonna have to get me a cloud jewel, a mark of honor, and some naturally formed quartz.");
 	elseif(e.message:findi("gorget")) then
-		e.self:Say("For the gorget yer gonna have to get me a sky jewel. a mark of gallantry. and a lunar marked stone.");
+		e.self:Say("For the gorget yer gonna have to get me a sky jewel, a mark of gallantry, and a lunar marked stone.");
 	elseif(e.message:findi("girdle")) then
-		e.self:Say("For the girdle yer gonna have to get me a meteor jewel. a mark of heart. a lexicon of the moon. and a dread leech eye.");
+		e.self:Say("For the girdle yer gonna have to get me a meteor jewel, a mark of heart, a lexicon of the moon, and a dread leech eye.");
 	elseif(e.message:findi("sword")) then
-		e.self:Say("For the sword yer gonna have to get me an astral jewel. a mark of the noble. a hardened clay sculpture. and a runic ear plug.");
+		e.self:Say("For the sword yer gonna have to get me an astral jewel, a mark of the noble, a hardened clay sculpture, and a runic ear plug.");
 	end
 end
 function event_trade(e)
 	local item_lib = require('items');
-	if(item_lib.check_turn_in(e.self, e.trade, {item2 = 5904, item3 = 5905})) then
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 4488, item2 = 5904, item3 = 5905})) then
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3978,10000); --
