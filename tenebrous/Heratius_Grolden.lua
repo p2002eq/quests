17b4b3de6cc177585f1811eca05ebc7558a5d8a4
Trust <fryguy503@gmail.com>
2017-02-04 23:31:44 -0500

    Tenebrous - Coterie Mask of the Dead

--- tenebrous/Heratius_Grolden.lua
+++ tenebrous/Heratius_Grolden.lua
@@ -13,3 +13,12 @@ end
 function event_death_complete(e)
     eq.unique_spawn(172184,0,0,-1425,576,317,64); -- Valdanov Zevfeer
 end
+
+function event_signal(e)
+    if(e.signal == 1) then
+        local random_result = math.random(100);
+        if(random_result <= 50) then
+            eq.unique_spawn(172183,0,0,-1425,576,317,64); -- Heratius Grolden
+        end
+    end
+end
