2b1635f63d8743e4e8e02924aff975b8be54914a
Vlad <vpresnyak@gmail.com>
2017-02-10 01:27:02 -0500

    Umbral - Doomshare further adjustments

--- umbral/A_Dark_Master.lua
+++ umbral/A_Dark_Master.lua
@@ -13,9 +13,9 @@ function event_death_complete(e)
 	eq.zone_emote(1, 'master counter is ' .. total);
 	
 	local qglobals = eq.get_qglobals(e.self);
-	if total <= 1 and qglobals['Doomshade'] ~= nil then
+	if total <= 1 and qglobals['Doomshade'] == nil then
 		-- eq.set_global("Doomshade","COOLDOWN",3,"D3");
-		eq.set_global("Doomshade","COOLDOWN",3,"M15");
+		eq.set_global("Doomshade","COOLDOWN",3,"M5");
 		eq.unique_spawn(176216, 0, 0, 127, -297, 0);
 		eq.zone_emote(1, "A tortured scream echoes throughout the plains as a tormented spirit is released upon the land.");
 	end
