26a8e390f0578d0efeb4a495108f0c7088c2856e
Vlad <vpresnyak@gmail.com>
2017-02-10 01:39:03 -0500

    Umbral - Doomshade is good  #CURRENTLY SET TO TESTING TIMER, NEEDS CHANGED BEFORE PUSH TO LIVE#

--- umbral/A_Dark_Master.lua
+++ umbral/A_Dark_Master.lua
@@ -1,21 +1,20 @@
 -- A Dark Master, part of doomshade trigger
 
 function event_death_complete(e)
-    local npc_list = eq.get_entity_list():GetNPCList();
+    -- count Dark Masters (note that it counts itself as well)
+	local npc_list = eq.get_entity_list():GetNPCList();
 	local total = 0;
-	
     if (npc_list ~= nil) then
         for npc in npc_list.entries do
 			if (npc:GetNPCTypeID() == 176042) then total = total + 1; end
         end
     end
-
-	eq.zone_emote(1, 'master counter is ' .. total);
 	
+	-- if this is the last Dark Master and Doomshade lockout is not set, spawn Doomshade and set lockout
 	local qglobals = eq.get_qglobals(e.self);
 	if total <= 1 and qglobals['Doomshade'] == nil then
-		-- eq.set_global("Doomshade","COOLDOWN",3,"D3");
-		eq.set_global("Doomshade","COOLDOWN",3,"M5");
+		-- eq.set_global("Doomshade","lockout",3,"D3");
+		eq.set_global("Doomshade","lockout",3,"M5");
 		eq.unique_spawn(176216, 0, 0, 127, -297, 0);
 		eq.zone_emote(1, "A tortured scream echoes throughout the plains as a tormented spirit is released upon the land.");
 	end
