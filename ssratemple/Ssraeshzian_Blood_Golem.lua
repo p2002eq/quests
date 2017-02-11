0e52d3c92611d6c97fb1f710514e35b6cbce151f
Vlad <vpresnyak@gmail.com>
2017-02-11 00:26:13 -0500

    Trying new set of functions

--- ssratemple/Ssraeshzian_Blood_Golem.lua
+++ ssratemple/Ssraeshzian_Blood_Golem.lua
@@ -5,7 +5,7 @@ guard_list = { 162123, 162124, 162125, 162126, 162127, 162128, 162129, 162130 };
 
 function event_spawn(e) 
 	-- depop and deactivate itself if it pops for some reason while real Blood is active
-	if eq.get_entity_list():GetNPCByNPCTypeID(162189) ~= nil then
+	if eq.get_entity_list():IsMobSpawnedByNpcTypeID(162189) then
 		eq.set_timer('depop_golem', 500);
 		-- eq.get_entity_list():GetSpawnByID(368757):Disable();
 	end
