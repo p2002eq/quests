2e13a615f17cf043fdacb5610ea9574b028a8788
Vlad <vpresnyak@gmail.com>
2017-02-11 16:26:22 -0500

    Adding guards to Arch Lich cycle

--- ssratemple/162178.lua
+++ ssratemple/162178.lua
@@ -2,4 +2,8 @@
 
 function event_death_complete(e)
 	eq.unique_spawn(162192, 0, 0, 644, -280, 146, 196); -- spawn Rhag 2
+	eq.spawn2(162193, 0, 0, 561, -235, 138, 128);  -- guard
+	eq.spawn2(162194, 0, 0, 660, -309, 146, 218);  -- guard
+	eq.spawn2(162195, 0, 0, 660, -246, 146, 165);  -- guard
+	eq.spawn2(162196, 0, 0, 561, -323, 138, 0);  -- guard
 end
--- ssratemple/162192.lua
+++ ssratemple/162192.lua
@@ -2,4 +2,8 @@
 
 function event_death_complete(e)
 	eq.unique_spawn(162517, 0, 0, 418, -141, 264, 0); -- spawn Arch Lich
+	eq.spawn2(162518, 0, 0, 448, -161, 265, 224);  -- guard
+	eq.spawn2(162519, 0, 0, 390, -161, 265, 32);  -- guard
+	eq.spawn2(162090, 0, 0, 375, -64, 257, 64);  -- guard
+	eq.spawn2(162090, 0, 0, 467, -64, 257, 192);  -- guard
 end
