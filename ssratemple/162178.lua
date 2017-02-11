bb216175ddb991e0e3161e39cf66b2c8a893d636
Vlad <vpresnyak@gmail.com>
2017-02-11 03:36:17 -0500

    Ssratemple - Basic Rhag spawn chain

---
+++ ssratemple/162178.lua
@@ -0,0 +1,5 @@
+-- #Rhag`Zhezum (162178) for Rhag cycle AKA Rhag 1
+
+function event_death_complete(e)
+	eq.unique_spawn(162192, 0, 0, 644, -280, 146, 196); -- spawn Rhag 2
+end
---
+++ ssratemple/162192.lua
@@ -0,0 +1,5 @@
+-- #Rhag`Mozdezh (162192) for Rhag cycle AKA Rhag 2
+
+function event_death_complete(e)
+	eq.unique_spawn(162517, 0, 0, 418, -141, 264, 0); -- spawn Arch Lich
+end
