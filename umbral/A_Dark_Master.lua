fa151985500d97f7e1033c84e65a6658c3011347
Vlad <vpresnyak@gmail.com>
2017-02-10 19:02:26 -0500

    Umbral - Added variance to Doomshade respawn

--- umbral/A_Dark_Master.lua
+++ umbral/A_Dark_Master.lua
@@ -13,7 +13,8 @@ function event_death_complete(e)
 	-- if this is the last Dark Master and Doomshade lockout is not set, spawn Doomshade and set lockout
 	local qglobals = eq.get_qglobals(e.self);
 	if total <= 1 and qglobals['Doomshade'] == nil then
-		-- eq.set_global("Doomshade","lockout",3,"D3");
+		lockout = "H" .. (59 + math.random(25));
+		-- eq.set_global("Doomshade","lockout",3,lockout);
 		eq.set_global("Doomshade","lockout",3,"M5");
 		eq.unique_spawn(176216, 0, 0, 127, -297, 0);
 		eq.zone_emote(1, "A tortured scream echoes throughout the plains as a tormented spirit is released upon the land.");
