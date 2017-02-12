0b183a767a175580c7e4011280f8ffb14b3898fd
Vlad <vpresnyak@gmail.com>
2017-02-11 20:39:49 -0500

    Thedeep - massive burrower update

--- thedeep/A_massive_burrower.lua
+++ thedeep/A_massive_burrower.lua
@@ -1,9 +1,12 @@
 -- A massive burrower for The Burrower Beast event
 
 function event_spawn(e)
-	eq.set_proximity(e.self:GetX()-30,e.self:GetX()+30,e.self:GetY()-30,e.self:GetY()+30);
+	eq.set_proximity(e.self:GetX()-50,e.self:GetX()+50,e.self:GetY()-50,e.self:GetY()+50);
 end
 
 function event_enter(e)
-	e.self:AddToHateList(e.other, 1);
+	e.self:Attack(e.other);
+	e.self:Attack(e.other);
+	e.self:Attack(e.other);
+	e.self:Attack(e.other);
 end
