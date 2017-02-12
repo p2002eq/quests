4a829c5aaf3c41a235410febfcdfb7bd6f9959fd
Vlad <vpresnyak@gmail.com>
2017-02-12 17:18:20 -0500

    Umbral - Doomshade event final (hopefully)

--- umbral/A_Dark_Master.lua
+++ umbral/A_Dark_Master.lua
@@ -9,7 +9,7 @@ function event_timer(e)
 		local qglobals = eq.get_qglobals(e.self);
 		if qglobals['Doomshade'] == nil and qglobals['dark_chant'] == nil then
 			eq.zone_emote(1, 'An unearthly wail fills the air as the Dark Masters begin chanting for the coming of doom.');
-			eq.set_global("dark_chant","chanted",3,"M1");
+			eq.set_global("dark_chant","chanted",3,"M20");
 		end
 	end
 end
@@ -19,8 +19,7 @@ function event_death_complete(e)
 	local qglobals = eq.get_qglobals(e.self);
 	if not eq.get_entity_list():IsMobSpawnedByNpcTypeID(176042) and qglobals['Doomshade'] == nil then
 		lockout = "H" .. (59 + math.random(25));
-		-- eq.set_global("Doomshade","lockout",3,lockout);
-		eq.set_global("Doomshade","lockout",3,"M3");
+		eq.set_global("Doomshade","lockout",3,lockout);
 		eq.unique_spawn(176216, 0, 0, 127, -297, 0);
 		eq.zone_emote(1, "A tortured scream echoes throughout the plains as a tormented spirit is released upon the land.");
 	end
