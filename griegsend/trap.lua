67bc499dc30af2c968967d21c334b704ed6aea14
Vlad <vpresnyak@gmail.com>
2017-02-12 16:36:27 -0500

    Griegs end - trap spawned v1.2

--- griegsend/trap.lua
+++ griegsend/trap.lua
@@ -7,15 +7,18 @@ function event_spawn(e)
 end
 
 function event_enter(e)
-	local x = e.self:GetX();
-	local y = e.self:GetY();
-	local z = e.self:GetZ();
-	local h = e.self:GetHeading();
-	local mob_pick = eq.ChooseRandom(163386, 163375, 163106, 163376, 163354, 163150, 163383, 163135, 163378, 163379, 163380, 163381, 163385, 'rare');
-	if mob_pick == 'rare' then
-		local rare = eq.ChooseRandom(163124, 163065, 163062, 163384);
-		eq.spawn2(rare, 0, 0, x, y, z, h);
-	else
-		eq.spawn2(mob_pick, 0, 0, x, y, z, h);
+	if e.other:Admin() < 100 then
+		local x = e.self:GetX();
+		local y = e.self:GetY();
+		local z = e.self:GetZ();
+		local h = e.self:GetHeading();
+		local mob = eq.ChooseRandom(163386, 163375, 163106, 163376, 163354, 163150, 163383, 163135, 163378, 163379, 163380, 163381, 163385, 'rare');
+		if mob == 'rare' then
+			local rare = eq.ChooseRandom(163124, 163065, 163062, 163384);
+			eq.spawn2(rare, 0, 0, x, y, z, h);
+		else
+			eq.spawn2(mob, 0, 0, x, y, z, h);
+		end
+		eq.depop_with_timer()
 	end
 end
