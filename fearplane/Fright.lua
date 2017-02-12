c88fb16880e56534dc39bbc50a3bd0d5d962102a
Vlad <vpresnyak@gmail.com>
2017-02-12 05:22:33 -0500

    PoFear - Casting test

--- fearplane/Fright.lua
+++ fearplane/Fright.lua
@@ -28,6 +28,12 @@ function event_death_complete(e)
 	eq.spawn2(a,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 end
 
+function event_cast_begin(e)
+	if e.spell == 982 then
+		e.self:Shout(e.self:GetHateTop():GetName() .. "!!!")
+	end
+end
+
 --Submitted by: Jim Mills (Gilmore Girls`Is`Awesome`XOXO)
 -------------------------------------------------------------------------------------------------
 -- Converted to .lua using MATLAB converter written by Stryd
