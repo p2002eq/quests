41af730b33eb2fba745a6ed5a7bc48c4e79a9563
Vlad <vpresnyak@gmail.com>
2017-02-14 02:44:56 -0500

    Great Divide - updated GM control of Ring event to include help function

--- greatdivide/player.lua
+++ greatdivide/player.lua
@@ -7,17 +7,3 @@ function event_level_up(e)
 	local discs = require('disciplines');
 	discs:train_discs(e, e.self:GetLevel());
 end
-
-function event_say(e)
-	if e.self:Admin() > 100 then
-		if(e.message:findi("stage1")) then
-			eq.signal(118351, 1000);
-		elseif(e.message:findi("stage2")) then
-			eq.signal(118351, 2000);
-		elseif(e.message:findi("stage3")) then
-			eq.signal(118351, 3000);
-		elseif(e.message:findi("narandi")) then
-			eq.signal(118351, 4000);
-		end
-	end
-end
