f144b074143c6268e962728b0879add0c63d3fa6
Trust <fryguy503@gmail.com>
2017-02-14 21:57:10 -0500

    Twilight Armor - Rogue

--- twilight/Liteema_Agner.lua
+++ twilight/Liteema_Agner.lua
@@ -1,11 +1,11 @@
 ---- Twilight Armor - Rogue
 function event_say(e)
 	if(e.message:findi("Hail")) then
-		e.self:Say("looks up and smiles.  'Hiya. I just love fishing don't you?'");
+		e.self:Emote("looks up and smiles. 'Hiya. I just love fishing, don't you?'");
 	elseif(e.message:findi("armor")) then
 		e.self:Say("Oh! Tolli sent you over here didn't he. If you're a [rogue] and want some armor just let me know.");
 	elseif(e.message:findi("rogue")) then
-		e.self:Say("This stuff isn't free no sir! I have a mask. [cloak], [gauntlets], [gorget], [waistband] and a [dirk]. Just ask and I'll tell ya what ya have to do to get it.");
+		e.self:Say("This stuff isn't free, no sir! I have a mask. [cloak], [gauntlets], [gorget], [waistband] and a [dirk]. Just ask and I'll tell ya what ya have to do to get it.");
 	elseif(e.message:findi("mask")) then
 		e.self:Say("For the mask of concealment you're going to have to go get me an astral jewel, a mark of deceit, and an engraved fire opal statuette.");
 	elseif(e.message:findi("cloak")) then
@@ -15,7 +15,7 @@ function event_say(e)
 	elseif(e.message:findi("gorget")) then
 		e.self:Say("For the gorget of concealment you're going to have to go get me a star jewel, a mark of trickery, and an engraved wooden statuette.");
 	elseif(e.message:findi("waistband")) then
-		e.self:Say("For the waistband of concealment you're going to have to go get me a cloud jewel, a mark of silence, an engraved platinum statuette, and a true silver needle.");
+		e.self:Say("For the waistband of concealment you're going to have to go get me a cloud jewel, a mark of stealth, an engraved platinum statuette, and a true silver needle.");
 	elseif(e.message:findi("dirk")) then
 		e.self:Say("For the dirk of concealment you're going to have to go get me a sky jewel, a mark of greed, an engraved mithril statuette, and a dread hilt.");
 	end
@@ -38,7 +38,7 @@ function event_trade(e)
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3798,10000); --
-	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4491, item2 = 5285, item3 = 5297, item4 = 5298})) then
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 4491, item2 = 5296, item3 = 5297, item4 = 5298})) then
 		e.self:Emote("smiles.");
 		e.self:Say("Here is what I promised, ".. e.other:GetName() .. ".");
 		e.other:QuestReward(e.self,0,0,0,0,3799,10000); --
