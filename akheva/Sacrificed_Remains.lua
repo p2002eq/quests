6b7db7918d266aec04199c0b3e27264f8266eaea
Trust <fryguy503@gmail.com>
2017-02-17 23:03:09 -0500

    Akheva - Adjust Sacrificed Remains - Debug message

--- akheva/Sacrificed_Remains.lua
+++ akheva/Sacrificed_Remains.lua
@@ -1,4 +1,5 @@
 function event_death_complete(e)
+	e.self:Say("Aeeeeeeee");
 	local chance_spawn = math.random(100);
 	if(chance_spawn <= 20) then -- 20% chance
 		eq.spawn2(179356,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
