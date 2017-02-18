1be2a662383ee1d42121b14c9ab915625c233910
Trust <fryguy503@gmail.com>
2017-02-17 23:06:38 -0500

    Akheva - Adjust Sacrificed Remains - Update spawn rate. Maybe it will want to actually spawn.

--- akheva/Sacrificed_Remains.lua
+++ akheva/Sacrificed_Remains.lua
@@ -1,7 +1,7 @@
 function event_death_complete(e)
 	e.self:Say("Aeeeeeeee");
 	local chance_spawn = math.random(100);
-	if(chance_spawn <= 20) then -- 20% chance
+	if(chance_spawn <= 75) then -- 20% chance
 		eq.spawn2(179356,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),0);
 	end
 end
