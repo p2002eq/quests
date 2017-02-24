2db3feaf1aa25a897fc03339c444915664534147
Vlad <vpresnyak@gmail.com>
2017-02-24 00:26:39 -0500

    Twilight/Katta - Traitor to the Validus Custodus update

--- twilight/Legionnaire_Halle.lua
+++ twilight/Legionnaire_Halle.lua
@@ -1,12 +1,14 @@
 ---- Quest:Traitor to the Validus Custodus
 
 function event_say(e)
-	if e.message:findi("hail") then
-		e.self:Emote("coughs a few times and reaches out to you, 'Please help me.'")
-	elseif(e.message:findi("help")) then
-		e.self:Emote("reaches out and puts something in your hand, 'Find Magistrate Phenic, he is the only one that I know I can trust.' With a look of someone who has left unfinished business behind her, Halle lets go and dies.");
-		e.other:SummonItem(18330); -- Water-stained note
-		eq.depop_with_timer();
+	if(e.other:GetFaction(e.self) <= 4) then
+		if e.message:findi("hail") then
+			e.self:Emote("coughs a few times and reaches out to you, 'Please help me.'")
+		elseif(e.message:findi("help")) then
+			e.self:Emote("reaches out and puts something in your hand, 'Find Magistrate Phenic, he is the only one that I know I can trust.' With a look of someone who has left unfinished business behind her, Halle lets go and dies.");
+			e.other:SummonItem(18330); -- Water-stained note
+			eq.depop_with_timer();
+		end
 	end
 end
 
