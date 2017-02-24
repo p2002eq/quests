f71050d8a2edec61c899f3d3a292b38ca7314b85
Vlad <vpresnyak@gmail.com>
2017-02-24 00:05:20 -0500

    Twilight - Traitor to the Validus Custodus first piece

--- twilight/Legionnaire_Halle.lua
+++ twilight/Legionnaire_Halle.lua
@@ -1,10 +1,9 @@
 ---- Quest:Traitor to the Validus Custodus
-function event_spawn(e)
-	e.self:SetAppearance(3);
-end
 
 function event_say(e)
-	if(e.message:findi("I will help")) then
+	if e.message:findi("hail") then
+		e.self:Emote("coughs a few times and reaches out to you, 'Please help me.'")
+	elseif(e.message:findi("help")) then
 		e.self:Emote("reaches out and puts something in your hand, 'Find Magistrate Phenic, he is the only one that I know I can trust.' With a look of someone who has left unfinished business behind her, Halle lets go and dies.");
 		e.other:SummonItem(18330); -- Water-stained note
 		eq.depop_with_timer();
