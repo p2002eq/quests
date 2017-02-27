4a44df3842fd98bf4194d86c192656d6185c075d
Vlad <vpresnyak@gmail.com>
2017-02-27 01:02:24 -0500

    Acrylia - player.lua tweak

--- acrylia/player.lua
+++ acrylia/player.lua
@@ -4,9 +4,8 @@ function event_click_door(e)
 	local door = e.door:GetDoorID()
 	if door == 1 or door == 2 or door == 3 or door == 4 then
 		local ent_list = eq.get_entity_list()
-		ent_list:GetDoorsByDoorID(1):ForceOpen(e.self)
-		ent_list:GetDoorsByDoorID(2):ForceOpen(e.self)
-		ent_list:GetDoorsByDoorID(3):ForceOpen(e.self)
-		ent_list:GetDoorsByDoorID(4):ForceOpen(e.self)
+		for i=1,4 do
+			if i ~= door then ent_list:GetDoorsByDoorID(i):ForceOpen(e.self) end
+		end
 	end
 end
