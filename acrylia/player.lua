93eae93804a74918ab314a9b0f3218c81cb52dbf
Vlad <vpresnyak@gmail.com>
2017-02-27 00:52:11 -0500

    Acrylia - player.lua tweak

--- acrylia/player.lua
+++ acrylia/player.lua
@@ -1,7 +1,8 @@
 -- This quest file simply makes the fancy 4-paneled door operate properly.
 
 function event_click_door(e)
-	if e.door == 1 or e.door == 2 or e.door == 3 or e.door == 4 then
+	local door = e.door:GetDoorID()
+	if door == 1 or door == 2 or door == 3 or door == 4 then
 		local ent_list = eq.get_entity_list()
 		ent_list:GetDoorsByDoorID(1):ForceOpen(e.self)
 		ent_list:GetDoorsByDoorID(2):ForceOpen(e.self)
