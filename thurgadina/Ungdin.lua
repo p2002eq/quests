5e8d16ac2f5a539b60eaa5fe4ee6626cd109a7a0
Vlad <vpresnyak@gmail.com>
2017-03-07 20:12:48 -0500

    Thurgadina - Ungdin for Hammerfal quest

--- thurgadina/Ungdin.lua
+++ thurgadina/Ungdin.lua
@@ -1,17 +1,17 @@
 function event_say(e)
 	if (e.message:findi("hail")) then
-		e.self:Say("Uuuuhhh, how goes it," .. e.other:GetRace() .. "? Oh, I'm doin' jest fine down here, just hanging around... Mining! Yes mining for our fearless leader, the Dain. Yessiree... sssoooooo...");
+		e.self:Say("Uuuuhhh, how goes it," .. e.other:Race() .. "? Oh, I'm doin' jest fine down here, just hanging around... Mining! Yes mining for our fearless leader, the Dain. Yessiree... sssoooooo...");
 	end
 end
 
 function event_trade(e)
 
 	local item_lib = require('items');
-	local qglobals = eq.get_globals(e.other);
+	local qglobals = eq.get_qglobals(e.other);
 
 	if (item_lib.check_turn_in(e.self, e.trade, {item1 = 1725})) then
 		e.self:Say("Hmmm, not sure why you're giving me all this. <cough> 50 pieces <cough> of platinum <cough> Whew, wow, I may be coming down with <cough> and the note <cough> something.");
-		eq.set_global("shipment", "1", 0, "D1");
+		eq.set_global("shipment", "1", 0, "F");
 	elseif qglobals['shipment'] == '1' and item_lib.check_turn_in(e.self, e.trade, {platinum = 50}) then	
 		e.self:Emote("'s eyes dart about the room before retrieving a large box from under the table. He takes the platinum and the note, then hands you the box without a word.");
 		e.other:SummonItem(29064);
