c90c066bc9fc9e5e5ced9109337559e61c24e2ea
Vlad <vpresnyak@gmail.com>
2017-03-07 20:23:34 -0500

    Thurgadina - fixing some questerrors

--- thurgadina/Saramor.lua
+++ thurgadina/Saramor.lua
@@ -5,7 +5,7 @@ function event_signal(e)
 	elseif (e.signal == 2) then
 		e.self:Say("Get him boys!");
 	elseif (e.signal == 3) then
-		e.self:SetRunning(1);
+		e.self:SetRunning(true);
 		eq.start(6);
 	elseif (e.signal == 4) then
 		e.self:Say("Well, don't ya just keep us in suspense now. What in the world was it?");
@@ -18,7 +18,7 @@ end
 function event_waypoint_arrive(e)
 
 	if (e.wp == 4) then
-		e.self:SetRunning(0);
+		e.self:SetRunning(false);
 	elseif (e.wp == 9) then
 		eq.stop()
 		e.self:Say("So good to see you, Rumagur. I hope you're feelin better today.");
