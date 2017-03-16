1a2ce4034fa570c96ae25a361ff78a5716e9f15f
Vlad <vpresnyak@gmail.com>
2017-03-16 17:47:39 -0400

    Revert "Cazicthule - Gnome walk rework"
    This reverts commit 8200dcdd42d4a7061f356e959891fd9303701baa.

--- cazicthule/a_Thul_Tae_Ew_Tracker.lua
+++ cazicthule/a_Thul_Tae_Ew_Tracker.lua
@@ -1,12 +1,3 @@
--- a_Thul_Tae_Ew_Tracker (48377) for Gnome Escort in CT
-
-function event_spawn(e)
-	e.self:Say("So, there you are, little Gnome. Thought you evaded us? Your friends will never protect you. You will all fall before our power!")
-	eq.set_timer('depop', 10 * 60 * 1000)
-end
-
-function event_timer(e)
-	if e.timer == 'depop' then
-		eq.depop()
-	end
+function event_death(e)
+	eq.signal(48040,1);
 end
