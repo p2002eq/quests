d121a31e4f69f98a1c6dd4720119c5a17aa9a013
Trust <fryguy503@gmail.com>
2017-03-17 22:39:24 -0400

    Dawnshroud - Initial Hopper cave cycle logic

---
+++ dawnshroud/#Sambata_Tribal_Leader_Garn.lua
@@ -0,0 +1,8 @@
+---- Rockhopper/Sambata Cycle
+function event_death_complete(e)
+    local qglobals = eq.get_qglobals(e.self,e.other);
+    if(qglobals["Rockhopper_Sambata"] == nil) then
+        eq.spawn_condition(dawnshroud,0,2,1);
+        eq.spawn_condition(dawnshroud,0,1,0);
+    end
+end
