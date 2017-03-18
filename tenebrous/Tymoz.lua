ec3c8341f825111b63704557512ddd9e02014ced
Trust <fryguy503@gmail.com>
2017-03-18 14:42:54 -0400

    Tenebrous - Task #334 - Adjusted Item Reward for Blood Dipped Darts 1-8 random.

--- tenebrous/Tymoz.lua
+++ tenebrous/Tymoz.lua
@@ -10,14 +10,13 @@ end
 function event_trade(e)
     local item_lib = require("items");
     if(item_lib.check_turn_in(e.self, e.trade, {item1 = 31748, item2 = 31748})) then -- 2x Damaged Custodus Legionnaire Helm
-        e.self:Say("Excellent! Two less of thos self righteous Validus Custodus goons to worry about!");
+        e.self:Say("Excellent! Two less of those self righteous Validus Custodus goons to worry about!");
         e.other:Faction(55, 1); -- Coterie of Eternal Night
         e.other:Faction(349, 1); -- Valdanov Zevfeer
         e.other:Faction(228, -1); -- Nathyn Illuminious
         e.other:Faction(350, -1); -- Validus Custodus
-        e.other:SummonItem(eq.ChooseRandom(1001,0,0)); -- 33% Chance - Blood Dipped Dart
-        e.other:SummonItem(eq.ChooseRandom(1001,0,0)); -- 33% Chance - Blood Dipped Dart
-        e.other:QuestReward(e.self,0,0,0,0,31750,5000); -- Blood Dipped Dart
+        e.other:SummonItem(31750,eq.ChooseRandom(1,2,3,4,5,6,7,8)); -- 1-3 Blood Dipped Darts
+        e.other:QuestReward(e.self,0,0,0,0,0,5000); -- Exp
     elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 31749, item2 = 31749})) then -- 2x Damaged Custodus Centurion Helm
         e.self:Say("Excellent! Two less of thos self righteous Validus Custodus goons to worry about!");
         e.other:Faction(55, 1); -- Coterie of Eternal Night
