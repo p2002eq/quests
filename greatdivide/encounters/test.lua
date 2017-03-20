dcb2e443577ede85f9acf493a0eb761b96a4e3d4
Vlad <vpresnyak@gmail.com>
2017-03-19 22:51:34 -0400

    TESTING UPDATE ONLY - REVERT BEFORE MERGING

---
+++ greatdivide/encounters/test.lua
@@ -0,0 +1,7 @@
+
+function event_encounter_load(e)
+    eq.zone_emote(1, "ENCOUNTER LOADED");
+    eq.depop_all(118063);
+    eq.depop_all(118323);
+    eq.depop_all(118324);
+end
--- greatdivide/player.lua
+++ greatdivide/player.lua
@@ -7,3 +7,13 @@ function event_level_up(e)
 	local discs = require('disciplines');
 	discs:train_discs(e, e.self:GetLevel());
 end
+
+function event_say(e)
+	if(e.message:findi("start")) then
+		eq.load_encounter("test");
+		eq.zone_emote(1, "ENCOUNTER LOAD TRIGGER");
+	elseif(e.message:findi("stop")) then
+		eq.unload_encounter("test");
+		eq.zone_emote(1, "ENCOUNTER UNLOADED");
+	end
+end
