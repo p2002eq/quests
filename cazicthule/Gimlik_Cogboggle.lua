a639807c247ba46ec9e62a8c58331d69b0e8a47f
Vlad <vpresnyak@gmail.com>
2017-03-20 01:43:04 -0400

    Cazicthule - Gnome walk 2.0 with new grid

--- cazicthule/Gimlik_Cogboggle.lua
+++ cazicthule/Gimlik_Cogboggle.lua
@@ -1,3 +1,60 @@
+-- Gimlik Cogboggle for CT 'gnome walk'
+
+function event_spawn(e)
+	tracker = 0;          -- tracking of event progress
+	encount2 = false;
+	encount3 = false;
+	encount4 = false;
+	trade = true;
+	event = false;
+end
+
+function event_waypoint_arrive(e)
+    if (e.wp == 10 and tracker < 1) then     -- encounter1
+        tracker = 1;
+		e.self:Say("My map is paying off. This mask is just where they said it would be. Just give me a minute to scribble this down and we'll continue. Please continue to scan the room. I'll feel much better if you're eaten first... err... if you watch my back.");
+		eq.set_timer('adds', math.random(30) * 1000);
+	elseif (e.wp == 23 and tracker < 2) then     -- encounter2
+		tracker = 2;
+		e.self:Say("Well look at this thing! A splendid example of a Tae Ew sacrificial death mask or something. At first glance, this thing looked like beautifully carved wood, but alas it is just skin.");
+        eq.set_timer('adds', math.random(30) * 1000);
+	elseif (e.wp == 37 and  tracker < 3) then     -- encounter3
+		tracker = 3;
+		e.self:Say("Well, that was a nice little trek. Take a breather while I note this area in my journal. I know that you must be tired. I haven't seen a head as large as yours in quite some time. Lean it against the wall or something. We'll need to leave in a moment.")
+        eq.set_timer('adds', math.random(30) * 1000);
+	elseif (e.wp == 50 and  tracker < 4) then     -- encounter4
+		tracker = 4;
+		e.self:Say("Oh joy, there's nothing down this hallway. That will teach me to follow you again. Let me look at my map and see if I can undo your handy work. One moment please. Shheeesh!")
+        eq.set_timer('adds', math.random(30) * 1000);
+	elseif (e.wp == 67 and tracker < 5) then     -- BOSS
+		tracker = 5;
+        eq.spawn2(48377, 0, 0, e.self:GetX()+5, e.self:GetY()+5, e.self:GetZ(), 256-e.self:GetHeading());
+		e.self:Say("Ack ack ack! Eat them not me!");
+	elseif (e.wp == 76 and tracker == 5) then	-- returned to pyramid
+		e.self:Say("Well, here we are. See, you didn't even have to break a sweat. I'm all ready to... hmm... wait, I seem to have dropped my favorite quill. Did you pick it up by chance? I'll add a little something to your payment if you did.");
+		event = false;
+		trade = true;
+    end
+end
+
+function event_timer(e)
+	if e.timer == 'adds' then
+		spawn_adds(e.self)
+	end
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+	
+	if trade and item_lib.check_turn_in(e.self, e.trade, {item1 = 8723}) then
+		e.self:Emote("begins to cast a spell. Gimlik says, 'Well, you've got a good eye on you for being so daft. Thank you for returning my quill. Take care!' Gimlik gates.")
+		e.other:QuestReward(e.self,0,0,0,math.random(50),eq.ChooseRandom(8730,8728,8727,8729,8726),250000) -- random chance for reward out of the 5 items.
+		eq.depop_with_timer()
+	end
+	
+	item_lib.return_items(e.self, e.other, e.trade);
+end
+
 function event_say(e)
 	if not event then
 		if e.message:findi("hail") then
@@ -6,6 +63,17 @@ function event_say(e)
 			e.self:SetNPCFactionID(1006);
 			e.self:AssignWaypoints(151);
 			e.self:Say("Well look at you! You see, you're off to a great start! Now, just follow me and yell out if something stabs you or maims you in anyway. Never fear, I've memorized gate and have complete confidence in your ability to fend off danger for at least five seconds. Follow me... follow me.");
+			event = true;
+			trade = false;
 		end
+	end
+end
+
+function spawn_adds(e_self)
+	local spawnNum = math.random(2,4);
+	for i = 1, spawnNum do
+		local xoff = eq.ChooseRandom(5, -5)
+		local yoff = eq.ChooseRandom(5, -5)
+		spawn_mob(48073, 0, 0, e_self:GetX()+xoff, e_self:GetY()+yoff, e_self:GetZ(), 256-e_self:GetHeading());
 	end
 end
