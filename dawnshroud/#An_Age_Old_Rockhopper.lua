e0732c68b1e8be2cff0ca36df4ad9fcaeb1c6340
Trust <fryguy503@gmail.com>
2017-04-09 19:11:41 -0400

    DSP - Rockhopper/Sambata Cycle
    Fixing spawn_condition "" for zone name (Prevent crashes)

--- dawnshroud/#An_Age_Old_Rockhopper.lua
+++ dawnshroud/#An_Age_Old_Rockhopper.lua
@@ -2,7 +2,7 @@
 function event_death_complete(e)
     local qglobals = eq.get_qglobals();
     if(qglobals["Rockhopper_Sambata"] == nil) then
-        eq.spawn_condition(dawnshroud,0,2,0);
-        eq.spawn_condition(dawnshroud,0,1,1);
+        eq.spawn_condition("dawnshroud",0,2,0);
+        eq.spawn_condition("dawnshroud",0,1,1);
     end
 end
