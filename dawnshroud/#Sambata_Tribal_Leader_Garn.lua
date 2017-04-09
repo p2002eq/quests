649895a507361a451a9cb5e2f199ffbd1a767f6d
Trust <fryguy503@gmail.com>
2017-04-09 19:08:46 -0400

    DSP - Rockhopper/Sambata Cycle
    Fixing QGlobal call.

--- dawnshroud/#Sambata_Tribal_Leader_Garn.lua
+++ dawnshroud/#Sambata_Tribal_Leader_Garn.lua
@@ -1,6 +1,6 @@
 ---- Rockhopper/Sambata Cycle
 function event_death_complete(e)
-    local qglobals = eq.get_qglobals(e.self,e.other);
+    local qglobals = eq.get_qglobals();
     if(qglobals["Rockhopper_Sambata"] == nil) then
         eq.spawn_condition(dawnshroud,0,2,1);
         eq.spawn_condition(dawnshroud,0,1,0);
