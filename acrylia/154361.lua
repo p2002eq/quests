cda4b25e7809d7690f609830790693795d885c54
Vlad <vpresnyak@gmail.com>
2017-04-13 02:11:24 -0400

    acrylia - depop timers for named in ring of fire

---
+++ acrylia/154361.lua
@@ -0,0 +1,16 @@
+-- #A_Possessed_Warmaster (154361) in Acrylia for Ring of Fire
+
+function event_combat(e)
+    if e.joined then
+        eq.stop_timer('depop');
+    else
+        eq.set_timer('depop', 60 * 1000);
+    end
+end
+
+function event_timer(e)
+    if e.timer == 'depop' then
+        e.self:Say("The tresspassers have been slain. Glory to the master!");
+        eq.depop();
+    end
+end
