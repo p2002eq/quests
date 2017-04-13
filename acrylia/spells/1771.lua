20be0791a28affdb62bfa5f99781193ba5ec29c4
Vlad <vpresnyak@gmail.com>
2017-04-13 00:55:00 -0400

    acrylia - working CoH block in acrylia complete with Grimling Hero

--- acrylia/spells/1771.lua
+++ acrylia/spells/1771.lua
@@ -2,7 +2,7 @@
 
 function event_spell_effect(e)
     local caster = eq.get_entity_list():GetClientByID(e.caster_id);
-    caster:Message(0, "A voice whispers in your ear: There are no heroes here... except this one.";
+    caster:Message(13, "A voice whispers in your ear: There are no heroes here... except this one.");
 
     eq.spawn2(154373, 0, 0, caster:GetX() + math.random(-5, 5), caster:GetY() + math.random(-5, 5), caster:GetZ() + 1, 0):AddToHateList(caster, 100);
     
