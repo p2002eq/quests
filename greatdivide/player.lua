d56e95e97edf17f289e7d2d7f7ca26331820b751
Vlad <vpresnyak@gmail.com>
2017-04-14 03:15:00 -0400

    greatdivide - cleanup

--- greatdivide/player.lua
+++
@@ -1,9 +0,0 @@
-function event_enter_zone(e)
-	local discs = require('disciplines');
-	discs:update_discs(e, e.self:GetLevel());
-end
-
-function event_level_up(e)
-	local discs = require('disciplines');
-	discs:train_discs(e, e.self:GetLevel());
-end
