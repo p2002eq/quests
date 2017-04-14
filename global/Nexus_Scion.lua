2fc9a24b21a99772725386f6e1e1aca95312f5e9
Vlad <vpresnyak@gmail.com>
2017-04-14 03:30:50 -0400

    testing global nexus scion script

--- global/Nexus_Scion.lua
+++ global/Nexus_Scion.lua
@@ -1,8 +1,9 @@
--- Nexus_Scion (118086) in Great Divide
+-- Nexus_Scion in multiple zones
 
 local ThreadManager = require("thread_manager");
  
 function event_spawn(e)
+    port_locs = { e.self:GetX()-100, e.self:GetX()+100, e.self:GetY()-100, e.self:GetY()+100 }
 	eq.set_timer("heartbeat", 500);
     
 	ThreadManager:Clear();
@@ -24,17 +25,18 @@ function event_say(e)
 end
 
 function luclin_cycle(evt)
+    local locs = { evt.self:GetX(), evt.self:GetY(), evt.self:GetZ() };
     while true do
-        eq.zone_emote(0, "A Mystic Voice says 'The portal to Luclin will become active in five minutes. Please begin gathering at the Spires if you wish to travel.'")
-        ThreadManager:Wait(120)
-        eq.zone_emote(0, "A Mystic Voice says 'As a reminder, the portal will become active in three minutes.'")
-        ThreadManager:Wait(60);
-        eq.zone_emote(0, "A Mystic Voice says 'Two minutes till teleportation. Please be prepared to step inside the circle of Spires.'")
-        ThreadManager:Wait(60);
-        eq.zone_emote(0, "A Mystic Voice says 'One minute till teleportation. Please stand inside the circle of Spires and have a shard upon your person.'")
-        ThreadManager:Wait(60);
+        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'The portal to Luclin will become active in five minutes. Please begin gathering at the Spires if you wish to travel.'")
+        ThreadManager:Wait(12)
+        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'As a reminder, the portal will become active in three minutes.'")
+        ThreadManager:Wait(6);
+        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'Two minutes till teleportation. Please be prepared to step inside the circle of Spires.'")
+        ThreadManager:Wait(6);
+        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'One minute till teleportation. Please stand inside the circle of Spires and have a shard upon your person.'")
+        ThreadManager:Wait(6);
         luclin_port(eq.get_entity_list():GetClientList());
-        ThreadManager:Wait(600);
+        ThreadManager:Wait(60);
     end
 end
 
@@ -42,7 +44,7 @@ function luclin_port(player_list)
 	if player_list ~= nil then
 		for player in player_list.entries do
             local x, y = player:GetX(), player:GetY();
-            if player:HasItem(19720) and x > -2795 and x < -2605 and y > -1985 and y < -1805 then -- has stone, is within spires, then port
+            if player:HasItem(19720) and x > port_locs[1] and x < port_locs[2] and y > port_locs[3] and port_locs[4] then -- has stone, is within spires, then port
                 player:NukeItem(19720);
 				player:SpellFinished(2935, player);
 			end
