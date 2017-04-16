034ee25772027721981b9bfccce74620d34f000e
Vlad <vpresnyak@gmail.com>
2017-04-15 21:14:30 -0400

    thegrey - changing trigger mobs

--- thegrey/a_creature.lua
+++ thegrey/a_creature.lua
@@ -1,9 +1,9 @@
--- TRAP_PH (171071) in the Grey for trap mobs
+-- a_creature (171062) in the Grey for rock trap mobs
 
 function event_spawn(e)
 	local xloc = e.self:GetX();
 	local yloc = e.self:GetY();
-	eq.set_proximity(xloc - 30, xloc + 30, yloc - 30, yloc + 30);
+	eq.set_proximity(xloc - 40, xloc + 40, yloc - 40, yloc + 40);
 end
 
 function event_enter(e)
@@ -12,30 +12,22 @@ function event_enter(e)
 		local x, y, z, h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
         
         local mob;
-        if chance <= 20 then
+        if chance <= 50 then
             mob = 171073;
-        elseif chance <= 40 then
+        elseif chance <= 64 then
             mob = 171074;
-        elseif chance <= 60 then
-            mob = 171076;
-        elseif chance <= 78 then
-            mob = 171077;
         elseif chance <= 96 then
-            mob = 171078;
+            mob = 171076;
         elseif chance <= 98 then
             mob = 171081;
         elseif chance <= 100 then
             mob = 171086;
         end
-        
-        if chance > 60 and chance <= 96 then
-            eq.local_emote({ x, y, z }, 0, 150, 'The sand at your feet explodes into violent motion!');
-        else
-            eq.local_emote({ x, y, z }, 0, 150, 'A creature of living stone leaps out at you from amongst the rocks!');
-        end
-        
+
 		local spawned = eq.spawn2(mob, 0, 0, x, y, z, h);
         eq.set_timer('depop', 17 * 60 * 1000, spawned);
-		eq.depop_with_timer()
+        
+        e.self:Emote("of living stone leaps out at you from amongst the rocks!");
+        eq.depop_with_timer();
 	end
 end
