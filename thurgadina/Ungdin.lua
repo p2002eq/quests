807037092bfd3d22844a2cdd9c2d67f98ba4607e
Trust <fryguy503@gmail.com>
2017-05-12 18:37:36 -0400

    Velium Retrieval - Cleaned up

--- thurgadina/Ungdin.lua
+++ thurgadina/Ungdin.lua
@@ -7,19 +7,15 @@ function event_say(e)
 end
 
 function event_trade(e)
-
 	local item_lib = require('items');
 	local qglobals = eq.get_qglobals(e.self, e.other);
-
-	if item_lib.check_turn_in(e.self, e.trade, {item1 = 1725}) then
+	if item_lib.check_turn_in(e.self, e.trade, {item1 = 1725}) then -- Velium Delivery Note
 		e.self:Say("Hmmm, not sure why you're giving me all this. <cough> 50 pieces <cough> of platinum <cough> Whew, wow, I may be coming down with <cough> and the note <cough> something.");
 		eq.set_global("shipment", "1", 0, "F");
 	elseif qglobals['shipment'] == '1' and item_lib.check_turn_in(e.self, e.trade, {platinum = 50}) then	
 		e.self:Emote("'s eyes dart about the room before retrieving a large box from under the table. He takes the platinum and the note, then hands you the box without a word.");
-		e.other:SummonItem(29064);
+		e.other:SummonItem(29064); -- A shipment of Velium Ore
 		eq.delete_global("shipment");
 	end
-
-	item_lib.return_items(e.self, e.other, e.trade);	
-
+	item_lib.return_items(e.self, e.other, e.trade);
 end
