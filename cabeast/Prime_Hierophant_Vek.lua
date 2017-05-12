f1901b70daf6e1d20d8e35b239855b67fbb235d8
Trust <fryguy503@gmail.com>
2017-05-12 16:43:55 -0400

    Trooper Scale Armor - Revamp
    Fixed syntax

--- cabeast/Prime_Hierophant_Vek.lua
+++ cabeast/Prime_Hierophant_Vek.lua
@@ -48,9 +48,3 @@ function event_trade(e)
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
-
---Submitted by Jim Mills, with thanks to BWStripes
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
