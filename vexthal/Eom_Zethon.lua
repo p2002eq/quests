0049c891f56e216728c5bf15f80f4c8860007fd4
Eric <erog84@gmail.com>
2017-05-23 00:07:16 -0400

    Added all mobs that split in VT ready to be tested

---
+++ vexthal/Eom_Zethon.lua
@@ -0,0 +1,26 @@
+function event_death_complete(e)
+   local ran = math.random(1, 5)
+   local mob
+   local x, y, z , h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
+   if ran == 1 then
+      --qua 
+      mob = 158021
+   elseif ran == 2 then
+      --zov
+      mob = 158022
+   elseif ran == 3 then
+      --zun
+      mob = 158070
+   elseif ran == 4 then
+      --pli
+      mob = 158036
+   elseif ran == 5 then
+      -- eom
+      mob = 158061
+   end
+
+   local spawned = eq.spawn2(mob, 0, 0, x, y, z, h);
+   eq.set_timer('depop', 30 * 60 * 1000, spawned);
+   eq.depop_with_timer();
+
+end
---
+++ vexthal/Zov_Va_Liako.lua
@@ -0,0 +1,26 @@
+function event_death_complete(e)
+   local ran = math.random(1, 3)
+   local mob
+   local x, y, z , h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
+   if ran == 1 then
+      --qua 
+      mob = 158075
+   elseif ran == 2 then
+      --zov
+      mob = 158065
+   elseif ran == 3 then
+      --zun
+      mob = 158018
+   elseif ran == 4 then
+      --pli
+      mob = 158072
+   elseif ran == 5 then
+      -- eom
+      mob = 158057
+   end
+
+   local spawned = eq.spawn2(mob, 0, 0, x, y, z, h);
+   eq.set_timer('depop', 30 * 60 * 1000, spawned);
+   eq.depop_with_timer();
+
+end
