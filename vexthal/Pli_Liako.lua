780c0acc4af239241fc837213982dc6ac47ac860
Eric <erog84@gmail.com>
2017-05-22 23:39:35 -0400

    Added Liakos to split mobs

--- vexthal/Pli_Liako.lua
+++ vexthal/Pli_Liako.lua
@@ -1,22 +1,22 @@
 function event_death(e)
-   local ran = math.random(0, 6)
+   local ran = math.random(1, 4)
    local mob
    local x, y, z , h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
    if ran == 1 then
-      --zun
-      mob = 158018
-   elseif ran == 2 then
-      --zov
-      mob = 158065
-   elseif ran == 3 then
       --qua 
       mob = 158019
+   elseif ran == 2 then
+      --zov
+      mob = 158055
+   elseif ran == 3 then
+      --zun
+      mob = 158071
    elseif ran == 4 then
       --pli
       mob = 158037
    elseif ran == 5 then
       -- eom
-      mob = 158057
+      mob = 158073
    end
 
    local spawned = eq.spawn2(mob, 0, 0, x, y, z, h);
--- vexthal/Qua_Liako.lua
+++ vexthal/Qua_Liako.lua
@@ -1,5 +1,5 @@
-function event_death(e)
-   local ran = math.random(0, 4)
+function event_death_complete(e)
+   local ran = math.random(1, 3)
    local mob
    local x, y, z , h = e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading();
    if ran == 1 then
@@ -7,22 +7,19 @@ function event_death(e)
       mob = 158019
    elseif ran == 2 then
       --zov
-      mob = 158065
+      mob = 158055
    elseif ran == 3 then
       --zun
-      mob = 158018
+      mob = 158071
    elseif ran == 4 then
       --pli
       mob = 158037
    elseif ran == 5 then
       -- eom
-      mob = 158057
+      mob = 158073
    end
-
    local spawned = eq.spawn2(mob, 0, 0, x, y, z, h);
    eq.set_timer('depop', 30 * 60 * 1000, spawned);
    eq.depop_with_timer();
 
 end
-
-
