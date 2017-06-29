b3bf949b026ec93cbaa7054443e9a5ed1c26e4b0
Lance - Daeron <lancervt@gmail.com>
2017-06-28 23:25:01 -0400

    qglobals added for Idol/AoW cycle

--- kael/#The_Idol_of_Rallos_Zek.lua
+++ kael/#The_Idol_of_Rallos_Zek.lua
@@ -1,5 +1,6 @@
 --on death spawns the Avatar of War
 
 function event_death_complete(e)
-	eq.spawn2(113627,0,0,1291,1252,-97,254.5);
+	eq.spawn2(113627,0,0,1291,1252,-97,254.5);  --spawns Avatar of War
+	eq.set_global("Idol","2",3,"D6");			--sets qglobal value to 2 (Idol dead)
 end
--- kael/The_Statue_of_Rallos_Zek.lua
+++ kael/The_Statue_of_Rallos_Zek.lua
@@ -1,9 +1,13 @@
--- On death, spawns the Idol of Rallos Zek
+--The_Statue_of_Rallos_Zek(NPC # 113628)
+-- On death, spawns the Idol of Rallos Zek and sets qglobal to indicate position in cycle
+
 function event_death_complete(e)
     e.self:Shout("Protect the Idol of Zek!");
 	eq.spawn2(113629,0,0,1292,1267,-35,254.5); -- Idol of Rallos Zek
+	eq.set_global("Idol","1",3,"D6");
 end
 
 function event_spawn(e)
-    eq.depop_all(113629);
+    eq.depop_all(113629);  --Despawns Idol of Zek
+	eq.delete_global("Idol");
 end
