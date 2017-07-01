f721127e86b8ec8613d617012719d12a5900d50d
Trust <fryguy503@gmail.com>
2017-06-30 20:43:39 -0400

    The Grammar Manual - Cleaned up and noted. Also moved iceclad perls into the perl directory.

--- iceclad/an_agitated_snowfang.lua
+++ iceclad/an_agitated_snowfang.lua
@@ -1,5 +1,6 @@
+-- The Grammar Manual
 function event_spawn(e)
-	eq.set_timer("depop", 60000); -- set timer for 1 minute
+	eq.set_timer("depop", 300000); -- set timer for 5 minute
 end
 
 function event_timer(e)
@@ -12,7 +13,7 @@ function event_say(e)
 		e.self:Emote("shivers and growls to itself.");
 		e.self:Say("Much thanks must I give. Furless one still wants magic thing, after all the troubles? Say quickly. Much pain from attack. Must go, shaman help.");
 	elseif(e.message:findi("magic thing")) then
-		e.other:QuestReward(e.self,0,0,0,0,30042);
+		e.other:QuestReward(e.self,0,0,0,0,30042); -- Chapter P
 		eq.depop();
 	    eq.stop_timer("depop");
 	end
