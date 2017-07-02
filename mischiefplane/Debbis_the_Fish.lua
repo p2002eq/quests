720d822f356f3398e6ce4664f1b04bb1b15923a5
Lance - Daeron <lancervt@gmail.com>
2017-07-02 01:03:53 -0400

    Debbis the fish quest updates for LUA

---
+++ mischiefplane/Debbis_the_Fish.lua
@@ -0,0 +1,16 @@
+--Debbis the Fish (NPCID# 126232)
+--zone:  mischiefplane
+
+--Quest script to handle turn-in of rabbit foot off Stomples to restore Denizens of Mischief Faction
+
+
+function event_trade(e)
+    local item_lib = require("items");
+    if(item_lib.check_turn_in(e.self, e.trade, {item1 = 6883})) then --Check for a rabbit foot
+        e.self:QuestSay(e.other,"Oh wow! A rabbits foot!! Thank you so much. Here take this, I won't need this smelly thing any more.");  
+		e.other:QuestReward(e.self,0,0,0,0,3190,100);  --  Ivy Etched Gauntlets
+		e.other:Faction(391,51);   -- +51 to Denizens of Mischief Faction
+    end
+
+    item_lib.return_items(e.self, e.other, e.trade)
+end
