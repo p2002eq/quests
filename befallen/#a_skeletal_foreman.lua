c6abcea0549856d74ed9682891038bffc453c6a5
Trust <fryguy503@gmail.com>
2017-07-05 08:58:25 -0400

    Befallen Revamp - Needs updates to Resurrection of Marnek the Wicked

--- befallen/#a_skeletal_foreman.lua
+++ befallen/#a_skeletal_foreman.lua
@@ -1,8 +1,15 @@
-function event_death_complete(e)
-	eq.unique_spawn(36091,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); ----Sir Ariam Arcanum
+function event_spawn(e)
+	e.self:Shout("Dig! Dig faster, you fools! Our masters say this ghost is all that stands in the way of the great one's return. Uncover his hiding place so that the forces of the Burning Dead might destroy him and recover its legacy!");
+	eq.set_timer("shout", 300000); -- 5 Minutes
 end
 
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
+function event_timer(e)
+	if (e.timer == "shout") then
+		e.self:Shout("Dig! Dig faster, you fools! Our masters say this ghost is all that stands in the way of the great one's return. Uncover his hiding place so that the forces of the Burning Dead might destroy him and recover its legacy!");
+		eq.set_timer("shout", 300000); -- 5 Minutes
+	end
+end
+
+function event_death_complete(e)
+	eq.unique_spawn(36091,0,0,-43.5,-339.8,-10.9); ----Sir Ariam Arcanum
+end
