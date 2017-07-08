cdf575b7c8538b205d4a2878852acae87af08930
Trust <fryguy503@gmail.com>
2017-07-08 10:32:01 -0400

    Crushbone - Added comments to lua and updated reward functions

--- crushbone/a_dwarven_smith.lua
+++ crushbone/a_dwarven_smith.lua
@@ -8,25 +8,16 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 10351})) then -- Brass Earring
 		e.self:Say("Outstanding! If you can kill the taskmaster, you might prove useful in recovering the items the orcs took from me when they caught me out in the Faydarks. Interested in helping me out?");
-		e.other:SummonItem(13850); -- Unfinished Blade Mold
-		e.other:Ding();
-		e.other:AddEXP(3000);
+		e.other:QuestReward(e.self,0,0,0,0,13850,3000); -- Unfinished Blade Mold
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 9307})) then -- Shiny Brass Shield
 		e.self:Say("MY SHIELD!! Excellent, laddie! Next item on the list is my ringmail. Once I have my [ringmail], I am out of here for good. I will finally be able to leave!");
-		e.other:SummonItem(13851); -- Unfinished Blade Mold
-		e.other:Ding();
-		e.other:AddEXP(3000);
-	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 3301})) then
+		e.other:QuestReward(e.self,0,0,0,0,13851,3000); -- Unfinished Blade Mold
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 3301})) then -- Dwarven Ringmail Tunic
 		e.self:Say("AHA!! My ringmail!! My, you ARE a resourceful one! Here is your reward as promised. It is exquisite, if I do say so myself. You might also wish to tell the priest in the cell over there that the king is dead. I am sure he will be pleased to hear that and might even be able to help you further. Farewell! It's time for me to make my escape!");
-		e.other:SummonItem(6315);
-		e.other:Ding();
-		e.other:AddEXP(3000);
+		e.other:QuestReward(e.self,0,0,0,0,6315,3000); -- Dwarven Mace
 		eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade);
 end
-
--- EOF a dwarven smith
