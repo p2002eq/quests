11f78d9ae0c4a2c538479f22f566e5efba094dab
Trust <fryguy503@gmail.com>
2017-07-23 18:14:33 -0400

    fungusgrove - Saylinks

--- fungusgrove/Rebecca_Cabbageleaf.lua
+++ fungusgrove/Rebecca_Cabbageleaf.lua
@@ -3,9 +3,9 @@
 
 function event_say(e)
 	if e.message:findi("hail") then
-		e.self:Emote(string.format("seems lost in thought and looks up only briefly from her work. 'Oh, hello %s, welcome to the New Thicket Inn! If you'll excuse me, I'm a bit busy copying this [scroll]. I'll be with you in a moment to chat, or to trade if you've come about the [earring]'", e.other:GetName()));
+		e.self:Emote(string.format("seems lost in thought and looks up only briefly from her work. 'Oh, hello %s, welcome to the New Thicket Inn! If you'll excuse me, I'm a bit busy copying this [" .. eq.say_link("scroll") .. "]. I'll be with you in a moment to chat, or to trade if you've come about the [" .. eq.say_link("earring") .. "]'", e.other:GetName()));
 	elseif e.message:findi("scroll") then
-		e.self:Emote("looks up again with a quiet smile, 'I'm making a copy of a new Protection spell, it's very exciting! The druids of Norrath have just completed an important ritual, and the gods have blessed us with this new spell! I'd be happy to scribe you a copy, if you were to fetch me the [ingredients].'");
+		e.self:Emote("looks up again with a quiet smile, 'I'm making a copy of a new Protection spell, it's very exciting! The druids of Norrath have just completed an important ritual, and the gods have blessed us with this new spell! I'd be happy to scribe you a copy, if you were to fetch me the [" .. eq.say_link("ingredients") .. "].'");
 	elseif e.message:findi("ingredients") then
 		e.self:Say("Oh, let me see... I'll need a sheet of fancy Blessed parchment, a small flask of Dark Root ink, and of course a large quill. I think a Stormraven's quill would be best, certainly better than this one I've been using so far. Bring those items to me and I'll be happy to make you a copy for yourself. I'm not sure where you'll find all of those, I seem to have bought up the last of the stock in several places. Someone's bound to have them for sale, good luck with it!");
 	elseif e.message:findi("earring") then
