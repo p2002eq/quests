9b3a84d6a386c1ebf1d8647bc00dd3b09ca5d3f9
Trust <fryguy503@gmail.com>
2017-07-22 23:10:30 -0400

    Cazic - Saylinks

--- cazicthule/Gimlik_Cogboggle.lua
+++ cazicthule/Gimlik_Cogboggle.lua
@@ -60,7 +60,7 @@ end
 function event_say(e)
 	if not event then
 		if e.message:findi("hail") then
-			e.self:Say("Well met, noble soul! I'm Gimlik Cogboggle. You are correct, the Gimlik Cogboggle, renowned adventurer, scholar, and yes... creator of spells. Please don't goggle or touch. Your opportunity has arrived, my friend. I was sent here to complete a very important task and for that, I will need some slight assistance. If you will be kind enough to follow me, I'll show you to your doom... err... get this show on the road. Just let me know when you're [ready].");
+			e.self:Say("Well met, noble soul! I'm Gimlik Cogboggle. You are correct, the Gimlik Cogboggle, renowned adventurer, scholar, and yes... creator of spells. Please don't goggle or touch. Your opportunity has arrived, my friend. I was sent here to complete a very important task and for that, I will need some slight assistance. If you will be kind enough to follow me, I'll show you to your doom... err... get this show on the road. Just let me know when you're [" .. eq.say_link("ready") .. "].");
 		elseif e.message:findi("ready") then
 			e.self:SetNPCFactionID(1006);
 			e.self:AssignWaypoints(151);
