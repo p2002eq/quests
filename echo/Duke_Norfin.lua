014f290f955f1cbb1609d1758585efffcb7afc5f
Trust <fryguy503@gmail.com>
2017-07-23 01:58:58 -0400

    echo - Saylinks

--- echo/Duke_Norfin.lua
+++ echo/Duke_Norfin.lua
@@ -3,11 +3,11 @@ function event_say(e)
     fac = e.other:GetFaction(e.self);
     if(fac <= 5) then
         if(e.message:findi("hail")) then
-            e.self:Say("Hello, " .. e.other:GetName() .. ". My guards seem to find you harmless enough, but I have yet to determine if you are trustworthy. We are always on the lookout for additional hands to assist in our work. Perhaps you are... sympathetic to our [situation].");
+            e.self:Say("Hello, " .. e.other:GetName() .. ". My guards seem to find you harmless enough, but I have yet to determine if you are trustworthy. We are always on the lookout for additional hands to assist in our work. Perhaps you are... sympathetic to our [" .. eq.say_link("situation") .. "].");
         elseif(e.message:findi("situation")) then
-            e.self:Say("Those blasted Houses in Shadow Haven. They think they have the right to own all trade going through the Haven. Well it's about time they learn that trade will continue with or without their consent. I know a few people on the Trade Commission who understand the way things really [work] and they don't care who does the trading as long as they get their cut.");
+            e.self:Say("Those blasted Houses in Shadow Haven. They think they have the right to own all trade going through the Haven. Well it's about time they learn that trade will continue with or without their consent. I know a few people on the Trade Commission who understand the way things really [" .. eq.say_link("work") .. "] and they don't care who does the trading as long as they get their cut.");
         elseif(e.message:findi("work")) then
-            e.self:Say("Excellent! We like to keep changing who we use for deliveries. Keeps those dumb Defenders guessing and reduces the chances of goods not reaching their [destination].");
+            e.self:Say("Excellent! We like to keep changing who we use for deliveries. Keeps those dumb Defenders guessing and reduces the chances of goods not reaching their [" .. eq.say_link("destination") .. "].");
         elseif(e.message:findi("destination")) then
             e.self:Say("I have an important shipment coming from Katta Castellum that's late. The courier who was picking up the shipment should still be there. I'd like for you to go over there and see what the hold up is. Take this note and show it to Jarin Lorean so he knows you're on official business. Report back to me with news. Better yet, return with the shipment if you can.");
             e.other:SummonItem(19841); -- Note to Jarin
