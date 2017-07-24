95ffb9fbcffea89029d0b0ac9cb69dc1646d221f
Trust <fryguy503@gmail.com>
2017-07-24 18:42:10 -0400

    Froglok Disable Update

--- load2/player.lua
+++ load2/player.lua
@@ -5,11 +5,18 @@ function event_enter_zone(e)
     end
     e.self:MovePC(185,-436.45,604,-87.1,62);
     e.self:Message(14,"Your race is not destined for this world. You have been banished here, you have 3 minutes to make your peace, you will no longer be able to return to this world.");
-    eq.set_timer("FrogDeath", 1000);
+    eq.set_timer("FrogDeath", 1000); -- 1 Sec
 end
 
 function event_timer(e)
     if(e.timer == "FrogDeath") then
+        e.self:Message(13,"Frogloks are not an allowed race. Please log off and delete the character. If you are found to be attempting to circumvent this lockout you will be actioned.");
+        eq.set_timer("FrogDeath2", 30000); -- 30 Sec
+        eq.set_timer("FrogDeath3", 180000); -- 3 Min
+    elseif(e.timer == "FrogDeath2") then
+        e.self:Message(13,"Frogloks are not an allowed race. Please log off and delete the character. If you are found to be attempting to circumvent this lockout you will be actioned.");
+        eq.set_timer("FrogDeath2", 30000); -- 30 Sec
+    elseif(e.timer == "FrogDeath3") then
         eq.set_global("FrogDeath","1",0,"F");
         e.self:Kick();
     end
