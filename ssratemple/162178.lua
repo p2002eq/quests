b77617fec1435b3aba35247b8c47eb1f97477edb
Lance - Daeron <lancervt@gmail.com>
2017-07-27 17:16:48 +0000

    remove depop function

--- ssratemple/162178.lua
+++ ssratemple/162178.lua
@@ -1,19 +1,5 @@
 -- #Rhag`Zhezum (162178) for Rhag cycle AKA Rhag 1
 
-function event_spawn(e)
-	eq.delete_global("AL_Cycle");	--reset qglobal trigger
-	eq.depop(162192);  			--depop Rhag`Mozdezh if up
-	eq.depop(162193);			--depop Rhag 2 guard
-	eq.depop(162194);			--depop Rhag 2 guard
-	eq.depop(162195);			--depop Rhag 2 guard
-	eq.depop(162196);			--depop Rhag 2 guard
-	eq.depop(162517);  			--depop Arch Lich if up
-	eq.depop(162518); 			--depop AL Guard
-	eq.depop(162519); 			--depop AL Guard
-	eq.depop(162090); 			--depop AL Guard
-	eq.depop(162090); 			--depop AL Guard
-end
-
 function event_death_complete(e)
 	eq.unique_spawn(162192, 0, 0, 644, -280, 146, 196); -- spawn Rhag`Mozdezh
 	eq.spawn2(162193, 0, 0, 561, -235, 138, 128);  -- guard
