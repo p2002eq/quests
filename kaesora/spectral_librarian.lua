69ce21a20f36af79b149146f4a8e0056aa20de98
Trust <fryguy503@gmail.com>
2017-07-27 17:11:57 -0400

    Kaesora - Saylinks

--- kaesora/spectral_librarian.lua
+++ kaesora/spectral_librarian.lua
@@ -1,5 +1,4 @@
 -- Test of Charm - enchanter epic - Enchanted Diamond
-
 function event_trade(e)
 	local item_lib = require("items");
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 10631})) then
@@ -10,8 +9,3 @@ function event_trade(e)
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
-
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
--- kaesora/tortured_librarian.lua
+++ kaesora/tortured_librarian.lua
@@ -13,8 +13,3 @@ function event_trade(e)
 	end
 	item_lib.return_items(e.self, e.other, e.trade) -- return unused items
 end
-
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
