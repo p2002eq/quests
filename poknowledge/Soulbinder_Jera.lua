e234b84aff1bce048c19f9dc77b5827817fe945b
Trust <fryguy503@gmail.com>
2017-07-28 19:16:59 -0400

    Buff and corpse - SB

--- poknowledge/Soulbinder_Jera.lua
+++ poknowledge/Soulbinder_Jera.lua
@@ -1,7 +1,8 @@
 function event_say(e)
 	if(e.message:findi("Hail")) then
 		e.self:Say("Greetings " .. e.other:GetName() .. ". When a hero of our world is slain their soul returns to the place it was last bound and the body is reincarnated. As a member of the Order of Eternity  it is my duty to [" .. eq.say_link("bind my soul",false, "bind your soul") .. "] to this location if that is your wish.");
-		e.self:Say("'Would you like to be ported to the [" .. eq.say_link("Arena") .. "], [" .. eq.say_link("Jaggedpine") .. "] or the [" .. eq.say_link("Plane of Mischief") .. "]?");
+		e.self:Say("'Would you like to be ported to the [" .. eq.say_link("Arena") .. "], [" .. eq.say_link("Jaggedpine") .. "] or the [" .. eq.say_link("Plane of Mischief") .. "]? I can also [" .. eq.say_link("Trust is my god and he will bless me with buffs",false,"Buff you") .. "]");
+		e.self:Say("Also please stop leaving your [" .. eq.say_link("corpses") .. "] around.");
 	elseif(e.message:findi("bind my soul")) then
 		e.self:Say("Binding your soul. You will return here when you die.");
 		e.self:CastSpell(2049,e.other:GetID(),0,1);
@@ -13,6 +14,17 @@ function event_say(e)
 		e.other:MovePC(126, -395, -1410, 112.85, 0);
 	elseif(e.message:findi("arena")) then
 		e.other:MovePC(77, 1265, -73, 1.8, 253);
+	elseif(e.message:findi('buff')) then
+		e.self:SpellFinished(3025,e.other);
+		e.self:SpellFinished(2696,e.other);
+		e.self:SpellFinished(2680,e.other);
+		e.self:SpellFinished(2696,e.other);
+		e.self:SpellFinished(3023,e.other);
+		e.self:SpellFinished(3022,e.other);
+		e.self:SpellFinished(3024,e.other);
+		e.self:SpellFinished(2517,e.other);
+	elseif(e.message:findi('corpses')) then
+		eq.summon_all_player_corpses(e.other:CharacterID(), e.other:GetX(), e.other:GetY(), e.other:GetZ(), e.other:GetHeading());
 	end
 end
 
