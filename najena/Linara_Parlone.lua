104f393cc4d21a737823e955a55c7b41cae55924
Trust <fryguy503@gmail.com>
2017-07-28 08:47:37 -0400

    Najena - Saylinks

--- najena/Linara_Parlone.lua
+++ najena/Linara_Parlone.lua
@@ -2,9 +2,9 @@
 
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Please [help] me out of this horrid place! My family must be terribly worried!");
+		e.self:Say("Please [" .. eq.say_link("help") .. "] me out of this horrid place! My family must be terribly worried!");
 	elseif(e.message:findi("help")) then
-		e.self:Say("If you can find the [key] to my restraints. I should be able to escape.");
+		e.self:Say("If you can find the [" .. eq.say_link("key") .. "] to my restraints. I should be able to escape.");
 	elseif(e.message:findi("key")) then
 		e.self:Say("Last I knew, Najena's priest had the key. I fear he has left this place though. I heard talk of traveling to some estate. I know not where. Oh dear, oh dear! I shall never leave this place!");
 	end
@@ -12,7 +12,6 @@ end
 
 function event_trade(e)
 	local item_lib = require("items");
-	
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 12269})) then -- tarnished bronze key
 		e.self:Say("Oh my! Thank you so very much! here, take this note to my father. I am sure he will reward you well for my safety.");
 		e.other:SummonItem(5573);
--- najena/an_injured_halfling.lua
+++ najena/an_injured_halfling.lua
@@ -1,10 +1,8 @@
 function event_trade(e)
 	local item_lib = require("items");
-	
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 13009})) then
 		e.self:Say("Your kindness is appreciated! If someone can bring me more of these, I might make it out alive!");
 		e.other:SummonItem(34028);
 	end
-	
 	item_lib.return_items(e.self, e.other, e.trade);
 end
