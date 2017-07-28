fcabf00c5b2a987b3550b674444e0118d3ff754e
Lance - Daeron <lancervt@gmail.com>
2017-07-27 22:54:27 -0400

    AL Cycle updated guards to unique spawns

--- ssratemple/162178.lua
+++ ssratemple/162178.lua
@@ -2,10 +2,10 @@
 
 function event_death_complete(e)
 	eq.unique_spawn(162192, 0, 0, 644, -280, 146, 196); -- spawn Rhag`Mozdezh
-	eq.spawn2(162193, 0, 0, 561, -235, 138, 128);  -- guard
-	eq.spawn2(162194, 0, 0, 660, -309, 146, 218);  -- guard
-	eq.spawn2(162195, 0, 0, 660, -246, 146, 165);  -- guard
-	eq.spawn2(162196, 0, 0, 561, -323, 138, 0);  -- guard
+	eq.unique_spawn(162193, 0, 0, 561, -235, 138, 128);  -- guard
+	eq.unique_spawn(162194, 0, 0, 660, -309, 146, 218);  -- guard
+	eq.unique_spawn(162195, 0, 0, 660, -246, 146, 165);  -- guard
+	eq.unique_spawn(162196, 0, 0, 561, -323, 138, 0);  -- guard
 	eq.set_global("AL_Cycle_Rhag2","UP",3,"F");  --sets global to "R2" to flag Rhag`Mozdezh as next target in cycle in case of zone crash/reboot
 end
 
--- ssratemple/AL_Cycle.lua
+++ ssratemple/AL_Cycle.lua
@@ -5,15 +5,15 @@ function event_spawn(e)
 	
 	if (qglobals["AL_Cycle_Rhag2"] == "UP" and eq.get_entity_list():IsMobSpawnedByNpcTypeID(162192) == false) then  --checks for Rhag`Zhezum death and verifies Rhag`Mozdezh is not already up
 		eq.unique_spawn(162192, 0, 0, 644, -280, 146, 196); -- spawn Rhag`Mozdezh
-		eq.spawn2(162193, 0, 0, 561, -235, 138, 128);  -- Rhag 2 guard
-		eq.spawn2(162194, 0, 0, 660, -309, 146, 218);  -- Rhag 2 guard
-		eq.spawn2(162195, 0, 0, 660, -246, 146, 165);  -- Rhag 2 guard
-		eq.spawn2(162196, 0, 0, 561, -323, 138, 0);  	-- Rhag 2 guard
+		eq.unique_spawn(162193, 0, 0, 561, -235, 138, 128);  -- Rhag 2 guard
+		eq.unique_spawn(162194, 0, 0, 660, -309, 146, 218);  -- Rhag 2 guard
+		eq.unique_spawn(162195, 0, 0, 660, -246, 146, 165);  -- Rhag 2 guard
+		eq.unique_spawn(162196, 0, 0, 561, -323, 138, 0);  	-- Rhag 2 guard
 	elseif (qglobals["AL_Cycle_ArchLich"] == "UP" and eq.get_entity_list():IsMobSpawnedByNpcTypeID(162517) == false) then  --checks for Rhag`Mozdezh death and verifies Rhag`Mozdezh is not already up
 		eq.unique_spawn(162517, 0, 0, 418, -141, 264, 0); --Spawns Arch Lich
-		eq.spawn2(162518, 0, 0, 448, -161, 265, 224);  -- AL guard
-		eq.spawn2(162519, 0, 0, 390, -161, 265, 32);  -- AL guard
-		eq.spawn2(162090, 0, 0, 375, -64, 257, 64);  -- AL guard
-		eq.spawn2(162090, 0, 0, 467, -64, 257, 192);  -- AL guard
+		eq.unique_spawn(162518, 0, 0, 448, -161, 265, 224);  -- AL guard
+		eq.unique_spawn(162519, 0, 0, 390, -161, 265, 32);  -- AL guard
+		eq.unique_spawn(162090, 0, 0, 375, -64, 257, 64);  -- AL guard
+		eq.unique_spawn(162090, 0, 0, 467, -64, 257, 192);  -- AL guard
 	end
 end
