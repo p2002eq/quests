270b718a59577c6a454242f8b18960174a75999b
Trust <fryguy503@gmail.com>
2017-08-01 19:08:24 -0400

    nro - Say Links

--- nro/Dorn_B-Dynn.lua
+++ nro/Dorn_B-Dynn.lua
@@ -4,7 +4,7 @@ function event_say(e)
 	elseif(e.message:findi("Zimbittle sent me")) then
 		e.self:Say("I do not know if I could trust you. You certainly do not look as though you are from Grobb, although your stench does hint of that place. If you are truly sent by Zimbittle, what is the passwrod?");
 	elseif(e.message:findi("Mukk")) then
-		e.self:Say("So you were sent by Zimbittle. Take this note Kraagia of Da Bashers in Grobb. Do not lose the note. If it were to fall into the wrong hands of [Goonda], this whole plan could be disrupted. Now, leave, I can no longer tolerate you foul stench!");
+		e.self:Say("So you were sent by Zimbittle. Take this note Kraagia of Da Bashers in Grobb. Do not lose the note. If it were to fall into the wrong hands of [" .. eq.say_link("Goonda") .. "], this whole plan could be disrupted. Now, leave, I can no longer tolerate you foul stench!");
 		e.other:SummonItem(18886);
 	elseif(e.message:findi("Goonda")) then
 		e.self:Say("In the western planes of Karana, you will find Goonda. He seems to be the Ogre shaman in charge out there.");
