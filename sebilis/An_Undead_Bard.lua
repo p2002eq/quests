90fb34edfce56985c9b2e1fd90dafcc635f84560
Trust <fryguy503@gmail.com>
2017-08-02 20:20:34 -0400

    sebilis - Say Links

--- sebilis/An_Undead_Bard.lua
+++ sebilis/An_Undead_Bard.lua
@@ -5,19 +5,19 @@ end
 
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Hail, " .. e.other:GetName() .. ". Are you perchance a minstrel?");
+		e.self:Say("Hail, " .. e.other:GetName() .. ". Are you perchance a [" .. eq.say_link("i am a minstrel",false,"minstrel") .. "]?");
 	elseif(e.message:findi("i am a minstrel")) then
-		e.self:Say("I, too, am a bard, or was rather. I have been cursed by that blasted pile of bones who calls itself, Trakanon.");
+		e.self:Say("I, too, am a bard, or was rather. I have been cursed by that blasted pile of bones who calls itself, [" .. eq.say_link("Trakanon") .. "].");
 	elseif(e.message:findi("trakanon")) then
-		e.self:Say("He is the undead dragon who rules this realm. I came on a quest seeking strings for my new instrument.");
+		e.self:Say("He is the undead dragon who rules this realm. I came on a quest seeking strings for my [" .. eq.say_link("new instrument") .. "].");
 	elseif(e.message:findi("new instrument")) then
-		e.self:Say("I sought to create a Magical Lute.");
+		e.self:Say("I sought to create a [" .. eq.say_link("Magical Lute") .. "].");
 	elseif(e.message:findi("magical lute")) then
-		e.self:Say("One that would make me the greatest bard in all the lands! Are you on a similar quest?");
+		e.self:Say("One that would make me the greatest bard in all the lands! Are you on a [" .. eq.say_link("i am on a similar quest",false,"similar quest") .. "]?");
 	elseif(e.message:findi("i am on a similar quest")) then
-		e.self:Say("Excellent. I have stayed here far too long and I wish to go to my eternal slumber. But before I do so, I ask for one last request.");
+		e.self:Say("Excellent. I have stayed here far too long and I wish to go to my eternal slumber. But before I do so, I ask for one [" .. eq.say_link("last request") .. "].");
 	elseif(e.message:findi("last request")) then
-		e.self:Say("Well then, I would ask you a favor for one in return. If I bring that cursed dragon here, will you kill it for me?");
+		e.self:Say("Well then, I would ask you a favor for one in return. If I bring that cursed dragon here, will you [" .. eq.say_link("i will kill it for you",false,"kill it for me") .. "]?");
 	elseif(e.message:findi("i will kill it for you")) then
 		e.self:Say("Give me the Mystical Lute Body you have. I will return it, don't worry. I only wish to hold an instrument one last time before I leave.");
 	end
@@ -40,8 +40,3 @@ function event_death_complete(e)
 		spawn = 0;
 	end
 end
-
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
