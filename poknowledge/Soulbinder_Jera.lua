e24255f7c9785c7b3463a8201b8d65b872ad958f
Trust <fryguy503@gmail.com>
2017-08-03 10:32:28 -0400

    (Anti Taian Measures) Fixed PoK Soulbinder - Removed Test weekend functionality

--- poknowledge/Soulbinder_Jera.lua
+++ poknowledge/Soulbinder_Jera.lua
@@ -4,25 +4,8 @@ function event_say(e)
 	elseif(e.message:findi("bind my soul")) then
 		e.self:Say("Binding your soul. You will return here when you die.");
 		e.self:CastSpell(2049,e.other:GetID(),0,1);
-	--elseif(e.message:findi("illusion")) then -- Halloween Event	
-	--	e.other:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
-	elseif(e.message:findi("Jaggedpine")) then
-		e.other:MovePC(181, 1800, 1319, -14.78, 12);
-	elseif(e.message:findi("Plane of Mischief")) then
-		e.other:MovePC(126, -395, -1410, 112.85, 0);
-	elseif(e.message:findi("arena")) then
-		e.other:MovePC(77, 1265, -73, 1.8, 253);
-	elseif(e.message:findi('buff')) then
-		e.self:SpellFinished(3025,e.other);
-		e.self:SpellFinished(2696,e.other);
-		e.self:SpellFinished(2680,e.other);
-		e.self:SpellFinished(2696,e.other);
-		e.self:SpellFinished(3023,e.other);
-		e.self:SpellFinished(3022,e.other);
-		e.self:SpellFinished(3024,e.other);
-		e.self:SpellFinished(2517,e.other);
-	elseif(e.message:findi('corpses')) then
-		eq.summon_all_player_corpses(e.other:CharacterID(), e.other:GetX(), e.other:GetY(), e.other:GetZ(), e.other:GetHeading());
+		--elseif(e.message:findi("illusion")) then -- Halloween Event
+		--	e.other:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
 	end
 end
 
