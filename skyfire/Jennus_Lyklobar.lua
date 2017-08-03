74e69643aecea6e03d74382d5b853aa6101eaed9
Trust <fryguy503@gmail.com>
2017-08-02 21:21:57 -0400

    Skyfire - Say Links

--- skyfire/Jennus_Lyklobar.lua
+++ skyfire/Jennus_Lyklobar.lua
@@ -1,6 +1,6 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Hello and well met! A " .. e.other:Race() .. " like you does not often come here.. Do you come seeking what I possess? Fire, fire, fire and fire is mine! I surround myself with fire, I bathe in fire, I consume fire, I am the Master of Fire! Hahahaha, but I digress. Have you come for the element of Fire, seeking to fall to the same fate as Trilith Magi'kot?");
+		e.self:Say("Hello and well met! A " .. e.other:Race() .. " like you does not often come here.. Do you come seeking what I possess? Fire, fire, fire and fire is mine! I surround myself with fire, I bathe in fire, I consume fire, I am the Master of Fire! Hahahaha, but I digress. Have you come for the [" .. eq.say_link("i have come for the element of fire",false,"element of Fire") .. "], seeking to fall to the same fate as Trilith Magi'kot?");
 	elseif(e.message:findi("i have come for the element of fire")) then
 		e.self:Say("The Element of Fire I can provide you, but you must prove you are able to contain its power. Bring me the Torch of the Elements now kept by a doomed lord of merchants, the burning embers from a notable member of wurmkind who can be found a short journey from here and a blazing wand, which you should be familiar with. And you must surrender the tome Rykas gave you. But I think you would be happier if you stayed with me and learned more of the beauty of the one true element, FIRE! FIRE! Hahahahahahah!");
 	end
@@ -14,9 +14,3 @@ function event_trade(e)
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
-
-
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd and manual edits by Speedz
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
