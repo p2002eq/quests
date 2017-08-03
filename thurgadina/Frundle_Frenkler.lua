e1419547fbd596396cc7ff6c6514faee067a93e9
Trust <fryguy503@gmail.com>
2017-08-02 23:43:06 -0400

    thurgadina - Say Links

--- thurgadina/Frundle_Frenkler.lua
+++ thurgadina/Frundle_Frenkler.lua
@@ -1,6 +1,6 @@
 function event_say(e)
 	if(e.message:findi("velium vapors")) then	
-		e.self:Say("Ahh, you come seeking my family recipe? After all you've done for our people it's the least I can do. First you must fashion a velium vial, then combine the vial with two small pieces of velium in this jar. That's all there is to it. Please keep the recipe a secret.");
+		e.self:Say("Ahh, you come seeking my family recipe? After all you've done for our people it's the least I can do. First you must fashion a [" .. eq.say_link("velium vial") .. "], then combine the vial with two small pieces of velium in this jar. That's all there is to it. Please keep the recipe a secret.");
 	elseif(e.message:findi("velium vial")) then			
 		e.self:Say("I'm no potter mind you, but if I remember correctly it's a small brick of velium, three crystalline silks, and two glass shards. Then you'll have to finish up by firing it in a kiln with a high quality firing sheet.");
 		e.other:SummonItem(17513);
--- thurgadina/Mordin_Frostcleaver.lua
+++ thurgadina/Mordin_Frostcleaver.lua
@@ -1,6 +1,6 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Ah, allo there, and welcome to me shop, "..e.other:GetName()..". Ye'll have to excuse the mess but I've been havin a bloody good time here lately. . . HAR HAR, ye get it? Anyways, thanks to you offlanders I now have a selection of some exotic meats never afore seen here on Velious.  I've been amazed at the demand for some of 'em.  I can't seem to keep shark meat in stock any more.  I wonder if eatin' raw fish is catching on?  In addition to supplyin' the freshest meat in Thurgadin I also offer tasks to would-be hunters in the wastes.");
+		e.self:Say("Ah, allo there, and welcome to me shop, "..e.other:GetName()..". Ye'll have to excuse the mess but I've been havin a bloody good time here lately. . . HAR HAR, ye get it? Anyways, thanks to you offlanders I now have a selection of some exotic meats never afore seen here on Velious.  I've been amazed at the demand for some of 'em.  I can't seem to keep shark meat in stock any more.  I wonder if eatin' raw fish is catching on?  In addition to supplyin' the freshest meat in Thurgadin I also offer [" .. eq.say_link("tasks") .. "] to would-be hunters in the wastes.");
 	elseif(e.message:findi("tasks")) then	
 		e.self:Say("Sorry, "..e.other:GetName()..", but I've got no tasks to offer ye this week.  If ye be seriously lookin fer sumthin ta do ask ole Borannin 'bout duties fer the crown..");
 	elseif(e.message:findi("tundrabear sandwich")) then	
@@ -19,10 +19,10 @@ function event_trade(e)
 	local item_lib = require('items');
 
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 1420})) then
-		e.self:Say("Hullo, "..e.other:GetName()..", I see you've signed on for a bit of food service eh? Well, we can always use an extra pair o' hands here dat's fer sure. First things first, warsh yer grubby mitts an' cook up dinner fer good ol' Leif. Should'n be too tough fer ye, he has simple enough taste. Make him a Tundrabear sandwich an' lemme know when he's been fed. Don'tcha ferget ta give him this with his meal.");
+		e.self:Say("Hullo, "..e.other:GetName()..", I see you've signed on for a bit of food service eh? Well, we can always use an extra pair o' hands here dat's fer sure. First things first, warsh yer grubby mitts an' cook up dinner fer good ol' Leif. Should'n be too tough fer ye, he has simple enough taste. Make him a [" .. eq.say_link("Tundrabear sandwich") .. "] an' lemme know when he's been fed. Don'tcha ferget ta give him this with his meal.");
 		e.other:SummonItem(1415);
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 1419 })) then	
-		e.self:Say("Well don, outlander! We'll make a chef of you before we're through. Next you'll need to whip up some Snow Bunny stew fer Missus Coldheart. Again, give her this napkin with her meal an' lemme know when she's been served.");
+		e.self:Say("Well don, outlander! We'll make a chef of you before we're through. Next you'll need to whip up some [" .. eq.say_link("SnowBunny stew") .. "] fer Missus Coldheart. Again, give her this napkin with her meal an' lemme know when she's been served.");
 		e.other:SummonItem(1416);
 		e.other:AddEXP(100000);
 		e.other:Faction(49, 10); --coldain
@@ -30,7 +30,7 @@ function event_trade(e)
 		e.other:Faction(188, -30); --kromrif
 		e.other:Faction(189, -30); --kromzek
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 1423 })) then	
-		e.self:Say("Now yer cookin! Letsee here... Guard Leif, check... Trita Coldheart, check... Ahh yes, next we need an Ulthork meat pie fer Brita. She's a doll, smart too, but she can be testy when she's hungry so get movin'!");
+		e.self:Say("Now yer cookin! Letsee here... Guard Leif, check... Trita Coldheart, check... Ahh yes, next we need an [" .. eq.say_link("Ulthork meat pie") .. "] fer Brita. She's a doll, smart too, but she can be testy when she's hungry so get movin'!");
 		e.other:SummonItem(1417);
 		e.other:AddEXP(150000);
 		e.other:Faction(49, 10); --coldain
@@ -38,7 +38,7 @@ function event_trade(e)
 		e.other:Faction(188, -30); --kromrif
 		e.other:Faction(189, -30); --kromzek
 	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 1424})) then	
-		e.self:Say("More dirty dishes, eh? Well outlander, only one more meal to serve and I'll be done with ye. Make a snow griffin souffle fer the Grand Historian himself. It's his favorite.");
+		e.self:Say("More dirty dishes, eh? Well outlander, only one more meal to serve and I'll be done with ye. Make a [" .. eq.say_link("snow griffin souffle") .. "] fer the Grand Historian himself. It's his favorite.");
 		e.other:SummonItem(1418);
 		e.other:AddEXP(200000);
 		e.other:Faction(49, 10); --coldain
