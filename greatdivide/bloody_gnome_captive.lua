171bdfea71f468f4aa84dab59f697c78b8322e99
Trust <fryguy503@gmail.com>
2017-08-03 15:48:21 -0400

    Greatdivide - Say Links

--- greatdivide/bloody_gnome_captive.lua
+++ greatdivide/bloody_gnome_captive.lua
@@ -5,7 +5,7 @@ end
 function event_say(e)
 	
     if(e.message:findi("hail")) then
-	    e.self:Say("Uh, hi. Hey, that was great work and all. I couldn't believe that. Knowing that bear, I was sure my time was up. I'm heading back to Ak'Anon. I've had enough of pirates, mountains, snow, and bears ten times my size. If you want a map to get back you can have it.");
+	    e.self:Say("Uh, hi. Hey, that was great work and all. I couldn't believe that. Knowing that bear, I was sure my time was up. I'm heading back to Ak'Anon. I've had enough of pirates, mountains, snow, and bears ten times my size. If you want a [" .. eq.say_link("map") .. "] to get back you can have it.");
     elseif(e.message:findi("map")) then
 	    e.other:QuestReward(e.self,0,0,0,0,30045,100);
 	    eq.depop();
