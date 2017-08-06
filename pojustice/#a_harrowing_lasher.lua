45405aa056924d05b79405ba96f4e9ec07215d00
Trust <fryguy503@gmail.com>
2017-08-06 12:57:15 -0400

    PoJustice - adjusted trash agro range to see if that will fix all going for the same prisoner

--- pojustice/#a_harrowing_lasher.lua
+++ pojustice/#a_harrowing_lasher.lua
@@ -9,7 +9,7 @@ function event_spawn(e)
    -- Aggro the closest prisoner (201495) a tormented prisoner
    local npc = eq.get_entity_list():GetMobByNpcTypeID(201455);
    if (npc.valid) then
-      if ( npc:CalculateDistance(e.self:GetX(), e.self:GetY(), e.self:GetZ()) < 200) then
+      if ( npc:CalculateDistance(e.self:GetX(), e.self:GetY(), e.self:GetZ()) < 50) then
          e.self:AddToHateList(npc,1);
       end
    end
