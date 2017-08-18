69a0fe83a65709e36884290ac1c8fd16ad7e6269
Lance - Daeron <lancervt@gmail.com>
2017-08-18 18:14:13 -0400

    typo fix in quest

--- erudsxing/Abe_the_Abandoned.lua
+++ erudsxing/Abe_the_Abandoned.lua
@@ -29,7 +29,7 @@ function event_timer(e)
 	if (counting == 1) then
 		e.self:Say("Shaman? My time will soon be at hand and I fear I will never see our friends again. But the arrow must be given, our holy [" .. eq.say_link("broken arrow") .. "]. Shaman, will you bear our obligation and [" .. eq.say_link("give them the arrow") .. "]? You will know who to give it to when it is time.");
 	elseif (counting == 2) then
-		e.self:Emote("suddenly gasps and says, 'I have found it! I know the answer! Come with me and I will tell you. Eyes are everywhere and this knowledeg is not for everyone. Finally my centuries of waiting are over hahahaha! Follow me Shaman!");  
+		e.self:Emote("suddenly gasps and says, 'I have found it! I know the answer! Come with me and I will tell you. Eyes are everywhere and this knowledge is not for everyone. Finally my centuries of waiting are over hahahaha! Follow me Shaman!");  
 		eq.start(4);
 	elseif (counting == 3) then
 		eq.spawn2(98052,0,0,4176.4,-1563.6,-291.4,115.3);
