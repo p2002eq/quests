94c052a9b758a54f0b4e87ebe9f8f501d7c88efa
Michael <5864295+fryguy503@users.noreply.github.com>
2017-09-16 17:02:04 -0400

    WIP: Instances

--- kael/The_Statue_of_Rallos_Zek.lua
+++ kael/The_Statue_of_Rallos_Zek.lua
@@ -5,7 +5,7 @@ function event_death_complete(e)
 	-- Get Instance ID
 	local instance_id = eq.get_zone_instance_id();
 	-- Set QGlobal Lockout to Instance ID
-	eq.set_global(instance_id.. "_idol",1,3,"D6");
+	eq.set_global(instance_id.. "_idol","1",3,"D6");
 	-- Emote and Spawn Idol
     e.self:Shout("Protect the Idol of Zek!");
 	eq.spawn2(113629,0,0,1292,1267,-35,254.5); -- Idol of Rallos Zek
