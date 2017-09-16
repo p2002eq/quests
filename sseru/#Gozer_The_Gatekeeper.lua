e145f6b8de4eaa7d929b9277a8b72aa5531611ae
Michael <5864295+fryguy503@users.noreply.github.com>
2017-09-16 17:28:36 -0400

    WIP: Instances, adding depop to Gozer if in instance.

--- sseru/#Gozer_The_Gatekeeper.lua
+++ sseru/#Gozer_The_Gatekeeper.lua
@@ -78,3 +78,10 @@ function check_group_guild(cur_group, guildId)
         return false;
     end
 end
+
+function event_spawn(e)
+    local instance_id = eq.get_zone_instance_id();
+    if(instance_id ~= 0) then
+        eq.depop()
+    end
+end
