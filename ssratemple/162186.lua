8f60e9321ea1d69543ffcdecbaae99445b424ae0
Michael <5864295+fryguy503@users.noreply.github.com>
2017-09-22 13:35:55 -0400

    Instance - SSRA - Named Depop
     Acolyte_Wivlx (162492)
     Disciple_Yelwinz (162511)
     Disciple_Zhorluhx (162512)
     Guard_Sklinus (162513)
     Shissar_Assassin (162514)
     Shissar_Mystic (162186)
     The_Shissar_Magister (162516)
     Zhroushe_Mezhkazh (162520)

---
+++ ssratemple/162186.lua
@@ -0,0 +1,8 @@
+-- Depop (No Repsawn) if in an instance
+
+function event_spawn(e)
+    local instance_id = eq.get_zone_instance_id();
+    if(instance_id ~= 0) then
+        eq.depop()
+    end
+end
