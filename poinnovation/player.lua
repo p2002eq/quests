b6463381fa2561b4e84b4316571ec2de325c7e99
Lance - Daeron <lancervt@gmail.com>
2017-11-28 19:45:49 -0500

    player.lua and flagging scirpts added

---
+++ poinnovation/player.lua
@@ -0,0 +1,48 @@
+--player lua for PoInnovation
+function event_enter_zone(e)
+    local instance_id = eq.get_zone_instance_id();
+    local instance_version = eq.get_zone_instance_version();
+    local discs = require('disciplines');
+    discs:update_discs(e, e.self:GetLevel());
+
+    --e.self:Message(1,string.format("[DEBUG], InstanceID = %i, Instance Version = %i",instance_id,instance_version));
+
+    if(instance_id ~= 0) then
+        e.self:Message(15,"You have entered a Instanced Version of the zone.");
+    end
+end
+
+function event_level_up(e)
+    local discs = require('disciplines');
+    discs:train_discs(e, e.self:GetLevel());
+end
+
+function event_click_door(e)	
+	local door = e.door:GetDoorID()
+	--eq.zone_emote(14,"Door ID is: " .. door);   --debug to easily check door IDs
+	
+	--Sets permissions to open factory door
+	local instance_id = eq.get_zone_instance_id();
+	local qglobals = eq.get_qglobals(e.self);
+	
+	if door == 7 then
+		if qglobals.pop_poi_dragon ~= nil or e.self:GetGM() then 
+			e.self:Message(15,"You remember Nitram's words - 'three small turns to the right on the bottommost rivet should open the door'.");
+		else 
+			e.self:Message(4,"It's locked and you're not holding the key.");
+			e.door:ForceClose(e.self);
+		end
+	end	
+	
+	if door == 145 then		--CHECK FOR ENTRY INTO PLANE OF TIME
+		if pop_time_maelin ~= nil or e.self:GetGM() then 
+			e.self:SetZoneFlag(219);		--potimea
+			e.self:SetZoneFlag(223);		--potimeb
+			e.self:Message(15,"You have received a character flag!");
+			e.self:Message(15,"The ages begin to tear through your body. You wake to find yourself in another time.");
+			e.self:MovePC(219,223,140,9,94);	
+		end
+	end	
+end
+
+
