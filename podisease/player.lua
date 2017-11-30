0861cc53a17173cf6f4d9931d5dbdb478580f41b
Lance - Daeron <lancervt@gmail.com>
2017-11-29 22:22:35 -0500

    convert player.lua

---
+++ podisease/player.lua
@@ -0,0 +1,32 @@
+--player.lua for
+--Plane of Disease 
+function event_enter_zone(e)
+    local instance_id = eq.get_zone_instance_id();
+    local instance_version = eq.get_zone_instance_version();
+    local discs = require('disciplines');
+    discs:update_discs(e, e.self:GetLevel());
+
+    --e.self:Message(1,string.format("[DEBUG], InstanceID = %i, Instance Version = %i",instance_id,instance_version));
+
+    if(instance_id ~= 0) then
+        e.self:Message(15,"You have entered a Instanced Version of the zone.");
+    end
+end
+
+function event_level_up(e)
+    local discs = require('disciplines');
+    discs:train_discs(e, e.self:GetLevel());
+end
+
+--if initially zoning into CoDecay after Grummus
+function event_zone(e)
+	local qglobals = eq.get_qglobals(e.self);
+	local tier2 = 55;
+	if e.zone_id == 200 then
+		if e.self:GetLevel() >= tier2 and (qglobals.pop_pod_grimmus_planar_projection ~= nil or qglobals.pop_alt_access_codecay ~= nil) then
+			if not e.self:HasZoneFlag(200) then
+				e.self:SetZoneFlag(200);
+			end
+		end
+	end
+end
