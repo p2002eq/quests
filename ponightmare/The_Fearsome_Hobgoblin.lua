973e38c7a2fde0dc645c28d6e71ece51cbb1eef1
Lance - Daeron <lancervt@gmail.com>
2017-12-22 21:10:56 -0500

    PoN - Hobgoblin Anguish Lord Event

---
+++ ponightmare/The_Fearsome_Hobgoblin.lua
@@ -0,0 +1,10 @@
+--The_Fearsome_Hobgoblin (204472)
+--Trigger Mob for Hobgoblin_Anguish_Lord (204473)
+--Hobgoblin_Anguish_Lord Cycle
+--ponightmare
+
+function event_death_complete(e)
+	eq.unique_spawn(204473, 0, 0, 1486, 2652, -80, 190)	--spawns Hobgoblin_Anguish_Lord (204473)
+	eq.unique_spawn(204477, 0, 0, 1486, 2663, -80, 190)	--spawns a_fearsome_bat(rooted)
+	eq.unique_spawn(204476, 0, 0, 1486, 2640, -80, 190)	--spawns a_malevolent_mephit (rooted)
+end
