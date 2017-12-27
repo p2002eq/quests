cd0b81f5b0453cbfdd145875d4cdbde24648fd3b
Lance - Daeron <lancervt@gmail.com>
2017-12-26 22:25:46 -0500

    PoP HohA - Trydan Faye Trial (Rydda_Dar)

---
+++ hohonora/#Trydan_Faye.lua
@@ -0,0 +1,27 @@
+--#Trydan_Faye NPCID 211053
+--Rydda`Dar - Trial #1(Flag only version - spawns after successful event)
+--hohonora
+
+local counter;
+
+function event_spawn(e)
+	eq.set_timer("depop",10*60*1000);	--10 min depop
+	counter = 0;
+end
+
+function event_timer(e)
+	if e.timer == "depop" then
+		e.self:Say("Congratulations on your victory.");		
+		eq.depop();
+	end
+end
+
+function event_say(e)
+	local qglobals = eq.get_qglobals(e.self, e.other);
+	if e.message:findi("hail") and qglobals.pop_hoh_faye == nil and counter < 72 then
+		e.other:Message(7,"An ethereal mist descends from the air and surrounds your very being. Your body begins to emanate with the power of Trydan Faye.");
+		eq.set_global("pop_hoh_faye", "1", 5, "F");
+		e.other:Message(4,"You receive a character flag!");
+		counter = counter + 1;
+	end
+end
--- hohonora/Trydan_Faye.lua
+++ hohonora/Trydan_Faye.lua
@@ -1,9 +1,15 @@
+--Trydan_Faye NPCID 211051
+--Rydda`Dar (Event Trigger NPC)
+--hohonora
+
 function event_say(e)
 	if e.message:findi("Hail") then
-		e.self:Say("Weakling! How dare you approach me.  Access to Lord Marrs temple is reserved only for the honorable!  You will never be [" .. eq.say_link("ready") .. "]...");
+		e.self:Say("Hello my friend. I can see that you've journeyed far to come before me. You've passed your first task, but you have many more [" .. eq.say_link("Trials") .. "] to face before you can enter the Temple of Marr.");
+	elseif e.message:findi("Trials") then
+		e.self:Say("There are three other Trials you must undergo before you can prove yourself worthy. When you're [" .. eq.say_link("ready") .. "] we'll begin the first Trial.");
 	elseif e.message:findi("ready") then
-		e.self:Say("Be warned, " .. e.other:GetName() .. ", if you believe you are ready, you will fail, even if you can kill Lord Marrs servants!");
-		eq.spawn2(211075,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --Rydda`Dar
+		e.self:Say("Good luck my friends.");
+		eq.spawn2(211121,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --#_A_Custodian_of_Marr
 		eq.depop_with_timer();
 	end
 end
