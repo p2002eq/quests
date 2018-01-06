9d874fd1ef7d0b5c999dc7553a5caf42006e3bcd
Lance - Daeron <lancervt@gmail.com>
2018-01-05 21:43:16 -0500

    PoP PoStorms - KelekVor Giant Fort Scripts

--- postorms/Zertuken_the_Unyielding.lua
+++ postorms/Zertuken_the_Unyielding.lua
@@ -1,5 +1,35 @@
---Notifies Neffiken Lord of Kelek`Vor  that npc was killed in the camp.
+--Zertuken_the_Unyielding (210027)
+--Forest Giant Fort
+--spawns 5-10 a_tumultuous_storm (210467) adds on intervals when engaged
+--postorms
 
-function event_death_complete(e)
-	eq.signal(210176,0);
+function event_combat(e)
+	if e.joined then
+		eq.set_timer("storms", 30 * 1000);
+	else
+		eq.stop_timer("storms");
+	end
+end
+
+function event_timer(e)
+	if e.timer == "storms" and e.self:IsEngaged() and player_check(e) then
+		rand = math.random(5,10);
+		for n = 1,rand do
+			storm = eq.spawn2(210467,0,0,e.self:GetX() + math.random(-5,5), e.self:GetY() + math.random(-5,5), e.self:GetZ(), e.self:GetHeading());	--a_tumultuous_storm (210467)
+			storm:AddToHateList(e.self:GetHateRandom(),1);
+		end
+	end
+end
+
+function player_check(e)
+	client_list = eq.get_entity_list():GetClientList();
+	
+	if client_list ~= nil then
+		for client in client_list.entries do
+			if client:CalculateDistance(e.self:GetX(), e.self:GetY(), e.self:GetZ()) <= 30 and not client:GetFeigned() then	--checks client in range and not FD (to not spawn mobs during pull/split)
+				return true; --client in range
+			end
+		end
+	end
+	return false;
 end
--- postorms/an_irmin_fannsk.lua
+++ postorms/an_irmin_fannsk.lua
@@ -1,5 +1,8 @@
---Notifies Neffiken Lord of Kelek`Vor  that npc was killed in the camp.
+--an_irmin_fannsk (210038) & an_irmin_fannsk (210411)
+--Forest Giant Fort
+--Notifies #Neffiken,_Lord_of_Kelek`Vor (210470) that npc was killed in the camp.
+--postorms
 
 function event_death_complete(e)
-	eq.signal(210176,0);
+	eq.signal(210470,0);	--#Neffiken,_Lord_of_Kelek`Vor (210470)
 end
