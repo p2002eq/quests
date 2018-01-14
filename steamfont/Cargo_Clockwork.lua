f157dfe069355bf21c9c4e44e32c79d9c59af1fa
Lance - Daeron <lancervt@gmail.com>
2018-01-14 12:48:59 -0500

    Akanon - Cargo Clockwork script

--- steamfont/Cargo_Clockwork.lua
+++ steamfont/Cargo_Clockwork.lua
@@ -1,37 +1,56 @@
 local delivery = 0;
 
 function event_signal(e)
-	local qglobal = eq.get_qglobals();
-
-	if(e.signal == 1) then
-		e.self:Emote("Chuga.. Chug..Chug.. 'This unit requires maintenance.'");
-	elseif(e.signal == 2) then
-		if(qglobal[" .. eq.say_link(""CargoClockwork"") .. "] == nil) then
+	local qglobal = eq.get_qglobals(e.self);
+	if(e.signal == 2) then
+		if(qglobal["CargoClockwork"] == nil) then
+			eq.depop_all(56178);
+			eq.depop_all(56179);
+			eq.depop_all(56180);
 			eq.set_global("CargoClockwork","1",7,"H2");
-			eq.start(5); --Path to windmills
+			e.self:Say("This unit requires oiling.");
+			eq.start(181); --Path to windmills
 		end	
 	end
 end
 
-function event_waypoint_depart(e)
-	if(e.wp == 1) then
-		if(delivery == 1) then
-			eq.stop();
-			delivery = 0;
-		end
-	elseif(e.wp == 8) then
-		e.self:Say("kachunk .. kachunk..");
-		eq.signal(56155,1); --Watchman_Halv
-	elseif(e.wp == 10) then
+function event_waypoint_arrive(e)
+	if(e.wp == 12) then
+		e.self:Say("This unit requires maintenance.");
+	elseif(e.wp == 17) then
 		if(delivery == 0) then
 			delivery = 1;
 			e.self:Emote("Chuga.. Chug..Chug..");
-			e.self:Emote("The chugging of the Cargo Clockwork comes to a halt.");
+			eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, 7, 150, "The chugging of the Cargo Clockwork comes to a halt.");
 			eq.spawn2(56178,0,0,30,-700,-109,62); --Hector
 			eq.spawn2(56179,0,0,95,-732,-108,240); --Renaldo
 			eq.spawn2(56180,0,0,53,-615,-107,113); --Jerald
 			e.self:Say("This is highway robbery.");
 		end
+	elseif(e.wp == 26) then
+		eq.get_entity_list():GetSpawnByID(353473):Repop(5);	--Repop Cargo Clockwork at bind
+		eq.depop();
+	end
+end
+
+function event_waypoint_depart(e)
+	if(e.wp == 1) then
+		e.self:Emote("Chuga... Chug... Chug...");
+	elseif(e.wp == 4) then
+		e.self:Emote("Chuga... Chug... Chug...");
+	elseif(e.wp == 6) then
+		e.self:Say("kachunk .. kachunk..");
+		eq.signal(56155,1,2*1000); --Watchman_Halv
+	elseif(e.wp == 8) then
+		e.self:Emote("Chuga... Chug... Chug...");
+	elseif(e.wp == 10) then
+		e.self:Emote("Chuga... Chug... Chug...");
+	elseif(e.wp == 13) then
+		e.self:Emote("Chuga... Chug... Chug...");
+	elseif(e.wp == 15) then
+		e.self:Emote("Chuga... Chug... Chug...");
+	elseif(e.wp == 18) then
+		e.self:Emote("Chuga... Chug... Chug...");	
 	end
 end
 
--- steamfont/cargo_trigger.lua
+++ steamfont/cargo_trigger.lua
@@ -1,9 +1,14 @@
 function event_spawn(e)
-	eq.set_timer("set",5000);
+	eq.set_timer("time_check",10 * 1000);
 end
 
 function event_timer(e)
-	eq.signal(56105,2);
-	eq.stop_timer("set");
+	if e.timer == "time_check" then
+		ZoneTime = eq.get_zone_time();
+		if ZoneTime.zone_hour == 7 then
+			eq.signal(56105,2);
+			eq.depop_with_timer();
+		end
+	end
 end
 		
