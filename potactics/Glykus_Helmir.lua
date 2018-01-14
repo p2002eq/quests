431d45a955ccd62ea3000a40abbcb48124c5c0f8
Lance - Daeron <lancervt@gmail.com>
2018-01-14 12:33:50 -0500

    PoP PoTactics - remove unneeded scripts

--- potactics/Glykus_Helmir.lua
+++
@@ -1,8 +0,0 @@
-function event_death_complete(e)
-     eq.spawn2(214122,0,0,e.self:GetX()+10,e.self:GetY()+10,e.self:GetZ(),e.self:GetHeading());  --Enraged War Boar
-     eq.spawn2(214122,0,0,e.self:GetX()+10,e.self:GetY()-10,e.self:GetZ(),e.self:GetHeading());
-     eq.spawn2(214122,0,0,e.self:GetX()-10,e.self:GetY()+10,e.self:GetZ(),e.self:GetHeading());
-     eq.spawn2(214122,0,0,e.self:GetX()-10,e.self:GetY()-10,e.self:GetZ(),e.self:GetHeading());
-end
-
--- End of File  Zone: PoTactics  ID: 214053  -- #Glykus_Helmir
