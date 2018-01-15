94ae0aa8a5ec996e1b6f8ce3d3549f0421bf8c55
Lance - Daeron <lancervt@gmail.com>
2018-01-14 19:45:49 -0500

    PoP PoTactics - update rztw event

--- potactics/a_Chaos_Boar.lua
+++ potactics/a_Chaos_Boar.lua
@@ -7,6 +7,7 @@ function event_combat(e)
 		eq.stop_timer("depop")
 	else
 		eq.set_timer("depop", 15 * 60 * 1000);	--15 min depop
+		e.self:SaveGuardSpot(true);
 	end
 end
 
--- potactics/player.lua
+++ potactics/player.lua
@@ -19,7 +19,6 @@ function event_level_up(e)
     discs:train_discs(e, e.self:GetLevel());
 end
 
---[[
 function event_click_door(e)	
 	local door = e.door:GetDoorID();
 	local zone_id = eq.get_zone_id();
@@ -27,18 +26,9 @@ function event_click_door(e)
 	local qglobals = eq.get_qglobals(e.self); 
 	eq.zone_emote(14,"Door ID is: " .. door);   --debug to easily check door IDs
 	
-	if door == 14 or door == 15 then
-		door1 = eq.get_entity_list():GetDoorsByDoorID(14);
-		door2 = eq.get_entity_list():GetDoorsByDoorID(15);
-		if qglobals[instance_id .. "_rztw_event"] == nil or not e.self:GetGM() then 
-			door1:ForceClose(e.self);		
-			door2:ForceClose(e.self);
-		else
-			if door == 14 then
-				door2:ForceOpen(e.self);
-			else
-				door1:ForceOpen(e.self);
-			end
+	if door == 14 or door == 15 or door == 16 or door == 17 then
+		if qglobals[instance_id .. "_rztw_event"] == nil and not e.self:GetGM() then 
+			e.door:ForceClose(e.self);
 		end
 	end	
-end]]
+end
