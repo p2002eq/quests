1820576e2703892bb367242f2104ff24991c11f9
Lance - Daeron <lancervt@gmail.com>
2018-01-17 00:34:00 -0500

    PoP SRT - Arlyxir and Galremos

--- solrotower/Arlyxir.lua
+++ solrotower/Arlyxir.lua
@@ -1,3 +1,6 @@
+--Arlyxir (212023)
+--solrotower
+
 function event_death_complete(e)
-	eq.spawn2(202367,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+	eq.spawn2(212065,0,0,e.self:GetX(), e.self:GetY(), e.self:GetZ()-10, e.self:GetHeading());	--A_Planar_Projection
 end
