f18d09c2621e5f241c7f81b2a8d61b7bdd44dd7e
Lance - Daeron <lancervt@gmail.com>
2018-01-17 01:08:01 -0500

    PoP SRT - Solusek Ro

--- solrotower/Solusek_Ro.lua
+++ solrotower/Solusek_Ro.lua
@@ -1,3 +1,6 @@
+--Solusek_Ro (212025)
+--solrotower
+
 function event_death_complete(e)
-	eq.spawn2(218068,0,0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading());
+	eq.spawn2(212070,0,0, e.self:GetX(), e.self:GetY(), e.self:GetZ() - 15, e.self:GetHeading());	--A_Planar_Projection (212070)
 end
