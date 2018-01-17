03973a02870a3bdc2a8f55f04bebd8a798b2d56c
Lance - Daeron <lancervt@gmail.com>
2018-01-17 00:36:06 -0500

    PoP SRT - Xuzl

--- solrotower/212069.lua
+++ solrotower/212069.lua
@@ -1,8 +1,26 @@
+--A_Planar_Projection (212069)
+--Spawns on death of Xuzl
+--solrotower
+
+local counter;
+
 function event_spawn(e)
-	eq.set_timer("depop", 30*60*1000);	--30 min depop
+	eq.set_timer("depop", 10 * 60 * 1000); --10 min
+	counter = 0;
+end
+
+function event_say(e)
+	local qglobals = eq.get_qglobals(e.self, e.other);
+	if e.message:findi("hail") and counter < 72 and qglobals.pop_sol_ro_xuzl == nil then
+		eq.set_global("pop_sol_ro_xuzl", "1", 5, "F");
+		e.other:Message(7,"As the planar projection's thoughts enter your own, you feel arcane wisdom pulsing in your mind.")
+		e.other:Message(15,"You receive a character flag!");
+		counter = counter + 1;
+	end
 end
 
 function event_timer(e)
-	eq.stop_timer(e.timer)
-	eq.depop();
+	if e.timer == "depop" then
+		eq.depop();
+	end
 end
--- solrotower/Xuzl.lua
+++ solrotower/Xuzl.lua
@@ -1,3 +1,30 @@
+--Xuzl (212055)
+--solrotower
+
+function event_combat(e)
+	if e.joined then
+		eq.set_timer("adds", 45 * 1000);
+		eq.stop_timer("reset");
+	else
+		eq.stop_timer("adds");
+		eq.set_timer("reset", 30 * 1000);
+	end
+end
+
+function event_timer(e)
+	if e.timer == "adds" then
+		eq.stop_timer(e.timer);
+		eq.set_timer("adds", 30 * 1000);	--intial timer slower - set to 30 sec after first wave
+		for n = 1,8 do 
+			sword = eq.spawn2(212071,0,0,e.self:GetX() + math.random(-5,5), e.self:GetY() + math.random(-5,5), e.self:GetZ(),0);	--Xuzl`s pet (212071)
+			sword:AddToHateList(e.self:GetHateRandom(),1);
+		end
+	elseif e.timer == "reset" then
+		eq.depop_all(212071);	--Xuzl`s pet (212071)
+	end
+end
+
 function event_death_complete(e)
-	eq.spawn2(214105,0,0, e.self:GetX(), e.self:GetY(), e.self:GetZ(), e.self:GetHeading());
+	eq.depop_all(212071);	--Xuzl`s pet (212071)
+	eq.spawn2(212069,0,0, e.self:GetX(), e.self:GetY(), e.self:GetZ() - 15, e.self:GetHeading());	--A_Planar_Projection
 end
