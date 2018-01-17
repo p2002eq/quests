2e6563990d38aef3f467459c000efedf580f39ee
Lance - Daeron <lancervt@gmail.com>
2018-01-17 00:35:38 -0500

    PoP SRT - Rizlona

--- solrotower/#Rizlona.lua
+++ solrotower/#Rizlona.lua
@@ -1,3 +1,7 @@
+--#Rizlona
+--Dragon Version
+--solrotower
+
 function event_death_complete(e)
-	eq.spawn2(202368,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());	--spawn dragon version
+	eq.spawn2(212068,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());	--A_Planar_Projection (212068)
 end
