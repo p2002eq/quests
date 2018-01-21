af4be7e7bfcc3bc77987d0d2eaebcec0b5bc5af9
Lance <lancerVT@gmail.com>
2018-01-20 22:50:12 -0500

    5th Necro Skullcap - fix invalid trade function syntax

--- overthere/93182.lua
+++ overthere/93182.lua
@@ -8,15 +8,11 @@
 
 function event_trade(e)
 	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 12848, item2 = 12850, item3 = 12851, item4 = 12610}, 0)) then
-		if(e.other:GetLevel() > 20) then
-			e.other:QuestReward(e.self,0,0,0,0,12849);
-		else
-			e.self:Say('You will have to be more skilled to complete that quest.');
-		end
-	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 12848}, 0)) then
-		e.self:Emote("a skeleton worker stops working and begins to open his creaking jaw. 'I live to study and quench my thirst. I live to Bash the Faces of Pariah and entangle myself in the ivy of evergreen. I live. I want to remember.'");
-		e.other:QuestReward(e.self,0,0,0,0,12848);
+	if(e.other:GetLevel() >= 20) and (item_lib.check_turn_in(e.self, e.trade, {item1 = 12848, item2 = 12850, item3 = 12851, item4 = 12610})) then
+		e.other:QuestReward(e.self,0,0,0,0,12849,2000);	--A metal key
+	elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 12848})) then	--Requires A Spectacle
+		e.self:Emote("stops working and begins to open his creaking jaw. 'I live to study and quench my thirst. I live to Bash the Faces of Pariah and entangle myself in the ivy of evergreen. I live. I want to remember.'");
+		e.other:QuestReward(e.self,0,0,0,0,12848);	--return A Spectacle
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
