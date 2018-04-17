6260908380d5212500c214a0d7d3609a2100d68e
Lance - Daeron <lancervt@gmail.com>
2018-04-16 22:44:36 -0400

    CoD - Paffa event tweaks

--- codecay/A_deep.lua
+++ codecay/A_deep.lua
@@ -3,20 +3,13 @@
 local started;
 
 function event_spawn(e)
+	eq.unload_encounter("Overlord_Banord_Paffa");
 	local started = false;
-	eq.set_timer("prox_refresh", 5 * 1000);
 	ProximitySetup(e);
-end
-
-function event_timer(e)
-	if e.timer == "prox_refresh" then
-		ProximitySetup(e);
-	end
 end
 
 function event_enter(e)
 	if eq.get_entity_list():IsMobSpawnedByNpcTypeID(200020) and not started then-- #Banord_Paffa (200020)
-		eq.stop_timer("prox_refresh");
 		e.self:Emote("voice bellows from just ahead saying, 'Come fools see if you can match the power of Banord and his puslings.  Puslings come your master has need of you!' Wretched howls of the twisted puslings begin to echo throughout the room.");
 		eq.load_encounter("Overlord_Banord_Paffa");
 		started = true;
@@ -28,7 +21,7 @@ function event_signal(e)
 		eq.unload_encounter("Overlord_Banord_Paffa");
 		eq.depop_with_timer();
 	elseif e.signal == 2 then	--reset signal - encounter failed
-		eq.set_timer("prox_refresh", 5 * 1000);
+		ProximitySetup(e)
 		eq.unload_encounter("Overlord_Banord_Paffa");
 		started = false;
 	end
@@ -37,9 +30,8 @@ end
 function ProximitySetup(e)
 	started = false;
 	eq.clear_proximity();
-	local xloc = e.self:GetX();
-	local yloc = e.self:GetY();
-	eq.set_proximity(xloc - 30, xloc + 30, yloc - 30, yloc + 30);
+	local x,y,z = e.self:GetX(), e.self:GetY(), e.self:GetZ()	
+	eq.set_proximity(x - 10, x + 10, y - 10, y + 10, z - 20, z + 20);
 end
 		
 
