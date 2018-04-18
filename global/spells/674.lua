c10045174284d93ed9706f83fb5a7fe8d6fe4d75
Lance - Daeron <lancervt@gmail.com>
2018-04-18 12:36:38 -0400

    fix wrong zone id

--- global/spells/674.lua
+++ global/spells/674.lua
@@ -2,7 +2,7 @@
 --script for dispell upon entering Plane of Sky
 
 function event_spell_effect(e)
-	if eq.get_zone_id() ~= 215	then	--dispell should only occur when entering zone.  No support that dispell should occur from teleport within zone
+	if eq.get_zone_id() ~= 71	then	--dispell should only occur when entering zone.  No support that dispell should occur from teleport within zone
 		e.target:BuffFadeAll();
 	end
 end
