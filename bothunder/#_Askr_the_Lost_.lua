38ea00990940225a40a4a65749161421f912b382
Lance - Daeron <lancervt@gmail.com>
2018-05-05 22:34:33 -0400

    PoP Agnarr Event - Askr script update

--- bothunder/#_Askr_the_Lost_.lua
+++ bothunder/#_Askr_the_Lost_.lua
@@ -11,7 +11,7 @@ function event_say(e)
 		e.self:Say("Well done " .. e.other:GetName() .. ".  You have succeed where I have failed, but now comes the most dangerous peril the Bastion holds.  The final level of Torden holds the restless spirit of the Rainkeeper himself.  You must find a way to free him from his tormented sleep.  His dreams and visions have coalesced into a dark shade of himself which controls the full fury of the [" .. eq.say_link("Storm") .. "].");
 	elseif e.message:findi("Storm") then
 		e.self:Say("The spirit of chaos itself guards the chamber where Karana sleeps.  Chaos will most certain protect the embodiment of his tortured conscious as if it were the Rainkeeper himself.  I am afraid that there is no assistance that I can offer in the task laid out before you aside from opening the vortex to Chaos and offer you Karana's blessing.");
-		eq.spawn2(209157,0,0,-1108,-1734,1740,128);	--A_Chaos_Vortex (209157)
+		eq.unique_spawn(209157,0,0,-1108,-1734,1740,128);	--A_Chaos_Vortex (209157)
 	end
 end
 
