7a6c5a6a135b60d399102a453a2707879ed9655f
Lance - Daeron <lancervt@gmail.com>
2018-05-11 00:30:54 -0400

    Charasis - keyring add included for Hand of Glory (Temp keys disabled)

---
+++ charasis/player.lua
@@ -0,0 +1,25 @@
+-- player.lua for Howling Stones (Charasis)
+	
+function event_enter_zone(e)
+    local instance_id = eq.get_zone_instance_id();
+	local zone_id = eq.get_zone_id();
+    local instance_version = eq.get_zone_instance_version();
+    local discs = require('disciplines');
+    discs:update_discs(e, e.self:GetLevel());
+
+    --e.self:Message(1,string.format("[DEBUG], InstanceID = %i, Instance Version = %i",instance_id,instance_version));
+
+    if(instance_id ~= 0) then
+        e.self:Message(15,"You have entered an Instanced Version of the zone.");
+    end
+end
+
+
+function event_click_door(e)	
+	local door = e.door:GetDoorID();	
+	if door == 8 or door == 10 or door == 11 or door == 35 or door == 40 or door == 46 then	
+		if e.self:HasItem(17274) and not e.self:KeyRingCheck(17274) then	--check for Hand of Glory
+			e.self:KeyRingAdd(17274);	--Add to keyring (keyring add is disabled in DB due to other keys used on doors being temporary items)
+		end
+	end	
+end
