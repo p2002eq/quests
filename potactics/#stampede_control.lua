d77500de746ad76df442779bb2d2e164f4261ff7
Lance - Daeron <lancervt@gmail.com>
2018-06-21 00:30:29 -0400

    PoTactics - prevent boar stampede within instanced zones

--- potactics/#stampede_control.lua
+++ potactics/#stampede_control.lua
@@ -2,6 +2,12 @@
 --potactics
 
 function event_spawn(e)
+	--check if in instance (if so depop)
+	local instance_id = eq.get_zone_instance_id();
+	if instance_id ~= 0 then
+		eq.depop();
+	end
+	
 	local qglobals = eq.get_qglobals(e.self);
 	
 	if qglobals[tostring(eq.get_zone_instance_id()) .. "_rztw_event"] == nil then
