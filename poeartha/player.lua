ff916e88722530d6e15fee01569660d9dd3ca566
Lance - Daeron <lancervt@gmail.com>
2018-07-05 23:54:28 -0400

    PoEarthA - update player.lua to give PoEb zoneflag

--- poeartha/player.lua
+++ poeartha/player.lua
@@ -17,3 +17,19 @@ function event_level_up(e)
     discs:train_discs(e, e.self:GetLevel());
 end
 
+function event_click_door(e)
+	local door = e.door:GetDoorID();
+	local instance_id = eq.get_zone_instance_id();
+	local zone_id = eq.get_zone_id();
+	local qglobals = eq.get_qglobals(e.self);
+	--e.self:Message(14,"Door ID is: [" .. door .. "] Open Type: [" .. e.door:GetOpenType() .. "] Lock Pick: [" .. e.door:GetLockPick() .. "] Key Item: [" .. e.door:GetKeyItem() .. "]");   --debug to easily check door IDs
+	
+	if door == 9 or door == 10 or door == 11 then
+		if (qglobals.pop_elemental_grand_librarian ~= nil and qglobals.pop_eartha_arbitor_projection ~= nil) then
+			if not e.self:HasZoneFlag(222) then
+				e.self:SetZoneFlag(222);
+			end
+		end
+	end	
+end
+
