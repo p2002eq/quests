0d5d2be8570bb9bbf1bed6b3cee987e6ec956948
Lance - Daeron <lancervt@gmail.com>
2018-07-26 17:24:12 -0400

    HoHa - added checks to prevent double spawning of Maidens & Villager trials

--- hohonora/Alekson_Garn.lua
+++ hohonora/Alekson_Garn.lua
@@ -4,6 +4,7 @@
 
 function event_spawn(e)
 	eq.unload_encounter("Alekson_Trial");	--unload incase failure on last attempt
+	trial = false;
 end
 
 function event_say(e)
@@ -11,8 +12,11 @@ function event_say(e)
 		e.self:Say("Greetings, young " .. e.other:GetName() .. ". I can see by the look in your eyes that you're [" .. eq.say_link("ready") .. "] to begin the Trials.");
 	elseif e.message:findi("ready") then
 		e.self:Say("Good luck my friend. You'll need it.");
-		eq.spawn2(211114,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --#A_Custodian_of_Marr (211114)
-		eq.load_encounter("Alekson_Trial");
+		if not trial then
+			trial = true;
+			eq.unique_spawn(211114,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); --#A_Custodian_of_Marr (211114)
+			eq.load_encounter("Alekson_Trial");
+		end
 		eq.depop_with_timer();
 	end
 end
--- hohonora/Rhaliq_Trell.lua
+++ hohonora/Rhaliq_Trell.lua
@@ -4,6 +4,7 @@
 
 function event_spawn(e)
 	eq.unload_encounter("Rhaliq_Trial");	--unload incase failure on last attempt
+	trial = false;
 end
 
 function event_say(e)
@@ -11,8 +12,11 @@ function event_say(e)
 		e.self:Emote("nods in your direction and says 'The trials have only just begun. When you're [" .. eq.say_link("ready") .. "] to be tested we shall begin.");
 	elseif e.message:findi("ready") then
 		e.self:Say("Good luck to you my friend...");
-		eq.spawn2(211115,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());	--#A_Custodian_of_Marr (211115)
-		eq.load_encounter("Rhaliq_Trial");
+		if not trial then
+			trial = true;
+			eq.unique_spawn(211115,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());	--#A_Custodian_of_Marr (211115)
+			eq.load_encounter("Rhaliq_Trial");
+		end
 		eq.depop_with_timer();
 	end
 end
