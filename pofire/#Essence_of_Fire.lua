aa62011dc830afaff269f2d501c245623c2a0abf
Lance - Daeron <lancervt@gmail.com>
2018-08-14 19:34:08 -0400

    Add item check for elemental orbs  rather than qglobal check

--- pofire/#Essence_of_Fire.lua
+++ pofire/#Essence_of_Fire.lua
@@ -17,11 +17,15 @@ end
 
 function event_say(e)
 	local qglobals = eq.get_qglobals(e.self,e.other);
-	if e.message:findi("hail") and  qglobals.pop_fire_fennin_projection == nil and counter < 72 then
+	if e.message:findi("hail") and not e.other:HasItem(29147) and counter < 72 then
+		e.self:Say("wat")
+		if qglobals.pop_fire_fennin_projection == nil then
+			eq.set_global("pop_fire_fennin_projection", "1", 5, "F");
+			e.other:Message(15,"You've received a character flag!");
+		end
 		e.other:Message(7,"Flames begin to surround your body.  You feel them moving around you, but your body does not burn.  Suddenly the flames begin to coalesce in your hands.");
-		e.other:Message(15,"You've received a character flag!");
-		eq.set_global("pop_fire_fennin_projection", "1", 5, "F");
 		e.other:SummonItem(29147); 	--Globe of Dancing Flame
 		counter = counter + 1;
 	end
+	e.other:Message(15,string.format("item [%s]  counter [%s]",tostring(e.other:HasItem(29147)),counter));
 end
--- powater/#Essence_of_Water.lua
+++ powater/#Essence_of_Water.lua
@@ -17,10 +17,12 @@ end
 
 function event_say(e)
 	local qglobals = eq.get_qglobals(e.self,e.other);
-	if e.message:findi("hail") and  qglobals.pop_water_coirnav_projection == nil and counter < 72 then
+	if e.message:findi("hail") and not e.other:HasItem(29163) and counter < 72 then
+		if qglobals.pop_water_coirnav_projection == nil then
+			eq.set_global("pop_water_coirnav_projection", "1", 5, "F");
+			e.other:Message(15,"You've received a character flag!");
+		end
 		e.other:Message(7,"Fast currents sweep around you.  The pressure is extreme but you are not caught in it.  Suddenly the current begins to coalesce in your hand.");
-		e.other:Message(15,"You've received a character flag!");
-		eq.set_global("pop_water_coirnav_projection", "1", 5, "F");
 		e.other:SummonItem(29163); 	--Sphere of Coalesced Water
 		counter = counter + 1;
 	end
