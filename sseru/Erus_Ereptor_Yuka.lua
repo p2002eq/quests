3282b952961d40efdcc5c7aec2f6b54640130254
Lance - Daeron <lancervt@gmail.com>
2018-09-27 20:59:43 -0400

    Search for Tergus Raslin quest implementation

--- sseru/Erus_Ereptor_Yuka.lua
+++ sseru/Erus_Ereptor_Yuka.lua
@@ -1,4 +1,7 @@
----- Quest:Search for Tergus Raslin
+--Erus_Ereptor_Yuka (159374)
+--Quest:Search for Tergus Raslin
+--sseru
+
 function event_say(e)
     if(e.message:findi("hail")) then
         e.self:Say("So you think you think you can help the Eye? I doubt it. You see we have plenty of men rooted in all the jobs that need to be done. So any service you have to offer would be of little use to us. I know every move that the Loyalists make. So please move along maybe someone has some other menial task for you. If you don't mind I need to get back to these [" .. eq.say_link("diagrams") .. "].");
@@ -11,12 +14,17 @@ function event_say(e)
 end
 
 function event_trade(e)
-    local item_lib = require("items");
-    if(item_lib.check_turn_in(e.self, e.trade, {item1 = 3495})) then --
-        e.self:Say("  " .. e.other:GetName() .. "  ");
-        e.other:Faction(96,-5); -- Faction
-        e.other:SummonItem(); --
-        e.other:QuestReward(e.self,0,0,0,0,item,exp); --
-    end
+	local item_lib = require("items");
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 29869})) then -- Shattered Praetor Blade
+		e.self:Say("He was caught? I cannot believe one of my own men would fail me. If he was caught he deserved whatever they gave to him. Still though tainting him with vampyrism is something that could have seriously affected this town. He could have done much damage before his time for the question arose. You have done a great justice to Seru by purging us of the evil.");
+		e.other:Faction(96,10); -- eye of seru
+		e.other:Faction(139,-1); -- hand of seru
+		e.other:Faction(142,-1); -- heart of seru
+		e.other:Faction(284,1); -- seru
+		e.other:Faction(141,-1); -- haven smugglers
+		e.other:Faction(350,-1); -- validus custodus
+		e.other:Faction(206,-2); -- magus conlegium
+		e.other:QuestReward(e.self,0,0,0,0,29870,10000); -- Praetor's Blade of Debility
+	end
     item_lib.return_items(e.self, e.other, e.trade)
 end
