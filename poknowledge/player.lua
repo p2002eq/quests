4cfefd3e8696165a510d25d179dcc28fccb3ea09
Michael <fryguy503@gmail.com>
2018-09-30 22:58:15 -0400

    Halloween PoK

--- poknowledge/player.lua
+++ poknowledge/player.lua
@@ -1,6 +1,6 @@
 function event_enter_zone(e)
     -- Halloween Event
-    --e.self:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
+    e.self:SetRace(eq.ChooseRandom(58,216,123,230,85,108,151,154,161,131,181,243,252));
 end
 
 function event_level_up(e)
