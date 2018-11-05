25f51ff2ec0227e5424aa86d354d155073d01aa2
Lance - Daeron <lancervt@gmail.com>
2018-11-05 17:19:56 -0500

    Vampyre Troubles - Kerzar Clawtore will now yield before death

--- tenebrous/Kerzar_Clawtore.lua
+++ tenebrous/Kerzar_Clawtore.lua
@@ -1,4 +1,8 @@
 ---- Quest:Vampyre Troubles
+function event_spawn(e)
+	eq.set_timer("depop", 1800000);
+end
+
 function event_say(e)
     if(e.message:findi("hail")) then
         e.self:Emote("glares with eyes that seem to pierce through you like fiery daggers.  'Run while you can weakling!  My appetite grows stronger with the presence of your mortal scent!'");
@@ -13,37 +17,29 @@ function event_trade(e)
         e.other:QuestReward(e.self,0,0,0,0,31757,1000); -- Katta Castellum Badge of Service (*Third Badge of Service)
         e.self:Emote("roars flashing glistening fangs that are sharper and longer than the average Vah Shirs!");
         e.self:AddToHateList(e.other,1);
+		handin = true;
     end
     item_lib.return_items(e.self, e.other, e.trade)
 end
 
 function event_combat(e)
-    if(e.joined == true) then
-        eq.stop_all_timers(e.self);
-        eq.set_next_hp_event(15);
+    if(e.joined) then
+        eq.stop_timer("depop");
+	else
+		eq.set_timer("depop", 1800000);
     end
 end
 
-function event_hp(e)
-    if (e.hp_event == 15) then
-        e.self:SetAppearance(2);
-        e.self:SetInvul(true)
-        e.self:WipeHateList();
-        eq.set_timer("kerzar_fight_over", 300000);
-        eq.spawn2(172188,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
-        eq.depop();
-    end
-end
 
-function event_death_complete(e)
-    if eq.get_qglobals()["vampyre_vahshir_combat"] == 1 then
-        eq.unique_spawn(172188,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
-    end
+function event_death(e)
+	if handin then
+		eq.spawn2(172188,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+		eq.depop();
+	end
 end
 
 function event_timer(e)
-    if (e.timer == "kerzar_fight_over") then
-        eq:depop();
+    if (e.timer == "depop") then
+        eq.depop();
     end
-    eq.stop_timer(e.timer)
 end
---
+++ tenebrous/a_coterie_general.lua
@@ -0,0 +1,14 @@
+-- a_coterie_general (172001)
+--spawned adds as part of Vampyre Troubles quest
+--tenebrous
+
+function event_spawn(e)
+    eq.set_timer("depop", 600000)
+end
+
+
+function event_timer(e)
+    if (e.timer == "depop") then
+        eq.depop();
+    end
+end
