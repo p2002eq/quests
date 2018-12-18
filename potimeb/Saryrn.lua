6d6fba2a0b8966f6a020522ff82aaacae25f8cd7
Lance - Daeron <lancervt@gmail.com>
2018-12-18 11:37:49 -0500

    PoTime - update p4 lockouts

--- potimeb/Saryrn.lua
+++ potimeb/Saryrn.lua
@@ -9,10 +9,7 @@ function event_death_complete(e)
 	eq.signal(223097,5);
 	-- get the zone instance id
 	local instance_id = eq.get_zone_instance_id();
-	-- load qglobals and set bit 2
-	local qglobals = eq.get_qglobals();
-	local phase_bit = tonumber(qglobals[instance_id.."_potimeb_phase_bit"]);
-	eq.set_global(instance_id.."_potimeb_phase_bit",tostring(bit.bor(phase_bit,2)),7,"H13");
+	eq.set_global(instance_id .. "_potimeb_p4_saryrn","Dead",7,"H132");
 end
 
 function event_spawn(e)
