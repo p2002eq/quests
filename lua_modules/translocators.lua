0d2aa480c1e4e31e23e562f8341e4a1c8a933d26
Infestation the Gnome <gwenmorgana@gmail.com>
2019-03-01 22:40:12 -0800

    Update translocators.lua
    variable adjustments

--- lua_modules/translocators.lua
+++ lua_modules/translocators.lua
@@ -5,7 +5,7 @@ local helper = {}
 function helper.hail_text(e, zone, location)
   local qglobals = eq.get_qglobals(); -- Added by Dhampyre for test
     if (e.message:findi('hail')) then
-        e.self:Say("Greetings ".. e.other:GetCleanName() ..", I am ".. e.self:GetCleanName() ..". I have been sent here by the Gnomish Academy of Science to assist you in traveling the oceans of norrath. If you would like to [" ..eq.say_link("Travel to .. zone .. ") .."] please let me know.");      
+        e.self:Say("Greetings ".. e.other:GetCleanName() ..", I am ".. e.self:GetCleanName() ..". I have been sent here by the Gnomish Academy of Science to assist you in traveling the oceans of norrath. If you would like to [" ..eq.say_link("Travel to " .. zone .." ") .."] please let me know.");      
         --OLD(Dhampyre) e.self:Say("Greetings ".. e.other:GetCleanName() ..", I am ".. e.self:GetCleanName() ..". I have been sent here by the Gnomish Academy of Science to assist you in traveling the oceans of norrath. If you would like to [Travel to ".. zone .."] please let me know.");
     elseif (e.message:findi("Travel to ".. zone)) then
         e.self:Say("Beaming you up!");
