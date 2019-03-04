9cddd9278aa86d9d77233bcda961f2a812b62b27
Infestation the Gnome <gwenmorgana@gmail.com>
2019-03-03 16:39:49 -0800

    Update Mister_Moo_Moo.lua
    reward block streamlined.

--- poknowledge/Mister_Moo_Moo.lua
+++ poknowledge/Mister_Moo_Moo.lua
@@ -35,10 +35,7 @@ function event_trade(e)
 
   if(item_lib.check_turn_in(e.trade, {item1 = 19150,item2 = 19150,item3 = 19150, item4 = 19150})) then -- Bladder of Otter milk
     e.self:Say("Ahh! I feel much better now! Mooooooooooo.");
-    e.other:Ding(); -- creates quest sound
-    e.other:AddEXP(10); -- gives XP
-    e.other:GiveCash(0,0,4,0);
-    e.other:SummonItem(13466); -- give Kabob
+    e.other:QuestReward(e.self,0,0,4,0,13466,10);
   else
     e.self:Say("If you call this milk, you must be a High Elf...  Can't you see these udders?")
   end
