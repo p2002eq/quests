56c447bbfd5654b8e61a73a3c71b9cdd230f2233
Trust <fryguy503@gmail.com>
2019-06-13 01:36:53 +0000

    Charm - Day - Night - No Haste

--- global/items/CHRMNight.lua
+++ global/items/CHRMNight.lua
@@ -1,1 +1,12 @@
--- TODO
+-- CHRMNight
+
+function event_scale_calc(e)
+
+    local zone_time = eq.get_zone_time();
+
+    if(zone_time["zone_hour"] >= 0 and zone_time["zone_hour"] <= 7) or (zone_time["zone_hour"] >= 19 and zone_time["zone_hour"] <= 24) then
+        e.self:SetScale(1);
+    else
+        e.self:SetScale(0);
+    end
+end
