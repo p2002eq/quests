4ea1123294c9f411d45c2e72013729ea93bee581
Vlad <vpresnyak@gmail.com>
2019-07-15 17:26:24 -0400

    Spires and Scion revamp - teleporter functionality

--- global/Nexus_Scion.lua
+++ global/Nexus_Scion.lua
@@ -1,53 +1,25 @@
--- Nexus_Scion in multiple zones
+-- Nexus Scion in multiple zones
+-- this is a massively simplified version of spires script, which has been altered from its original functionality
+-- this is meant as a quality of life improvement for both devs and players who don't have to wait for next port
 
-local ThreadManager = require("thread_manager");
- 
-function event_spawn(e)
-    port_locs = { e.self:GetX()-100, e.self:GetX()+100, e.self:GetY()-100, e.self:GetY()+100 }
-	eq.set_timer("heartbeat", 500);
-    
-	ThreadManager:Clear();
-	ThreadManager:Create("Luclin", luclin_cycle, e);
-end
- 
-function event_timer(e)
-    evt = e;
-    ThreadManager:Resume("Luclin");
-end
-
+-- say block
 function event_say(e)
     if e.message:findi("hail") then
-        e.self:Say("Hello, traveler! Do you wish to [journey to luclin]?");
+        e.self:Say("Hello, traveler! Do you wish to [" .. eq.say_link("I wish to journey to Luclin", false, "journey to Luclin") .. "]?");
     elseif e.message:findi("journey to luclin") then
-        e.self:Say("Take this, " .. e.other:GetName() .. ". You must stay inside the circle of the Spires, and keep this shard on your person. Should you need another, simply ask me. I do not think we will run out of shards any time soon! If you do NOT wish to travel to Luclin, destroy that which I gave you, or step well away from the Spires lest you be caught up in the portal. It will open soon.");
+        e.self:Say("Take this, " .. e.other:GetName() .. ". When you are ready to begin your journey, hand me the shard and you will be on your way.");
         e.other:SummonItem(19720); -- Spire Stone
     end
 end
 
-function luclin_cycle(evt)
-    local locs = { evt.self:GetX(), evt.self:GetY(), evt.self:GetZ() };
-    while true do
-        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'The portal to Luclin will become active in five minutes. Please begin gathering at the Spires if you wish to travel.'")
-        ThreadManager:Wait(120)
-        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'As a reminder, the portal will become active in three minutes.'")
-        ThreadManager:Wait(60);
-        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'Two minutes till teleportation. Please be prepared to step inside the circle of Spires.'")
-        ThreadManager:Wait(60);
-        eq.local_emote(locs, 0, 250, "A Mystic Voice says 'One minute till teleportation. Please stand inside the circle of Spires and have a shard upon your person.'")
-        ThreadManager:Wait(60);
-        luclin_port(eq.get_entity_list():GetClientList());
-        ThreadManager:Wait(600);
+-- trade block
+function event_trade(e)
+    local item_lib = require("items");
+    
+	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 19720})) then -- Spire Stone
+		e.self:Say("Fare well on your journey, " .. e.other:GetName() .. "!");
+        e.self:SpellFinished(2935, e.other);
     end
-end
-
-function luclin_port(player_list)
-	if player_list ~= nil then
-		for player in player_list.entries do
-            local x, y = player:GetX(), player:GetY();
-            if player:HasItem(19720) and x > port_locs[1] and x < port_locs[2] and y > port_locs[3] and y < port_locs[4] then -- has stone, is within spires, then port
-                player:NukeItem(19720);
-				player:SpellFinished(2935, player);
-			end
-		end
-	end
+    
+	item_lib.return_items(e.self, e.other, e.trade)
 end
