07537e82cc9706018dca960495ed9e9de5e75596
Vlad <vpresnyak@gmail.com>
2019-07-15 17:26:24 -0400

    Spires and Scion revamp - teleporter functionality

--- nexus/Velious_Scion.lua
+++ nexus/Velious_Scion.lua
@@ -1,11 +1,18 @@
+-- Velious Scion in Nexus
+-- this is a massively simplified version of spires script, which has been altered from its original functionality
+-- this is meant as a quality of life improvement for both devs and players who don't have to wait for next port
+
+-- say block
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Greetings " .. e.other:GetName() .. ". I am here to assist and watch over those who wish to return to the continent of Velious. Due to the limited space I share this area with the portal to Odus. The [teleport system] back to Norrath is completely automated. I have no direct control over when the portal will turn on, although I can say that the system works on a schedule of approximately ten minutes.");
-	elseif(e.message:findi("teleport system")) then
-		e.self:Say("The teleport system is pretty simple from your perspective. Simply listen for the voice saying that the portals will become active soon. When the time nears, you should stand on the elevated pad you see next to me. Shortly after you see the message that the teleporters are active, you will be transported to your destination..");
+		e.self:Say("Greetings " .. e.other:GetName() .. ". I am here to assist and watch over those who wish to return to the continent of Velious. Due to the limited space I share this area with the portal to Odus. The teleport system back to Norrath is completely automated. I can activate it for you whenever you wish to " .. eq.say_link("I wish to travel to Velious", false, "travel to Velious") .. ".");
+	elseif(e.message:findi("travel to Velious")) then
+		e.self:Say("Good journey to you, " .. e.other:GetName() .. ".");
+		e.self:SpellFinished(2062, e.other);
 	end
 end
 
+-- returns items
 function event_trade(e)
 	local item_lib = require("items");
 	item_lib.return_items(e.self, e.other, e.trade)
