d8464e27be86e8c8526bc4031756921994ddb374
Vlad <vpresnyak@gmail.com>
2019-07-15 12:58:41 -0400

    Gunthak - High level research fix v3

--- gunthak/Researcher_Alvarik.lua
+++ gunthak/Researcher_Alvarik.lua
@@ -1,45 +1,62 @@
 -- Researcher Alvarik for High Level Research quest in Gunthak
 
--- helper function for say events
+-- helper functions for say events
 function liar(speaker)
     speaker:Say("Do you take me for a fool? It is obvious that you are not what you state. Begone from my sight, your false statements disgust me and insult the realms of the arcane.");
+end
+
+function newbie(speaker)
+    speaker:Say("You seem to be keeping up on your studies. Without true knowledge of research, the original research notes will be too complicated for you to understand. Please come back whne you are more knowledgeable of these things.");
 end
 
 -- say block
 function event_say(e)
     if(e.message:findi("Hail")) then
         e.self:Say("Hello there. Are you too a student of the [" .. eq.say_link("I am a student of the arcane.", false, "arcane") .. "]");
+
     elseif(e.message:findi("arcane")) then
         e.self:Say("Excellent. I hold myself as a master of researching the arcane. Even though I study mostly in the art of wizardry, I still carry tomes of the other realms. If you are a master of research and would like to borrow a copy of the research tome, let me know what school of magic you belong to and I may be able to help you out. I also carry with me hand copied works of the masters. If you like you can buy one from me for a small fee.");
 
     elseif(e.message:findi("Enchanter")) then
-        if (e.other:GetClass() == 14 and e.other:GetSkill(58) > 100) then -- Enchanter
-            e.self:Say("So you fancy yourself an enchanter? I dabble a bit in it myself, though I am more interested in the art of explosions! Nevertheless, here is the Phantasmist's Tome. Good luck to you in your studies!");
-            e.other:SummonItem(317652); -- Phantasmal Tome
+        if(e.other:GetClass() == 14) then -- Enchanter
+            if (e.other:GetSkill(58) > 100) then 
+                e.self:Say("So you fancy yourself an enchanter? I dabble a bit in it myself, though I am more interested in the art of explosions! Nevertheless, here is the Phantasmist's Tome. Good luck to you in your studies!");
+                e.other:SummonItem(317652); -- Phantasmal Tome
+            else
+                newbie(e.self);
         else
             liar(e.self);
         end
 
     elseif(e.message:findi("Wizard")) then
-        if (e.other:GetClass() == 12 and e.other:GetSkill(58) > 100) then -- Wizard
-            e.self:Say("So you too study wizardry! Isn't it the most fascinating of the four realms of magic? Well I won't keep you, here is the Sorcerer's Lexicon. Good luck to you in your studies.");
-            e.other:SummonItem(317655); -- Sorcerer's Lexicon
+        if(e.other:GetClass() == 12) -- Wizard
+            if(e.other:GetSkill(58) > 100) then 
+                e.self:Say("So you too study wizardry! Isn't it the most fascinating of the four realms of magic? Well I won't keep you, here is the Sorcerer's Lexicon. Good luck to you in your studies.");
+                e.other:SummonItem(317655); -- Sorcerer's Lexicon
+            else
+                newbie(e.self);
         else
             liar(e.self);
         end
 
     elseif(e.message:findi("Magician")) then
-        if (e.other:GetClass() == 13 and e.other:GetSkill(58) > 100) then -- Magician
-            e.self:Say("So you too study summoning! Isn't it the most fascinating of the four realms of magic? Well I won't keep you, here is the Arch Magus Grimoire. Good luck to you in your studies.");
-            e.other:SummonItem(317653); -- Arch Magus Grimoire
+        if(e.other:GetClass() == 13) then -- Magician
+            if(e.other:GetSkill(58) > 100) then 
+                e.self:Say("So you too study summoning! Isn't it the most fascinating of the four realms of magic? Well I won't keep you, here is the Arch Magus Grimoire. Good luck to you in your studies.");
+                e.other:SummonItem(317653); -- Arch Magus Grimoire
+            else
+                newbie(e.self);
         else
             liar(e.self);
         end
     
     elseif(e.message:findi("Necromancer")) then
-        if (e.other:GetClass() == 11 and e.other:GetSkill(58) > 100) then -- Necromancer
-            e.self:Say("So you study necromancy! Isn't it the most fascinating of the four realms of magic? Well I won't keep you, here is the Warlock's Book of Binding. Good luck to you in your studies.");
-            e.other:SummonItem(317654); -- Warlock's Book of Binding
+        if(e.other:GetClass() == 11) then -- Necromancer
+            if(e.other:GetSkill(58) > 100) then 
+                e.self:Say("So you study necromancy! Isn't it the most fascinating of the four realms of magic? Well I won't keep you, here is the Warlock's Book of Binding. Good luck to you in your studies.");
+                e.other:SummonItem(317654); -- Warlock's Book of Binding
+            else
+                newbie(e.self);
         else
             liar(e.self);
         end
