ab4ecedd0a057f75c7adc25a02cd169f2b290430
vpresnyak <>
2019-07-24 06:00:22 +0000

    Gunthak - sailor quest fix

--- erudnext/Jordan_Sretin.lua
+++ erudnext/Jordan_Sretin.lua
@@ -6,7 +6,7 @@ function event_say(e)
 		e.self:Say("Welcome to the Port of Erudin. What can I help you with today?");
     elseif(e.message:findi("Glory")) then
         e.self:Emote("shuffles through some paperwork that is strewn messily before him.  'The Light's Glory, let's see.  Yes it is a merchant ship headed from Freeport to Erudin.  Yes it should have been here a week ago.  Seems we have had a couple ships disappear as of late.  Rumors tell of increased pirate activity.  Seems none of the local guards wants to get involved in investigating this.  Any assistance you could provide would be most appreciated.  I suggest that you travel to Erud's Crossing and talk to a Kerran named Nifta.  He lives near the dock on the isle and has a great wealth of knowledge of the sea and ships that pass through the crossing.  Ask him if he has seen The Light's Glory.");
-    elseif(e.message:findi("safe passage"))
+    elseif(e.message:findi("safe passage")) then
         e.self:Say("Fine, we will bargain with this despicable troll.' Jordin quickly scrawls something down on a note.  'Take this back to the troll.  It will buy him a safe trip to Antonica.'");
         e.other:SummonItem(355007);
     end
